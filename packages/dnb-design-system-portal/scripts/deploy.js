cafb45596bbb50c777eb81753cb736232d632dcb
Tobias Høegh <tobias@tujo.no>
2018-11-01 19:34:31 +0100

    make gh-pages config with relevant info

--- packages/dnb-design-system-portal/scripts/deploy.js
+++ packages/dnb-design-system-portal/scripts/deploy.js
@@ -12,13 +12,26 @@ const run = () => {
   const log = ora()
   log.start('Starting the deploy process...')
 
+  const config = process.env.GH_USERNAME
+    ? {
+        silent: true,
+        repo: `https://${
+          process.env.GH_TOKEN
+        }@github.com/dnbexperience/eufemia.git`,
+        user: process.env.GH_USERNAME,
+        email: process.env.GH_EMAIL
+      }
+    : {}
+
   /**
    * This adds commits with a custom message.
    */
   ghpages.publish(
     'public',
     {
-      message: `Auto-generated deploy commit v${pkg.version}`
+      message: `Auto-generated deploy commit v${pkg.version}`,
+      branch: 'develop',
+      ...config
     },
     error => {
       if (error) {
