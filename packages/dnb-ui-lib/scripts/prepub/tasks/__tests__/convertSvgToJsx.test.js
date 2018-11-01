d76585fecc8947a44c716269388379cc5a1e59d9
Tobias Høegh <tobias@tujo.no>
2018-11-01 14:35:27 +0100

    update icon conversion test

--- packages/dnb-ui-lib/scripts/prepub/tasks/__tests__/convertSvgToJsx.test.js
+++ packages/dnb-ui-lib/scripts/prepub/tasks/__tests__/convertSvgToJsx.test.js
@@ -5,33 +5,26 @@
 
 import React from 'react'
 import path from 'path'
+import del from 'del'
 import { shallow, toJson } from '../../../../src/core/jest/jestSetup'
 import convertSvgToJsx from '../convertSvgToJsx'
 
 beforeAll(async () => {
   await convertSvgToJsx({
     preventDelete: true,
-    srcPath: path.resolve(__dirname, 'test-files/bell.svg'),
-    destPath: path.resolve(__dirname, 'test-files/dist')
+    srcPath: path.resolve(__dirname, './test-files/bell.svg'),
+    destPath: path.resolve(__dirname, './test-files/dist')
   })
 })
 
-describe('convertSvgToJsx as ES Modules', () => {
+describe('convertSvgToJsx to CJ Modules', () => {
   it('has to convert correctly and match snapshot', async () => {
-    const {
-      default: { bell: Svg }
-    } = require('./test-files/dist/es/index.js')
+    const { default: Svg } = await import('./test-files/dist/bell')
     const ComponentWrap = shallow(<Svg />)
     expect(toJson(ComponentWrap)).toMatchSnapshot()
   })
 })
 
-describe('convertSvgToJsx as CJS Modules', () => {
-  it('has to convert correctly and match snapshot', async () => {
-    const {
-      default: { bell: Svg }
-    } = require('./test-files/dist/cjs/index.js')
-    const ComponentWrap = shallow(<Svg />)
-    expect(toJson(ComponentWrap)).toMatchSnapshot()
-  })
+afterAll(async () => {
+  await del(path.resolve(__dirname, `./test-files/dist`))
 })
