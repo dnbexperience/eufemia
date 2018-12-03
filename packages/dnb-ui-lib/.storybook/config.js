6e82b87291461639ff279dd99196e39d4e470ad5
Tobias <tobias@tujo.no>
2018-12-03 14:11:58 +0100

    update repo dependencies and remove unneeded tools

--- packages/dnb-ui-lib/.storybook/config.js
+++ packages/dnb-ui-lib/.storybook/config.js
@@ -3,8 +3,6 @@
  *
  */
 
-// looks like babel-preset-es2015 is used for react-velocity
-// therefor we add this to the .babelrc file
 import { configure } from '@storybook/react'
 
 configure(() => require('../stories'), module)
