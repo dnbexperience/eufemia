2191170a06dae5a324ade0e6c5e320b6d5e3ba05
Tobias <tobias@tujo.no>
2018-12-13 20:13:33 +0100

    fixup! fix new frame naming issue

--- packages/dnb-ui-lib/scripts/figma/tasks/iconsConverter.js
+++ packages/dnb-ui-lib/scripts/figma/tasks/iconsConverter.js
@@ -76,7 +76,7 @@ const runFrameIconsFactory = async ({
   forceReconvert = null
 }) => {
   const frameId = frameDoc.id
-  const originalFrameName = frameDoc.name
+  const originalFrameName = String(frameDoc.name).replace(/^[0-9]+_/, '') // because the frame name conains a number first
   const frameName = formatIconName(originalFrameName)
 
   // select all icons in the frame
@@ -296,7 +296,6 @@ const formatIconName = n =>
     .toLowerCase()
     .replace(/[^a-z0-9_]/g, '_')
     .replace(/\s|_{2,}/g, '_')
-    .replace(/^[0-9]+_/, '') // because the frame name conains a number first
 
 const lockFileDest = path.resolve(
   __dirname,
