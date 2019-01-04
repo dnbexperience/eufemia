69d230b277972e06e720c8cdac0eaed14bf80106
tujoworker <tobias@tujo.no>
2019-01-04 06:31:02 +0000

    feat: some files where updated/added | bell_medium.svg, calendar.svg, calendar_medium.svg, check.svg, check_medium.svg, version.lock, bell_medium.js, calendar.js, calendar_medium.js, check.js, check_medium.js, icons.lock, index.js, primary_icons.js, secondary_icons.js, secondary_icons_medium.js, logout.svg, logout_medium.svg, trash.svg, trash_medium.svg, logout.js, logout_medium.js, trash.js, trash_medium.js

--- packages/dnb-ui-lib/src/icons/secondary_icons_medium.js
+++ packages/dnb-ui-lib/src/icons/secondary_icons_medium.js
@@ -2,5 +2,7 @@
 
 import fullscreen_medium from './fullscreen_medium.js'
 import hamburger_medium from './hamburger_medium.js'
+import logout_medium from './logout_medium.js'
+import trash_medium from './trash_medium.js'
 
-export { fullscreen_medium, hamburger_medium }
+export { fullscreen_medium, hamburger_medium, logout_medium, trash_medium }
