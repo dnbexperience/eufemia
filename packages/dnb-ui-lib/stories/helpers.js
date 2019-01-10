0f13d28d775c45cec20205f219d581a7805c77f9
Tobias <tobias@tujo.no>
2019-01-10 21:41:11 +0100

    enhance storybook examples

---
+++ packages/dnb-ui-lib/stories/helpers.js
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled'
+
+export const Wrapper = styled.div`
+  padding: 1rem;
+`
+
+export const Box = styled.div`
+  padding: 2rem;
+  margin-bottom: 2rem;
+
+  background-color: #fff;
+  border-bottom: solid 1px #ececec;
+`
