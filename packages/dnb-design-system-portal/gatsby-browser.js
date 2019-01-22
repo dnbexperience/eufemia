e50152ac7753f7365d01a58501e29016ad2552c1
Tobias <tobias@tujo.no>
2019-01-22 14:21:35 +0100

    make it more clear on what is imported of styles

--- packages/dnb-design-system-portal/gatsby-browser.js
+++ packages/dnb-design-system-portal/gatsby-browser.js
@@ -6,7 +6,8 @@
 // UI Style
 if (process.env.NODE_ENV === 'development') {
   require('dnb-ui-lib/src/style/patterns') // import ony patterns
-  require('dnb-ui-lib/src/style') // import both all components and the default theme
+  require('dnb-ui-lib/src/style/components') // import ony components
+  require('dnb-ui-lib/src/style/themes/ui') // import the default theme
 } else {
   // As the gatsby-plugin-postcss plugin dont processes the scss file in the package "dnb-ui-lib" - we have to use the preprocessed version
   // import both all components, patterns and the default theme
