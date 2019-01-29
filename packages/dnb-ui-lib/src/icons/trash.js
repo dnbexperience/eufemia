e85b20e8f6e1ae496c654548910ed4a00a82a6aa
tujoworker <tobias@tujo.no>
2019-01-29 12:03:41 +0000

    feat: some files where updated/added | bell.svg, check_alt_01.svg, exclamation.svg, fullscreen.svg, logout.svg, trash.svg, trash_medium.svg, version.lock, bell.js, check_alt_01.js, exclamation.js, fullscreen.js, icons.lock, logout.js, trash.js, trash_medium.js

--- packages/dnb-ui-lib/src/icons/trash.js
+++ packages/dnb-ui-lib/src/icons/trash.js
@@ -16,7 +16,7 @@ const trash = props => (
         clipRule="evenodd"
         d="M9.502.75h-3a1 1 0 0 0-1 1v1.5h5v-1.5a1 1 0 0 0-1-1z"
       />
-      <path d="M6.502 11.75v-5M9.502 11.75v-5" />
+      <path d="M6.502 11.75v-5m3 5v-5" />
       <path
         clipRule="evenodd"
         d="M12.58 14.333a1 1 0 0 1-.998.917H4.423a1 1 0 0 1-.997-.917L2.502 3.25h11l-.923 11.083z"
