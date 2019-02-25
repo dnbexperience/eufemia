4725006adf82299e0de29c83d1a8d3e416c06f94
Tobias <tobias@tujo.no>
2019-02-25 16:54:34 +0100

    add stories and refactor example

--- packages/dnb-ui-lib/src/components/switch/Example.js
+++ packages/dnb-ui-lib/src/components/switch/Example.js
@@ -3,23 +3,12 @@
  *
  */
 
-import React, { PureComponent } from 'react'
-import PropTypes from 'prop-types'
-import Switch from './Switch'
-import FormLabel from '../../components/form-label/FormLabel'
+import React, { PureComponent, Fragment } from 'react'
+import ComponentBox from '../../../../dnb-design-system-portal/src/shared/tags/ComponentBox'
+// import Switch from './Switch'
+// import FormLabel from '../../components/form-label/FormLabel'
 
-// we call this later on componentDidMount()
-// Switch.enableWebComponent()
-
-class SwitchDemo extends PureComponent {
-  static propTypes = {
-    onStateUpdate: PropTypes.func.isRequired,
-    onChange: PropTypes.func.isRequired
-  }
-  static defaultProps = {
-    active_second: false
-  }
-
+class Example extends PureComponent {
   static onChangeHandler = state => {
     console.log('onChangeHandler', state)
   }
@@ -28,80 +17,69 @@ class SwitchDemo extends PureComponent {
   }
 
   render() {
+    const {
+      onStateUpdateHandler: onStateUpdate,
+      onChangeHandler: onChange
+    } = this
     return (
-      <Switch
-        id="switch-1"
-        text_positive="Yes"
-        text_negative="No"
-        labelledby="switch-1-label"
-        title="Ths is the title"
-        value="Value of switch"
-        default_state={false}
-        checked={false}
-        on_state_update={this.props.onStateUpdate}
-        on_change={this.props.onChange}
-        attributes={{
-          'data-fake:on_change': 'SwitchDemo.onChangeHandler()',
-          'data-fake:on_state_update': 'SwitchDemo.onStateUpdateHandler()'
-        }}
-      />
-    )
+      <Fragment>
+        <ComponentBox
+          caption="Default Swtich"
+          data-dnb-test="swtich-default"
+          scope={{ onStateUpdate, onChange }}
+        >
+          {/* @jsx */ `
+<FormLabel
+  for_id="switch-1"
+  text="Switch label:"
+/>
+<Switch
+  id="switch-1"
+  text_positive="Yes"
+  text_negative="No"
+  // labelledby="switch-1-label"
+  title="Ths is the title"
+  value="Value of switch"
+  default_state={false}
+  checked={false}
+  on_state_update={onStateUpdate}
+  on_change={onChange}
+/>
+          `}
+        </ComponentBox>
+        <ComponentBox
+          caption="Disabled Swtich"
+          data-dnb-test="swtich-default"
+          scope={{ onStateUpdate, onChange }}
+        >
+          {/* @jsx */ `
+<SwitchDemo
+  active_second={this.state.active_second}
+  onStateUpdate={({ checked }) =>
+      this.setState({
+        active_first: checked,
+        active_second: checked
+      })
+    }
+  onChange={({ checked }) =>
+    this.setState({
+      active_first: checked,
+      active_second: checked
+    })
   }
-}
-
-class Example extends PureComponent {
-  state = {
-    active_first: false,
-    active_second: false
-  }
-
-  // Web Component stuff, also the static functions
-  componentDidMount() {
-    Switch.enableWebComponent()
-    window.SwitchDemo = SwitchDemo
-  }
-
-  render() {
-    return (
-      <div className="example-box">
-        <form className="dnb-form">
-          <div className="dnb-form__item">
-            <div className="dnb-form__cell">
-              <FormLabel
-                id="switch-1-label"
-                for_id="switch-1"
-                text="Form Label Text"
-              />
-            </div>
-            <div className="dnb-form__cell">
-              <SwitchDemo
-                active_second={this.state.active_second}
-                onStateUpdate={({ checked }) =>
-                  this.setState({
-                    active_first: checked,
-                    active_second: checked
-                  })
-                }
-                onChange={({ checked }) =>
-                  this.setState({
-                    active_first: checked,
-                    active_second: checked
-                  })
-                }
-              />
-              <dnb-switch
-                id="switch-2"
-                title="Ths is the title"
-                disabled
-                default_state="true"
-                checked={this.state.active_second ? 'true' : 'false'}
-                on_change="SwitchDemo.onChangeHandler"
-                on_state_update="SwitchDemo.onStateUpdateHandler"
-              />
-            </div>
-          </div>
-        </form>
-      </div>
+/>
+<dnb-switch
+  id="switch-2"
+  title="Ths is the title"
+  disabled
+  default_state="true"
+  checked={this.state.active_second ? 'true' : 'false'}
+  on_change="SwitchDemo.onChangeHandler"
+  on_state_update="SwitchDemo.onStateUpdateHandler"
+/>
+          `}
+        </ComponentBox>
+      </Fragment>
     )
   }
 }
