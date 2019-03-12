336d4c26ab732f7162bc0e0a3c682b4069928685
Tobias <tobias@tujo.no>
2019-03-12 10:16:04 +0100

    fix screenshot config typo

--- packages/dnb-ui-lib/src/core/jest/setupJestScreenshot.js
+++ packages/dnb-ui-lib/src/core/jest/setupJestScreenshot.js
@@ -9,11 +9,9 @@ const { setupJestScreenshot } = require('jest-screenshot')
 jest.setTimeout(30e3)
 
 setupJestScreenshot({
-  screenshotConfig: {
-    detectAntialiasing: true,
-    // local we check for 0% accuracy
-    // due to the differences of font rendering between the os (linux/mac/win)
-    // we have to have a hight threshold of 5%
-    pixelThresholdRelative: isCI ? 0.05 : 0
-  }
+  detectAntialiasing: true,
+  // local we check for 0% accuracy
+  // due to the differences of font rendering between the os (linux/mac/win)
+  // we have to have a hight threshold of 5%
+  pixelThresholdRelative: isCI ? 0.05 : 0
 })
