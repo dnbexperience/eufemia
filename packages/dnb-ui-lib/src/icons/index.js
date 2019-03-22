434870776bb0fd81b1125217c22671a25d327de0
tujoworker <tobias@tujo.no>
2019-03-22 13:53:34 +0000

    feat: some files where updated/added | version.lock, icons.lock, index.js, secondary_icons.js, secondary_icons_medium.js, bubble.svg, bubble_medium.svg, link_out.svg, link_out_medium.svg, pencil.svg, pencil_medium.svg, save_alt_01.svg, save_alt_01_medium.svg, bubble.js, bubble_medium.js, link_out.js, link_out_medium.js, pencil.js, pencil_medium.js, save_alt_01.js, save_alt_01_medium.js

--- packages/dnb-ui-lib/src/icons/index.js
+++ packages/dnb-ui-lib/src/icons/index.js
@@ -4,6 +4,8 @@ import add from './add.js'
 import add_medium from './add_medium.js'
 import bell from './bell.js'
 import bell_medium from './bell_medium.js'
+import bubble from './bubble.js'
+import bubble_medium from './bubble_medium.js'
 import calendar from './calendar.js'
 import calendar_medium from './calendar_medium.js'
 import check from './check.js'
@@ -30,15 +32,21 @@ import hamburger from './hamburger.js'
 import hamburger_medium from './hamburger_medium.js'
 import information from './information.js'
 import information_medium from './information_medium.js'
+import link_out from './link_out.js'
+import link_out_medium from './link_out_medium.js'
 import logout from './logout.js'
 import logout_medium from './logout_medium.js'
 import more from './more.js'
 import more_medium from './more_medium.js'
+import pencil from './pencil.js'
+import pencil_medium from './pencil_medium.js'
 import print from './print.js'
 import print_medium from './print_medium.js'
 import question from './question.js'
 import question_medium from './question_medium.js'
 import save from './save.js'
+import save_alt_01 from './save_alt_01.js'
+import save_alt_01_medium from './save_alt_01_medium.js'
 import save_medium from './save_medium.js'
 import search from './search.js'
 import search_medium from './search_medium.js'
@@ -52,6 +60,8 @@ export {
   add_medium,
   bell,
   bell_medium,
+  bubble,
+  bubble_medium,
   calendar,
   calendar_medium,
   check,
@@ -78,15 +88,21 @@ export {
   hamburger_medium,
   information,
   information_medium,
+  link_out,
+  link_out_medium,
   logout,
   logout_medium,
   more,
   more_medium,
+  pencil,
+  pencil_medium,
   print,
   print_medium,
   question,
   question_medium,
   save,
+  save_alt_01,
+  save_alt_01_medium,
   save_medium,
   search,
   search_medium,
