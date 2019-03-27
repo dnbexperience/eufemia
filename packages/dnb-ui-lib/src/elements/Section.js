6dc7518a1765ac6e748924df3e0eb2ca301acbd5
Tobias <tobias@tujo.no>
2019-03-27 09:58:08 +0100

    fix section element with className usage

--- packages/dnb-ui-lib/src/elements/Section.js
+++ packages/dnb-ui-lib/src/elements/Section.js
@@ -7,9 +7,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
 
-const Section = ({ useSpacing, ...p }) => (
+const Section = ({ useSpacing, className, ...p }) => (
   <div
     className={classnames(
+      className,
       'dnb-section',
       useSpacing && 'dnb-section--spacing'
     )}
@@ -18,10 +19,12 @@ const Section = ({ useSpacing, ...p }) => (
 )
 
 Section.propTypes = {
-  useSpacing: PropTypes.bool
+  useSpacing: PropTypes.bool,
+  className: PropTypes.string
 }
 Section.defaultProps = {
-  useSpacing: false
+  useSpacing: false,
+  className: null
 }
 
 export default Section
