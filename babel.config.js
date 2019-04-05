c75cf9fb800616f7db67858aa9c2fd32dcd6b1eb
Tobias <tobias@tujo.no>
2019-04-05 22:09:30 +0200

    remove runtime helpers as there is not a positive size gain

--- babel.config.js
+++ babel.config.js
@@ -16,7 +16,6 @@ module.exports = {
   plugins: [
     '@babel/plugin-proposal-export-default-from',
     '@babel/plugin-proposal-object-rest-spread',
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-transform-runtime'
+    '@babel/plugin-proposal-class-properties'
   ]
 }
--- packages/dnb-ui-lib/babel.config.js
+++ packages/dnb-ui-lib/babel.config.js
@@ -27,8 +27,7 @@ const production = {
     ],
     '@babel/plugin-proposal-export-default-from',
     '@babel/plugin-proposal-object-rest-spread',
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-transform-runtime'
+    '@babel/plugin-proposal-class-properties'
   ]
 }
 
