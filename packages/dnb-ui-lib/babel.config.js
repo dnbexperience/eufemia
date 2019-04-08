f9d98e92d7096c61e4818810b97868e2c6e54060
Tobias <tobias@tujo.no>
2019-04-08 10:44:02 +0200

    feat: lower overall package size by using babel transform runtime

--- packages/dnb-ui-lib/babel.config.js
+++ packages/dnb-ui-lib/babel.config.js
@@ -27,7 +27,8 @@ const production = {
     ],
     '@babel/plugin-proposal-export-default-from',
     '@babel/plugin-proposal-object-rest-spread',
-    '@babel/plugin-proposal-class-properties'
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-transform-runtime'
   ]
 }
 
