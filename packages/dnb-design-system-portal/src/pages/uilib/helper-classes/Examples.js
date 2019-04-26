db66689322cd46545558cf39e162fe59e68e9d1b
Tobias <tobias@tujo.no>
2019-04-26 16:24:37 +0200

    small fixup regarding DNB section helper class

--- packages/dnb-design-system-portal/src/pages/uilib/helper-classes/Examples.js
+++ packages/dnb-design-system-portal/src/pages/uilib/helper-classes/Examples.js
@@ -19,19 +19,19 @@ class Example extends PureComponent {
         >
           {/* @html */ `
 <div className="dnb-section">
-  Visuall DNB Section, declared with <code className="dnb-code">.dnb-section</code>
+  Visual DNB Section, declared with <code className="dnb-code">.dnb-section</code>
 </div>
 <div className="dnb-section dnb-section--spacing">
-  Visuall DNB Section, declared with <code className="dnb-code">.dnb-section--spacing</code>
+  Visual DNB Section, declared with <code className="dnb-code">.dnb-section--spacing</code>
 </div>
 <div className="dnb-section dnb-section--spacing dnb-section--emerald-green">
-  Visuall DNB Section, declared with <code className="dnb-code">.dnb-section--emerald-green</code>
+  Visual DNB Section, declared with <code className="dnb-code">.dnb-section--emerald-green</code>
 </div>
 <div className="dnb-section dnb-section--spacing dnb-section--signal-orange">
-  Visuall DNB Section, declared with <code className="dnb-code">.dnb-section--signal-orange</code>
+  Visual DNB Section, declared with <code className="dnb-code">.dnb-section--signal-orange</code>
 </div>
 <div className="dnb-section dnb-section--spacing dnb-section--white">
-  Visuall DNB Section, declared with <code className="dnb-code">.dnb-section--signal-white</code>
+  Visual DNB Section, declared with <code className="dnb-code">.dnb-section--signal-white</code>
 </div>
         `}
         </CodeBlock>
