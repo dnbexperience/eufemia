dc4ada4171d8de41d060e703a890e4607ac4a632
Tobias <tobias@tujo.no>
2019-05-27 13:22:26 +0200

    fix: forward #label attributes

--- packages/dnb-ui-lib/src/components/form-label/FormLabel.js
+++ packages/dnb-ui-lib/src/components/form-label/FormLabel.js
@@ -72,8 +72,11 @@ export default class FormLabel extends PureComponent {
       className,
       id,
       disabled,
-      class: _className
-      // ...otherProps
+      class: _className,
+
+      text: _text, // eslint-disable-line
+
+      ...otherProps
     } = this.props
 
     const content = FormLabel.getContent(this.props)
@@ -83,8 +86,8 @@ export default class FormLabel extends PureComponent {
       htmlFor: for_id,
       id,
       title,
-      disabled
-      // ...otherProps
+      disabled,
+      ...otherProps
     }
 
     // also used for code markup simulation
