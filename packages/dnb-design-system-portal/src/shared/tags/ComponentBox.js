c8ca38bf8786a0e289829c3ba3b3abe9bf0ac6ae
Tobias <tobias@tujo.no>
2019-06-06 20:16:16 +0200

    make elements available to `ComponentBox` for easier usage in example code

--- packages/dnb-design-system-portal/src/shared/tags/ComponentBox.js
+++ packages/dnb-design-system-portal/src/shared/tags/ComponentBox.js
@@ -8,11 +8,19 @@ import PropTypes from 'prop-types'
 import CodeBlock from './CodeBlock'
 import styled from '@emotion/styled'
 import { getComponents } from 'dnb-ui-lib/src/components/lib'
+import { getElements } from 'dnb-ui-lib/src/elements/lib'
 
 const ComponentBox = ({ children, scope = {}, ...rest }) => {
   return (
     <CodeBlock
-      scope={{ ...getComponents(), useEffect, useState, styled, ...scope }}
+      scope={{
+        ...getComponents(),
+        ...getElements(),
+        useEffect,
+        useState,
+        styled,
+        ...scope
+      }}
       {...rest}
     >
       {children}
