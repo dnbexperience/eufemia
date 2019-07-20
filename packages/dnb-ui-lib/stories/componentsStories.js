0a7f8846da773a33edb073074fd8ed56cac773ef
Tobias <tobias@tujo.no>
2019-07-20 19:21:41 +0200

    chore: fix IE11 styling

--- packages/dnb-ui-lib/stories/componentsStories.js
+++ packages/dnb-ui-lib/stories/componentsStories.js
@@ -24,15 +24,8 @@ import FormRowStory from './components/FormRow'
 import FormSetStory from './components/FormSet'
 import Dropdown from './components/Dropdown'
 import Space from './components/Space'
-import {
-  Button,
-  Tabs,
-  Icon,
-  IconPrimary,
-  // FormLabel,
-  Logo
-} from '../src/components'
-import { H2 } from '../src/elements'
+import Tabs from './components/Tabs'
+import { Button, Icon, IconPrimary, Logo } from '../src/components'
 
 const stories = []
 export default stories
@@ -54,6 +47,7 @@ stories.push(FormRowStory)
 stories.push(FormSetStory)
 stories.push(Dropdown)
 stories.push(Space)
+stories.push(Tabs)
 
 stories.push([
   'Logo',
@@ -69,64 +63,6 @@ stories.push([
         <p className="dnb-p">
           Text with the DNB Logo <Logo />
         </p>
-      </Box>
-    </Wrapper>
-  )
-])
-
-const tablistDataWithContent = [
-  { title: 'First', key: 'first', content: <H2>First</H2> },
-  { title: 'Second', key: 'second', content: () => <H2>Second</H2> }
-]
-
-stories.push([
-  'Tabs',
-  () => (
-    <Wrapper>
-      <Box>
-        <Tabs data={tabsData}>{exampleTabsContent}</Tabs>
-      </Box>
-      <Box>
-        <Tabs data={tablistDataWithContent} />
-      </Box>
-      <Box>
-        <Tabs
-          selected_key="second"
-          data={{
-            first: { title: 'First', content: () => <H2>First</H2> },
-            second: { title: 'Second', content: () => <H2>Second</H2> }
-          }}
-        />
-      </Box>
-      <Box>
-        <Tabs section_style="mint-green">
-          <Tabs.Content title="First">
-            <H2>First</H2>
-          </Tabs.Content>
-          <Tabs.Content title="Second" selected>
-            <H2>Second</H2>
-          </Tabs.Content>
-        </Tabs>
-      </Box>
-      <Box>
-        <Tabs
-          align="right"
-          label="Some Tabs label"
-          data={tabsData}
-          render={({ Wrapper, Content, TabsList, Tabs }) => {
-            return (
-              <Wrapper>
-                <TabsList className="dnb-section">
-                  <small>I'm on the left side</small>
-                  <Tabs />
-                </TabsList>
-                <Content />
-              </Wrapper>
-            )
-          }}
-        >
-          {exampleTabsContent}
-        </Tabs>
       </Box>
     </Wrapper>
   )
@@ -209,24 +145,6 @@ stories.push([
     </Wrapper>
   )
 ])
-
-const exampleTabsContent = {
-  first: () => <H2>First</H2>,
-  second: () => <Input>Focus me with next Tab key</Input>,
-  third: () => (
-    <p className="dnb-p">
-      Eros semper blandit tellus mollis primis quisque platea sollicitudin
-      ipsum
-    </p>
-  ),
-  fourth: () => <H2>Fourth</H2>
-}
-const tabsData = [
-  { title: 'First', key: 'first' },
-  { title: 'Second', key: 'second' },
-  { title: 'Third', key: 'third', disabled: true },
-  { title: 'Fourth', key: 'fourth' }
-]
 
 const showMe = e => {
   console.log('showMe', e)
