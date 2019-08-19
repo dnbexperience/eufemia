36ddbb90c801b7094a7cdb3426ca638b630e64f3
Tobias <tobias@tujo.no>
2019-08-19 15:50:36 +0200

    feat: refactor tertiary #button styles to make them work with medium sized #icon

--- packages/dnb-design-system-portal/src/docs/uilib/components/button/Examples.js
+++ packages/dnb-design-system-portal/src/docs/uilib/components/button/Examples.js
@@ -115,7 +115,7 @@ class Example extends PureComponent {
         </ComponentBox>
         <ComponentBox
           scope={{ Bell }}
-          caption="Medium is equalent to 24, but responsive"
+          caption="Medium is equalent to 24, but responsive. To import custom icons, use: `import { bell_medium as Bell } from 'dnb-ui-lib/icons'`"
         >
           {/* @jsx */ `
 <Button
@@ -126,7 +126,10 @@ class Example extends PureComponent {
 />
           `}
         </ComponentBox>
-        <ComponentBox scope={{ Bell }}>
+        <ComponentBox
+          scope={{ Bell }}
+          caption="Large Signal button with medium sized icon. To import custom icons, use: `import { bell_medium as Bell } from 'dnb-ui-lib/icons'`"
+        >
           {/* @jsx */ `
 <Button
   variant="signal"
