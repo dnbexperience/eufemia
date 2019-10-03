47980d94c65f7f6839bd78573419801df6dd485f
Tobias Høegh <tobias@tujo.no>
2019-10-03 06:28:12 +0200

    fix: make outside click on touch more natural by just listening to a click

--- packages/dnb-ui-lib/src/shared/__tests__/component-helper.test.js
+++ packages/dnb-ui-lib/src/shared/__tests__/component-helper.test.js
@@ -91,13 +91,6 @@ describe('"detectOutsideClick" should', () => {
       event: new CustomEvent('mousedown', { bubbles: true }),
       calledTimes: 2
     })
-
-    // test touchstart
-    testEvent({
-      mockedEvent: jest.fn(),
-      event: new CustomEvent('touchstart', { bubbles: true }),
-      calledTimes: 2
-    })
   })
 })
 
