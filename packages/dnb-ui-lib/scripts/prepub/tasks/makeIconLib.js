9c337a611be76dc64ee32e8b2af195ecd8d07fad
Tobias Høegh <tobias@tujo.no>
2019-10-19 21:26:27 +0200

    feat: create es build of all #icons `/es/icons` to support tree shaking

--- packages/dnb-ui-lib/scripts/prepub/tasks/makeIconLib.js
+++ packages/dnb-ui-lib/scripts/prepub/tasks/makeIconLib.js
@@ -29,16 +29,54 @@ const runFactory = ({ preventDelete = false } = {}) =>
       await del([`./icons/**`])
     }
     try {
-      gulp
-        .src(filesExist([`./src/icons/**/*.js`, '!**/*_not_in_use*']), {
-          cwd: process.env.ROOT_DIR
+      await Promise.all([
+        new Promise((resolve, reject) => {
+          gulp
+            .src(
+              filesExist([`./src/icons/**/*.js`, '!**/*_not_in_use*']),
+              {
+                cwd: process.env.ROOT_DIR
+              }
+            )
+            .pipe(transform('utf8', transformContent))
+            .pipe(babel())
+            .pipe(uglify())
+            .pipe(gulp.dest(`./icons`, { cwd: process.env.ROOT_DIR }))
+            .on('end', resolve)
+            .on('error', reject)
+        }),
+        new Promise((resolve, reject) => {
+          gulp
+            .src(
+              filesExist([`./src/icons/**/*.js`, '!**/*_not_in_use*']),
+              {
+                cwd: process.env.ROOT_DIR
+              }
+            )
+            .pipe(transform('utf8', transformContent))
+            .pipe(
+              babel({
+                presets: [
+                  [
+                    '@babel/preset-env',
+                    {
+                      targets: {
+                        esmodules: true
+                      },
+                      modules: false,
+                      useBuiltIns: false // no polyfill
+                    }
+                  ]
+                ]
+              })
+            )
+            .pipe(gulp.dest(`./es/icons`, { cwd: process.env.ROOT_DIR }))
+            .on('end', resolve)
+            .on('error', reject)
         })
-        .pipe(transform('utf8', transformContent))
-        .pipe(babel())
-        .pipe(uglify())
-        .pipe(gulp.dest(`./icons`, { cwd: process.env.ROOT_DIR }))
-        .on('end', resolve)
-        .on('error', reject)
+      ])
+
+      resolve()
     } catch (e) {
       reject(e)
     }
