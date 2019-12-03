af02724d9864bf1079fdac00793f21cec4f3bc2b
Tobias Høegh <tobias@tujo.no>
2019-12-03 12:45:00 +0100

    fix: enhance #date-picker screen reader support

--- packages/dnb-ui-lib/src/shared/locales/nb-NO.js
+++ packages/dnb-ui-lib/src/shared/locales/nb-NO.js
@@ -1,6 +1,12 @@
 export default {
   'nb-NO': {
     DatePicker: {
+      day: 'dag',
+      month: 'måned',
+      year: 'år',
+      start: 'fra',
+      end: 'til',
+      open_picker_text: 'åpne datovelger',
       mask_order: 'dd/mm/yyyy',
       mask_placeholder: 'dd/mm/åååå', // have to be same setup as "mask" - but can be like: dd/mm/åååå
       date_format: 'yyyy-MM-dd', // in v1 of date-fns we where more flexible in terms of the format
