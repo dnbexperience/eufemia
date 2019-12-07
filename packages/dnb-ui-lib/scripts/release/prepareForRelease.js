53c44f149628d6f0b778668fb1e201503455b509
Tobias Høegh <tobias@tujo.no>
2019-12-07 21:56:00 +0100

    chore: enhance release routine, so it fails on CI if release fails

--- packages/dnb-ui-lib/scripts/release/prepareForRelease.js
+++ packages/dnb-ui-lib/scripts/release/prepareForRelease.js
@@ -9,51 +9,35 @@ import fs from 'fs-extra'
 import packpath from 'packpath'
 import prettier from 'prettier'
 
-export const cleanupPackage = ({ packageJsonString, filepath }) =>
-  new Promise(async (resolve, reject) => {
-    try {
-      const packageJson = JSON.parse(packageJsonString)
-      delete packageJson.release
-      delete packageJson.scripts
-      delete packageJson.devDependencies
+export const cleanupPackage = async ({ packageJsonString, filepath }) => {
+  const packageJson = JSON.parse(packageJsonString)
+  delete packageJson.release
+  delete packageJson.scripts
+  delete packageJson.devDependencies
 
-      const prettierrc = JSON.parse(
-        await fs.readFile(
-          path.resolve(packpath.self(), '.prettierrc'),
-          'utf-8'
-        )
-      )
+  const prettierrc = JSON.parse(
+    await fs.readFile(
+      path.resolve(packpath.self(), '.prettierrc'),
+      'utf-8'
+    )
+  )
 
-      const formattedPackageJson = prettier.format(
-        JSON.stringify(packageJson),
-        {
-          ...prettierrc,
-          filepath
-        }
-      )
-
-      resolve(formattedPackageJson)
-    } catch (e) {
-      reject(e)
-    }
+  return prettier.format(JSON.stringify(packageJson), {
+    ...prettierrc,
+    filepath
   })
+}
 
-const prepareForRelease = () =>
-  new Promise(async (resolve, reject) => {
-    try {
-      const filepath = path.resolve(packpath.self(), './package.json')
-      const packageJsonString = await fs.readFile(filepath, 'utf-8')
-      const formattedPackageJson = await cleanupPackage({
-        packageJsonString,
-        filepath
-      })
-      if (isCI) {
-        await fs.writeFile(filepath, formattedPackageJson)
-      }
-    } catch (e) {
-      reject(e)
-    }
-    resolve()
+const prepareForRelease = async () => {
+  const filepath = path.resolve(packpath.self(), './package.json')
+  const packageJsonString = await fs.readFile(filepath, 'utf-8')
+  const formattedPackageJson = await cleanupPackage({
+    packageJsonString,
+    filepath
   })
+  if (isCI) {
+    await fs.writeFile(filepath, formattedPackageJson)
+  }
+}
 
 export default prepareForRelease
