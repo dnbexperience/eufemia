45df9f797083bcee1f9840e84685b51ed4dedfbd
Tobias Høegh <tobias@tujo.no>
2020-01-31 06:33:45 +0000

    Auto-generated deploy commit by Drone 1/31/2020, 7:29:11 AM [CI SKIP]

--- sw.js
+++ sw.js
@@ -27,10 +27,10 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-587f4ffd98304b8a8557.js"
+    "url": "webpack-runtime-d6d05fa7a2013176d596.js"
   },
   {
-    "url": "styles.2539b2f8ea2fad46aedd.css"
+    "url": "styles.e2f0d047e2fa347101d1.css"
   },
   {
     "url": "styles-31d0edcd78b60c230294.js"
@@ -39,14 +39,14 @@ self.__precacheManifest = [
     "url": "commons-eb50044eccae56e0c478.js"
   },
   {
-    "url": "app-23e1ab852fed33faf388.js"
+    "url": "app-7ba5e9595c19d960e61e.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-c3c3c2d97e1b942d6fb0.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "69a9e8df6b91715e686fb2c501595f37"
+    "revision": "a2b872388f9d7b391819d708200b04be"
   },
   {
     "url": "static/DNB-Regular-8126d0ca4ea2694cf9e3f1205d2a3678.woff2"
@@ -152,7 +152,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-23e1ab852fed33faf388.js`))) {
+  if (!resources || !(await caches.match(`/app-7ba5e9595c19d960e61e.js`))) {
     return await fetch(event.request)
   }
 
