defff63493c7899eeda3136a6295196a888715af
Tobias Høegh <tobias@tujo.no>
2020-02-19 14:46:30 +0100

    portal: fix ssr root element issue

--- packages/dnb-design-system-portal/gatsby-ssr.js
+++ packages/dnb-design-system-portal/gatsby-ssr.js
@@ -3,6 +3,7 @@
  *
  */
 
-import { rootElement } from './src/core/portalProviders'
-
-export const wrapRootElement = rootElement
+// NB: We do not need to wrap the root element during SSR
+// Using it actually makes things worse, as it removes some semantic/ or styles
+// import { rootElement } from './src/core/portalProviders'
+// export const wrapRootElement = rootElement
