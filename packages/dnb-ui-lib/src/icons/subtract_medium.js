39ce7b678fc5a8771a6b530d642d3118079c9009
Tobias Høegh <tobias@tujo.no>
2020-02-20 11:43:08 +0000

    chore: some icons/assets got added/changed during CI | add.svg, add_medium.svg, arrow_down.svg, arrow_down_medium.svg, arrow_left.svg, arrow_left_medium.svg, arrow_right.svg, arrow_right_medium.svg, arrow_up.svg, arrow_up_medium.svg, attachment.svg, attachment_medium.svg, bell.svg, bell_medium.svg, bubble.svg, bubble_medium.svg, calendar.svg, calendar_medium.svg, check.svg, check_alt_01.svg, check_alt_01_medium.svg, check_medium.svg, chevron_down.svg, chevron_down_medium.svg, chevron_left.svg, chevron_left_medium.svg, chevron_right.svg, chevron_right_medium.svg, chevron_up.svg, chevron_up_medium.svg, close.svg, close_medium.svg, download.svg, download_medium.svg, exclamation.svg, exclamation_medium.svg, file.svg, file_add.svg, file_add_medium.svg, file_medium.svg, fullscreen.svg, fullscreen_medium.svg, hamburger.svg, hamburger_medium.svg, home.svg, home_medium.svg, information.svg, information_medium.svg, link_out.svg, link_out_medium.svg, list.svg, list_medium.svg, login.svg, login_medium.svg, logout.svg, logout_medium.svg, more.svg, more_medium.svg, pencil.svg, pencil_medium.svg, print.svg, print_medium.svg, question.svg, question_medium.svg, redo.svg, redo_medium.svg, refresh.svg, refresh_medium.svg, reset.svg, reset_medium.svg, save.svg, save_medium.svg, search.svg, search_medium.svg, send.svg, send_medium.svg, settings.svg, settings_medium.svg, subtract.svg, subtract_medium.svg, trash.svg, trash_medium.svg, undo.svg, undo_medium.svg, upload.svg, upload_medium.svg, view.svg, view_medium.svg, view_off.svg, view_off_medium.svg, version.lock, add.js, add_medium.js, arrow_down.js, arrow_down_medium.js, arrow_left.js, arrow_left_medium.js, arrow_right.js, arrow_right_medium.js, arrow_up.js, arrow_up_medium.js, attachment.js, attachment_medium.js, bell.js, bell_medium.js, bubble.js, bubble_medium.js, calendar.js, calendar_medium.js, check.js, check_alt_01.js, check_alt_01_medium.js, check_medium.js, chevron_down.js, chevron_down_medium.js, chevron_left.js, chevron_left_medium.js, chevron_right.js, chevron_right_medium.js, chevron_up.js, chevron_up_medium.js, close.js, close_medium.js, download.js, download_medium.js, exclamation.js, exclamation_medium.js, file.js, file_add.js, file_add_medium.js, file_medium.js, fullscreen.js, fullscreen_medium.js, hamburger.js, hamburger_medium.js, home.js, home_medium.js, icons.lock, information.js, information_medium.js, link_out.js, link_out_medium.js, list.js, list_medium.js, login.js, login_medium.js, logout.js, logout_medium.js, more.js, more_medium.js, pencil.js, pencil_medium.js, print.js, print_medium.js, question.js, question_medium.js, redo.js, redo_medium.js, refresh.js, refresh_medium.js, reset.js, reset_medium.js, save.js, save_medium.js, search.js, search_medium.js, send.js, send_medium.js, settings.js, settings_medium.js, subtract.js, subtract_medium.js, trash.js, trash_medium.js, undo.js, undo_medium.js, upload.js, upload_medium.js, view.js, view_medium.js, view_off.js, view_off_medium.js

--- packages/dnb-ui-lib/src/icons/subtract_medium.js
+++ packages/dnb-ui-lib/src/icons/subtract_medium.js
@@ -5,14 +5,11 @@ import React from 'react'
 function subtract_medium(props) {
   return (
     <svg width={24} height={24} viewBox="0 0 24 24" fill="none" {...props}>
-      <line
-        x1={20.249}
-        y1={12}
-        x2={3.74902}
-        y2={12}
-        stroke="black"
+      <path
+        stroke="#000"
         strokeWidth={1.5}
         strokeLinecap="round"
+        d="M20.249 12h-16.5"
       />
     </svg>
   )
