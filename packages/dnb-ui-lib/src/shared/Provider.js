4e8f1ffd8cd6c41a55e435593fa3a8dabf2629ff
Tobias Høegh <tobias@tujo.no>
2020-03-26 12:36:34 +0100

    fix: use class component for the shared Provider

--- packages/dnb-ui-lib/src/shared/Provider.js
+++ packages/dnb-ui-lib/src/shared/Provider.js
@@ -3,42 +3,65 @@
  *
  */
 
-import React from 'react'
+import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import Context, { defaultContext } from './Context'
 
 // fill with data
 import { prepareFormRowContext } from '../components/form-row/FormRow'
 
-const Provider = ({ children, ...providerProps }) => {
-  // 1. Set default context to be overwirtter by the provider props
-  const context = defaultContext(providerProps)
-
-  // 2. The reset will extend the Provider Context
-
-  // chore preparation
-  if (context.formRow) {
-    context.formRow = prepareFormRowContext(context.formRow)
+class Provider extends PureComponent {
+  static propTypes = {
+    children: PropTypes.node.isRequired
   }
 
-  // general context update
-  if (!context.update) {
-    context.update = props => setContext(defaultContext(props))
+  constructor(props) {
+    super(props)
+
+    const {
+      children, // eslint-disable-line
+      ...providerProps
+    } = this.props
+
+    // 1. Set default context to be overwirtter by the provider props
+    const context = defaultContext(providerProps)
+
+    this.state = { usedContext: context }
   }
 
-  // make it posible to change the locale during runtime
-  if (!context.setLocale) {
-    context.setLocale = locale => setContext(defaultContext({ locale }))
+  setContext(state) {
+    this.setState({ usedContext: defaultContext(state) })
   }
 
-  const [usedContext, setContext] = React.useState(context)
+  render() {
+    const { children, ...providerProps } = this.props
 
-  return (
-    <Context.Provider value={usedContext}>{children}</Context.Provider>
-  )
-}
-Provider.propTypes = {
-  children: PropTypes.node.isRequired
+    // 1. Set default context to be overwirtter by the provider props
+    const context = defaultContext(providerProps)
+
+    // 2. The reset will extend the Provider Context
+
+    // chore preparation
+    if (context.formRow) {
+      context.formRow = prepareFormRowContext(context.formRow)
+    }
+
+    // general context update
+    if (!context.update) {
+      context.update = props => this.setContext(props)
+    }
+
+    // make it posible to change the locale during runtime
+    if (!context.setLocale) {
+      context.setLocale = locale => this.setContext({ locale })
+    }
+
+    return (
+      <Context.Provider value={this.state.usedContext}>
+        {children}
+      </Context.Provider>
+    )
+  }
 }
 
 export default Provider
