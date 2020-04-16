fe0e0be151cff0fee1548025d33527971673e498
Tobias Høegh <tobias@tujo.no>
2020-04-16 09:44:29 +0200

    fix: fix #pagination buttons spacing of last child

--- packages/dnb-ui-lib/src/components/pagination/__tests__/Pagination.screenshot.test.js
+++ packages/dnb-ui-lib/src/components/pagination/__tests__/Pagination.screenshot.test.js
@@ -1,20 +1,33 @@
 /**
  * Screenshot Test
- * This file will not run on "test:staged" because we dont require any related files
+ *
  */
 
 import {
-  // isCI,
   testPageScreenshot,
   setupPageScreenshot
 } from '../../../core/jest/jestSetupScreenshots'
 
-describe.skip('Pagination screenshot', () => {
+describe('Pagination screenshot', () => {
   setupPageScreenshot({ url: '/uilib/components/pagination/demos' })
 
-  it('have to match the calendar', async () => {
+  it('have to match the default pagination bar', async () => {
     const screenshot = await testPageScreenshot({
-      selector: '[data-dnb-test="pagination-bar"] .dnb-pagination'
+      selector: '[data-dnb-test="pagination-default"]'
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('have to match the centered pagination bar', async () => {
+    const screenshot = await testPageScreenshot({
+      selector: '[data-dnb-test="pagination-centered"]'
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('have to match infinity load button', async () => {
+    const screenshot = await testPageScreenshot({
+      selector: '[data-dnb-test="pagination-infinity-load-button"]'
     })
     expect(screenshot).toMatchImageSnapshot()
   })
