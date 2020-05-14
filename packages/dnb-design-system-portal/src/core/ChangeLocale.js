4e14b6fa9086135404900dffba52d555aa76e00b
Tobias Høegh <tobias@tujo.no>
2020-05-14 15:01:00 +0200

    fix: fix handling of props in the global #provider

--- packages/dnb-design-system-portal/src/core/ChangeLocale.js
+++ packages/dnb-design-system-portal/src/core/ChangeLocale.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { Dropdown } from 'dnb-ui-lib/src'
 
 import Context from 'dnb-ui-lib/src/shared/Context'
-import { getLang, setLang } from './portalProviders'
+import { setLang } from './portalProviders'
 
 export default function ChangeLocale(props) {
   const { locale, setLocale } = React.useContext(Context)
 
   return (
     <Dropdown
-      value={getLang(locale)}
+      value={locale}
       data={{ 'en-US': 'English', 'nb-NO': 'Norsk' }}
       on_change={({ data: { selected_key } }) => {
         setLocale(selected_key)
