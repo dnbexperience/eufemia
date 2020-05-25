728289591bebce61add5abb7156e10571e1d8dc8
Tobias Høegh <tobias@tujo.no>
2020-05-25 15:56:40 +0200

    feat: add `<hr />` element

--- packages/dnb-ui-lib/src/elements/Hr.js
+++ packages/dnb-ui-lib/src/elements/Hr.js
@@ -5,7 +5,23 @@
 
 import React from 'react'
 import E from './Element'
+import PropTypes from 'prop-types'
+import classnames from 'classnames'
 
-const Hr = (p) => <E is="hr" {...p} />
+const Hr = ({ fullscreen, ...p } = {}) => {
+  if (fullscreen) {
+    p.className = classnames(p.className, 'dnb-hr--fullscreen')
+  }
+
+  return <E is="hr" {...p} />
+}
 Hr.tagName = 'dnb-hr'
+Hr.propTypes = {
+  className: PropTypes.string,
+  fullscreen: PropTypes.bool
+}
+Hr.defaultProps = {
+  className: null,
+  fullscreen: null
+}
 export default Hr
---
+++ packages/dnb-ui-lib/src/style/elements/__tests__/Hr.screenshot.test.js
@@ -0,0 +1,28 @@
+/**
+ * Screenshot Test
+ * This file will not run on "test:staged" because we dont require any related files
+ */
+
+import {
+  testPageScreenshot,
+  setupPageScreenshot
+  // isCI
+} from '../../../core/jest/jestSetupScreenshots'
+
+describe('List screenshot', () => {
+  setupPageScreenshot({
+    url: '/uilib/elements/horizontal-rule'
+  })
+  it('have to match default horizontal rule', async () => {
+    const screenshot = await testPageScreenshot({
+      selector: '[data-dnb-test="hr-default"]'
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+  it('have to match fullscreen horizontal rule', async () => {
+    const screenshot = await testPageScreenshot({
+      selector: '[data-dnb-test="hr-fullscreen"]'
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+})
