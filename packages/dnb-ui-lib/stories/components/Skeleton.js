9bbf7a17700e32445ce7190baa45edbaa2f103ff
Tobias Høegh <tobias@tujo.no>
2020-08-21 23:36:36 +0200

    fix: enhance skeleton coparation compatibility with other elements

--- packages/dnb-ui-lib/stories/components/Skeleton.js
+++ packages/dnb-ui-lib/stories/components/Skeleton.js
@@ -18,7 +18,7 @@ import {
   Skeleton
   // FormLabel
 } from '../../src/components'
-import { H1, H2, P } from '../../src/elements'
+import { H1, H2, P, Ol, Li, Ul, Dl, Dt, Dd } from '../../src/elements'
 import Provider from '../../src/shared/Provider'
 
 const CustomStyle = styled.div`
@@ -52,6 +52,7 @@ export default [
           <Wrapper>
             <Box>
               <ToggleButton
+                skeleton={false}
                 checked={showSkeleton}
                 on_change={() => setSkeletonState((s) => !s)}
               >
@@ -101,12 +102,110 @@ export default [
                 {/* <Skeleton show top>
                   <P>Paragraph</P>
                 </Skeleton> */}
-                end
               </WidthLimit>
+            </Box>
+
+            <Box>
+              <Ul>
+                <Li>Item 1</Li>
+                <Li>Item 2</Li>
+                <Li>
+                  Item 3
+                  <Ul>
+                    <Li>
+                      Item 1 <br />
+                      Break with a{' '}
+                      <a className="dnb-anchor" href="/">
+                        Anchor (Text Link
+                      </a>
+                    </Li>
+                    <Li>Item 2</Li>
+                  </Ul>
+                </Li>
+                <Li>Item 4</Li>
+              </Ul>
+            </Box>
+            <Box>
+              <Ol nested>
+                <Li>Item</Li>
+                <Li>
+                  Item
+                  <Ol>
+                    <Li>
+                      Item
+                      <Ol>
+                        <Li>Item</Li>
+                        <Li>Item</Li>
+                      </Ol>
+                    </Li>
+                    <Li>
+                      Item
+                      <Ol>
+                        <Li>Item</Li>
+                        <Li>Item</Li>
+                      </Ol>
+                    </Li>
+                  </Ol>
+                </Li>
+                <Li>Item</Li>
+              </Ol>
+            </Box>
+            <Box>
+              <ListWidthLimit>
+                <Ol nested className="dnb-ol--outside">
+                  <Li>
+                    Using <code className="dnb-code">dnb-ol--outside</code>{' '}
+                    (default): Using Porta commodo tempus interdum habitant
+                    urna magna aliquet quam nisl
+                    <Ol>
+                      <Li>
+                        Porta commodo tempus interdum habitant urna magna
+                        aliquet quam nisl
+                      </Li>
+                    </Ol>
+                  </Li>
+                </Ol>
+                <Ol nested className="dnb-ol--inside">
+                  <Li>
+                    New ol, using{' '}
+                    <code className="dnb-code">dnb-ol--inside</code>: Porta
+                    commodo tempus interdum habitant urna magna aliquet
+                    quam nisl
+                    <Ol>
+                      <Li>
+                        Porta commodo tempus interdum habitant urna magna
+                        aliquet quam nisl
+                      </Li>
+                    </Ol>
+                  </Li>
+                </Ol>
+              </ListWidthLimit>
+            </Box>
+            <Box>
+              <Dl>
+                <Dt>Term</Dt>
+                <Dd>Description</Dd>
+                <Dt>Term</Dt>
+                <Dd>Description 1</Dd>
+                <Dd>Description 2</Dd>
+                <Dd>Description 3</Dd>
+                <dl className="dnb-dl">
+                  <Dt>Sub Term</Dt>
+                  <Dd>Sub Description</Dd>
+                </dl>
+              </Dl>
             </Box>
           </Wrapper>
         </CustomStyle>
+        end
       </Provider>
     )
   }
 ]
+
+const ListWidthLimit = styled.div`
+  max-width: 22rem;
+  .dnb-ol li::before {
+    font-weight: var(--font-weight-bold);
+  }
+`
