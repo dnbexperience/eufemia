1c1d0d6b8d703cdfc8d63b7ff076ee619f0dbf19
Tobias Høegh <tobias@tujo.no>
2020-10-02 11:33:56 +0200

    chore: enhance reliability on #skeleton screen tests

--- packages/dnb-ui-lib/src/components/skeleton/__tests__/Skeleton.screenshot.test.js
+++ packages/dnb-ui-lib/src/components/skeleton/__tests__/Skeleton.screenshot.test.js
@@ -12,13 +12,14 @@ describe('Skeleton screenshot', () => {
   setupPageScreenshot({ url: '/uilib/components/skeleton/demos' })
   it('have to match excluded components', async () => {
     const screenshot = await testPageScreenshot({
-      selector: '[data-dnb-test="skeleton-exclude"]',
-      waitBeforeSimulate: 10
+      style: { width: '30rem' },
+      selector: '[data-dnb-test="skeleton-exclude"]'
     })
     expect(screenshot).toMatchImageSnapshot()
   })
   it('have to match a removed skeleton', async () => {
     const screenshot = await testPageScreenshot({
+      style: { width: '30rem' },
       selector: '[data-dnb-test="skeleton-exclude"]',
       simulateSelector: '[data-dnb-test="skeleton-exclude"] .dnb-button',
       simulate: 'click'
@@ -27,14 +28,14 @@ describe('Skeleton screenshot', () => {
   })
   it('have to match all components - vertical', async () => {
     const screenshot = await testPageScreenshot({
-      // style: { width: '20rem' },
+      style: { width: '60rem' },
       selector: '[data-dnb-test="skeleton-all-vertical"]'
     })
     expect(screenshot).toMatchImageSnapshot()
   })
   it('have to match all components - horizontal', async () => {
     const screenshot = await testPageScreenshot({
-      // style: { width: '20rem' },
+      style: { width: '60rem' },
       selector: '[data-dnb-test="skeleton-all-horizontal"]'
     })
     expect(screenshot).toMatchImageSnapshot()
