8b8c796747bb6cbd8dfbc1e600b4cbdfaf3e6033
Tobias Høegh <tobias@tujo.no>
2020-10-25 22:14:29 +0100

    feat: add `spacing` prop to #modal and make `fullscreen="auto"` as the default behavior

--- packages/dnb-ui-lib/src/components/modal/__tests__/Modal.screenshot.test.js
+++ packages/dnb-ui-lib/src/components/modal/__tests__/Modal.screenshot.test.js
@@ -73,3 +73,41 @@ describe('Drawer screenshot', () => {
     expect(screenshot).toMatchImageSnapshot()
   })
 })
+
+describe('Additional Modal screenshot', () => {
+  setupPageScreenshot({
+    url: '/uilib/components/modal/demos',
+    pageViewport
+  })
+  it('have to match modal without spacing', async () => {
+    const screenshot = await testPageScreenshot({
+      selector: 'body', // only to make sure we have a valid selector
+      simulate: 'click',
+      simulateSelector:
+        '[data-dnb-test="modal-no-spacing"] .dnb-modal button',
+      secreenshotSelector: '.dnb-modal__content'
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+})
+
+describe('Additional Modal screenshot', () => {
+  const pageViewport = {
+    width: 400,
+    height: 400
+  }
+  setupPageScreenshot({
+    url: '/uilib/components/modal/demos',
+    pageViewport
+  })
+  it('have to match a fullscreen modal', async () => {
+    const screenshot = await testPageScreenshot({
+      selector: 'body', // only to make sure we have a valid selector
+      simulate: 'click',
+      simulateSelector:
+        '[data-dnb-test="modal-fullscreen"] .dnb-modal button',
+      secreenshotSelector: '.dnb-modal__content'
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+})
