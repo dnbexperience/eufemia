b326478edb816fca566d835c31d5fc9d2e297ec8
Tobias Høegh <tobias@tujo.no>
2020-11-24 22:21:30 +0000

    Auto-generated deploy commit by Drone 24.11.2020, 23:16:20 [CI SKIP]

--- sw.js
+++ sw.js
@@ -27,10 +27,10 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-b842f85db1fa569e0552.js"
+    "url": "webpack-runtime-0a66f8a8ee9f6ee867d7.js"
   },
   {
-    "url": "styles.075d2cdd87209c6827ee.css"
+    "url": "styles.a716bfe5ac8e1805af7b.css"
   },
   {
     "url": "styles-e8fd4555a67a3390b64e.js"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "cff1eae3020f2ff1b873d820942ab107"
+    "revision": "28ec16c34ef1ed62da90cb6778e1c3d0"
   },
   {
     "url": "static/DNB-Regular-9684ae7fd43ccd0ffce1eee01f1a04ef.woff2"
@@ -157,7 +157,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-68f00df6a39074be8640.js`))) {
+  if (!resources || !(await caches.match(`/app-cc6622389f29ad4fe94f.js`))) {
     return await fetch(event.request)
   }
 
