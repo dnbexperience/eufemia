54bf265e01e05c0fea0a085c5312e1c6dd663bd6
Tobias Høegh <tobias@tujo.no>
2021-01-19 11:49:50 +0100

    chore: enhance storybook setup for TS usage

--- packages/dnb-ui-lib/stories/helpers/Types.tsx
+++ packages/dnb-ui-lib/stories/helpers/Types.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 
 // import { Button } from '../../src/components'
 import Button from '../../src/components/button/Button'
+import GlobalError from '../../src/components/global-error/GlobalError'
 import Input from '../../src/components/input/Input'
 import { H1, P } from '../../src/elements'
 import { bell } from '../../src/icons'
@@ -14,17 +15,31 @@ export default {
 export const TypesSandbox = () => {
   return (
     <div>
-      {/* <H1 size="small">Hello Eufemia</H1>
-      <P>Start editing to see some magic happen!</P> */}
+      <H1 size="small">Hello Eufemia</H1>
+      <P>Start editing to see some magic happen!</P>
       <Button
         variant="primary"
         space={{ right: 1 }}
         right="1rem"
         icon={bell}
+        on_click={() => {}}
+        onKeyDown={() => {}}
       >
         Button
       </Button>
       <Input size="default">test</Input>
     </div>
   )
+}
+
+interface CustomProps extends React.DOMAttributes<React.SyntheticEvent> {
+  children?: React.ReactNode
+}
+const Custom = ({ children }: CustomProps) => {
+  return <button onKeyDown={() => {}}>{children}</button>
+}
+
+export const TypesTesting = () => {
+  const myProps = GlobalError.defaultProps
+  return <Custom onKeyDown={() => {}} {...myProps} />
 }
