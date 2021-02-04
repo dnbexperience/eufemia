2975fe407b813d83131c5ef3c8a65f0781dbea34
Tobias Høegh <tobias@tujo.no>
2021-02-04 22:44:22 +0100

    chore(types): add static class property relations

--- packages/dnb-ui-lib/scripts/prepub/tasks/generateTypes/babelPluginExtendTypes.js
+++ packages/dnb-ui-lib/scripts/prepub/tasks/generateTypes/babelPluginExtendTypes.js
@@ -4,49 +4,110 @@ import { parse } from '@babel/parser'
 import traverse from '@babel/traverse'
 import { babylonConfigDefaults } from './babelPluginConfigDefaults'
 
-export function babelPluginExtendTypes(plugin, { file } = {}) {
+export function babelPluginExtendTypes(babel, { file } = {}) {
   const basename = nodePath.basename(file)
   const componentName = basename.replace(nodePath.extname(file), '')
-  const componentNameWithProps = `${componentName}Props`
 
-  const { types: t } = plugin
+  const { types: t } = babel
+
+  const handleDefaultPropsProperty = (path) => {
+    if (
+      path.node?.body?.type === 'ClassBody' &&
+      hasClassProperty({
+        file,
+        componentName,
+        property: 'defaultProps'
+      })
+    ) {
+      inertClassProperty({
+        t,
+        path,
+        property: 'defaultProps',
+        type: 'object'
+      })
+    }
+  }
 
   return {
     visitor: {
-      TSInterfaceDeclaration(path) {
-        path.traverse({
-          Identifier(path) {
-            if (
-              path.isIdentifier({ name: componentNameWithProps }) &&
-              path.parentPath.isTSInterfaceDeclaration() &&
-              !(path.parentPath.node?.extends?.length > 0)
-            ) {
-              path.node.name = `${componentNameWithProps} extends React.HTMLProps<HTMLElement>`
+      Program(root) {
+        root.traverse({
+          ClassDeclaration(path) {
+            if (path.parentPath.isExportDefaultDeclaration()) {
+              /**
+               * Insert "extends React.HTMLProps<HTMLElement>" if the prop type of the component exists
+               */
+              path.traverse({
+                TSTypeReference(path) {
+                  if (
+                    path.parentPath.isTSTypeParameterInstantiation() &&
+                    path.node.typeName
+                  ) {
+                    const name = path.node.typeName.name
+
+                    root.traverse({
+                      TSInterfaceDeclaration(path) {
+                        path.traverse({
+                          Identifier(path) {
+                            if (
+                              path.parentPath.isTSInterfaceDeclaration() &&
+                              path.isIdentifier({
+                                name
+                              })
+                            ) {
+                              path.node.name = `${name} extends React.HTMLProps<HTMLElement>`
+                              path.stop()
+                            }
+                          }
+                        })
+                      }
+                    })
+                  }
+                }
+              })
             }
+
+            /**
+             * Insert "defaultProps" if it actually exists
+             * Not that important, but nice to have
+             */
+            handleDefaultPropsProperty(path)
           }
         })
-      },
 
-      ClassDeclaration(path) {
-        if (
-          path.node?.body?.type === 'ClassBody' &&
-          hasClassProperty({
-            file,
-            componentName,
-            property: 'defaultProps'
-          })
-        ) {
-          path.node.body?.body?.unshift(
-            t.classProperty(
-              t.identifier('defaultProps'),
-              null,
-              t.typeAnnotation(
-                t.genericTypeAnnotation(t.identifier('object'))
-              ),
-              null,
-              false,
-              true
-            )
+        const classProperties = getListOfClassProperties({ file })
+
+        if (classProperties) {
+          classProperties.forEach(
+            ({
+              // className,
+              // sourceFile,
+              keyName,
+              valueName,
+              importStatement
+            }) => {
+              root.traverse({
+                ImportDeclaration(path) {
+                  path.insertAfter(babel.template.ast(importStatement))
+                  path.stop()
+                }
+              })
+
+              root.traverse({
+                ClassDeclaration(path) {
+                  if (path.parentPath.isExportDefaultDeclaration()) {
+                    inertClassProperty({
+                      t,
+                      path,
+                      property: keyName,
+                      value: valueName
+                    })
+
+                    path.stop()
+                  }
+                }
+              })
+            }
           )
         }
       }
@@ -54,7 +115,22 @@ export function babelPluginExtendTypes(plugin, { file } = {}) {
   }
 }
 
-export function hasClassProperty({ file, componentName, property }) {
+function inertClassProperty({ t, path, property, value, type }) {
+  path.node.body?.body?.unshift(
+    t.classProperty(
+      t.identifier(property),
+      value ? t.identifier(value) : null,
+      type
+        ? t.typeAnnotation(t.genericTypeAnnotation(t.identifier(type)))
+        : null,
+      null,
+      false,
+      true
+    )
+  )
+}
+
+function hasClassProperty({ file, componentName, property }) {
   let exists = false
 
   const code = fs.readFileSync(nodePath.resolve(file), 'utf-8')
@@ -78,4 +154,59 @@ export function hasClassProperty({ file, componentName, property }) {
   })
 
   return exists
+}
+
+function getListOfClassProperties({ file }) {
+  const results = []
+  const listOfImportDeclaration = []
+
+  const code = fs.readFileSync(nodePath.resolve(file), 'utf-8')
+  const ast = parse(code, babylonConfigDefaults)
+
+  traverse(ast, {
+    ImportDeclaration(path) {
+      listOfImportDeclaration.push(path)
+    },
+    ClassDeclaration(path) {
+      const className = path.node.id.name
+
+      if (path.parentPath.isExportDefaultDeclaration()) {
+        path.traverse({
+          ClassProperty(path) {
+            const keyName = path.node.key.name
+            const valueName = path.node.value.name
+
+            if (
+              path.isClassProperty({ static: true }) &&
+              /^[A-Z]/.test(keyName) &&
+              /^[A-Z]/.test(valueName)
+            ) {
+              listOfImportDeclaration.forEach((path) => {
+                path.traverse({
+                  Identifier(path) {
+                    if (path.isIdentifier({ name: valueName })) {
+                      const sourceFile =
+                        path.parentPath.parentPath.node.source.value
+
+                      results.push({
+                        className,
+                        keyName,
+                        valueName,
+                        sourceFile,
+                        importStatement: path.parentPath.parentPath.toString()
+                      })
+
+                      path.stop()
+                    }
+                  }
+                })
+              })
+            }
+          }
+        })
+      }
+    }
+  })
+
+  return results
 }
