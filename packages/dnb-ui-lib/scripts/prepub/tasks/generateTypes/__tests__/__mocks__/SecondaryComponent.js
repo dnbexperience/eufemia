12b00e1ecdc73d22bc14ed5560ceda4bc7b7eefe
Tobias Høegh <tobias@tujo.no>
2021-02-08 13:55:20 +0100

    fix(types): fix element types for when using className etc. React element props

--- packages/dnb-ui-lib/scripts/prepub/tasks/generateTypes/__tests__/__mocks__/SecondaryComponent.js
+++ packages/dnb-ui-lib/scripts/prepub/tasks/generateTypes/__tests__/__mocks__/SecondaryComponent.js
@@ -16,3 +16,16 @@ export const propTypes = {
 export const defaultProps = {
   secondary: null
 }
+
+const Element = () => {
+  return null
+}
+Element.propTypes = {
+  ...propTypes,
+  children: PropTypes.node
+}
+Element.defaultProps = {
+  children: null
+}
+
+export default Element
