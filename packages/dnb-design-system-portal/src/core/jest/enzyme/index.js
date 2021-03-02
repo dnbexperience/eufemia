958a9397f4b6f495165ea7b9a07b2f520bfaa2d6
Tobias Høegh <tobias@tujo.no>
2021-03-02 11:56:47 +0100

    chore(deps): update to react v17

--- packages/dnb-design-system-portal/src/core/jest/enzyme/index.js
+++ packages/dnb-design-system-portal/src/core/jest/enzyme/index.js
@@ -7,7 +7,7 @@
 const enzyme = require('enzyme')
 
 // TODO: This may not be needed in future
-const Adapter = require('enzyme-adapter-react-16')
-enzyme.configure({ adapter: new Adapter() })
+// const Adapter = require('enzyme-adapter-react-...')
+// enzyme.configure({ adapter: new Adapter() })
 
 module.exports = enzyme
