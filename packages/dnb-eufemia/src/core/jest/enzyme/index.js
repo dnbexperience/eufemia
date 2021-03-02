46454e2b9641d28ba051608cdbb305db51d461ae
Tobias Høegh <tobias@tujo.no>
2021-03-02 11:56:47 +0100

    chore(deps): update to react v17

--- packages/dnb-eufemia/src/core/jest/enzyme/index.js
+++ packages/dnb-eufemia/src/core/jest/enzyme/index.js
@@ -7,7 +7,7 @@
 const enzyme = require('enzyme')
 
 // TODO: This may not be needed in future
-const Adapter = require('enzyme-adapter-react-16')
+const Adapter = require('@wojtekmaj/enzyme-adapter-react-17')
 enzyme.configure({ adapter: new Adapter() })
 
 module.exports = enzyme
