a692eca3ce2e205a66d29f63ee92a12ace1831b1
Tobias Høegh <tobias@tujo.no>
2021-03-02 12:10:17 +0100

    chore: update auto generated file

--- packages/dnb-eufemia/src/components/Space.js
+++ packages/dnb-eufemia/src/components/Space.js
@@ -5,7 +5,7 @@
  */
 
 /**
- * Library Index space to autogenerate all the components and patterns
+ * Library Index space to autogenerate all the components and extensions
  * Used by "prepareSpaces"
  */
 
