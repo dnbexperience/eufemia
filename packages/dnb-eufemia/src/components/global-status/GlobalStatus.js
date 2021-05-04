881d7fdaec8d7dbee28be71f52881efe5eb71b03
Tobias Høegh <tobias@tujo.no>
2021-05-04 17:55:04 +0200

    fix: include #global-status id in DOM

--- packages/dnb-eufemia/src/components/global-status/GlobalStatus.js
+++ packages/dnb-eufemia/src/components/global-status/GlobalStatus.js
@@ -631,7 +631,7 @@ export default class GlobalStatus extends React.PureComponent {
       status_anchor_text,
       skeleton,
 
-      id, // eslint-disable-line
+      id,
       item, // eslint-disable-line
       items, // eslint-disable-line
       autoclose, // eslint-disable-line
@@ -647,6 +647,7 @@ export default class GlobalStatus extends React.PureComponent {
     } = props
 
     const wrapperParams = {
+      id,
       key: 'global-status',
       className: classnames(
         'dnb-global-status__wrapper',
@@ -660,13 +661,6 @@ export default class GlobalStatus extends React.PureComponent {
       onKeyDown: this.onKeyDownHandler,
       tabIndex: '-1'
     }
-
-    // if (!isActive) {
-    //   // because of screen readers will else read the content on page load, if:
-    //   // 1. if "show" is true from beginning, then we never come here
-    //   // to make sure we double check that situation
-    //   return <div {...wrapperParams} />
-    // }
 
     const state = this.correctStatus(rawState)
     const iconToRender = GlobalStatus.getIcon({
