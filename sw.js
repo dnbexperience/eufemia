b76e74b21bea7b933550e8e0f8d3a9245682be3d
Tobias Høegh <tobias@tujo.no>
2021-08-30 20:22:11 +0000

    Auto-generated deploy commit by Drone 30.8.2021, 22:10:48 [CI SKIP]

--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "d6bdc4262afda8cb591ac4e1cce751be"
+    "revision": "c801b44458f1f965acf6655688580492"
   },
   {
     "url": "google4f78509f2ca83a08.html",
@@ -40,13 +40,13 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "81d9237fe835692f318d548cf6cd5e98"
+    "revision": "364351fa74034127170dfbf6de2084fa"
   },
   {
     "url": "framework-b345cd5b99df85fc883a.js"
   },
   {
-    "url": "styles.b875af920606a2d203bd.css"
+    "url": "styles.c275de813a1678b869f5.css"
   },
   {
     "url": "09744744-b85f6410c87cb62150f6.js"
@@ -58,11 +58,11 @@ self.__precacheManifest = [
     "url": "dc6a8720040df98778fe970bf6c000a41750d3ae-dc80b8ab8da41549d0b5.js"
   },
   {
-    "url": "app-8ed81b5aba1e3d0cb742.js"
+    "url": "app-56c46ccee7ed2aed4662.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "0e9390f2ea0c7fd0df8218eb4d9e635c"
+    "revision": "aba72caff95c305fa5124c9e1ad2fda8"
   },
   {
     "url": "static/DNB-Regular-54d35ae78c18491c35feab30a836875a.woff2"
@@ -74,7 +74,7 @@ self.__precacheManifest = [
     "url": "static/DNBMono-Regular-322db7bbbe1d833cb61311e4f598b3fb.woff2"
   },
   {
-    "url": "webpack-runtime-ce2be41a692f2381a956.js"
+    "url": "webpack-runtime-ef74982a675e7d108334.js"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0696bac6c4350130981d.js"
@@ -176,7 +176,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-8ed81b5aba1e3d0cb742.js`))) {
+  if (!resources || !(await caches.match(`/app-56c46ccee7ed2aed4662.js`))) {
     return await fetch(event.request)
   }
 
