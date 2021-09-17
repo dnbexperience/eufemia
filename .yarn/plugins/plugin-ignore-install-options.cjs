9b40dd89ee72627323e0588c86a5786d8b93f1ab
Tobias Høegh <tobias@tujo.no>
2021-09-17 22:30:51 +0200

    chore: add yarn plugin to ignore install options

---
+++ .yarn/plugins/plugin-ignore-install-options.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  name: 'plugin-ignore-install-options',
+  factory: () => {
+    if (process.argv.includes('install')) {
+      const validOptions = [
+        '-h',
+        '--help',
+        '--json',
+        '--immutable',
+        '--immutable-cache',
+        '--check-cache',
+        '--inline-builds',
+        '--mode',
+      ]
+
+      process.argv = process.argv
+        .map((arg, i) => {
+          if (i === 0 || i === 1 || validOptions.includes(arg)) {
+            return arg
+          }
+
+          return null
+        })
+        .filter(Boolean)
+    }
+
+    return {}
+  },
+}
