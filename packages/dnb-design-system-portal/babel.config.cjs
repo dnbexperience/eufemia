03990c4b63e7964f7caf17b656c2bd9d815e3ee3
Tobias Høegh <tobias@tujo.no>
2021-09-21 20:40:30 +0200

    chore: replace drone.io with Github Actions

--- packages/dnb-design-system-portal/babel.config.cjs
+++ packages/dnb-design-system-portal/babel.config.cjs
@@ -1,17 +1,4 @@
 require('dotenv').config()
-const isCI = require('is-ci')
-const { getCurrentBranchName } = require('./src/uilib/utils/git')
-
-const runModernBuild =
-  // for local builds
-  process.env.BUILD_LEGACY_BROWSERS !== '1' &&
-  // for local builds
-  (!isCI ||
-    // for CI
-    (isCI &&
-      !process.env.GATSBY_CLOUD &&
-      !process.env.NETLIFY &&
-      !/(release|portal|beta|alpha)/.test(getCurrentBranchName())))
 
 module.exports = {
   presets: [
@@ -20,20 +7,16 @@ module.exports = {
       {
         useBuiltIns: 'usage',
         corejs: 3,
-        targets: runModernBuild
-          ? {
-              chrome: '90', // Like puppeteer v8
-            }
-          : {
-              // Should reflect: https://eufemia.dnb.no/uilib/usage#supported-browsers-and-platforms
-              browsers: [
-                'ie 11',
-                'edge >= 14',
-                'firefox >= 52',
-                'chrome >= 49',
-                'safari >= 10',
-              ],
-            },
+        targets: {
+          // Should reflect: https://eufemia.dnb.no/uilib/usage#supported-browsers-and-platforms
+          browsers: [
+            'ie 11',
+            'edge >= 14',
+            'firefox >= 52',
+            'chrome >= 49',
+            'safari >= 10',
+          ],
+        },
       },
     ],
     '@babel/preset-react',
