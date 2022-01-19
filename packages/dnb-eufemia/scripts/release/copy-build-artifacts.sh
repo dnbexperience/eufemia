cb436d3e5b33ba0bee91aad549efb476393ad93f
Tobias Høegh <tobias@tujo.no>
2022-01-19 09:57:15 +0100

    fix(build): break build when step fails

--- packages/dnb-eufemia/scripts/release/copy-build-artifacts.sh
+++ packages/dnb-eufemia/scripts/release/copy-build-artifacts.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e # Exit immediately if a command exits with a non-zero status.
+
 echo 'Copy artifacts ...'
 
 rm -rf build/**/{__tests__,cjs}
--- packages/dnb-eufemia/scripts/release/postbuild.sh
+++ packages/dnb-eufemia/scripts/release/postbuild.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e # Exit immediately if a command exits with a non-zero status.
+
 echo 'Postbuild started ...'
 
 yarn build:types
@@ -10,5 +12,9 @@ yarn build:umd
 echo 'Can be enabled in future if needed -> yarn build:resources'
 yarn build:copy
 yarn prettier:other
+
+echo 'Testing the postbuild ...'
+
+yarn test:build
 
 echo 'Postbuild done!'
