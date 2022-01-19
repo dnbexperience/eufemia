0da753e0e6e795da5233c23efc50974aca597b04
Tobias Høegh <tobias@tujo.no>
2022-01-19 10:27:32 +0100

    chore: customize plugin options

--- packages/dnb-eufemia/babel.config.js
+++ packages/dnb-eufemia/babel.config.js
@@ -75,7 +75,13 @@ const productionPlugins = [
       mode: 'unsafe-wrap',
     },
   ],
-  'babel-plugin-fully-specified',
+  [
+    'babel-plugin-fully-specified',
+    {
+      ensureFileExists: ['umd'].includes(process.env.BABEL_ENV),
+      includePackages: ['date-fns'],
+    },
+  ],
 ]
 
 if (typeof process.env.BABEL_ENV !== 'undefined') {
