f97a7acc812f406804283a0cdb091bedf8cd597d
Tobias Høegh <tobias@tujo.no>
2022-01-19 14:13:28 +0100

    chore(Tag): refactor examples to not include space on left side

--- packages/dnb-design-system-portal/src/docs/uilib/components/tag/Examples.js
+++ packages/dnb-design-system-portal/src/docs/uilib/components/tag/Examples.js
@@ -9,32 +9,31 @@ import { car_1 as Car, send as Send } from '@dnb/eufemia/src/icons'
 export const TagDefault = () => (
   <ComponentBox data-visual-test="tag-default">
     {() => /* jsx */ `
-      <Tag>Payment</Tag>
-    `}
+<Tag>Payment</Tag>
+`}
   </ComponentBox>
 )
 
 export const TagWithIcon = () => (
   <ComponentBox data-visual-test="tag-icon">
-      {() => /* jsx */ `
-        <Tag icon="calendar" text="Savings agreement"/>
-      `}
+    {() => /* jsx */ `
+<Tag icon="calendar" text="Savings agreement"/>
+`}
   </ComponentBox>
 )
 
 export const TagWithSecondaryIcon = () => (
   <ComponentBox data-visual-test="tag-secondary-icon" scope={{ Car }}>
-      {() => /* jsx */ `
-        <Tag icon={Car} text="Vehicle"/>
-      `}
+    {() => /* jsx */ `
+<Tag icon={Car} text="Vehicle"/>
+`}
   </ComponentBox>
 )
 
 export const TagClickable = () => (
   <ComponentBox data-visual-test="tag-clickable" scope={{ Send }}>
-      {() => /* jsx */ `
-        <Tag icon={Send} text="Send" onClick={() => {console.log("I was sent!")}}/>
-      `}
+    {() => /* jsx */ `
+<Tag icon={Send} text="Send" onClick={() => { console.log("I was sent!") }}/>
+`}
   </ComponentBox>
 )
-
