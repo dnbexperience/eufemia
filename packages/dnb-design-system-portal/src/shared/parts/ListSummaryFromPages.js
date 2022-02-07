f4f0952344b03ba4f7f14545b4ff8c610dd4d9e7
Tobias Høegh <tobias@tujo.no>
2022-02-07 15:03:20 +0100

    docs: make automatic list of headings/pages in index files

---
+++ packages/dnb-design-system-portal/src/shared/parts/ListSummaryFromPages.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+
+import { P, Anchor, Ul, Li } from '@dnb/eufemia/src/elements'
+import AutoLinkHeader from '../tags/AutoLinkHeader'
+
+const ListSummaryFromDocs = ({ slug, useAsIndex = false }) => {
+  console.log('slug', slug)
+  const {
+    allMdx: { edges },
+  } = useStaticQuery(graphql`
+    {
+      allMdx(
+        filter: {
+          frontmatter: { title: { ne: "" }, draft: { ne: true } }
+          fileAbsolutePath: { glob: "**/uilib/**" }
+        }
+      ) {
+        edges {
+          node {
+            frontmatter {
+              title
+              description
+            }
+            slug
+          }
+        }
+      }
+    }
+  `)
+
+  const Wrapper = useAsIndex ? Ul : React.Fragment
+
+  return (
+    <>
+      {edges
+        .filter(({ node: { slug: s } }) =>
+          s.includes(String(slug).replace(/^\//, ''))
+        )
+        .map(
+          (
+            {
+              node: {
+                frontmatter: { title, description },
+                slug,
+              },
+            },
+            i
+          ) => {
+            return (
+              <Wrapper key={i}>
+                {useAsIndex ? (
+                  <Li>
+                    <Anchor href={'/' + slug}>{title}</Anchor>
+                    <br />
+                  </Li>
+                ) : (
+                  <AutoLinkHeader useSlug={'/' + slug} title={title}>
+                    <Anchor href={'/' + slug}>{title}</Anchor>
+                  </AutoLinkHeader>
+                )}
+
+                {description && <P>{description}</P>}
+              </Wrapper>
+            )
+          }
+        )}
+    </>
+  )
+}
+
+export default ListSummaryFromDocs
