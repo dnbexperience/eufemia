9185d26e57c4abd7b95024e0d2362fd9c506c3f4
Tobias <tobias@tujo.no>
2022-06-08 21:24:31 +0200

    chore(deps): bump Gatsby related deps
    Closes #1432 + update Gatsby deps

--- packages/dnb-design-system-portal/babel.config.cjs
+++ packages/dnb-design-system-portal/babel.config.cjs
@@ -1,5 +1,12 @@
 module.exports = {
-  presets: ['babel-preset-gatsby'],
+  presets: [
+    [
+      'babel-preset-gatsby',
+      {
+        reactRuntime: 'automatic',
+      },
+    ],
+  ],
   ignore: [
     // Needed in order to run on IE11
     '.*node_modules/(?!rewrite-pattern|ansi-regex|strip-ansi|regexpu-core|buble|react-live).*',
