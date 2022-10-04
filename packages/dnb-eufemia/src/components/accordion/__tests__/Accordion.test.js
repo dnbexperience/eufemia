e0057dd7299a1021a038d4c6f488991ad5fb124d
Tobias Høegh <tobias@tujo.no>
2022-10-04 06:56:39 +0200

    chore(Accordion): rewrite tests from Enzyme to TestingLib (#1598)
    * chore(Accordion): rewrite tests from Enzyme to TestingLib
    
    * Update packages/dnb-eufemia/src/components/accordion/__tests__/Accordion.test.js
    
    Co-authored-by: Anders <anderslangseth@gmail.com>
    
    Co-authored-by: Anders <anderslangseth@gmail.com>

--- packages/dnb-eufemia/src/components/accordion/__tests__/Accordion.test.js
+++ packages/dnb-eufemia/src/components/accordion/__tests__/Accordion.test.js
@@ -10,11 +10,12 @@ import {
   toJson,
   loadScss,
 } from '../../../core/jest/jestSetup'
-import Component from '../Accordion'
+import Accordion from '../Accordion'
 import {
   add_medium as AddIcon,
   subtract_medium as SubtractIcon,
 } from '../../../icons'
+import { render, fireEvent } from '@testing-library/react'
 
 const props = {}
 props.id = 'accordion'
@@ -24,32 +25,30 @@ props.title = 'title'
 
 describe('Accordion component', () => {
   it('have to match snapshot', () => {
-    const Comp = mount(<Component {...props} />)
+    const Comp = mount(<Accordion {...props} />)
 
     expect(toJson(Comp)).toMatchSnapshot()
   })
 
   it('has correct state after "click" trigger', () => {
-    const Comp = mount(<Component {...props} />)
+    const { rerender } = render(<Accordion {...props} />)
 
     // default expanded value has to be false
-    expect(Comp.state().expanded).toBe(false)
     expect(
-      Comp.find('.dnb-accordion__header')
-        .instance()
+      document
+        .querySelector('.dnb-accordion__header')
         .getAttribute('aria-expanded')
     ).toBe('false')
-    Comp.find('.dnb-accordion__header').simulate('click') // we could send inn the event data structure like this: , { target: { expanded: true } }
-    expect(Comp.state().expanded).toBe(true)
+    fireEvent.click(document.querySelector('.dnb-accordion__header')) // we could send inn the event data structure like this: , { target: { expanded: true } }
     expect(
-      Comp.find('.dnb-accordion__header')
-        .instance()
+      document
+        .querySelector('.dnb-accordion__header')
         .getAttribute('aria-expanded')
     ).toBe('true')
-    Comp.setProps({ expanded: false })
+    rerender(<Accordion {...props} expanded={false} />)
     expect(
-      Comp.find('.dnb-accordion__header')
-        .instance()
+      document
+        .querySelector('.dnb-accordion__header')
         .getAttribute('aria-expanded')
     ).toBe('false')
   })
@@ -57,8 +56,8 @@ describe('Accordion component', () => {
   it('has "on_change" event which will trigger on click', () => {
     const my_event = jest.fn()
     const myEvent = jest.fn()
-    const Comp = mount(
-      <Component
+    render(
+      <Accordion
         {...props}
         on_change={my_event}
         onChange={myEvent}
@@ -67,7 +66,7 @@ describe('Accordion component', () => {
     )
 
     // first click
-    Comp.find('.dnb-accordion__header').simulate('click')
+    fireEvent.click(document.querySelector('.dnb-accordion__header'))
     expect(my_event).toHaveBeenCalled()
     expect(my_event.mock.calls[0][0].expanded).toBe(true)
     expect(myEvent.mock.calls.length).toBe(1)
@@ -75,26 +74,24 @@ describe('Accordion component', () => {
     expect(myEvent.mock.calls[0][0].expanded).toBe(true)
 
     // second click
-    Comp.find('.dnb-accordion__header').simulate('click')
+    fireEvent.click(document.querySelector('.dnb-accordion__header'))
     expect(my_event.mock.calls[1][0].expanded).toBe(false)
   })
 
   it('has a disabled attribute, once we set disabled to true', () => {
-    const Comp = mount(<Component {...props} />)
+    const { rerender } = render(<Accordion {...props} />)
 
-    Comp.setProps({
-      disabled: true,
-    })
+    rerender(<Accordion {...props} disabled={true} />)
     expect(
-      Comp.find('.dnb-accordion__header')
-        .instance()
+      document
+        .querySelector('.dnb-accordion__header')
         .hasAttribute('disabled')
     ).toBe(true)
   })
 
   it('supports an icon for expanded state ', () => {
-    const Comp = mount(
-      <Component
+    render(
+      <Accordion
         {...props}
         icon={{
           closed: AddIcon,
@@ -102,62 +99,68 @@ describe('Accordion component', () => {
         }}
       />
     )
-    expect(Comp.find('.dnb-accordion').find('svg').html()).toBe(
-      mount(<AddIcon />).html()
+
+    const { container: c1 } = render(<AddIcon />)
+    const { container: c2 } = render(<SubtractIcon />)
+
+    expect(document.querySelector('.dnb-accordion svg').outerHTML).toBe(
+      c1.innerHTML
     )
-    Comp.setProps({ expanded: true })
-    expect(Comp.find('.dnb-accordion').find('svg').html()).toBe(
-      mount(<SubtractIcon />).html()
+
+    fireEvent.click(document.querySelector('.dnb-accordion__header'))
+
+    expect(document.querySelector('.dnb-accordion svg').outerHTML).toBe(
+      c2.innerHTML
     )
   })
 
   it('supports default outlined variant', () => {
-    const Comp = mount(<Component />)
+    render(<Accordion />)
     expect(
-      Comp.find('.dnb-accordion')
-        .instance()
+      document
+        .querySelector('.dnb-accordion')
         .classList.contains('dnb-accordion__variant--outlined')
     ).toBe(true)
   })
 
   it('supports plain variant', () => {
-    const Comp = mount(<Component {...props} variant="plain" />)
+    render(<Accordion {...props} variant="plain" />)
     expect(
-      Comp.find('.dnb-accordion')
-        .instance()
+      document
+        .querySelector('.dnb-accordion')
         .classList.contains('dnb-accordion__variant--plain')
     ).toBe(true)
   })
 
   it('should have hidden content when "prerender" is true but closed', () => {
-    const Comp = mount(<Component {...props} prerender={true} />)
+    render(<Accordion {...props} prerender={true} />)
 
     expect(
-      Comp.find('.dnb-accordion__content')
-        .instance()
-        .classList.contains('dnb-accordion__content--hidden')
-    ).toBe(true)
+      Array.from(document.querySelector('.dnb-accordion__content'))
+    ).toBeTruthy()
 
     expect(
-      Comp.find('.dnb-accordion')
-        .instance()
+      document
         .querySelector('.dnb-accordion__content__inner')
         .classList.contains(
           'dnb-accordion__content__inner--remove-content'
         )
     ).toBe(true)
 
-    Comp.find('.dnb-accordion__header').simulate('click')
+    fireEvent.click(document.querySelector('.dnb-accordion__header'))
+
+    expect(document.querySelector('.dnb-accordion__content')).toBeTruthy()
+    expect(
+      Array.from(
+        document.querySelector('.dnb-accordion__content').classList
+      )
+    ).toEqual([
+      'dnb-accordion__content',
+      'dnb-accordion__content--is-animating',
+    ])
 
     expect(
-      Comp.find('.dnb-accordion__content')
-        .instance()
-        .classList.contains('dnb-accordion__content--hidden')
-    ).toBe(false)
-
-    expect(
-      Comp.find('.dnb-accordion')
-        .instance()
+      document
         .querySelector('.dnb-accordion__content__inner')
         .classList.contains(
           'dnb-accordion__content__inner--remove-content'
@@ -166,7 +169,7 @@ describe('Accordion component', () => {
   })
 
   it('should validate with ARIA rules', async () => {
-    const Comp = mount(<Component {...props} />)
+    const Comp = render(<Accordion {...props} />)
 
     expect(await axeComponent(Comp)).toHaveNoViolations()
   })
@@ -174,7 +177,7 @@ describe('Accordion component', () => {
 
 describe('Accordion store API', () => {
   it('will save and read the states for a single accordion', () => {
-    const inst = Component.Store('accordion-id')
+    const inst = Accordion.Store('accordion-id')
 
     inst.saveState(true)
     expect(inst.getState()).toBe(true)
@@ -190,7 +193,7 @@ describe('Accordion store API', () => {
   })
 
   it('will save and read the states for an accordion group', () => {
-    const inst = Component.Group.Store('group-id')
+    const inst = Accordion.Group.Store('group-id')
 
     inst.saveState(true, 'remembered-state-2')
     expect(inst.getState('remembered-state-2')).toBe(true)
@@ -207,71 +210,73 @@ describe('Accordion store API', () => {
 })
 
 describe('Accordion group component', () => {
-  const Comp = mount(
-    <Component.Group expanded id="group">
-      <Component id="accordion-1" title="Accordion 1">
-        Accordion 1
-      </Component>
-      <Component id="accordion-2" title="Accordion 2" expanded={false}>
-        Accordion 2
-      </Component>
-    </Component.Group>
-  )
-  it('has to have correct aria-pressed', () => {
+  it('has to inherit expanded from group', () => {
+    render(
+      <Accordion.Group expanded id="group">
+        <Accordion id="accordion-1" title="Accordion 1">
+          Accordion 1
+        </Accordion>
+        <Accordion id="accordion-2" title="Accordion 2" expanded={false}>
+          Accordion 2
+        </Accordion>
+      </Accordion.Group>
+    )
+
     expect(
-      Comp.find('#accordion-1').exists('.dnb-accordion__content--hidden')
-    ).toBe(false)
+      document.querySelector('#accordion-1 .dnb-accordion__content')
+    ).toBeTruthy()
     expect(
-      Comp.find('#accordion-2').exists('.dnb-accordion__content--hidden')
-    ).toBe(true)
+      document.querySelector(
+        '#accordion-2 .dnb-accordion__content--hidden'
+      )
+    ).toBeTruthy()
   })
+
   it('has "on_change" event which will trigger on a button click', () => {
     const my_event = jest.fn()
     const myEvent = jest.fn()
-    const Comp = mount(
-      <Component.Group
+    render(
+      <Accordion.Group
         id="group"
         on_change={my_event}
         onChange={myEvent}
         value="second"
         data-prop="group-value"
       >
-        <Component
+        <Accordion
           id="accordion-1"
           text="Accordion 1"
           value="first"
           data-prop="value-1"
           attributes={{ 'data-attr': 'value' }}
         />
-        <Component
+        <Accordion
           id="accordion-2"
           text="Accordion 2"
           value="second"
           data-prop="value-2"
           attributes={{ 'data-attr': 'value' }}
         />
-      </Component.Group>
+      </Accordion.Group>
     )
 
-    Comp.find('#accordion-1')
-      .find('.dnb-accordion__header')
-      .simulate('click')
+    fireEvent.click(
+      document.querySelector('#accordion-1 .dnb-accordion__header')
+    )
     expect(my_event).toHaveBeenCalled()
     expect(my_event.mock.calls[0][0].id).toBe('accordion-1')
     expect(my_event.mock.calls[0][0].expanded).toBe(true)
     expect(myEvent.mock.calls.length).toBe(1)
 
-    Comp.find('#accordion-2')
-      .find('.dnb-accordion__header')
-      .at(0)
-      .simulate('click')
+    fireEvent.click(
+      document.querySelector('#accordion-2 .dnb-accordion__header')
+    )
     expect(my_event.mock.calls[1][0].id).toBe('accordion-2')
     expect(my_event.mock.calls[1][0].expanded).toBe(true)
 
-    Comp.find('#accordion-1')
-      .find('.dnb-accordion__header')
-      .at(0)
-      .simulate('click')
+    fireEvent.click(
+      document.querySelector('#accordion-1 .dnb-accordion__header')
+    )
     expect(my_event.mock.calls[2][0].expanded).toBe(true)
   })
 })
@@ -296,74 +301,92 @@ describe('Accordion container component', () => {
     )
   }
 
-  const Comp = mount(
-    <>
-      <Increment />
-      <Component.Group
-        id="container"
-        single_container
-        prevent_rerender
-        remember_state
-      >
-        <Component id="accordion-1" title="Accordion 1">
-          Accordion 1
-          <DidRender id="mounted-1" />
-        </Component>
-        <Component id="accordion-2" title="Accordion 2" expanded={true}>
-          Accordion 2
-          <DidRender id="mounted-2" />
-        </Component>
-        <Component id="accordion-3" title="Accordion 3">
-          Accordion 3
-          <DidRender id="mounted-3" />
-        </Component>
-      </Component.Group>
-    </>
-  )
-
   it('has only to render the expanded accordion content', () => {
-    expect(Comp.find('button#increment').text()).toBe('1')
-    Comp.find('button#increment').simulate('click')
-    expect(Comp.find('button#increment').text()).toBe('2')
+    render(
+      <>
+        <Increment />
+        <Accordion.Group
+          id="container"
+          single_container
+          prevent_rerender
+          remember_state
+        >
+          <Accordion id="accordion-1" title="Accordion 1">
+            Accordion 1
+            <DidRender id="mounted-1" />
+          </Accordion>
+          <Accordion id="accordion-2" title="Accordion 2" expanded={true}>
+            Accordion 2
+            <DidRender id="mounted-2" />
+          </Accordion>
+          <Accordion id="accordion-3" title="Accordion 3">
+            Accordion 3
+            <DidRender id="mounted-3" />
+          </Accordion>
+        </Accordion.Group>
+      </>
+    )
 
-    expect(Comp.find('div#mounted-1').exists()).toBe(false)
-    expect(Comp.find('div#mounted-2').text()).toBe('true')
-    expect(Comp.find('div#mounted-3').exists()).toBe(false)
+    expect(document.querySelector('button#increment').textContent).toBe(
+      '1'
+    )
+
+    fireEvent.click(document.querySelector('button#increment'))
+    expect(document.querySelector('button#increment').textContent).toBe(
+      '2'
+    )
+
+    expect(document.querySelector('div#mounted-1')).toBeFalsy()
+    expect(document.querySelector('div#mounted-2').textContent).toBe(
+      'true'
+    )
+    expect(document.querySelector('div#mounted-3')).toBeFalsy()
     expect(
-      Comp.find('#accordion-2')
-        .find('.dnb-accordion__header')
-        .instance()
+      document
+        .querySelector('#accordion-2 .dnb-accordion__header')
         .getAttribute('aria-expanded')
     ).toBe('true')
 
-    Comp.find('#accordion-1')
-      .find('.dnb-accordion__header')
-      .simulate('click')
+    fireEvent.click(
+      document.querySelector('#accordion-1 .dnb-accordion__header')
+    )
 
-    Comp.find('button#increment').simulate('click')
-    expect(Comp.find('button#increment').text()).toBe('3')
+    fireEvent.click(document.querySelector('button#increment'))
+    expect(document.querySelector('button#increment').textContent).toBe(
+      '3'
+    )
 
-    expect(Comp.find('div#mounted-1').text()).toBe('true')
-    expect(Comp.find('div#mounted-2').text()).toBe('true')
-    expect(Comp.find('div#mounted-3').exists()).toBe(false)
+    expect(document.querySelector('div#mounted-1').textContent).toBe(
+      'true'
+    )
+    expect(document.querySelector('div#mounted-2').textContent).toBe(
+      'true'
+    )
+    expect(document.querySelector('div#mounted-3')).toBeFalsy()
 
-    Comp.find('#accordion-2')
-      .find('.dnb-accordion__header')
-      .simulate('click')
+    fireEvent.click(
+      document.querySelector('#accordion-2 .dnb-accordion__header')
+    )
 
-    Comp.find('button#increment').simulate('click')
-    expect(Comp.find('button#increment').text()).toBe('4')
+    fireEvent.click(document.querySelector('button#increment'))
+    expect(document.querySelector('button#increment').textContent).toBe(
+      '4'
+    )
 
-    expect(Comp.find('div#mounted-3').exists()).toBe(false)
+    expect(document.querySelector('div#mounted-3')).toBeFalsy()
 
-    Comp.find('#accordion-3')
-      .find('.dnb-accordion__header')
-      .simulate('click')
+    fireEvent.click(
+      document.querySelector('#accordion-3 .dnb-accordion__header')
+    )
 
-    Comp.find('button#increment').simulate('click')
-    expect(Comp.find('button#increment').text()).toBe('5')
+    fireEvent.click(document.querySelector('button#increment'))
+    expect(document.querySelector('button#increment').textContent).toBe(
+      '5'
+    )
 
-    expect(Comp.find('div#mounted-3').text()).toBe('true')
+    expect(document.querySelector('div#mounted-3').textContent).toBe(
+      'true'
+    )
   })
 })
 
@@ -372,6 +395,7 @@ describe('Accordion scss', () => {
     const scss = loadScss(require.resolve('../style/dnb-accordion.scss'))
     expect(scss).toMatchSnapshot()
   })
+
   it('have to match default theme snapshot', () => {
     const scss = loadScss(
       require.resolve('../style/themes/dnb-accordion-theme-ui.scss')
