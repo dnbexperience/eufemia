f0ec46950330ca963c0a1ba838a25e03dc71424f
Tobias Høegh <tobias@tujo.no>
2022-10-11 16:13:06 +0200

    chore(deps): update Gatsby to latest because of useful fix (#1603)
    Because of this [fix](https://github.com/gatsbyjs/gatsby/pull/36623) which I think if have seen happening in our Portal as well.
    
    Run lint with NODE_ENV=test to prevent babel parsing error (because of babel config change it gets picket up now)

--- packages/dnb-design-system-portal/babel.config.js
+++ packages/dnb-design-system-portal/babel.config.js
@@ -3,6 +3,10 @@ module.exports = {
     [
       'babel-preset-gatsby',
       {
+        stage:
+          process.env.NODE_ENV === 'development'
+            ? 'develop'
+            : 'build-javascript',
         reactRuntime: 'automatic',
       },
     ],
