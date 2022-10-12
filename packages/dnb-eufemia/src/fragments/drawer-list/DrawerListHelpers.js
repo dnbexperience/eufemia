0f15c4339d92a1618f143fda84c7eddf387cd802
Tobias <tobias@tujo.no>
2022-10-12 10:22:14 +0200

    dobble check

--- packages/dnb-eufemia/src/fragments/drawer-list/DrawerListHelpers.js
+++ packages/dnb-eufemia/src/fragments/drawer-list/DrawerListHelpers.js
@@ -453,10 +453,10 @@ export const prepareDerivedState = (props, state) => {
   }
 
   if (
-    state.selected_item !== props.value &&
-    (state._value !== props.value ||
-      state._data !== props.data ||
-      isTrue(props.prevent_selection))
+    // state.selected_item !== props.value &&
+    state._value !== props.value ||
+    state._data !== props.data ||
+    isTrue(props.prevent_selection)
   ) {
     if (props.value === 'initval') {
       state.selected_item = null
