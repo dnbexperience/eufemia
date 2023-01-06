7af8b41cc8934bd9218e707a118a9ae9c1669fc8
Audun Beck <17355570+AudunBeck@users.noreply.github.com>
2023-01-06 11:56:55 +0100

    chore: change P.test to use testing library

--- packages/dnb-eufemia/src/elements/__tests__/P.test.js
+++ packages/dnb-eufemia/src/elements/__tests__/P.test.js
@@ -11,6 +11,7 @@ import {
   axeComponent,
 } from '../../core/jest/jestSetup'
 import Component from '../P'
+import { render } from '@testing-library/react'
 
 const props = fakeProps(require.resolve('../P'), {
   optional: true,
@@ -24,29 +25,47 @@ describe('P element', () => {
     expect(toJson(Comp)).toMatchSnapshot()
   })
   it('has correct size when size is defined', () => {
-    const Comp = mount(<Component size="large" />)
-    expect(Comp.find('.dnb-p__size--large').exists()).toBe(true)
+    const { container } = render(<Component size="large" />)
+
+    expect(
+      container.firstChild.classList.contains('dnb-p__size--large')
+    ).toBe(true)
   })
   it('has correct style when size and a modifier is defined', () => {
-    const Comp = mount(<Component size="medium" modifier="medium" />)
-    expect(Comp.find('.dnb-p__size--medium').exists()).toBe(true)
-    expect(Comp.find('.dnb-p--medium').exists()).toBe(true)
+    const { container } = render(
+      <Component size="medium" modifier="medium" />
+    )
+    expect(
+      container.firstChild.classList.contains('dnb-p__size--medium')
+    ).toBe(true)
+    expect(container.firstChild.classList.contains('dnb-p--medium')).toBe(
+      true
+    )
   })
   it('has correct style when several modifiers are defined', () => {
-    const Comp = mount(<Component modifier="medium small" />)
-    expect(Comp.find('.dnb-p__size--small').exists()).toBe(true)
-    expect(Comp.find('.dnb-p--medium').exists()).toBe(true)
+    render(<Component modifier="medium small" />)
+    const { container } = render(<Component modifier="medium small" />)
+    expect(
+      container.firstChild.classList.contains('dnb-p__size--small')
+    ).toBe(true)
+    expect(container.firstChild.classList.contains('dnb-p--medium')).toBe(
+      true
+    )
   })
   it('has correct style when medium is set to true', () => {
-    const Comp = mount(<Component medium />)
-    expect(Comp.find('.dnb-p--medium').exists()).toBe(true)
+    const { container } = render(<Component medium />)
+    expect(container.firstChild.classList.contains('dnb-p--medium')).toBe(
+      true
+    )
   })
   it('has correct style when bold is set to true', () => {
-    const Comp = mount(<Component bold />)
-    expect(Comp.find('.dnb-p--bold').exists()).toBe(true)
+    const { container } = render(<Component bold />)
+    expect(container.firstChild.classList.contains('dnb-p--bold')).toBe(
+      true
+    )
   })
   it('should validate with ARIA rules as a p element', async () => {
-    const Comp = mount(<Component {...props} />)
+    const Comp = render(<Component {...props} />)
     expect(await axeComponent(Comp)).toHaveNoViolations()
   })
 })
