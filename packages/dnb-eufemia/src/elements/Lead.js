7d777f797092809b352fcf69ddd5f747fab2c810
Anders <anderslangseth@gmail.com>
2023-01-06 15:14:04 +0100

    feat(P): deprecate style_type property (#1855)

--- packages/dnb-eufemia/src/elements/Lead.js
+++ packages/dnb-eufemia/src/elements/Lead.js
@@ -6,6 +6,6 @@
 import React from 'react'
 import P from './P'
 
-const Lead = (p) => <P style_type="lead" {...p} />
+const Lead = (p) => <P className="dnb-p--lead" {...p} />
 Lead.tagName = 'dnb-p--lead'
 export default Lead
