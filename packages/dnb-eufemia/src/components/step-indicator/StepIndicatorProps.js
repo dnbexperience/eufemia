415c3f98a78105715d3a017bd82e0726b4fa1cea
Anders <anderslangseth@gmail.com>
2023-01-06 08:07:44 +0100

    feat: deprecate StepIndicator v1 (#1840)

--- packages/dnb-eufemia/src/components/step-indicator/StepIndicatorProps.js
+++ packages/dnb-eufemia/src/components/step-indicator/StepIndicatorProps.js
@@ -18,15 +18,6 @@ export const stepIndicatorPropTypes = {
 
         on_click: PropTypes.func,
         on_render: PropTypes.func,
-
-        /* Deprecated */
-        url: PropTypes.string, // Deprecated
-        /* Deprecated */
-        is_active: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]), // Deprecated
-        /* Deprecated */
-        url_future: PropTypes.string, // Deprecated
-        /* Deprecated */
-        url_passed: PropTypes.string, // Deprecated
       })
     ),
   ]),
@@ -35,13 +26,7 @@ export const stepIndicatorPropTypes = {
   step_title: PropTypes.string,
   current_step: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 
-  /* Deprecated */
-  active_item: PropTypes.oneOfType([PropTypes.string, PropTypes.number]), // Deprecated
-  /* Deprecated */
-  active_url: PropTypes.string, // Deprecated
-
   hide_numbers: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
-  use_navigation: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]), // Deprecated
   on_item_render: PropTypes.func,
   no_animation: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
   skeleton: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
@@ -63,10 +48,7 @@ export const stepIndicatorDefaultProps = {
   step_title_extended: null,
   step_title: null,
   current_step: null,
-  active_item: null, // Deprecated
-  active_url: null, // Deprecated
   hide_numbers: false,
-  use_navigation: null, // Deprecated
   on_item_render: null,
   no_animation: null,
   skeleton: false,
