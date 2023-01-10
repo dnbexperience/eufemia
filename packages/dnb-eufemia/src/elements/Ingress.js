9c73fd89d89b9a69a862108bc1156a16fefcbe76
Anders <anderslangseth@gmail.com>
2023-01-10 15:50:03 +0100

    feat(P): remove deprecated class dnb-p--ingress (#1872)

--- packages/dnb-eufemia/src/elements/Ingress.js
+++ packages/dnb-eufemia/src/elements/Ingress.js
@@ -6,6 +6,6 @@
 import React from 'react'
 import P from './P'
 
-const Ingress = (p) => <P className="dnb-p--ingress" {...p} />
+const Ingress = (p) => <P medium {...p} />
 Ingress.tagName = 'dnb-p--ingress'
 export default Ingress
