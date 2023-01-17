1ff6dc15af2e7778df7d0bbb0bf10295544ae62f
Anders <anderslangseth@gmail.com>
2023-01-17 09:21:45 +0100

    feat(Number): Removes component (#1924)

--- packages/dnb-eufemia/src/components/Number.js
+++
@@ -1,8 +0,0 @@
-// Deprecation of Number component
-import React from 'react'
-import NumberFormat from './number-format/NumberFormat'
-export * from './number-format/NumberFormat'
-export default function Number(...props) {
-  console.warn('Please rename the Number component to => NumberFormat')
-  return <NumberFormat {...props} />
-}
