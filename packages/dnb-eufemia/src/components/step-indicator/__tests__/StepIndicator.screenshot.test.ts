fcc9348c86b52d79d1949a13120e17e782da4d42
Tobias Høegh <tobias@tujo.no>
2023-01-31 15:32:39 +0100

    feat: replace visual test driver to jest-image-snapshot and Firefox (Playwright) (#1945)
    * feat: replace jest visual test driver to jest-image-snapshot
    
    * chore: add .jest-image-snapshot-touched-files to .gitignore
    
    * chore: remove outdated deps update limitation
    
    * chore: refactor/align script commands
    
    * chore: remove screenshot as a part of the description
    
    * chore: remove all old snapshots
    
    * feat: ensure page and ScrollView is not scrolling
    
    * feat: refactor setupPageScreenshot
    
    * chore: rename testPageScreenshot to makeScreenshot
    
    * chore: upload diff images to GitHub Action as an artifact
    
    * chore: convert from puppeteer to playwright
    
    Background:
    
    I tried these versions of puppeteer: 8 to 15 + 19
    
    Version 8, 9 and 10 did work fine. While version 10.2.0 has a change in its method of calculating page offsets. Here is it [better explained](https://github.com/puppeteer/puppeteer/issues/7514#issuecomment-1005917527).
    
    This change leads to a negative side effect so snapshots differ from when run on CI macOS vs local macOS. A typical comparison looks then as so:
    
    ![avatar-src-have-to-match-png-image-of-local-src snap-diff](https://user-images.githubusercontent.com/1501870/213706793-3b41d185-0782-4088-ba72-7d5eb7b54b94.png)
    
    I also see that "jest-image-snapshot" has not so many "parameters" to tweak. We basically have `failureThreshold`. Before we had [several more](https://github.com/dnbexperience/eufemia/blob/88279d1e6903497fce40d6c4322dd9b7c7b9ac3b/packages/dnb-eufemia/src/core/jest/jestSetupScreenshots.js#L39).
    
    When running our local generated snapshots on the CI, we typical get this:
    
    ![button-icon-have-to-match-icon-button snap-diff](https://user-images.githubusercontent.com/1501870/214040509-b0345bed-0d88-4653-93df-e7f650524bd8.png)
    
    * chore: align visual test and update snapshots
    
    Align Dialog
    
    Align Paragraph
    
    Align Anchor (new)
    
    Align Hr
    
    Align Lists
    
    Align Typography
    
    Align Heading
    
    Align Autocomplete
    
    Align Avatar
    
    Align Badge
    
    Align Breadcrumb
    
    Align Button
    
    Align Checkbox
    
    Align Drawer
    
    Align Dropdown
    
    Align FormLabel
    
    Align FormRow and FormSet
    
    Align FormStatus
    
    Align GlobalError
    
    Align GlobalStatus
    
    Align HelpButton
    
    Align Icon
    
    Algin InfoCard
    
    Align Input
    
    Align Modal
    
    Align NumberFormat
    
    Align Pagination
    
    Align ProgressIndicator
    
    Align Radio button
    
    Align Seciton
    
    Align Skeleton
    
    Align Slider
    
    Align Space
    
    Align StepIndicator
    
    Align switch
    
    Align Tabs
    
    Align Tag
    
    Align Textarea
    
    Align Timeline
    
    Align ToggleButton
    
    Align Tooltip
    
    Align Upload
    
    Align VisuallyHidden
    
    Align Image
    
    Align Blockquote
    
    Align DrawerList
    
    Align DatePicker
    
    Align Table
    
    Align PaymentCard
    
    Align HelperClasses
    
    Align Logo
    
    s

---
+++ packages/dnb-eufemia/src/components/step-indicator/__tests__/StepIndicator.screenshot.test.ts
@@ -0,0 +1,95 @@
+/**
+ * Screenshot Test
+ * This file will not run on "test:staged" because we don't require any related files
+ */
+
+import {
+  makeScreenshot,
+  setupPageScreenshot,
+} from '../../../core/jest/jestSetupScreenshots'
+
+describe('StepIndicator', () => {
+  setupPageScreenshot({ url: '/uilib/components/step-indicator/demos' })
+
+  it('have to match loose mode', async () => {
+    const screenshot = await makeScreenshot({
+      selector: '[data-visual-test="step-indicator-loose"]',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('have to match loose mode after click simulation', async () => {
+    const screenshot = await makeScreenshot({
+      selector: '[data-visual-test="step-indicator-loose"]',
+      simulateSelector:
+        '[data-visual-test="step-indicator-loose"] .dnb-step-indicator__item:nth-of-type(3) button',
+      simulate: 'click',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('have to match strict mode', async () => {
+    const screenshot = await makeScreenshot({
+      selector: '[data-visual-test="step-indicator-strict"]',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('have to match strict mode after click simulation', async () => {
+    const screenshot = await makeScreenshot({
+      selector: '[data-visual-test="step-indicator-strict"]',
+      simulateSelector:
+        '[data-visual-test="step-indicator-strict"] .dnb-step-indicator__item:nth-of-type(1) button',
+      simulate: 'click',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('have to match static mode after click simulation', async () => {
+    const screenshot = await makeScreenshot({
+      selector: 'div#dnb-modal-root',
+      simulateSelector:
+        '[data-visual-test="step-indicator-static"] .dnb-step-indicator__trigger__button',
+      simulate: 'click',
+      screenshotSelector: '.dnb-modal__content',
+      rootClassName: 'hide-page-content',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+})
+
+describe('StepIndicator', () => {
+  setupPageScreenshot({
+    url: '/uilib/components/step-indicator/demos',
+    pageViewport: {
+      width: 500,
+      height: 600,
+    },
+  })
+
+  it('have to match loose mode for small screens', async () => {
+    const screenshot = await makeScreenshot({
+      selector: '[data-visual-test="step-indicator-loose"]',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('have to match strict mode for small screens', async () => {
+    const screenshot = await makeScreenshot({
+      selector: '[data-visual-test="step-indicator-strict"]',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('have to match strict mode for small screens after click simulation', async () => {
+    const screenshot = await makeScreenshot({
+      selector: 'div#dnb-modal-root',
+      simulateSelector:
+        '[data-visual-test="step-indicator-strict"] .dnb-step-indicator__trigger__button',
+      simulate: 'click',
+      screenshotSelector: '.dnb-modal__content',
+      rootClassName: 'hide-page-content',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+})
---
+++ packages/dnb-eufemia/src/components/tabs/__tests__/Tabs.screenshot.test.ts
@@ -0,0 +1,189 @@
+/**
+ * Screenshot Test
+ * This file will not run on "test:staged" because we don't require any related files
+ */
+
+import { makeScreenshot } from '../../../core/jest/jestSetupScreenshots'
+
+describe('Tabs', () => {
+  it('have to match the "Tablist"', async () => {
+    const screenshot = await makeScreenshot({
+      url: '/uilib/components/tabs/demos',
+      selector: '[data-visual-test="tabs-tablist"] .dnb-tabs__tabs',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('have to match a tablist with a click handler', async () => {
+    const screenshot = await makeScreenshot({
+      url: '/uilib/components/tabs/demos',
+      selector: '[data-visual-test="tabs-clickhandler"] .dnb-tabs__tabs',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('have to match horizontal aligned tabs', async () => {
+    const screenshot = await makeScreenshot({
+      url: '/uilib/components/tabs/demos',
+      style: {
+        width: '60rem',
+      },
+      selector: '[data-visual-test="tabs-horizontal-aligned"]',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('have to match max-width tabs', async () => {
+    const screenshot = await makeScreenshot({
+      url: '/uilib/components/tabs/demos',
+      style: {
+        width: '40rem',
+        height: '4rem',
+      },
+      selector: '[data-visual-test="tabs-max-width"]',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('have to match custom section styles', async () => {
+    const screenshot = await makeScreenshot({
+      url: '/uilib/components/tabs/demos',
+      style: {
+        width: '40rem',
+        height: '14rem',
+        padding: '0 2rem 4rem 2rem',
+      },
+      selector: '[data-visual-test="tabs-section-styles"]',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('have to match tabs with no bottom border', async () => {
+    const screenshot = await makeScreenshot({
+      url: '/uilib/components/tabs/demos',
+      style: {
+        width: '40rem',
+        height: '10rem',
+        padding: '0 2rem 4rem 2rem',
+      },
+      selector: '[data-visual-test="tabs-no-border"]',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('have to match ignored scrollable tabs', async () => {
+    const screenshot = await makeScreenshot({
+      url: '/uilib/components/tabs/demos',
+      style: {
+        width: '80rem',
+        padding: '0 2rem 4rem 2rem',
+      },
+      selector: '[data-visual-test="tabs-tablist-scrollable"]',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('have to match the "Tablist" on focus state', async () => {
+    const screenshot = await makeScreenshot({
+      url: '/uilib/components/tabs/demos',
+      selector: '[data-visual-test="tabs-tablist"] .dnb-tabs__tabs',
+      style: { margin: '0 2rem' },
+      simulateSelector:
+        '[data-visual-test="tabs-tablist"] .dnb-tabs__tabs__tablist',
+      simulate: 'focus',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('have to match a tab button in focus state', async () => {
+    const screenshot = await makeScreenshot({
+      url: '/uilib/components/tabs/demos',
+      selector: '[data-visual-test="tabs-tablist"] .dnb-tabs__tabs',
+      simulateSelector:
+        '[data-visual-test="tabs-tablist"] .dnb-tabs__tabs__tablist .dnb-tabs__button__snap:nth-of-type(2) button',
+      simulate: 'focus',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('have to match tabs content with focus ring', async () => {
+    const screenshot = await makeScreenshot({
+      url: '/uilib/components/tabs/demos',
+      selector:
+        '[data-visual-test="tabs-section-styles"] .dnb-tabs__content',
+      simulateSelector:
+        '[data-visual-test="tabs-section-styles"] .dnb-tabs__content',
+      simulate: 'focus',
+      executeBeforeSimulate: () => {
+        document.documentElement.setAttribute('data-whatinput', 'keyboard')
+      },
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('have to match scrollable tabs narrow', async () => {
+    const screenshot = await makeScreenshot({
+      url: '/uilib/components/tabs/demos',
+      pageViewport: {
+        width: 480, // 30rem
+      },
+      style: {
+        width: '30rem',
+        padding: '0 3rem 4rem 3rem',
+      },
+      selector: '[data-visual-test="tabs-tablist-scrollable"]',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('have to match scrollable tabs', async () => {
+    const screenshot = await makeScreenshot({
+      url: '/uilib/components/tabs/demos',
+      pageViewport: {
+        width: 800, // 50rem
+      },
+      style: {
+        width: '50rem',
+        padding: '0 3rem 4rem 3rem',
+      },
+      selector: '[data-visual-test="tabs-tablist-scrollable"]',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('have to match first scrollable tabs', async () => {
+    const screenshot = await makeScreenshot({
+      pageViewport: {
+        width: 800, // 50rem
+      },
+      url: '/uilib/components/tabs/demos',
+      style: {
+        width: '50rem',
+        padding: '0 3rem 4rem 3rem',
+      },
+      selector: '[data-visual-test="tabs-tablist-scrollable"]',
+      simulateSelector:
+        '[data-visual-test="tabs-tablist-scrollable"] .dnb-tabs__tabs__tablist .dnb-tabs__button__snap:first-of-type button',
+      simulate: 'click',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('have to match last scrollable tabs', async () => {
+    const screenshot = await makeScreenshot({
+      pageViewport: {
+        width: 800, // 50rem
+      },
+      url: '/uilib/components/tabs/demos',
+      style: {
+        width: '50rem',
+        padding: '0 3rem 4rem 3rem',
+      },
+      selector: '[data-visual-test="tabs-tablist-scrollable"]',
+      simulateSelector:
+        '[data-visual-test="tabs-tablist-scrollable"] .dnb-tabs__tabs__tablist .dnb-tabs__button__snap:last-of-type button',
+      simulate: 'click',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+})
