6402aa5224acc53140a0e2509bd9335e4b601227
Tobias Høegh <tobias@tujo.no>
2023-01-31 15:32:39 +0100

    feat: replace visual test driver to jest-image-snapshot and Firefox (Playwright) (#1945)
    * feat: replace jest visual test driver to jest-image-snapshot
    
    * chore: add .jest-image-snapshot-touched-files to .gitignore
    
    * chore: remove outdated deps update limitation
    
    * chore: refactor/align script commands
    
    * chore: remove screenshot as a part of the description
    
    * chore: remove all old snapshots
    
    * feat: ensure page and ScrollView is not scrolling
    
    * feat: refactor setupPageScreenshot
    
    * chore: rename testPageScreenshot to makeScreenshot
    
    * chore: upload diff images to GitHub Action as an artifact
    
    * chore: convert from puppeteer to playwright
    
    Background:
    
    I tried these versions of puppeteer: 8 to 15 + 19
    
    Version 8, 9 and 10 did work fine. While version 10.2.0 has a change in its method of calculating page offsets. Here is it [better explained](https://github.com/puppeteer/puppeteer/issues/7514#issuecomment-1005917527).
    
    This change leads to a negative side effect so snapshots differ from when run on CI macOS vs local macOS. A typical comparison looks then as so:
    
    ![avatar-src-have-to-match-png-image-of-local-src snap-diff](https://user-images.githubusercontent.com/1501870/213706793-3b41d185-0782-4088-ba72-7d5eb7b54b94.png)
    
    I also see that "jest-image-snapshot" has not so many "parameters" to tweak. We basically have `failureThreshold`. Before we had [several more](https://github.com/dnbexperience/eufemia/blob/88279d1e6903497fce40d6c4322dd9b7c7b9ac3b/packages/dnb-eufemia/src/core/jest/jestSetupScreenshots.js#L39).
    
    When running our local generated snapshots on the CI, we typical get this:
    
    ![button-icon-have-to-match-icon-button snap-diff](https://user-images.githubusercontent.com/1501870/214040509-b0345bed-0d88-4653-93df-e7f650524bd8.png)
    
    * chore: align visual test and update snapshots
    
    Align Dialog
    
    Align Paragraph
    
    Align Anchor (new)
    
    Align Hr
    
    Align Lists
    
    Align Typography
    
    Align Heading
    
    Align Autocomplete
    
    Align Avatar
    
    Align Badge
    
    Align Breadcrumb
    
    Align Button
    
    Align Checkbox
    
    Align Drawer
    
    Align Dropdown
    
    Align FormLabel
    
    Align FormRow and FormSet
    
    Align FormStatus
    
    Align GlobalError
    
    Align GlobalStatus
    
    Align HelpButton
    
    Align Icon
    
    Algin InfoCard
    
    Align Input
    
    Align Modal
    
    Align NumberFormat
    
    Align Pagination
    
    Align ProgressIndicator
    
    Align Radio button
    
    Align Seciton
    
    Align Skeleton
    
    Align Slider
    
    Align Space
    
    Align StepIndicator
    
    Align switch
    
    Align Tabs
    
    Align Tag
    
    Align Textarea
    
    Align Timeline
    
    Align ToggleButton
    
    Align Tooltip
    
    Align Upload
    
    Align VisuallyHidden
    
    Align Image
    
    Align Blockquote
    
    Align DrawerList
    
    Align DatePicker
    
    Align Table
    
    Align PaymentCard
    
    Align HelperClasses
    
    Align Logo
    
    s

---
+++ packages/dnb-eufemia/src/components/progress-indicator/__tests__/ProgressIndicator.screenshot.test.ts
@@ -0,0 +1,51 @@
+/**
+ * Screenshot Test
+ * This file will not run on "test:staged" because we don't require any related files
+ */
+
+import {
+  makeScreenshot,
+  setupPageScreenshot,
+} from '../../../core/jest/jestSetupScreenshots'
+
+describe('ProgressIndicator', () => {
+  setupPageScreenshot({
+    url: '/uilib/components/progress-indicator/demos',
+  })
+
+  it('have to match the static primary circular with 50 percentage', async () => {
+    const screenshot = await makeScreenshot({
+      selector:
+        '[data-visual-test="progress-indicator-circular--primary"] .dnb-progress-indicator',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('have to match the static primary linear with 50 percentage', async () => {
+    const screenshot = await makeScreenshot({
+      style: {
+        width: '20rem',
+      },
+      selector:
+        '[data-visual-test="progress-indicator-linear--primary"] .dnb-progress-indicator',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+})
+
+describe('ProgressIndicator circular', () => {
+  setupPageScreenshot({
+    url: '/uilib/components/progress-indicator/visual-tests',
+  })
+
+  it('have to match static primary circular sizes', async () => {
+    const screenshot = await makeScreenshot({
+      style: {
+        height: '3.5rem',
+        width: '8rem',
+      },
+      selector: '[data-visual-test="progress-indicator-sizes"]',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+})
