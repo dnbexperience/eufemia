39ea96dfa1867de84d527e05f2c4ac1604a4abaa
-l <anderslangseth@gmail.com>
2023-02-21 09:29:48 +0100

    feat(Modal): remove deprecated mode prop

--- packages/dnb-eufemia/src/components/modal/__tests__/Modal.screenshot.test.ts
+++ packages/dnb-eufemia/src/components/modal/__tests__/Modal.screenshot.test.ts
@@ -6,39 +6,19 @@
 import { makeScreenshot } from '../../../core/jest/jestSetupScreenshots'
 
 describe('Modal', () => {
-  const url = '/uilib/components/modal/visual-tests/hidden-tests'
+  const url = '/uilib/components/modal/demos'
 
   it('have to match the default modal trigger button', async () => {
     const screenshot = await makeScreenshot({
       url,
-      selector: '[data-visual-test="modal-trigger-default"]',
-    })
-    expect(screenshot).toMatchImageSnapshot()
-  })
-
-  it('have to match the suffix help button usage', async () => {
-    const screenshot = await makeScreenshot({
-      url,
-      selector: '[data-visual-test="modal-help-button"]',
-    })
-    expect(screenshot).toMatchImageSnapshot()
-  })
-
-  it('have to match the default modal window with title', async () => {
-    const screenshot = await makeScreenshot({
-      url,
-      selector: 'div#dnb-modal-root', // only to make sure we have a valid selector
-      simulate: 'click',
-      simulateSelector:
-        '[data-visual-test="modal-trigger-default"] button:first-of-type',
-      screenshotSelector: '.dnb-dialog',
+      selector: '[data-visual-test="modal-standard"]',
     })
     expect(screenshot).toMatchImageSnapshot()
   })
 
   it('have to match the default modal window', async () => {
     const screenshot = await makeScreenshot({
-      url: '/uilib/components/modal/visual-tests/hidden-tests',
+      url: url,
       pageViewport: {
         width: 700,
         height: 600,
@@ -46,24 +26,7 @@ describe('Modal', () => {
       selector: 'div#dnb-modal-root', // only to make sure we have a valid selector
       simulate: 'click',
       simulateSelector:
-        '[data-visual-test="modal-trigger-default"] button:first-of-type',
-      screenshotSelector: '.dnb-modal__content',
-      rootClassName: 'hide-page-content',
-    })
-    expect(screenshot).toMatchImageSnapshot()
-  })
-
-  it('have to match a fullscreen modal', async () => {
-    const screenshot = await makeScreenshot({
-      url: '/uilib/components/modal/visual-tests/hidden-tests',
-      pageViewport: {
-        width: 400,
-        height: 400,
-      },
-      selector: 'div#dnb-modal-root', // only to make sure we have a valid selector
-      simulate: 'click',
-      simulateSelector:
-        '[data-visual-test="modal-fullscreen"] button:first-of-type',
+        '[data-visual-test="modal-standard"] button:first-of-type',
       screenshotSelector: '.dnb-modal__content',
       rootClassName: 'hide-page-content',
     })
