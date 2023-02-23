0a7cfddd9e1fd895cc5376aa85f058650f037631
Joakim Bjerknes <joakbjerk@gmail.com>
2023-02-23 11:06:35 +0100

    docs: refactor contribution guide getting started (#2008)
    * docs: refactor getting started in contribution guide
    
    * docs: make getting started documentation appear as own pages
    
    * chore: make a component that generates table of contents
    
    * chore: changed Toc to js from jsx to fix start issues
    
    * chore: replaced old table with new table component
    
    * chore: replaced tree converter funtion with gatsby tableofcontents response from graphql
    
    * chore: cleaned up documentation and table of contents component
    
    * chore: updated TableOfContents items type
    
    * chore: removed empty code block from documentation
    
    ---------
    
    Co-authored-by: Tobias Høegh <tobias@tujo.no>
    Co-authored-by: Joakim Bjerknes <joakimbjerknes@Joakims-MBP.home>

---
+++ packages/dnb-design-system-portal/src/shared/parts/TableOfContents.tsx
@@ -0,0 +1,67 @@
+import { Anchor, Ul, Li } from '@dnb/eufemia/src/elements'
+
+type TableOfContents = {
+  url: string
+  title: string
+  items?: Array<TableOfContents>
+}
+
+type TableOfContentsProps = {
+  edges: Array<{
+    node: {
+      frontmatter: { order: number }
+      tableOfContents: {
+        items: Array<TableOfContents>
+      }
+    }
+  }>
+}
+
+const TableOfContents = ({ edges }: TableOfContentsProps) => {
+  const orderedContents = edges
+    .sort((edgeA, edgeB) =>
+      edgeA.node.frontmatter.order > edgeB.node.frontmatter.order ? 1 : -1
+    )
+    .map(({ node }) => node.tableOfContents.items)
+    .reduce<Array<TableOfContents>>((allContent, currentContent) => {
+      currentContent.forEach((content) => allContent.push(content))
+      return allContent
+    }, [])
+
+  return (
+    <Ul>
+      {orderedContents.map((content, index) => (
+        <TableOfContentsItem
+          key={`${content.title}-${index}`}
+          {...content}
+        />
+      ))}
+    </Ul>
+  )
+}
+
+type TableOfContentsItemProps = TableOfContents
+
+function TableOfContentsItem({
+  title,
+  url,
+  items,
+}: TableOfContentsItemProps) {
+  return (
+    <Li>
+      <Anchor href={url}>{title}</Anchor>
+      {items?.length > 0 && (
+        <Ul>
+          {items.map((item, index) => (
+            <TableOfContentsItem
+              key={`${item.title}-${index}`}
+              {...item}
+            />
+          ))}
+        </Ul>
+      )}
+    </Li>
+  )
+}
+
+export default TableOfContents
