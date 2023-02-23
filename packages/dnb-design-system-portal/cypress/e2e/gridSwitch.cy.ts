026259468df4fb0ef5279fc91b4f3285e7e1399b
Tobias Høegh <tobias@tujo.no>
2023-02-23 10:17:45 +0100

    chore(Portal): add Cypress tests (#2000)
    * chore(Portal): add Cypress tests
    
    * chore: show job instead of action in Slack push

---
+++ packages/dnb-design-system-portal/cypress/e2e/gridSwitch.cy.ts
@@ -0,0 +1,23 @@
+/**
+ * Cypress e2e Test
+ *
+ */
+
+describe('Grid', () => {
+  beforeEach(() => {
+    cy.visit('/uilib')
+
+    // Check if app is mounted
+    cy.get('#dnb-drawer-list__portal', { timeout: 10000 }).should('exist')
+  })
+
+  afterEach(() => {
+    cy.clearLocalStorage('showGrid')
+  })
+
+  it('click on grid switch should enable the grid', () => {
+    cy.get('#portal-tools').click()
+    cy.get('#switch-grid').click()
+    cy.get('html').should('have.attr', 'show-dev-grid', 'true')
+  })
+})
