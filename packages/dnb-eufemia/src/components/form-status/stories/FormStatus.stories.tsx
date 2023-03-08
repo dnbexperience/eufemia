ae4029078450fd208d063dbc704eabe353622d99
Anders <anderslangseth@gmail.com>
2023-03-08 08:57:47 +0100

    chore(TypeScript): Rename stories.js to stories.tsx (#2057)

---
+++ packages/dnb-eufemia/src/components/form-status/stories/FormStatus.stories.tsx
@@ -0,0 +1,206 @@
+/**
+ * @dnb/eufemia Component Story
+ *
+ */
+
+import React, { useState } from 'react'
+import { Wrapper, Box } from 'storybook-utils/helpers'
+import styled from '@emotion/styled'
+
+import {
+  FormStatus,
+  Input,
+  FormSet,
+  FormRow,
+  DatePicker,
+  Switch,
+  Button,
+  ToggleButton,
+  Space,
+  Dialog,
+  HelpButton,
+} from '../..'
+import { Link } from '../../../elements'
+
+export default {
+  title: 'Eufemia/Components/FormStatus',
+}
+
+const CustomStatus = () => (
+  <>
+    {/* <H2>Custom Status</H2> */}
+    <Link href="/">Goto</Link> more text itae tortor metus nulla nunc
+    habitasse
+  </>
+)
+
+const SmallWidth = styled(Input)`
+  /* .dnb-form-status {
+      max-width: 16rem;
+    } */
+  .dnb-input__input {
+    width: 4rem;
+    text-align: center;
+  }
+`
+// const CustomStatuSandbox = () => (
+//   <>
+//     My info <Link href="/">with a link</Link> and more text
+//   </>
+// )
+
+export const FormStatusSandbox = () => {
+  return (
+    <Wrapper>
+      <Box>
+        <Input
+          label="Input with custom status:"
+          status={<CustomStatus />}
+          status_state="info"
+          value="Input value"
+        />
+      </Box>
+      <Box>
+        <SmallWidth
+          label="Small width input:"
+          value="4"
+          status="Adipiscing etiam laoreet et egestas dis massa quis dapibus nam diam est non curae ad hac dictumst"
+        />
+      </Box>
+      <Box>
+        <SmallWidth
+          label="Warning:"
+          value="4"
+          size={3}
+          status="Adipiscing etiam laoreet et egestas dis massa quis dapibus nam diam est non curae ad hac dictumst"
+          status_state="warn"
+        />
+      </Box>
+      <Box>
+        <FormStatus>Status</FormStatus>
+      </Box>
+      <Box>
+        <FormStatus state="info" size="large">
+          Long info text Ipsum habitant enim ullamcorper elit sit elementum
+          platea rutrum eu condimentum erat risus lacinia viverra magnis
+          lobortis nibh mollis suspendisse
+        </FormStatus>
+        <FormStatus state="info" size="large" stretch icon_size="small">
+          Long info text Ipsum habitant enim ullamcorper elit sit elementum
+          platea rutrum eu condimentum erat risus lacinia viverra magnis
+          lobortis nibh mollis suspendisse
+        </FormStatus>
+        <FormStatus state="marketing" size="large" stretch>
+          Long info text Ipsum habitant enim ullamcorper elit sit elementum
+          platea rutrum eu condimentum erat risus lacinia viverra magnis
+          lobortis nibh mollis suspendisses
+        </FormStatus>
+      </Box>
+      <Box>
+        <FormStatus>
+          <CustomStatus />
+        </FormStatus>
+      </Box>
+      <Box>
+        <Input
+          label="Input label:"
+          // style={{ width: '200px' }}
+          status={<CustomStatus />}
+        >
+          Value
+        </Input>
+      </Box>
+      <Box>
+        <Switch
+          label="Switch label"
+          status="Long text with status vitae tortor metus nulla nunc habitasse adipiscing purus porttitor viverra"
+        />
+      </Box>
+      <Box>
+        <FormSet
+          label_direction="vertical"
+          prevent_submit
+          on_submit={(event) => {
+            console.log('onSubmit', event)
+          }}
+        >
+          <FormRow
+            top="small"
+            label={
+              <Space element="span" className="dnb-h--large">
+                Legend:
+              </Space>
+            }
+          >
+            <DatePicker
+              show_input
+              right="small"
+              bottom="small"
+              status="Long text with status vitae tortor metus nulla nunc habitasse adipiscing purus porttitor viverra"
+            />
+            <Dialog right="small">Modal Content</Dialog>
+            <Button text="Submit" type="submit" />
+          </FormRow>
+        </FormSet>
+      </Box>
+    </Wrapper>
+  )
+}
+
+export const ToggleAnimation = () => {
+  const [status, setStatus] = React.useState(null)
+  const toggleStatus = () => {
+    setStatus((s) => (!s ? 'You have to fill in this field' : null))
+  }
+  const [showError, setShowError] = useState(false)
+
+  return (
+    <Wrapper>
+      <Box>
+        <FormSet
+          label_direction="vertical"
+          prevent_submit
+          on_submit={(event) => {
+            setShowError((v) => !v)
+            console.log('onSubmit', event)
+          }}
+        >
+          <FormRow
+            top="small"
+            label={
+              <Space element="span" className="dnb-h--large">
+                Legend:
+              </Space>
+            }
+          >
+            <DatePicker
+              show_input
+              right="small"
+              status={
+                showError &&
+                'Long text with status vitae tortor metus nulla nunc habitasse adipiscing purus porttitor viverra'
+              }
+              suffix={<HelpButton>Modal Content</HelpButton>}
+            />
+            <Button text="Submit" type="submit" top="small" />
+          </FormRow>
+        </FormSet>
+      </Box>
+      <Box>
+        <FormRow vertical={false}>
+          <Input
+            label="Input with status:"
+            status={status}
+            value="Input value"
+            suffix={<HelpButton>test</HelpButton>}
+            right
+            // size="small"
+          />
+          <ToggleButton top on_change={toggleStatus}>
+            Toggle
+          </ToggleButton>
+        </FormRow>
+      </Box>
+    </Wrapper>
+  )
+}
---
+++ packages/dnb-eufemia/src/components/pagination/stories/Pagination.stories.tsx
@@ -0,0 +1,345 @@
+/**
+ * @dnb/eufemia Component Story
+ *
+ */
+
+import React from 'react'
+import { Wrapper, Box } from 'storybook-utils/helpers'
+import styled from '@emotion/styled'
+
+import { P } from '../../../elements'
+import { Button, Section } from '../..'
+import Pagination, { createPagination } from '../Pagination'
+
+export default {
+  title: 'Eufemia/Components/Pagination',
+}
+
+const LargePage = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 15vw;
+
+  background-color: ${(props) => props.color || 'hotpink'};
+  font-size: 15vw;
+  font-weight: var(--font-weight-bold);
+  font-feature-settings: 'pnum' on, 'lnum' on;
+
+  color: var(--color-white);
+`
+const CustomIndicator = styled(LargePage)`
+  color: purple;
+`
+
+const tableItems = []
+for (let i = 1; i <= 300; i++) {
+  tableItems.push({ ssn: i, text: String(i), expanded: false })
+}
+
+export const InfinitySandbox = () => {
+  const props = {
+    current_page: 3,
+    // page_count: 30,
+  }
+
+  const action = ({ pageNumber, setContent }) => {
+    console.log('pageNumber', pageNumber)
+    setTimeout(() => {
+      setContent(
+        pageNumber,
+        <LargePage color="lightgreen">{pageNumber}</LargePage>
+      )
+    }, 100)
+  }
+
+  return (
+    <Pagination
+      mode="infinity"
+      {...props}
+      on_startup={action}
+      on_change={action}
+    />
+  )
+}
+
+export const PaginationNoChildren = () => (
+  <Wrapper>
+    <Pagination
+      page_count={10}
+      current_page={2}
+      on_change={(params) => {
+        console.log(params)
+      }}
+    />
+    <Box>
+      <Pagination
+        page_count={10}
+        current_page={2}
+        on_change={(params) => {
+          console.log(params)
+        }}
+        align="right"
+      />
+    </Box>
+    <Box>
+      <Pagination
+        page_count={10}
+        current_page={2}
+        on_change={(params) => {
+          console.log(params)
+        }}
+        align="left"
+      />
+    </Box>
+    <Box>
+      <Pagination
+        page_count={10}
+        current_page={2}
+        on_change={(params) => {
+          console.log(params)
+        }}
+        align="center"
+      />
+    </Box>
+    <Pagination
+      page_count={3}
+      current_page={2}
+      on_change={(params) => {
+        console.log(params)
+      }}
+    />
+  </Wrapper>
+)
+
+export const PaginationSandbox = () => (
+  <Wrapper>
+    <Box>
+      <PaginationRender />
+    </Box>
+    <Box>
+      <Pagination
+        page_count={10}
+        current_page={2}
+        on_change={(params) => {
+          console.log(params)
+        }}
+      />
+    </Box>
+    <Box>
+      <Pagination page_count={2}>
+        {({ pageNumber, setContent }) => {
+          // simulate server communication delay
+          const timeout = setTimeout(() => {
+            setContent(pageNumber, <LargePage>{pageNumber}</LargePage>)
+          }, Math.ceil(Math.random() * 500))
+
+          return () => clearTimeout(timeout)
+        }}
+      </Pagination>
+    </Box>
+
+    <Box>
+      <Pagination
+        page_count={30}
+        startup_page={15}
+        on_change={(pageNumber) => {
+          console.log('on_change:', pageNumber)
+        }}
+      >
+        {({ pageNumber }) => <P>Page {pageNumber}</P>}
+      </Pagination>
+    </Box>
+
+    <Box>
+      <PaginationWithState
+        align="center"
+        on_change={(pageNumber) => {
+          console.log('on_change:', pageNumber)
+        }}
+      >
+        {(pageNumber) => (
+          <LargePage color="HotPink">{pageNumber}</LargePage>
+        )}
+      </PaginationWithState>
+    </Box>
+
+    <Box>
+      <HeightLimit>
+        <InfinityPagination use_load_button startup_page={5}>
+          {(pageNumber, ref) => (
+            <LargePage ref={ref} color="LightCoral">
+              {pageNumber}
+            </LargePage>
+          )}
+        </InfinityPagination>
+      </HeightLimit>
+    </Box>
+
+    <Box>
+      <HeightLimit>
+        <InfinityPagination
+          indicator_element={() => (
+            <CustomIndicator>Loading ...</CustomIndicator>
+          )}
+          startup_page={3}
+          page_count={10}
+          min_wait_time={0}
+        >
+          {(pageNumber, ref) => (
+            <LargePage ref={ref} color="Indigo">
+              {pageNumber}
+            </LargePage>
+          )}
+        </InfinityPagination>
+      </HeightLimit>
+    </Box>
+
+    <Box>
+      <HeightLimit>
+        <Pagination
+          mode="infinity"
+          startup_count={2}
+          // parallel_load_count={1}
+          // page_count={10} // the last one we fill with "End"
+          min_wait_time={0}
+          on_load={({ pageNumber, setContent, endInfinity }) => {
+            console.log('on_load: ', pageNumber)
+            if (pageNumber > 10) {
+              endInfinity()
+            } else {
+              setContent(pageNumber, <LargePage>{pageNumber}</LargePage>)
+            }
+          }}
+          on_end={({ pageNumber, setContent }) => {
+            console.log('on_end: ', pageNumber)
+            setContent(
+              pageNumber,
+              <LargePage color="lightgreen">End</LargePage>
+            )
+          }}
+        />
+      </HeightLimit>
+    </Box>
+  </Wrapper>
+)
+
+const HeightLimit = styled.div`
+  height: 20rem;
+  overflow-y: scroll;
+  background-color: var(--color-white);
+  border: 4px solid blue;
+`
+
+// eslint-disable-next-line
+const PaginationWithState = ({ children, ...props }) => {
+  const [currentPage, setCurrentPage] = React.useState(1)
+
+  // create our Pagination instance
+  const [{ Pagination: PaginationInstance, setContent, resetContent }] =
+    React.useState(createPagination)
+  setContent(currentPage, children(currentPage))
+
+  // will reset the pagination
+  if (currentPage == 30) {
+    setTimeout(() => {
+      resetContent()
+      setCurrentPage(1)
+    }, 1)
+  }
+
+  return (
+    <PaginationInstance
+      {...props}
+      page_count={30}
+      current_page={currentPage}
+      on_change={({ pageNumber }) => {
+        console.log('PaginationWithState on_change:', pageNumber)
+        setCurrentPage(pageNumber)
+
+        // setTimeout(() => {
+        //   setContent(pageNumber, children(pageNumber))
+        // }, Math.ceil(Math.random() * 1e3))
+      }}
+    >
+      {/* {({ pageNumber, setContent }) => {
+        setTimeout(() => {
+          setContent(pageNumber, children(pageNumber))
+        }, Math.ceil(Math.random() * 1e3))
+      }} */}
+    </PaginationInstance>
+  )
+}
+
+// eslint-disable-next-line
+const InfinityPagination = ({ children, ...props }) => {
+  return (
+    <Pagination
+      mode="infinity"
+      {...props}
+      on_load={({ pageNumber, setContent }) => {
+        console.log('InfinityPagination on_load:', pageNumber)
+
+        setTimeout(() => {
+          setContent(pageNumber, children(pageNumber))
+        }, Math.ceil(Math.random() * 1e3))
+      }}
+      on_end={({ pageNumber, setContent }) => {
+        console.log('InfinityPagination on_end:', pageNumber)
+        setContent(pageNumber, <LargePage>End</LargePage>)
+      }}
+    >
+      {/* just a child */}
+    </Pagination>
+  )
+}
+
+function PaginationRender() {
+  const pageCount = 8
+  const startupPage = 2
+  const [currentPage, setCurrentPage] = React.useState(startupPage)
+  const [delayedCount, setDelayedCount] = React.useState(currentPage)
+
+  React.useEffect(() => {
+    setDelayedCount(currentPage)
+  }, [currentPage])
+
+  return (
+    <Section>
+      <Button
+        on_click={() => setCurrentPage(1)}
+        text="Reset"
+        bottom
+        size="small"
+        right
+      />
+      <Button
+        on_click={() => setCurrentPage((prevCount) => prevCount - 1)}
+        text="Decrease"
+        size="small"
+        variant="secondary"
+        right
+      />
+      <Button
+        on_click={() => setCurrentPage((prevCount) => prevCount + 1)}
+        text="Increase"
+        size="small"
+        variant="secondary"
+        right
+      />
+      <Pagination
+        page_count={pageCount}
+        startup_page={startupPage}
+        current_page={delayedCount}
+        on_change={({ pageNumber }) => {
+          setCurrentPage(pageNumber)
+        }}
+      >
+        <div className="pagination-content">
+          <code>{JSON.stringify({ currentPage, delayedCount })}</code>
+        </div>
+      </Pagination>
+    </Section>
+  )
+}
