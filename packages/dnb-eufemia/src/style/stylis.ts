110466132209cc2a3b68c4169669474d3ec7b269
Tobias Høegh <tobias@tujo.no>
2023-03-09 12:22:14 +0100

    fix(stylisPlugin): rewrite to TypeScript

--- packages/dnb-eufemia/src/style/stylis.ts
+++ packages/dnb-eufemia/src/style/stylis.ts
@@ -7,6 +7,32 @@
 import { matchAll } from '../shared/component-helper'
 import properties from './themes/theme-ui/properties'
 
+export interface StylisElement {
+  type: string
+  value: string
+  props: Array<string> | string
+  root: StylisElement | null
+  parent: StylisElement | null
+  children: Array<StylisElement> | string
+  line: number
+  column: number
+  length: number
+  return: string
+}
+export type StylisPluginCallback = (
+  element: StylisElement,
+  index: number,
+  children: Array<StylisElement>,
+  callback: StylisPluginCallback
+) => string | void
+
+export type StylisPlugin = (
+  element: StylisElement,
+  index: number,
+  children: Array<StylisElement>,
+  callback: StylisPluginCallback
+) => string | void
+
 const findRegExp = /var\(([^)]*)\)/g
 
 const stylisPlugin = (context, content, props = null) => {
@@ -37,7 +63,7 @@ const stylisPlugin = (context, content, props = null) => {
   )
 }
 
-export default stylisPlugin
+export default stylisPlugin as StylisPlugin
 
 export const withProperties = (properties) => (context, content) =>
-  stylisPlugin(context, content, properties)
+  stylisPlugin(context, content, properties) as StylisPlugin
