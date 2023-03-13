713f0af5626f283bbd02ca5d0f73d56ca3e55bac
Anders <anderslangseth@gmail.com>
2023-03-13 08:57:54 +0100

    chore(TypeScript): use HeadingProps in HeadingProvider (#2087)

--- packages/dnb-eufemia/src/components/heading/HeadingProvider.d.ts
+++ packages/dnb-eufemia/src/components/heading/HeadingProvider.d.ts
@@ -1,46 +1,9 @@
 import * as React from 'react';
-export type HeadingProviderLevel = number | string;
-export type HeadingProviderReset = number | boolean;
+import { HeadingProps } from './Heading';
 
 export interface HeadingProviderProps
-  extends React.HTMLProps<HTMLElement> {
-  id?: string;
-  group?: string;
-  level?: HeadingProviderLevel;
-
-  /**
-   * If set to true, the heading level will be incremented by 1.
-   */
-  increase?: boolean;
-
-  /**
-   * If set to true, the heading level will be decremented by 1.
-   */
-  decrease?: boolean;
-  up?: boolean;
-  down?: boolean;
-
-  /**
-   * If set to true, the heading last used level will be inherited. Also from inside a level context.
-   */
-  inherit?: boolean;
-  counter?: any;
-
-  /**
-   * If set to true, the heading level will be reset to 2. You can give it a custom level if you need to, e.g. `reset(1)`.
-   */
-  reset?: HeadingProviderReset;
-
-  /**
-   * <em>(required)</em> a heading, can be text or React.Node.
-   */
-  text?: any;
-
-  /**
-   * <em>(required)</em> a heading, can be text or React.Node.
-   */
-  children?: any;
-}
+  extends React.HTMLProps<HTMLElement>,
+    HeadingProps {}
 export default class HeadingProvider extends React.Component<
   HeadingProviderProps,
   any
