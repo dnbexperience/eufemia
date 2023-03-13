8f17af6c9141b0723fbf819d8f5524f3430e1433
Anders <anderslangseth@gmail.com>
2023-03-13 09:43:23 +0100

    chore(TypeScript): use DatePickerProps in DatePickerProvider (#2089)

--- packages/dnb-eufemia/src/components/date-picker/DatePickerProvider.d.ts
+++ packages/dnb-eufemia/src/components/date-picker/DatePickerProvider.d.ts
@@ -1,28 +1,9 @@
 import * as React from 'react';
-export type DatePickerProviderMinDate = Date | string;
-export type DatePickerProviderMaxDate = Date | string;
+import type { DatePickerProps } from './DatePicker';
 
-export interface DatePickerProviderProps
-  extends React.HTMLProps<HTMLElement> {
-  /**
-   * To limit a date range to a minimum `start_date`. Defaults to null.
-   */
-  min_date?: DatePickerProviderMinDate;
-
-  /**
-   * To limit a date range to a maximum `end_date`. Defaults to null.
-   */
-  max_date?: DatePickerProviderMaxDate;
-
-  /**
-   * Defines how the returned date, as a string, should be formatted as. Defaults to `yyyy-MM-dd`.
-   */
-  return_format?: string;
-
-  /**
-   * If the date picker should support a range of two dates (starting and ending date). Defaults to `false`.
-   */
-  range: boolean;
+interface DatePickerProviderProps
+  extends React.HTMLProps<HTMLElement>,
+    DatePickerProps {
   setReturnObject: (...args: any[]) => any;
   enhanceWithMethods?: Record<string, unknown>;
   attributes?: Record<string, unknown>;
