c6b91a47c71d92dfc1c3ce3dce0b95ad27319483
Anders <anderslangseth@gmail.com>
2023-03-14 12:53:19 +0100

    feat(Skeleton): removes style_type prop (#2095)

--- packages/dnb-eufemia/src/elements/stories/OtherElements.stories.tsx
+++ packages/dnb-eufemia/src/elements/stories/OtherElements.stories.tsx
@@ -273,11 +273,6 @@ export const ElementsAndSkeleton = () => {
         // no_animation
         // show
         // figure={() => <SkeletonArticle rows={2} />}
-        // style_type="shine"
-        // style_type="dots"
-        // style_type="norway"
-        // style_type="rainbow"
-        // style_type="brand"
       >
         <Hr top="large" bottom />
         <Span>span span</Span>
