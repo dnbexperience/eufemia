fb64d0aeebe65b252d283ad679f4922b452e0325
Tobias Høegh <tobias@tujo.no>
2023-03-17 18:43:50 +0100

    chore: add primary and secondary button styles (#2116)
    * style(Button): add primary and secondary styles
    
    * Add visual tests
    
    * Fix wrong font-size mapping
    
    * chore: remove Safari artefact fallback – as it seems to work fine with the latest versions
    
    * Update snapshots

--- packages/dnb-eufemia/src/components/button/__tests__/themes/Button.sbanken.screenshot.test.ts
+++ packages/dnb-eufemia/src/components/button/__tests__/themes/Button.sbanken.screenshot.test.ts
@@ -19,4 +19,59 @@ describe('Button with sbanken theme', () => {
     })
     expect(screenshot).toMatchImageSnapshot()
   })
+
+  it('have to match "dnb-button--primary" with hover state', async () => {
+    const screenshot = await makeScreenshot({
+      selector: '[data-visual-test="button-primary"]',
+      simulate: 'hover',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('have to match "dnb-button--primary" with active state', async () => {
+    const screenshot = await makeScreenshot({
+      selector: '[data-visual-test="button-primary"]',
+      simulate: 'active',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('have to match "dnb-button--primary" with focus state', async () => {
+    const screenshot = await makeScreenshot({
+      selector: '[data-visual-test="button-primary"]',
+      simulate: 'focus',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('have to match "dnb-button--secondary"', async () => {
+    const screenshot = await makeScreenshot({
+      selector: '[data-visual-test="button-secondary"]',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('have to match "dnb-button--secondary" with hover state', async () => {
+    const screenshot = await makeScreenshot({
+      selector: '[data-visual-test="button-secondary"]',
+      simulate: 'hover',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('have to match "dnb-button--secondary" with active state', async () => {
+    const screenshot = await makeScreenshot({
+      selector: '[data-visual-test="button-secondary"]',
+      simulate: 'active',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('have to match "dnb-button--secondary" with focus state', async () => {
+    const screenshot = await makeScreenshot({
+      selector: '[data-visual-test="button-secondary"]',
+      simulate: 'focus',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
 })
