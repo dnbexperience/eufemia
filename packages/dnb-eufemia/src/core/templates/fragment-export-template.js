a8cc39c04ea6900edaf567eb0cb967a92c59e71c
Tobias Høegh <tobias@tujo.no>
2023-03-21 19:52:32 +0100

    chore: make it possible to have elements in subfolders (#2130)
    * Collection element styles in prebuild factory
    
    * Collect and build export files
    
    * Collection element themes in theme factory

---
+++ packages/dnb-eufemia/src/core/templates/fragment-export-template.js
@@ -0,0 +1,8 @@
+/**
+ * Library Index template to autogenerate all the fragments and extensions
+ * Used by "prepareTemplates"
+ */
+
+import Template from './template/Template'
+export * from './template/Template'
+export default Template
