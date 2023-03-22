c609a762117571dadb591808b6e70e9a88d8d691
Joakim Bjerknes <joakbjerk@gmail.com>
2023-03-22 15:48:56 +0100

    convert Blockquote to tsx

--- packages/dnb-eufemia/src/elements/blockquote/Blockquote.tsx
+++ packages/dnb-eufemia/src/elements/blockquote/Blockquote.tsx
@@ -4,8 +4,6 @@
  */
 
 import React, { ReactNode } from 'react'
-//import PropTypes from 'prop-types'
-//import { spacingPropTypes } from '../../components/space/SpacingHelper'
 import { SpacingProps } from '../../components/space/types'
 import E from '../Element'
 
@@ -16,12 +14,5 @@ type BlockquoteProps = SpacingProps & {
 const Blockquote = React.forwardRef((props: BlockquoteProps, ref) => (
   <E as="blockquote" skeletonMethod="font" innerRef={ref} {...props} />
 ))
-/* Blockquote.propTypes = {
-  ...spacingPropTypes,
-  children: PropTypes.node,
-}
-Blockquote.defaultProps = {
-  children: null,
-} */
 
 export default Blockquote
