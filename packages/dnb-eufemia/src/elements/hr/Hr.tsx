8daf892679be240449104c7eef6aa78774e4a39d
Joakim Bjerknes <joakbjerk@gmail.com>
2023-03-28 11:17:40 +0200

    refactor hr to not overwrite className prop to set correct styling

--- packages/dnb-eufemia/src/elements/hr/Hr.tsx
+++ packages/dnb-eufemia/src/elements/hr/Hr.tsx
@@ -15,18 +15,25 @@ type HrProps = SpacingProps &
     fullscreen?: boolean
   }
 
-const Hr = ({ fullscreen, light, medium, ...props }: HrProps = {}) => {
-  if (fullscreen) {
-    props.className = classnames(props.className, 'dnb-hr--fullscreen')
-  }
-  if (light) {
-    props.className = classnames(props.className, 'dnb-hr--light')
-  }
-  if (medium) {
-    props.className = classnames(props.className, 'dnb-hr--medium')
-  }
-
-  return <E as="hr" {...props} />
+const Hr = ({
+  fullscreen,
+  light,
+  medium,
+  className,
+  ...props
+}: HrProps = {}) => {
+  return (
+    <E
+      as="hr"
+      className={classnames(
+        className,
+        light && 'dnb-hr--light',
+        medium && 'dnb-hr--medium',
+        fullscreen && 'dnb-hr--fullscreen'
+      )}
+      {...props}
+    />
+  )
 }
 
 export default Hr
