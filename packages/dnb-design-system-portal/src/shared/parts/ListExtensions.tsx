f5b64ff8815644725fd026eafabc1bfa483351cd
Tobias Høegh <tobias@tujo.no>
2023-03-29 12:36:56 +0200

    chore(Portal): fix listing of all components (#2166)
    * Make utils dir inside docs stand out more
    
    * Add GraphQl query per use case 👉 ListSummaryFromPages => ListSummaryFromEdges
    
    * Add e2e tests
    
    * Update packages/dnb-design-system-portal/src/shared/parts/ListFragments.tsx
    
    Co-authored-by: Anders <anderslangseth@gmail.com>
    
    * Update packages/dnb-design-system-portal/src/shared/parts/ListElements.tsx
    
    Co-authored-by: Anders <anderslangseth@gmail.com>
    
    ---------
    
    Co-authored-by: Anders <anderslangseth@gmail.com>

---
+++ packages/dnb-design-system-portal/src/shared/parts/ListExtensions.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import ListSummaryFromEdges from './ListSummaryFromEdges'
+
+export default function ListExtensions() {
+  const {
+    allMdx: { edges },
+  } = useStaticQuery(graphql`
+    {
+      allMdx(
+        filter: {
+          frontmatter: { title: { ne: "" }, draft: { ne: true } }
+          internal: { contentFilePath: { glob: "**/uilib/extensions/*" } }
+        }
+      ) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+              description
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  return <ListSummaryFromEdges edges={edges} />
+}
