27f3cbf7b1c01247b7813b17a268b7e4eeff85f6
Joakim Bjerknes <joakbjerk@gmail.com>
2023-03-31 14:12:57 +0200

    convert H element to tsx

---
+++ packages/dnb-eufemia/src/elements/typography/H.tsx
@@ -0,0 +1,51 @@
+/**
+ * HTML Element
+ *
+ */
+
+import React, { useEffect } from 'react'
+import classnames from 'classnames'
+import { SpacingProps } from '../../components/space/types'
+import E from '../Element'
+import { setNextLevel } from '../../components/heading/HeadingHelpers'
+
+export type HSize =
+  | 'xx-large'
+  | 'x-large'
+  | 'large'
+  | 'medium'
+  | 'basis'
+  | 'small'
+  | 'x-small'
+
+type HProps = SpacingProps &
+  React.HTMLAttributes<HTMLHeadingElement> & {
+    as?: string
+    level?: string
+    size?: HSize
+  }
+
+const H = ({
+  as = 'h1',
+  is,
+  level,
+  size = 'xx-large',
+  className,
+  ...props
+}: HProps) => {
+  useEffect(() => {
+    if (level === 'use') {
+      setNextLevel(parseFloat(String(as || is).substring(1)))
+    }
+  }, [])
+
+  return (
+    <E
+      as={as || is}
+      internalClass={classnames(size && `dnb-h--${size}`, className)}
+      {...props}
+    />
+  )
+}
+
+export default H
