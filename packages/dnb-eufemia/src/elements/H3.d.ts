777867974320368c5cdf856e52d47036aea6f95a
Joakim Bjerknes <joakbjerk@gmail.com>
2023-04-12 12:28:42 +0200

    chore(Typography): move to own subfolder (#2186)
    * convert link to ts
    
    * move typography elements to subfolder
    
    * add typography to prepareTemplates ignore list
    
    * convert H element to tsx
    
    * refactor headings to tsx
    
    * convert paragraph elements to tsx
    
    * move typography styling to subfolder
    
    * delete ui-elements file
    
    * import react in typography element files so tests wont fail
    
    * move typography tests to subfolder
    
    * update typography snapshots
    
    * update typography tests
    
    * update P element tests
    
    * move stories to own subfolder
    
    * refactor P to not overwrite modifier prop
    
    * update P snapshot
    
    * move modifier filter to reduce function

--- packages/dnb-eufemia/src/elements/H3.d.ts
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import type { SharedHProps } from './H';
-
-export type H3Props = SharedHProps;
-declare const H3: React.FC<H3Props>;
-export default H3;
--- packages/dnb-eufemia/src/elements/H6.d.ts
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import type { SharedHProps } from './H';
-
-export type H6Props = SharedHProps;
-declare const H6: React.FC<H6Props>;
-export default H6;
