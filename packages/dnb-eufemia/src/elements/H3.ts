b07a0108ef286a74b62a08463bfa83dd26abfc2a
Joakim Bjerknes <joakbjerk@gmail.com>
2023-04-12 12:28:42 +0200

    chore(Typography): move to own subfolder (#2186)
    * convert link to ts
    
    * move typography elements to subfolder
    
    * add typography to prepareTemplates ignore list
    
    * convert H element to tsx
    
    * refactor headings to tsx
    
    * convert paragraph elements to tsx
    
    * move typography styling to subfolder
    
    * delete ui-elements file
    
    * import react in typography element files so tests wont fail
    
    * move typography tests to subfolder
    
    * update typography snapshots
    
    * update typography tests
    
    * update P element tests
    
    * move stories to own subfolder
    
    * refactor P to not overwrite modifier prop
    
    * update P snapshot
    
    * move modifier filter to reduce function

---
+++ packages/dnb-eufemia/src/elements/H3.ts
@@ -0,0 +1,9 @@
+/**
+ * This file is created manually
+ * You can change the content
+ *
+ */
+
+import H3 from './typography/H3'
+export * from './typography/H3'
+export default H3
---
+++ packages/dnb-eufemia/src/elements/typography/Paragraph.tsx
@@ -0,0 +1,9 @@
+/**
+ * HTML Element
+ *
+ */
+import React from 'react'
+import P, { PProps } from './P'
+
+const Paragraph = (props: PProps) => <P {...props} />
+export default Paragraph
