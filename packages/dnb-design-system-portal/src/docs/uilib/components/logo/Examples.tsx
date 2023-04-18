d696611854d73fd5c51037ca56d2d32b4a76f22d
Tobias Høegh <tobias@tujo.no>
2023-04-18 14:04:36 +0200

    Add visual tests

--- packages/dnb-design-system-portal/src/docs/uilib/components/logo/Examples.tsx
+++ packages/dnb-design-system-portal/src/docs/uilib/components/logo/Examples.tsx
@@ -9,13 +9,13 @@ import { Logo } from '@dnb/eufemia/src'
 
 export const LogoDefaultExample = () => (
   <ComponentBox data-visual-test="logo-default">
-    <Logo height="100" />
+    <Logo height="96" />
   </ComponentBox>
 )
 
 export const LogoAutoSizeExample = () => (
   <ComponentBox data-visual-test="logo-auto-size">
-    <span style={{ fontSize: '12rem' }}>
+    <span style={{ fontSize: '6rem' }}>
       <Logo size="auto" />
     </span>
   </ComponentBox>
@@ -23,7 +23,7 @@ export const LogoAutoSizeExample = () => (
 
 export const LogoInheritSizeExample = () => (
   <ComponentBox data-visual-test="logo-inherit-size">
-    <span style={{ height: '12rem' }}>
+    <span style={{ height: '6rem' }}>
       <Logo size="inherit" />
     </span>
   </ComponentBox>
@@ -32,7 +32,7 @@ export const LogoInheritSizeExample = () => (
 export const LogoInheritColorExample = () => (
   <ComponentBox data-visual-test="logo-inherit-color">
     <span style={{ color: 'tomato' }}>
-      <Logo height="192" inherit_color />
+      <Logo height="96" inherit_color />
     </span>
   </ComponentBox>
 )
