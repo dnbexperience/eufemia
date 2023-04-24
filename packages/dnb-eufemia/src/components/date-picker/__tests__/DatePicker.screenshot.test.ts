cf2f25bae776d5c5d310abb9f7516ed2fb73f1a6
Joakim Bjerknes <joakbjerk@gmail.com>
2023-04-24 11:22:24 +0200

    chore: remove !isCi check from screenshot tests (#2248)
    * remove !isCi check from screenshot tests

--- packages/dnb-eufemia/src/components/date-picker/__tests__/DatePicker.screenshot.test.ts
+++ packages/dnb-eufemia/src/components/date-picker/__tests__/DatePicker.screenshot.test.ts
@@ -4,7 +4,6 @@
  */
 
 import {
-  isCI,
   makeScreenshot,
   setupPageScreenshot,
 } from '../../../core/jest/jestSetupScreenshots'
@@ -12,35 +11,31 @@ import {
 describe('DatePicker', () => {
   setupPageScreenshot({ url: '/uilib/components/date-picker/demos' })
 
-  if (!isCI) {
-    // skip the input fields, as there is a linux input issue
-
-    it('have to match the input fields', async () => {
-      const screenshot = await makeScreenshot({
-        selector:
-          '[data-visual-test="date-picker-input"] .dnb-date-picker__inner',
-      })
-      expect(screenshot).toMatchImageSnapshot()
+  it('have to match the input fields', async () => {
+    const screenshot = await makeScreenshot({
+      selector:
+        '[data-visual-test="date-picker-input"] .dnb-date-picker__inner',
     })
+    expect(screenshot).toMatchImageSnapshot()
+  })
 
-    it('have to match the date-picker with input in error state', async () => {
-      const screenshot = await makeScreenshot({
-        style: {
-          width: '400px', // make sure our input gets an explicit width, because of mac/linux rendering differences
-        },
-        selector:
-          '[data-visual-test="date-picker-input-error"] .dnb-date-picker__inner',
-      })
-      expect(screenshot).toMatchImageSnapshot()
+  it('have to match the date-picker with input in error state', async () => {
+    const screenshot = await makeScreenshot({
+      style: {
+        width: '400px', // make sure our input gets an explicit width, because of mac/linux rendering differences
+      },
+      selector:
+        '[data-visual-test="date-picker-input-error"] .dnb-date-picker__inner',
     })
+    expect(screenshot).toMatchImageSnapshot()
+  })
 
-    it('have to match the sizes', async () => {
-      const screenshot = await makeScreenshot({
-        selector: '[data-visual-test="date-picker-sizes"]',
-      })
-      expect(screenshot).toMatchImageSnapshot()
+  it('have to match the sizes', async () => {
+    const screenshot = await makeScreenshot({
+      selector: '[data-visual-test="date-picker-sizes"]',
     })
-  }
+    expect(screenshot).toMatchImageSnapshot()
+  })
 
   it('have to match the calendar', async () => {
     const screenshot = await makeScreenshot({
