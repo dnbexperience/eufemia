d16ee0f9f879c6f781c3c580804a41ba88ab6779
Joakim Bjerknes <joakbjerk@gmail.com>
2023-04-25 14:39:37 +0200

    add props for blockquote noBackground and graphicsOnTop

--- packages/dnb-eufemia/src/elements/blockquote/Blockquote.tsx
+++ packages/dnb-eufemia/src/elements/blockquote/Blockquote.tsx
@@ -4,13 +4,45 @@
  */
 
 import React from 'react'
+import classnames from 'classnames'
 import { SpacingProps } from '../../components/space/types'
 import E from '../Element'
 
-type BlockquoteProps = SpacingProps & React.HTMLAttributes<HTMLElement>
+type BlockquoteProps = SpacingProps &
+  React.HTMLAttributes<HTMLElement> & {
+    /**
+     * Hides the blockquote background by making it transperant
+     */
+    noBackground?: boolean
+    /**
+     * Forces blockquote align the graphics on top of the element,
+     * making the text content come below the graphic instead of next to it
+     */
+    showGraphicsOnTop?: boolean
+  }
 
-const Blockquote = React.forwardRef((props: BlockquoteProps, ref) => (
-  <E as="blockquote" skeletonMethod="font" innerRef={ref} {...props} />
-))
+const Blockquote = React.forwardRef(
+  (
+    {
+      noBackground,
+      showGraphicsOnTop,
+      className,
+      ...props
+    }: BlockquoteProps,
+    ref
+  ) => (
+    <E
+      as="blockquote"
+      skeletonMethod="font"
+      innerRef={ref}
+      className={classnames(
+        className,
+        noBackground && 'dnb-blockquote--no-background',
+        showGraphicsOnTop && 'dnb-blockquote--top'
+      )}
+      {...props}
+    />
+  )
+)
 
 export default Blockquote
