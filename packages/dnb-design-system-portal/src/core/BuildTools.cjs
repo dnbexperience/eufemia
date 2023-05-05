d4c27b7b75ab6618d3d891600c1f4a295982a085
Tobias Høegh <tobias@tujo.no>
2023-05-05 14:31:20 +0200

    chore(Portal): refactor and simplify global vars handling (#2287)

--- packages/dnb-design-system-portal/src/core/BuildTools.cjs
+++ packages/dnb-design-system-portal/src/core/BuildTools.cjs
@@ -4,11 +4,8 @@ const fs = require('fs-extra')
 function shouldUsePrebuild() {
   const rootPath = path.dirname(require.resolve('@dnb/eufemia'))
   const buildPath = path.resolve(rootPath, 'build')
+  const prebuildExists = fs.existsSync(buildPath)
 
-  global.PREBUILD_EXISTS = fs.existsSync(buildPath)
-
-  return Boolean(
-    global.PREBUILD_EXISTS && process.env.NODE_ENV === 'production'
-  )
+  return Boolean(prebuildExists && process.env.NODE_ENV === 'production')
 }
 module.exports.shouldUsePrebuild = shouldUsePrebuild
