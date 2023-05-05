91eb70c2f724787f3c1ea28ca55c56f1451c09b6
Tobias Høegh <tobias@tujo.no>
2023-05-05 13:10:31 +0200

    feat(Theming): add `VisibilityByTheme` as a shared component (#2280)
    Co-authored-by: Anders <anderslangseth@gmail.com>

---
+++ packages/dnb-eufemia/src/shared/VisibilityByTheme.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import useTheme from './useTheme'
+import type { ThemeNames, ThemeProps } from './Theme'
+
+type VisibilityByThemeProps = {
+  /**
+   * A valid theme name or object.
+   * Will pass children on a match.
+   */
+  visible?: ThemeParams
+
+  /**
+   * A valid theme name or object.
+   * Will omit passing children on a match.
+   * NB: "visible" takes presense over "hidden"
+   */
+  hidden?: ThemeParams
+
+  /**
+   * Any kind of a React Node that should render on a match.
+   */
+  children: React.ReactNode
+}
+
+type ThemeItem = ThemeNames | ThemeProps
+type ThemeParams = ThemeItem | Array<ThemeItem>
+
+export default function VisibilityByTheme({
+  children,
+  visible,
+  hidden,
+}: VisibilityByThemeProps) {
+  const theme = useTheme()
+
+  const visibleList = Array.isArray(visible) ? visible : [visible]
+  const hiddenList = Array.isArray(hidden) ? hidden : [hidden]
+
+  if (visible) {
+    if (!visibleList.some(match(theme))) {
+      return null
+    }
+  } else if (hidden) {
+    if (hiddenList.some(match(theme))) {
+      return null
+    }
+  }
+
+  return children as JSX.Element
+
+  function match(theme: ThemeProps) {
+    return (themeItem: ThemeItem) => {
+      return typeof themeItem === 'string'
+        ? theme.name === themeItem
+        : matchObject(theme, themeItem)
+    }
+  }
+
+  function matchObject(theme: ThemeProps, themeItem: ThemeItem) {
+    return Object.keys(themeItem).every((key) => {
+      return theme[key] === themeItem[key]
+    })
+  }
+}
