23fc652daeef794df1424db54f2a2c1a29ff9f26
Tobias Høegh <tobias@tujo.no>
2023-05-05 13:13:08 +0200

    Sync definitions fyles

--- packages/dnb-eufemia/src/components/logo/Logo.d.ts
+++ packages/dnb-eufemia/src/components/logo/Logo.d.ts
@@ -9,34 +9,34 @@ export interface LogoProps
   extends React.HTMLProps<HTMLElement>,
     SpacingProps {
   /**
-   * Define the size of the logo. Sets the height. The width will be calculated by the ratio. Also, `inherit` will use the inherited height. Defaults to `auto`
+   * Define the size of the logo. Sets the height. The width will be calculated by the ratio. Also, `inherit` will use the inherited height. Defaults to `auto`.
    */
   size?: LogoSize;
   ratio?: LogoRatio;
 
   /**
-   * Define either the width of the logo
+   * Define either the width of the logo.
    */
   width?: LogoWidth;
 
   /**
-   * Or define the height of the logo
+   * Or define the height of the logo.
    */
   height?: LogoHeight;
   alt?: string;
 
   /**
-   * Define the color of the logo
+   * Define the color of the logo.
    */
   color?: IconColor;
 
   /**
-   * Define which brand&#39;s logo to show. `ui` (DNB) or `sbanken`. Defaults to `ui`
+   * Define which brand&#39;s logo to show. `ui` (DNB) or `sbanken`. Defaults to `ui`.
    */
   brand?: string;
 
   /**
-   * Define the logo variant, if there is more than one variant of a brand&#39;s logo. Currently the only option other than default is a `compact` variant of the Sbanken logo. Defaults to `default`
+   * Define the logo variant, if there is more than one variant of a brand&#39;s logo. Currently the only option other than default is a `compact` variant of the Sbanken logo. Defaults to `default`.
    */
   variant?: string;
 
