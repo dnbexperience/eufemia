f016cfb8d6829144adebe5419e34ee2e308c85b9
Tobias Høegh <tobias@tujo.no>
2023-05-05 14:31:20 +0200

    chore(Portal): refactor and simplify global vars handling (#2287)

--- packages/dnb-design-system-portal/src/core/StyleImporter.cjs
+++ packages/dnb-design-system-portal/src/core/StyleImporter.cjs
@@ -4,6 +4,6 @@ function importStyles() {
    *
    * NB: Themes are imported by "gatsby-plugin-eufemia-theme-handler"
    */
-  require(process.env.STYLE_IMPORT_PATH)
+  require(global.STYLE_IMPORT_PATH)
 }
 module.exports.importStyles = importStyles
