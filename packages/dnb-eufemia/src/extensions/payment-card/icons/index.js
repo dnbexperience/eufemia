2470a7483ca2e7444a3f58539be42431fc938c8d
Anders <anderslangseth@gmail.com>
2023-05-05 08:45:07 +0200

    feat(PaymentCard): new card designs & new products (#2264)
    * feat(PaymentCard): new card designs & new products
    
    * chore(PaymentCard): /payment-card/icons to .tsx files

--- packages/dnb-eufemia/src/extensions/payment-card/icons/index.js
+++ packages/dnb-eufemia/src/extensions/payment-card/icons/index.js
@@ -2,20 +2,19 @@ import React from 'react'
 
 import BankAxept from './BankAxept'
 import DNB from './DNB'
-import DNBMetallic from './DNBMetallic'
 import Expired from './Clock'
 import Blocked from './Padlock'
 import MastercardDefault from './MastercardDefault'
-import MastercardMetallic from './MastercardMetallic'
-import MastercardMetallicBlack from './MastercardMetallicBlack'
+import MastercardDark from './MastercardDark'
 import Pluss from './Pluss'
+import Intro from './Intro'
+import Business from './Business'
+import Bedrift from './Bedrift'
 import PB from './PB'
-import PBPlatinum from './PBPlatinum'
 import SagaGold from './SagaGold'
 import SagaPlatinum from './SagaPlatinum'
-import SagaVisaPlatinum from './SagaVisaPlatinum'
 import VisaDefault from './VisaDefault'
-import VisaMetallic from './VisaMetallic'
+import VisaPlatinum from './VisaPlatinum'
 
 const BankLogo = ({ logoType, height }) =>
   logoType.cata({
@@ -26,35 +25,40 @@ const BankLogo = ({ logoType, height }) =>
         className="dnb-payment-card__card__bank-logo"
       />
     ),
-    Metallic: () => (
-      <DNBMetallic
-        height={height}
-        className="dnb-payment-card__card__bank-logo"
-      />
-    ),
   })
 
 const ProductLogo = ({ productType, cardDesign }) => {
   const id = 'dnb-payment-card__card__product-type'
   return productType.cata({
-    BankAxept: () =>
-      cardDesign.bankAxept.cata({
-        White: () => <BankAxept className={id} fill="#ffffff" />,
-        Black: () => <BankAxept className={id} fill="#757575" />,
-      }),
     Saga: () =>
       cardDesign.saga.cata({
         Gold: () => <SagaGold className={id} />,
         Platinum: () => <SagaPlatinum className={id} />,
-        VisaPlatinum: () => <SagaVisaPlatinum className={id} />,
         None: () => null,
       }),
-    Pluss: () => <Pluss className={id} fill="#A5E1D2" />,
+    Pluss: () => <Pluss className={id} />,
+    Intro: () => <Intro className={id} />,
+    Business: () => <Business className={id} />,
+    Bedrift: () => <Bedrift className={id} />,
     PrivateBanking: () =>
       cardDesign.privateBanking.cata({
         Default: () => <PB className={id} />,
-        Platinum: () => <PBPlatinum className={id} />,
         None: () => null,
+      }),
+    None: () => null,
+  })
+}
+
+const BankAxeptLogo = ({ bankAxept, cardDesign }) => {
+  const id = 'dnb-payment-card__card__bank-axept'
+  return bankAxept.cata({
+    BankAxept: () =>
+      cardDesign.bankAxept.cata({
+        White: () => <BankAxept className={id} fill="#ffffff" />,
+        Black20: () => <BankAxept className={id} fill="#cccccc" />,
+        Gray: () => <BankAxept className={id} fill="#b2b4b3" />,
+        Black: () => <BankAxept className={id} fill="#333333" />,
+        Gold: () => <BankAxept className={id} fill="#BFA970" />,
       }),
     None: () => null,
   })
@@ -80,19 +84,15 @@ const TypeLogo = ({ cardType, cardDesign }) => {
     Visa: () =>
       cardDesign.visa.cata({
         Colored: (color) => <VisaDefault className={id} fill={color} />,
-        Metallic: () => <VisaMetallic className={id} />,
+        Platinum: () => <VisaPlatinum className={id} />,
       }),
     Mastercard: () =>
       cardDesign.mastercard.cata({
         Default: () => <MastercardDefault className={id} />,
-        DefaultWhite: () => (
-          <MastercardDefault className={id} textFill="#ffffff" />
-        ),
-        Metallic: () => <MastercardMetallic className={id} />,
-        BlackMetallic: () => <MastercardMetallicBlack className={id} />,
+        Dark: () => <MastercardDark className={id} />,
       }),
     None: () => null,
   })
 }
 
-export { BankLogo, ProductLogo, StatusIcon, TypeLogo }
+export { BankLogo, ProductLogo, StatusIcon, TypeLogo, BankAxeptLogo }
---
+++ packages/dnb-eufemia/src/extensions/payment-card/utils/backgrounds/index.ts
@@ -0,0 +1,5 @@
+import myFirstImg from './my-first'
+import ungImg from './ung'
+import youthImg from './youth'
+
+export { myFirstImg, ungImg, youthImg }
