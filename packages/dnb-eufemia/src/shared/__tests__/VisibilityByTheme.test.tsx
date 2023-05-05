25926585d86111fece097b32aca1feeeb48a61d3
Tobias Høegh <tobias@tujo.no>
2023-05-05 13:10:31 +0200

    feat(Theming): add `VisibilityByTheme` as a shared component (#2280)
    Co-authored-by: Anders <anderslangseth@gmail.com>

---
+++ packages/dnb-eufemia/src/shared/__tests__/VisibilityByTheme.test.tsx
@@ -0,0 +1,159 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import Theme from '../Theme'
+import VisibilityByTheme from '../VisibilityByTheme'
+
+describe('VisibilityByTheme', () => {
+  it('renders content if not visible or hidden was given', () => {
+    const Component = (props) => (
+      <Theme name="eiendom" {...props}>
+        <VisibilityByTheme>
+          <p>I'm visible</p>
+        </VisibilityByTheme>
+      </Theme>
+    )
+
+    render(<Component />)
+
+    expect(document.body.textContent).toBe("I'm visible")
+  })
+
+  it('renders content on name match', () => {
+    const Component = (props) => (
+      <Theme name="eiendom" {...props}>
+        <VisibilityByTheme visible="eiendom">
+          <p>I'm visible</p>
+        </VisibilityByTheme>
+      </Theme>
+    )
+
+    const { rerender } = render(<Component />)
+
+    expect(document.body.textContent).toBe("I'm visible")
+
+    rerender(<Component name="sbanken" />)
+
+    expect(document.body.textContent).toBe('')
+  })
+
+  it('skips render when hidden matches', () => {
+    const Component = (props) => (
+      <Theme name="eiendom" {...props}>
+        <VisibilityByTheme hidden="sbanken">
+          <p>I'm visible</p>
+        </VisibilityByTheme>
+      </Theme>
+    )
+
+    const { rerender } = render(<Component />)
+
+    expect(document.body.textContent).toBe("I'm visible")
+
+    rerender(<Component name="sbanken" />)
+
+    expect(document.body.textContent).toBe('')
+
+    rerender(<Component name="ui" />)
+
+    expect(document.body.textContent).toBe("I'm visible")
+  })
+
+  it('prefers visible over hidden', () => {
+    const Component = (props) => (
+      <Theme name="eiendom" {...props}>
+        <VisibilityByTheme visible="eiendom" hidden="sbanken">
+          <p>I'm visible</p>
+        </VisibilityByTheme>
+      </Theme>
+    )
+
+    const { rerender } = render(<Component />)
+
+    expect(document.body.textContent).toBe("I'm visible")
+
+    rerender(<Component name="sbanken" />)
+
+    expect(document.body.textContent).toBe('')
+
+    rerender(<Component name="eiendom" />)
+
+    expect(document.body.textContent).toBe("I'm visible")
+  })
+
+  it('renders content on match from names in an array', () => {
+    const Component = (props) => (
+      <Theme name="eiendom" {...props}>
+        <VisibilityByTheme visible={['eiendom', 'sbanken']}>
+          <p>I'm visible</p>
+        </VisibilityByTheme>
+      </Theme>
+    )
+
+    const { rerender } = render(<Component />)
+
+    expect(document.body.textContent).toBe("I'm visible")
+
+    rerender(<Component name="sbanken" />)
+
+    expect(document.body.textContent).toBe("I'm visible")
+
+    rerender(<Component name="ui" />)
+
+    expect(document.body.textContent).toBe('')
+  })
+
+  it('renders content on match from names in an object inside an array', () => {
+    const Component = (props) => (
+      <Theme name="eiendom" {...props}>
+        <VisibilityByTheme
+          visible={[{ name: 'eiendom' }, { name: 'sbanken' }]}
+        >
+          <p>I'm visible</p>
+        </VisibilityByTheme>
+      </Theme>
+    )
+
+    const { rerender } = render(<Component />)
+
+    expect(document.body.textContent).toBe("I'm visible")
+
+    rerender(<Component name="sbanken" />)
+
+    expect(document.body.textContent).toBe("I'm visible")
+
+    rerender(<Component name="ui" />)
+
+    expect(document.body.textContent).toBe('')
+  })
+
+  it('renders content on match by several theme criterias', () => {
+    const Component = (props) => (
+      <Theme name="eiendom" variant="red" {...props}>
+        <VisibilityByTheme
+          visible={[
+            { name: 'eiendom', variant: 'red' },
+            { name: 'sbanken', variant: 'blue' },
+          ]}
+        >
+          <p>I'm visible</p>
+        </VisibilityByTheme>
+      </Theme>
+    )
+
+    const { rerender } = render(<Component />)
+
+    expect(document.body.textContent).toBe("I'm visible")
+
+    rerender(<Component name="sbanken" />)
+
+    expect(document.body.textContent).toBe('')
+
+    rerender(<Component name="sbanken" variant="blue" />)
+
+    expect(document.body.textContent).toBe("I'm visible")
+
+    rerender(<Component name="sbanken" variant="red" />)
+
+    expect(document.body.textContent).toBe('')
+  })
+})
