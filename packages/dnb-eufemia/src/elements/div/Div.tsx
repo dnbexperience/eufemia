511a1b4d92a0200ae65b6cfa7503477eebda05d9
Joakim Bjerknes <joakbjerk@gmail.com>
2023-05-09 13:24:31 +0200

    chore(Div): export DivProps (#2306)

--- packages/dnb-eufemia/src/elements/div/Div.tsx
+++ packages/dnb-eufemia/src/elements/div/Div.tsx
@@ -7,7 +7,7 @@ import React from 'react'
 import { SpacingProps } from '../../components/space/types'
 import E from '../Element'
 
-type DivProps = SpacingProps & React.HTMLAttributes<HTMLElement>
+export type DivProps = SpacingProps & React.HTMLAttributes<HTMLElement>
 
 const Div = React.forwardRef((props: DivProps, ref) => (
   <E as="div" skeletonMethod="shape" innerRef={ref} {...props} />
