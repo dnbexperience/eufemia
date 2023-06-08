d24c10d4032c6138db01f4e3fce5c552a31061ac
Tobias Høegh <tobias@tujo.no>
2023-06-08 12:57:56 +0200

    fix: include themed style in `ui-theme-tags` CSS package (#2383)

--- packages/dnb-eufemia/.storybook/preview.js
+++ packages/dnb-eufemia/.storybook/preview.js
@@ -1,3 +1,13 @@
 import '@dnb/eufemia/src/style/dnb-ui-core.scss'
-import '@dnb/eufemia/src/style/themes/theme-ui/ui-theme-components.scss'
+
+// DNB Universal Identity styles
 import '@dnb/eufemia/src/style/themes/theme-ui/ui-theme-basis.scss'
+import '@dnb/eufemia/src/style/themes/theme-ui/ui-theme-components.scss'
+
+// DNB Universal Identity Tags package
+// import '@dnb/eufemia/src/style/themes/theme-ui/ui-theme-basis.scss'
+// import '@dnb/eufemia/src/style/themes/theme-ui/ui-theme-tags.scss'
+
+// Sbanken styles
+// import '@dnb/eufemia/src/style/themes/theme-sbanken/sbanken-theme-basis.scss'
+// import '@dnb/eufemia/src/style/themes/theme-sbanken/sbanken-theme-components.scss'
