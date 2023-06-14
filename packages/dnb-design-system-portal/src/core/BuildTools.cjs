26b49e864232314a842d15783f6abf8a36d1bad2
Tobias Høegh <tobias@tujo.no>
2023-06-14 15:58:44 +0200

    chore: fix existing "/build" check (#2472)
    Locally, when running a prebuild, no package.json is generated.
    Therefore, we should check for an index file instead.

--- packages/dnb-design-system-portal/src/core/BuildTools.cjs
+++ packages/dnb-design-system-portal/src/core/BuildTools.cjs
@@ -4,8 +4,8 @@ const fs = require('fs-extra')
 function shouldUsePrebuild() {
   const rootPath = path.dirname(require.resolve('@dnb/eufemia'))
   const buildPath = path.resolve(rootPath, 'build')
-  const packageJsonPath = path.resolve(buildPath, 'package.json')
-  const prebuildExists = fs.existsSync(packageJsonPath)
+  const packageIndex = path.resolve(buildPath, 'index.js')
+  const prebuildExists = fs.existsSync(packageIndex)
 
   return Boolean(prebuildExists && process.env.NODE_ENV === 'production')
 }
