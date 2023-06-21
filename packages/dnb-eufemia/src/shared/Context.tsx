2fd31ca472c20efb9fce5765f4a98741afc91398
Anders <anderslangseth@gmail.com>
2023-06-21 13:46:32 +0200

    Update packages/dnb-eufemia/src/shared/Context.tsx

--- packages/dnb-eufemia/src/shared/Context.tsx
+++ packages/dnb-eufemia/src/shared/Context.tsx
@@ -36,7 +36,7 @@ import type { SkeletonProps, SkeletonShow } from '../components/Skeleton'
 import type { HelpButtonProps } from '../components/HelpButton'
 import type { TableProps } from '../components/Table'
 import type { GlobalErrorProps } from '../components/GlobalError'
-import type { ModalProps } from '../components/Modal/types'
+import type { ModalProps } from '../components/modal/types'
 
 import type { NumberFormatCurrency } from '../components/NumberFormat'
 
