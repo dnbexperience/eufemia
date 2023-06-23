4b616473e970bb7d9c028e84133694ea8d603be4
-l <anderslangseth@gmail.com>
2023-06-23 08:50:03 +0200

    add comments to replaceHydrateFunction

--- packages/dnb-design-system-portal/gatsby-browser.tsx
+++ packages/dnb-design-system-portal/gatsby-browser.tsx
@@ -19,6 +19,8 @@ function setIsTest(location) {
 }
 
 export const replaceHydrateFunction = () => {
+  // Added to solve the following errors, which prevented us from running screenshot tests
+  // https://github.com/gatsbyjs/gatsby/discussions/36232
   return (element, container) => {
     const root = ReactDOM.createRoot(container)
     root.render(element)
