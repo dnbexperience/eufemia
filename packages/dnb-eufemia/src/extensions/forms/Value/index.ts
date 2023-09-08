40aa10a722dbbc0f749dd9d35f11033af7182fc4
Tobias Høegh <tobias@tujo.no>
2023-09-08 13:19:05 +0200

    fix(Forms): remove FirstName, LastName and InfoCardSection

--- packages/dnb-eufemia/src/extensions/forms/Value/index.ts
+++ packages/dnb-eufemia/src/extensions/forms/Value/index.ts
@@ -4,8 +4,6 @@ import BooleanComponent from './Boolean'
 import Currency from './Currency'
 import DateComponent from './Date'
 import Email from './Email'
-import FirstName from './FirstName'
-import LastName from './LastName'
 import NationalIdentityNumber from './NationalIdentityNumber'
 import PhoneNumber from './PhoneNumber'
 import BankAccountNumber from './BankAccountNumber'
@@ -17,8 +15,6 @@ const Value = {
   Currency,
   Date: DateComponent,
   Email,
-  FirstName,
-  LastName,
   NationalIdentityNumber,
   PhoneNumber,
   BankAccountNumber,
