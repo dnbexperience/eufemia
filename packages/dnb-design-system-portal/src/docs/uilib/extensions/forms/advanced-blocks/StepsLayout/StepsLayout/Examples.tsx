658a9137ef13b982371e0ff60abe636d71e84934
Tobias Høegh <tobias@tujo.no>
2023-09-08 21:31:25 +0200

    Use fragments instead of divs

--- packages/dnb-design-system-portal/src/docs/uilib/extensions/forms/advanced-blocks/StepsLayout/StepsLayout/Examples.tsx
+++ packages/dnb-design-system-portal/src/docs/uilib/extensions/forms/advanced-blocks/StepsLayout/StepsLayout/Examples.tsx
@@ -7,25 +7,25 @@ export const Default = () => {
     <ComponentBox scope={{ Layout, StepsLayout }}>
       <StepsLayout>
         <StepsLayout.Step title="Step 1">
-          <div>
+          <>
             <P>Step 1 contents</P>
             <StepsLayout.NextButton />
-          </div>
+          </>
         </StepsLayout.Step>
         <StepsLayout.Step>
-          <div>
+          <>
             <P>Step 2 contents</P>
             <Layout.ButtonRow>
               <StepsLayout.PreviousButton />
               <StepsLayout.NextButton />
             </Layout.ButtonRow>
-          </div>
+          </>
         </StepsLayout.Step>
         <StepsLayout.Step>
-          <div>
+          <>
             <P>Step 3 contents</P>
             <StepsLayout.PreviousButton />
-          </div>
+          </>
         </StepsLayout.Step>
       </StepsLayout>
     </ComponentBox>
