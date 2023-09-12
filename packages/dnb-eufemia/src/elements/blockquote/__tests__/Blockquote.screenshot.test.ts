d63d30d6f584788c7ce9fb16ccad5f9bcc7558ed
Snorre Kim <snorre.kim@dnb.no>
2023-09-12 15:21:21 +0200

    feat(Blockquote): fix sbanken styling, added small screen version (#2639)
    ## Summary
    
    * Updated sbanken styling
    * Added sbanken styling for small screens
    * Use proper colors for DNB theme icon background

--- packages/dnb-eufemia/src/elements/blockquote/__tests__/Blockquote.screenshot.test.ts
+++ packages/dnb-eufemia/src/elements/blockquote/__tests__/Blockquote.screenshot.test.ts
@@ -8,8 +8,56 @@ import {
   setupPageScreenshot,
 } from '../../../core/jest/jestSetupScreenshots'
 
-describe.each(['ui', 'sbanken'])('Blockquote for %s', (themeName, url) => {
-  setupPageScreenshot({ themeName, url: '/uilib/elements/blockquote' })
+describe.each(['ui', 'sbanken'])('Blockquote for %s', (themeName) => {
+  setupPageScreenshot({
+    themeName,
+    url: '/uilib/elements/blockquote',
+  })
+
+  const style = {
+    width: '30rem', // make sure our input gets an explicit width, because of mac/linux rendering differences
+  }
+
+  it('have to match default "blockquote"', async () => {
+    const screenshot = await makeScreenshot({
+      style,
+      selector: '[data-visual-test="blockquote-default"]',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('have to match "blockquote" with top aligned graphic', async () => {
+    const screenshot = await makeScreenshot({
+      style,
+      selector: '[data-visual-test="blockquote-top"]',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('have to match "blockquote" with top aligned graphic and no background', async () => {
+    const screenshot = await makeScreenshot({
+      style,
+      selector: '[data-visual-test="blockquote-top-no-background"]',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('have to match "blockquote" with no background', async () => {
+    const screenshot = await makeScreenshot({
+      style,
+      selector: '[data-visual-test="blockquote-no-background"]',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+})
+
+describe.each(['sbanken'])('Blockquote on mobile for %s', (themeName) => {
+  setupPageScreenshot({
+    themeName,
+    url: '/uilib/elements/blockquote',
+    pageViewport: { width: 640 },
+  })
+
   const style = {
     width: '30rem', // make sure our input gets an explicit width, because of mac/linux rendering differences
   }
