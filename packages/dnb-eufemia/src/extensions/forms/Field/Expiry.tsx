98bcb661f78331560529045cfbeb319b7e7cc47a
Joakim Bjerknes <joakbjerk@gmail.com>
2023-09-26 12:30:49 +0200

    decouple keyhandler into own hook

--- packages/dnb-eufemia/src/extensions/forms/Field/Expiry.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Field/Expiry.tsx
@@ -1,9 +1,12 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import { Props as StringComponentProps } from './String'
-import TextMask from '../../../components/input-masked/TextMask'
+import TextMask, {
+  TextMaskProps,
+} from '../../../components/input-masked/TextMask'
 import Input from '../../../components/Input'
-import { correctCaretPosition } from '../../../components/input-masked/InputMaskedUtils'
-import { clearConfigCache } from 'prettier'
+import { makeUniqueId } from '../../../shared/component-helper'
+import DatePickerContext from '../../../components/date-picker/DatePickerContext'
+import Context from '../../../shared/Context'
 
 type ExpiryPlaceholderType = 'dashes' | 'spaces' | 'letters'
 
@@ -18,22 +21,13 @@ export type ExpiryValue = {
    * Example: 24
    */
   year: string
-  /**
-   * The raw unchanged input value
-   * Example: 0824
-   */
-  raw: string
-  /**
-   * Formatted input value that includes delimiter shown in placeholder
-   * Example: 08/24
-   */
-  formatted: string
 }
 
 type ExpiryProps = Omit<
   StringComponentProps,
   'placeholder' | 'onChange'
 > & {
+  id?: string
   /**
    * The placeholder which shows up once the field is empty. Can be set to `dashes`, `spaces` or `letters`
    * Default: `dashes`
@@ -56,8 +50,6 @@ const placeholders: Record<ExpiryPlaceholderType, string> = {
   letters: 'mm / yy',
 }
 
-const NUMBER_KEYS = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-
 function Expiry({
   placeholder = 'dashes',
   keep_placeholder = true,
@@ -66,26 +58,18 @@ function Expiry({
   const [month, setMonth] = useState<string>('')
   const [year, setYear] = useState<string>('')
 
+  const context = useContext(DatePickerContext) as any
+
+  const id = useRef(props.id || makeUniqueId()).current
   const monthRef = useRef<HTMLInputElement>(null)
   const yearRef = useRef<HTMLInputElement>(null)
 
-  useEffect(() => {
-    const monthInput = monthRef.current
-    const yearInput = yearRef.current
-
-    monthInput.addEventListener('keydown', onKeydown)
-    yearInput.addEventListener('keydown', onKeydown)
-
-    return () => {
-      monthInput.removeEventListener('keydown', onKeydown)
-      yearInput.removeEventListener('keydown', onKeydown)
-    }
-  }, [])
+  const { handleKeydown } = useHandleCursorPosition({ monthRef, yearRef })
 
   // Use effect to synchronise the two value states, instead of firing with an onChange bound to both inputs.
   // Not sure of this is a better solution though
   useEffect(() => {
-    if ((!month || month.length > 2) && (year || year.length > 2)) {
+    if (!(month?.length === 2 && year?.length === 2)) {
       return
     }
 
@@ -93,91 +77,35 @@ function Expiry({
       props.onChange({
         month,
         year,
-        raw: `${month}${year}`,
-        formatted: `${month}/${year}`,
       })
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [month, year])
 
-  async function onKeydown(event: KeyboardEvent) {
-    const input = event.currentTarget as HTMLInputElement
-
-    const pressedKey = event.key
-    const hasPressedShiftKey = event.shiftKey
-    const hasPressedTab = pressedKey === 'Tab'
-
-    const endPosition = Number(input.getAttribute('size'))
-    const startPosition = 0
-
-    const selectionStart = input.selectionStart
-    const selectionEnd = input.selectionEnd
-
-    const isAtFirstCaretPosition =
-      selectionStart === startPosition && selectionEnd === startPosition
-
-    const isAtLastCaretPosition =
-      selectionStart === endPosition && selectionEnd === endPosition
-
-    const monthInput = monthRef.current
-    const isMonthInputInFocus = monthInput === document.activeElement
-
-    const yearInput = yearRef.current
-    const isYearInputInFocus = yearInput === document.activeElement
-
-    // If user is at end or start of input and presses either tab or shift + tab, then the whole input should be selected
-    if (
-      (hasPressedTab && !hasPressedShiftKey && isAtFirstCaretPosition) ||
-      (hasPressedShiftKey && hasPressedTab && isAtLastCaretPosition)
-    ) {
-      event.preventDefault()
-      input.setSelectionRange(startPosition, endPosition)
-    }
-
-    // If user is at the end of month input, and presses either a number key or ArrowRight, then year input should be in focus
-    if (
-      isMonthInputInFocus &&
-      isAtLastCaretPosition &&
-      (pressedKey === 'ArrowRight' || NUMBER_KEYS.includes(pressedKey))
-    ) {
-      await wait(1)
-      yearInput.focus()
-      yearInput.setSelectionRange(startPosition, startPosition)
-      return
-    }
-
-    // If user is at the end of year input, and presses either Backspace or ArrowLeft, then month input should be in focus
-    if (
-      isYearInputInFocus &&
-      isAtFirstCaretPosition &&
-      (pressedKey == 'ArrowLeft' || pressedKey === 'Backspace')
-    ) {
-      await wait(1)
-      monthInput.focus()
-      monthInput.setSelectionRange(endPosition, endPosition)
-      return
-    }
-  }
-
   return (
     <Input
+      id={`${id}__input`}
       className="dnb-date-picker dnb-date-picker--show-input dnb-forms-field-expiry"
       input_element={
         <span className="dnb-date-picker__input__wrapper">
           <ExpiryDateField
+            id={id}
             type="month"
             value={month}
-            onChange={(e) => setMonth(e.currentTarget.value)}
             innerRef={monthRef}
+            onChange={(event) => setMonth(event.currentTarget.value)}
+            onKeyDown={handleKeydown}
           />
           <span className="dnb-date-picker--separator" aria-hidden>
             {' / '}
           </span>
           <ExpiryDateField
+            id={id}
             type="year"
             value={year}
-            onChange={(e) => setYear(e.currentTarget.value)}
             innerRef={yearRef}
+            onChange={(event) => setYear(event.currentTarget.value)}
+            onKeyDown={handleKeydown}
           />
         </span>
       }
@@ -186,11 +114,12 @@ function Expiry({
 }
 
 type ExpiryDateFieldProps = {
+  id: string
   type: 'month' | 'year'
   value: string
   onChange: (event: React.KeyboardEvent<HTMLInputElement>) => void
   innerRef: React.MutableRefObject<HTMLInputElement>
-}
+} & Partial<Omit<TextMaskProps, 'ref'>>
 
 type ExpiryDateFieldConfig = {
   placeholder: string
@@ -212,34 +141,115 @@ const dateFields: Record<
 }
 
 function ExpiryDateField({
+  id,
   type,
   value,
   onChange,
   innerRef,
+  ...rest
 }: ExpiryDateFieldProps) {
+  // const context = useContext(Context) as any
+
   return (
-    <TextMask
-      className="dnb-date-picker__input"
-      value={value}
-      onChange={onChange}
-      mask={dateFields[type].mask}
-      placeholderChar={dateFields[type].placeholder}
-      guide={true}
-      showMask={true}
-      keepCharPositions={false} // so we can overwrite next value, if it already exists
-      autoComplete="off"
-      autoCapitalize="none"
-      spellCheck={false}
-      autoCorrect="off"
-      size={2}
-      // Icky casting hack
-      inputRef={innerRef as unknown as Record<string, unknown>}
-    />
+    <>
+      <TextMask
+        id={`${id}-${type}`}
+        className="dnb-date-picker__input"
+        value={value}
+        onChange={onChange}
+        mask={dateFields[type].mask}
+        placeholderChar={dateFields[type].placeholder}
+        guide={true}
+        showMask={true}
+        keepCharPositions={false} // so we can overwrite next value, if it already exists
+        autoComplete="off"
+        autoCapitalize="none"
+        spellCheck={false}
+        autoCorrect="off"
+        size={2}
+        onFocus={handleFocus}
+        // Icky casting hack
+        inputRef={innerRef as unknown as Record<string, unknown>}
+        {...rest}
+      />
+      <label id={`${id}-month-label`} htmlFor={`${id}-${type}`} hidden>
+        {/* {context.translation.DatePicker[type]} */}
+      </label>
+    </>
   )
+
+  function handleFocus({ target }) {
+    target.focus()
+    target.select()
+  }
 }
 
 export default Expiry
 
-function wait(t) {
-  return new Promise((r) => setTimeout(r, t))
+function useHandleCursorPosition({ monthRef, yearRef }) {
+  function handleKeydown(event: React.KeyboardEvent) {
+    const input = event.currentTarget as HTMLInputElement
+
+    const pressedKey = event.key
+    const hasPressedNumberKey = /[0-9]/.test(pressedKey)
+
+    const startPosition = 0
+    const endPosition = Number(input.size)
+
+    const currentSelectionStart = input.selectionStart
+
+    window.requestAnimationFrame(() => {
+      const selectionStart = input.selectionStart
+      const selectionEnd = input.selectionEnd
+
+      const isAtFirstCaretPosition =
+        selectionStart === startPosition && selectionEnd === startPosition
+
+      const isAtLastCaretPosition =
+        selectionStart === endPosition && selectionEnd === endPosition
+
+      const isMonthInputInFocus = input === monthRef.current
+      const isYearInputInFocus = input === yearRef.current
+
+      if (
+        !(
+          hasPressedNumberKey ||
+          pressedKey === 'ArrowRight' ||
+          pressedKey === 'ArrowLeft' ||
+          pressedKey === 'Backspace'
+        )
+      ) {
+        return // stop here
+      }
+
+      // If user is at the end of month input, and presses either a number key or ArrowRight, then year input should be in focus
+      if (
+        isMonthInputInFocus &&
+        isAtLastCaretPosition &&
+        !(currentSelectionStart === 1 && pressedKey === 'ArrowRight')
+      ) {
+        yearRef.current.focus()
+        yearRef.current.setSelectionRange(startPosition, startPosition)
+
+        return // stop here
+      }
+
+      // If user is at the end of year input, and presses either Backspace or ArrowLeft, then month input should be in focus
+      if (
+        isYearInputInFocus &&
+        isAtFirstCaretPosition &&
+        !(
+          currentSelectionStart === 1 &&
+          (pressedKey == 'ArrowLeft' || pressedKey === 'Backspace')
+        )
+      ) {
+        monthRef.current.focus()
+        monthRef.current.setSelectionRange(endPosition, endPosition)
+
+        return // stop here
+      }
+    })
+  }
+
+  return { handleKeydown }
 }
