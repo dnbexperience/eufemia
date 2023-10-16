b495615add1ed0555bfaa0e8dacf0ded552411d3
Henrik Haugberg <henrik.haugberg@gmail.com>
2023-10-16 14:12:43 +0200

    Phone number merged with country code, bugfixes

--- packages/dnb-eufemia/src/extensions/forms/Field/PhoneNumber/PhoneNumber.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Field/PhoneNumber/PhoneNumber.tsx
@@ -1,10 +1,11 @@
-import React, { useContext, useCallback } from 'react'
-import { Div } from '../../../../elements'
+import React, { useMemo, useContext, useCallback } from 'react'
+import { Autocomplete, Flex } from '../../../../components'
 import { InputMaskedProps } from '../../../../components/InputMasked'
 import classnames from 'classnames'
-import CountryCode from '../CountryCode'
+import countries from '../../constants/countries'
 import StringComponent from '../String'
 import { useDataValue } from '../../hooks'
+import FieldBlock from '../../FieldBlock'
 import { FieldHelpProps, FieldProps } from '../../types'
 import { pickSpacingProps } from '../../../../components/flex/utils'
 import SharedContext from '../../../../shared/Context'
@@ -13,6 +14,7 @@ export type Props = FieldHelpProps &
   FieldProps<string, undefined> & {
     countryCodeFieldClassName?: string
     numberFieldClassName?: string
+    counttryCodePlaceholder?: string
     countryCodeLabel?: string
     numberMask?: InputMaskedProps['mask']
     width?: 'large' | 'stretch'
@@ -22,8 +24,7 @@ export type Props = FieldHelpProps &
 
 function PhoneNumber(props: Props) {
   const sharedContext = useContext(SharedContext)
-  const preparedProps: Props = {
-    ...props,
+  const defaultProps: Partial<Props> = {
     // Important for the default value to be defined here, and not after the useDataValue call, to avoid the UI jumping
     // back to +47 once the user empty the field so handleChange send out undefined.
     value: '+47',
@@ -32,11 +33,17 @@ function PhoneNumber(props: Props) {
       ...props?.errorMessages,
     },
   }
+  const preparedProps: Props = {
+    ...defaultProps,
+    ...props,
+  }
 
   const {
     className,
     countryCodeFieldClassName,
     numberFieldClassName,
+    layout = 'vertical',
+    counttryCodePlaceholder,
     placeholder,
     countryCodeLabel,
     label = sharedContext?.translation.Forms.phoneNumberLabel,
@@ -61,8 +68,20 @@ function PhoneNumber(props: Props) {
       ? value.match(/^(\+[^ ]+)? ?(.*)$/)
       : [undefined, '', '']
 
+  const countryCodeData = useMemo(
+    () =>
+      countries.map((country) => ({
+        selectedKey: `+${country.code}`,
+        selected_value: `${country.iso} (+${country.code})`,
+        content: `+${country.code} ${country.name}`,
+      })),
+    []
+  )
+
   const handleCountryCodeChange = useCallback(
-    (countryCode: string) => {
+    ({ data }: { data: { selectedKey: string } }) => {
+      const countryCode = data?.selectedKey?.trim() ?? emptyValue
+
       if (!countryCode && !phoneNumber) {
         handleChange?.(emptyValue)
         onCountryCodeChange?.(emptyValue)
@@ -90,63 +109,75 @@ function PhoneNumber(props: Props) {
   )
 
   return (
-    <Div
-      className={classnames(
-        'dnb-forms-field-phone-number',
-        width !== undefined &&
-          `dnb-forms-field-phone-number--width-${width}`,
-        className
-      )}
-      {...pickSpacingProps(preparedProps)}
+    <FieldBlock
+      className={classnames('dnb-forms-field-phone-number', className)}
+      width={width !== 'stretch' ? width : undefined}
+      info={info}
+      warning={warning}
+      error={error}
     >
-      <CountryCode
-        className={classnames(
-          'dnb-forms-field-phone-number__country-code',
-          countryCodeFieldClassName
-        )}
-        label={countryCodeLabel}
-        value={countryCode ?? '+47'}
-        disabled={disabled}
-        onChange={handleCountryCodeChange}
-      />
+      <Flex.Container direction="horizontal">
+        <Autocomplete
+          className={classnames(
+            'dnb-forms-field-phone-number__country-code',
+            countryCodeFieldClassName
+          )}
+          placeholder={counttryCodePlaceholder ?? ' '}
+          label_direction={layout}
+          label={
+            countryCodeLabel ??
+            sharedContext?.translation.Forms.countryCodeLabel
+          }
+          data={countryCodeData}
+          value={countryCode}
+          disabled={disabled}
+          on_focus={handleFocus}
+          on_blur={handleBlur}
+          on_change={handleCountryCodeChange}
+          independent_width
+          search_numbers
+          {...pickSpacingProps(props)}
+          stretch={width === 'stretch'}
+        />
 
-      <StringComponent
-        className={classnames(
-          'dnb-forms-field-phone-number__number',
-          numberFieldClassName
-        )}
-        type="tel"
-        emptyValue=""
-        layout="vertical"
-        label={label ?? ' '}
-        placeholder={placeholder ?? '00 00 00 00'}
-        mask={
-          numberMask ?? [
-            /\d/,
-            /\d/,
-            ' ',
-            /\d/,
-            /\d/,
-            ' ',
-            /\d/,
-            /\d/,
-            ' ',
-            /\d/,
-            /\d/,
-          ]
-        }
-        onFocus={handleFocus}
-        onBlur={handleBlur}
-        onChange={handleNumberChange}
-        value={phoneNumber}
-        info={info}
-        warning={warning}
-        error={error}
-        disabled={disabled}
-        width="stretch"
-        help={help}
-      />
-    </Div>
+        <StringComponent
+          className={classnames(
+            'dnb-forms-field-phone-number__number',
+            numberFieldClassName
+          )}
+          type="tel"
+          emptyValue=""
+          layout="vertical"
+          label={label ?? ' '}
+          placeholder={placeholder ?? '00 00 00 00'}
+          mask={
+            numberMask ?? [
+              /\d/,
+              /\d/,
+              ' ',
+              /\d/,
+              /\d/,
+              ' ',
+              /\d/,
+              /\d/,
+              ' ',
+              /\d/,
+              /\d/,
+            ]
+          }
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          onChange={handleNumberChange}
+          value={phoneNumber}
+          info={info}
+          warning={warning}
+          error={error}
+          disabled={disabled}
+          width="stretch"
+          help={help}
+        />
+      </Flex.Container>
+    </FieldBlock>
   )
 }
 
