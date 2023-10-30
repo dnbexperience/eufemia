b6aeec37f51ba0a4fdb8d72e4e45e02bb0208d57
Joakim Bjerknes <joakbjerk@gmail.com>
2023-10-30 07:16:50 +0100

    refactor DatePickerAddon

---
+++ packages/dnb-eufemia/src/components/date-picker/DatePickerAddon.tsx
@@ -0,0 +1,105 @@
+/**
+ * Web DatePicker Component
+ *
+ */
+
+import React, { useContext, useState } from 'react'
+import { convertStringToDate } from './DatePickerCalc'
+import Button from '../button/Button'
+import DatePickerContext from './DatePickerContext'
+import def from 'ajv/dist/vocabularies/discriminator'
+
+export type DatePickerAddonProps = React.HTMLProps<HTMLElement> & {
+  /**
+   * Gives you the possibility to set predefined dates and date ranges so the user can select these by one click. Define either a JSON or an object with the defined shortcuts. More info is below.
+   */
+  shortcuts?: any[] | React.ReactNode
+  renderElement?: React.ReactNode
+}
+
+function DatePickerAddon(props) {
+  const context = useContext(DatePickerContext)
+
+  function setDate({ shortcut, event }) {
+    const start_date = shortcut.date || shortcut.start_date
+    const end_date = shortcut.end_date
+
+    const startDate =
+      typeof start_date === 'function'
+        ? start_date(getCurrentDates())
+        : start_date
+        ? convertStringToDate(start_date)
+        : null
+    const endDate =
+      typeof end_date === 'function'
+        ? end_date(getCurrentDates())
+        : end_date
+        ? convertStringToDate(end_date)
+        : null
+
+    callOnChange({
+      startDate,
+      endDate: endDate || startDate,
+      event,
+    })
+
+    if (shortcut.close_on_select) {
+      context.hidePicker(event)
+    }
+  }
+
+  function getCurrentDates() {
+    const { startDate, endDate } = context
+    return {
+      date: startDate,
+      start_date: startDate,
+      end_date: endDate,
+    }
+  }
+
+  function callOnChange({ startDate, endDate, event = null } = {}) {
+    context.updateState({
+      startDate,
+      endDate,
+      changeMonthViews: true,
+    })
+    context.callOnChangeHandler({ startDate, endDate, event })
+  }
+
+  const { shortcuts, renderElement } = props
+
+  const shortcutsArray = shortcuts
+    ? typeof shortcuts === 'string'
+      ? JSON.parse(shortcuts)
+      : shortcuts
+    : []
+  const hasShortcuts = shortcutsArray && shortcutsArray.length > 0
+
+  if (!hasShortcuts && !renderElement) {
+    return <></>
+  }
+
+  const shortcutElements = hasShortcuts && (
+    <>
+      {shortcutsArray.map(({ title, ...shortcut }, i) => {
+        return (
+          <Button
+            key={i}
+            text={title}
+            variant="secondary"
+            onClick={(event) => setDate({ shortcut, event })}
+          />
+        )
+      })}
+    </>
+  )
+
+  return (
+    <div className="dnb-date-picker__addon">
+      {renderElement}
+      {shortcutElements}
+    </div>
+  )
+}
+
+export default DatePickerAddon
