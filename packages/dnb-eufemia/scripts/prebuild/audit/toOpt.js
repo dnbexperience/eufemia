a27937726c1e2989285e862880ab6afcc1fd5b6e
Tobias Høegh <tobias@tujo.no>
2023-11-01 20:54:37 +0100

    chore: update to CI lock file audit v6

--- packages/dnb-eufemia/scripts/prebuild/audit/toOpt.js
+++
@@ -1,9 +0,0 @@
-/**
- * Because of an Yarn bug, where devDependencies do not get checked by audit,
- * we rename devDependencies to optionalDependencies and vice versa
- *
- */
-
-import prepareForAudit from './prepareForAudit'
-
-prepareForAudit('fromDevToOpt')
