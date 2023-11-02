7612bf42254834d823880636ac6932d34b6a878d
Yngve Sundfjord <yngvesundfjord@gmail.com>
2023-11-02 10:50:11 +0100

    Further style and functionality improvements for inline HelpButton

--- packages/dnb-eufemia/src/components/help-button/HelpButtonInline.tsx
+++ packages/dnb-eufemia/src/components/help-button/HelpButtonInline.tsx
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import ReactDOM from 'react-dom'
 import { HelpButtonProps } from './HelpButton'
 import HelpButtonInstance from './HelpButtonInstance'
 import HeightAnimation from '../HeightAnimation'
+import { makeUniqueId } from '../../shared/component-helper'
 
 export type HelpButtonInlineProps = {
   isOpen?: boolean
 } & HelpButtonProps
 
+export type HelpButtonContentProps = {
+  isOpen: boolean
+  contentElement: Element
+  children: React.ReactNode | string
+  id: string
+}
+
 export default function HelpButtonInline(props: HelpButtonInlineProps) {
   const [isOpen, setIsOpen] = React.useState(false)
-
   const { contentElement, children, ...rest } = props
+
+  const contentElementId = useRef('')
+  useEffect(() => {
+    contentElementId.current =
+      contentElement.getAttribute('id') || makeUniqueId()
+  }, [contentElement])
 
   if (contentElement === null) {
     return null
@@ -20,16 +33,19 @@ export default function HelpButtonInline(props: HelpButtonInlineProps) {
   return (
     <>
       <HelpButtonInstance
+        className="dnb-help-button--inline"
         on_click={() => {
           setIsOpen((open) => !open)
         }}
         icon={isOpen ? 'close' : rest.icon}
+        aria-controls={contentElementId.current}
+        size={rest.size || 'small'}
         {...rest}
       />
       <HelpButtonContent
         isOpen={isOpen}
         contentElement={contentElement}
-        {...props}
+        id={contentElementId.current}
       >
         {children}
       </HelpButtonContent>
@@ -41,9 +57,10 @@ function HelpButtonContent({
   isOpen,
   contentElement,
   children,
-}: HelpButtonInlineProps) {
+  id,
+}: HelpButtonContentProps) {
   return ReactDOM.createPortal(
-    <span>
+    <span id={id}>
       <HeightAnimation open={isOpen}>
         <div className="dnb-help-button-content">{children}</div>
       </HeightAnimation>
