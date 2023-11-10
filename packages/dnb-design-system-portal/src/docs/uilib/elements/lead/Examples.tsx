86b08069e851482a487234fabbbd5a1c194e90ce
-l <anderslangseth@gmail.com>
2023-11-10 10:33:51 +0100

    docs(Lead): adds Lead to docs

---
+++ packages/dnb-design-system-portal/src/docs/uilib/elements/lead/Examples.tsx
@@ -0,0 +1,24 @@
+/**
+ * UI lib Component Example
+ *
+ */
+
+import React from 'react'
+import styled from '@emotion/styled'
+import ComponentBox from '../../../../shared/tags/ComponentBox'
+import Anchor from '@dnb/eufemia/src/components/Anchor'
+import Lead from '@dnb/eufemia/src/elements/Lead'
+
+export function LeadExamples() {
+  return (
+    <ComponentBox>
+      <div>
+        <Lead>Default lead</Lead>
+        <Lead>
+          Lead with <Anchor href="/">Anchor / Text Link</Anchor> looks
+          great!
+        </Lead>
+      </div>
+    </ComponentBox>
+  )
+}
