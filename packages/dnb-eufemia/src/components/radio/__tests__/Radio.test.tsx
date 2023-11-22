693f9d4b70a053536cbd190ce0095c4306f9ce91
Tobias Høegh <tobias@tujo.no>
2023-11-22 14:38:37 +0100

    fix(RadioGroup): omit rendering label when not given (#2928)

--- packages/dnb-eufemia/src/components/radio/__tests__/Radio.test.tsx
+++ packages/dnb-eufemia/src/components/radio/__tests__/Radio.test.tsx
@@ -3,7 +3,7 @@
  *
  */
 
-import { fireEvent, render, cleanup } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import React from 'react'
 import { axeComponent, loadScss } from '../../../core/jest/jestSetup'
 import Radio, { RadioProps } from '../Radio'
@@ -104,8 +104,6 @@ describe('Radio component', () => {
       // re-render + still false
       fireEvent.click(document.querySelector('button#rerender'))
       expect(document.querySelector('input').checked).toBe(false)
-
-      cleanup()
     }
 
     TestStates(<ControlledVsUncontrolled />)
@@ -173,199 +171,9 @@ describe('Radio component', () => {
   })
 })
 
-describe('Radio group component', () => {
-  it('has to set correct value using keys', () => {
-    const my_event = jest.fn()
-    render(
-      <Radio.Group
-        label="Label"
-        name="group"
-        id="group"
-        on_change={my_event}
-      >
-        <Radio id="radio-1" label="Radio 1" value="first" />
-        <Radio id="radio-2" label="Radio 2" value="second" checked />
-      </Radio.Group>
-    )
-    fireEvent.click(document.querySelectorAll('input')[0])
-    expect(my_event.mock.calls.length).toBe(1)
-    expect(my_event.mock.calls[0][0].value).toBe('first')
-
-    fireEvent.click(document.querySelectorAll('input')[1])
-    expect(my_event.mock.calls.length).toBe(2)
-    expect(my_event.mock.calls[1][0].value).toBe('second')
-  })
-
-  it('will disable a single button within a group', () => {
-    render(
-      <Radio.Group>
-        <Radio disabled />
-      </Radio.Group>
-    )
-
-    expect(document.querySelector('input[disabled]')).toBeInTheDocument()
-  })
-
-  it('will disable a single button, defined in the group', () => {
-    render(
-      <Radio.Group disabled>
-        <Radio />
-      </Radio.Group>
-    )
-
-    expect(document.querySelector('input[disabled]')).toBeInTheDocument()
-  })
-
-  it('will overwrite "disable" state, defined in the group', () => {
-    render(
-      <Radio.Group disabled>
-        <Radio disabled={false} />
-        <Radio disabled />
-      </Radio.Group>
-    )
-
-    expect(document.querySelectorAll('input')[0]).not.toHaveAttribute(
-      'disabled'
-    )
-    expect(document.querySelectorAll('input')[1]).toHaveAttribute(
-      'disabled'
-    )
-  })
-
-  it('should support spacing props', () => {
-    render(
-      <Radio.Group top="2rem">
-        <Radio id="radio-1" label="Radio 1" value="first" />
-        <Radio id="radio-2" label="Radio 2" value="second" checked />
-      </Radio.Group>
-    )
-
-    const element = document.querySelector('.dnb-radio-group')
-
-    expect(Array.from(element.classList)).toEqual([
-      'dnb-radio-group',
-      'dnb-radio-group--row',
-      'dnb-form-component',
-      'dnb-space__top--large',
-    ])
-  })
-
-  it('should inherit formElement vertical label', () => {
-    render(
-      <Provider formElement={{ label_direction: 'vertical' }}>
-        <Radio.Group label="Label" name="group" id="group">
-          <Radio id="radio-1" label="Radio 1" value="first" />
-          <Radio id="radio-2" label="Radio 2" value="second" checked />
-        </Radio.Group>
-      </Provider>
-    )
-
-    const element = document.querySelector('.dnb-radio-group')
-    const attributes = Array.from(element.attributes).map(
-      (attr) => attr.name
-    )
-
-    expect(attributes).toEqual(['class'])
-    expect(Array.from(element.classList)).toEqual([
-      'dnb-radio-group',
-      'dnb-radio-group--row',
-      'dnb-form-component',
-    ])
-    expect(
-      Array.from(
-        document.querySelector('.dnb-radio-group .dnb-flex-container')
-          .classList
-      )
-    ).toEqual([
-      'dnb-space',
-      'dnb-flex-container',
-      'dnb-flex-container--direction-vertical',
-      'dnb-flex-container--justify-flex-start',
-      'dnb-flex-container--align-baseline',
-      'dnb-flex-container--spacing-small',
-      'dnb-flex-container--wrap',
-      'dnb-flex-container--divider-space',
-    ])
-    expect(
-      Array.from(document.querySelector('.dnb-flex-container').classList)
-    ).toEqual([
-      'dnb-space',
-      'dnb-flex-container',
-      'dnb-flex-container--direction-vertical',
-      'dnb-flex-container--justify-flex-start',
-      'dnb-flex-container--align-baseline',
-      'dnb-flex-container--spacing-small',
-      'dnb-flex-container--wrap',
-      'dnb-flex-container--divider-space',
-    ])
-  })
-
-  it('should support vertical label', () => {
-    const { rerender } = render(
-      <Radio.Group label="Label" vertical>
-        <Radio />
-      </Radio.Group>
-    )
-
-    const element = document.querySelector('.dnb-radio-group')
-    const flexElement = element.querySelector('.dnb-flex-container')
-
-    expect(Array.from(flexElement.classList)).toEqual([
-      'dnb-space',
-      'dnb-flex-container',
-      'dnb-flex-container--direction-vertical',
-      'dnb-flex-container--justify-flex-start',
-      'dnb-flex-container--align-baseline',
-      'dnb-flex-container--spacing-x-small',
-      'dnb-flex-container--wrap',
-      'dnb-flex-container--divider-space',
-    ])
-
-    rerender(
-      <Radio.Group label="Label" label_direction="vertical">
-        <Radio />
-      </Radio.Group>
-    )
-
-    expect(Array.from(flexElement.classList)).toEqual([
-      'dnb-space',
-      'dnb-flex-container',
-      'dnb-flex-container--direction-vertical',
-      'dnb-flex-container--justify-flex-start',
-      'dnb-flex-container--align-baseline',
-      'dnb-flex-container--spacing-small',
-      'dnb-flex-container--wrap',
-      'dnb-flex-container--divider-space',
-    ])
-  })
-})
-
 describe('Radio ARIA', () => {
   it('should validate with ARIA rules for Radio', async () => {
     const Comp = render(<Radio {...props} />)
-    expect(
-      await axeComponent(Comp, {
-        rules: {
-          // NVDA fix
-          // because of the role="radio", we have to allow this
-          'aria-allowed-role': { enabled: false },
-        },
-      })
-    ).toHaveNoViolations()
-  })
-
-  it('should validate with ARIA rules for Radio.Group', async () => {
-    const Comp = render(
-      <Radio.Group
-        label="Label"
-        name="group"
-        id="group"
-        on_change={jest.fn()}
-      >
-        <Radio id="radio-1" label="Radio 1" value="first" />
-        <Radio id="radio-2" label="Radio 2" value="second" checked />
-      </Radio.Group>
-    )
     expect(
       await axeComponent(Comp, {
         rules: {
