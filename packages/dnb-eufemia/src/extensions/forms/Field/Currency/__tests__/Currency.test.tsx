3c64d4d147d5df9f4f9dbaa3523e8257cc54baeb
Tobias Høegh <tobias@tujo.no>
2023-12-01 15:22:41 +0100

    feat(Currency): add automatic locale and alignment support (#2956)
    [More
    info](https://dnb-it.slack.com/archives/CMXABCHEY/p1700752971296059)
    about this.

---
+++ packages/dnb-eufemia/src/extensions/forms/Field/Currency/__tests__/Currency.test.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import Currency from '../Currency'
+import { Provider } from '../../../../../shared'
+
+describe('Field.Currency', () => {
+  it('defaults to "kr" and use "NOK" when locale is en-GB', () => {
+    const { rerender } = render(
+      <Provider>
+        <Currency value={123} />
+      </Provider>
+    )
+
+    expect(document.querySelector('input').value).toBe('123 kr')
+
+    rerender(
+      <Provider locale="en-GB">
+        <Currency value={123} />
+      </Provider>
+    )
+
+    expect(document.querySelector('input').value).toBe('123 NOK')
+  })
+
+  it('placeholder should use correct currency format', () => {
+    const { rerender } = render(
+      <Provider>
+        <Currency />
+      </Provider>
+    )
+
+    expect(
+      document.querySelector('.dnb-input__placeholder').textContent
+    ).toBe('kr')
+
+    rerender(
+      <Provider locale="en-GB">
+        <Currency />
+      </Provider>
+    )
+
+    expect(
+      document.querySelector('.dnb-input__placeholder').textContent
+    ).toBe('NOK')
+  })
+
+  it('should allow rightAligned', () => {
+    render(<Currency value={123} rightAligned />)
+
+    const element = document.querySelector('.dnb-input')
+    expect(element.className).toContain('dnb-input__align--right')
+  })
+})
