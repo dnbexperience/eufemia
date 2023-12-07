2ccd4deda0dd0763efe3a5b44be13ad5a5ec15e7
Joakim Bjerknes <joakbjerk@gmail.com>
2023-12-07 14:24:17 +0100

    add tests for new prop

--- packages/dnb-eufemia/src/extensions/forms/Field/PostalCodeAndCity/__tests__/PostalCodeAndCity.test.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Field/PostalCodeAndCity/__tests__/PostalCodeAndCity.test.tsx
@@ -1,10 +1,47 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import PostalCodeAndCity, { Props } from '..'
+import { doc } from 'prettier'
+
+const props: Props = { postalCode: {}, city: {} }
 
 describe('Field.PostalCodeAndCity', () => {
   it('should render with props', () => {
-    const props: Props = { postalCode: {}, city: {} }
     render(<PostalCodeAndCity {...props} />)
+  })
+
+  it('should omit cities field when prop is set to true', () => {
+    const { rerender } = render(<PostalCodeAndCity {...props} />)
+
+    expect(
+      document.querySelector(
+        '.dnb-forms-field-postal-code-and-city__postal-code'
+      )
+    ).toBeInTheDocument()
+    expect(
+      document.querySelector('.dnb-forms-field-postal-code-and-city__city')
+    ).toBeInTheDocument()
+
+    rerender(<PostalCodeAndCity {...props} omitCityField />)
+
+    expect(
+      document.querySelector(
+        '.dnb-forms-field-postal-code-and-city__postal-code'
+      )
+    ).toBeInTheDocument()
+    expect(
+      document.querySelector('.dnb-forms-field-postal-code-and-city__city')
+    ).not.toBeInTheDocument()
+
+    rerender(<PostalCodeAndCity {...props} />)
+
+    expect(
+      document.querySelector(
+        '.dnb-forms-field-postal-code-and-city__postal-code'
+      )
+    ).toBeInTheDocument()
+    expect(
+      document.querySelector('.dnb-forms-field-postal-code-and-city__city')
+    ).toBeInTheDocument()
   })
 })
