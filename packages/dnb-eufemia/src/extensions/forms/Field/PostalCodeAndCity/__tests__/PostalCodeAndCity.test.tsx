dc1582f77b2b5dfa9415596e76d8018500af8630
Joakim Bjerknes <joakbjerk@gmail.com>
2023-12-08 12:33:27 +0100

    remove unused var

--- packages/dnb-eufemia/src/extensions/forms/Field/PostalCodeAndCity/__tests__/PostalCodeAndCity.test.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Field/PostalCodeAndCity/__tests__/PostalCodeAndCity.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import PostalCodeAndCity, { Props } from '..'
-import { doc } from 'prettier'
 
 const props: Props = { postalCode: {}, city: {} }
 
