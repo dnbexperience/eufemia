ca6dc370693f6b8e9f2d968f9befef6906a70e90
Anders <anderslangseth@gmail.com>
2023-12-12 09:29:54 +0100

    chore: defaults countries value to Prioritized (#3055)
    This defaults countries to the value `Prioritized`.
    
    Seems like there's one failing test in SelecCountry, `"should change
    locale" `🤔
    Will take a look at it later 🕐
    
    ---------
    
    Co-authored-by: Tobias Høegh <tobias@tujo.no>

--- packages/dnb-eufemia/src/extensions/forms/Field/PhoneNumber/PhoneNumber.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Field/PhoneNumber/PhoneNumber.tsx
@@ -92,7 +92,7 @@ function PhoneNumber(props: Props) {
     countryCodeLabel,
     label = sharedContext?.translation.Forms.phoneNumberLabel,
     numberMask,
-    countries: ccFilter,
+    countries: ccFilter = 'Prioritized',
     emptyValue,
     info,
     warning,
@@ -112,7 +112,7 @@ function PhoneNumber(props: Props) {
     updateValue,
     onCountryCodeChange,
     onNumberChange,
-    filterCountries = ccFilter
+    filterCountries = ccFilter !== 'Prioritized'
       ? makeCountryFilterSet(ccFilter)
       : undefined,
   } = useDataValue(preparedProps)
--- packages/dnb-eufemia/src/extensions/forms/Field/PhoneNumber/__tests__/PhoneNumber.test.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Field/PhoneNumber/__tests__/PhoneNumber.test.tsx
@@ -567,14 +567,37 @@ describe('Field.PhoneNumber', () => {
 
     const liElements = document.querySelectorAll('li:not([aria-hidden])')
     expect(liElements).toHaveLength(3)
-    expect(liElements[0].textContent).toBe('+45 Danmark')
-    expect(liElements[1].textContent).toBe('+47 Norge')
-    expect(liElements[2].textContent).toBe('+46 Sverige')
+    expect(liElements[0].textContent).toBe('+47 Norge')
+    expect(liElements[1].textContent).toBe('+46 Sverige')
+    expect(liElements[2].textContent).toBe('+45 Danmark')
 
     expect(
       document.querySelector('li.dnb-drawer-list__option--selected')
         .textContent
     ).toBe('+47 Norge')
+  })
+
+  it('should by default sort prioritized countries on top', () => {
+    render(<PhoneNumber />)
+
+    const codeElement: HTMLInputElement = document.querySelector(
+      '.dnb-forms-field-phone-number__country-code input'
+    )
+
+    // open
+    fireEvent.focus(codeElement)
+    fireEvent.keyDown(codeElement, {
+      key: 'Enter',
+      keyCode: 13,
+    })
+
+    const liElements = document.querySelectorAll('li:not([aria-hidden])')
+    expect(liElements.length).toBeGreaterThan(200)
+    expect(liElements[0].textContent).toContain('Norge')
+    expect(liElements[1].textContent).toContain('Sverige')
+    expect(liElements[2].textContent).toContain('Danmark')
+    expect(liElements[3].textContent).toContain('Finland')
+    expect(liElements[4].textContent).toContain('Afghanistan')
   })
 
   it('should show only Scandinavian countries', () => {
