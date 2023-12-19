d12850d44d9336e8ce621a09d591f3530b89bf4b
Tobias Høegh <tobias@tujo.no>
2023-12-19 12:30:33 +0100

    fix(PhoneNumber): make long labels wrap nicely (#3066)
    🔴 Both these cases will with this PR,
    <img width="367" alt="Screenshot 2023-12-12 at 20 34 11"
    src="https://github.com/dnbexperience/eufemia/assets/1501870/cca5c56f-761b-47ef-90ab-4677b1eb0e6f">
    <img width="372" alt="Screenshot 2023-12-12 at 20 31 56"
    src="https://github.com/dnbexperience/eufemia/assets/1501870/787af181-8f39-41d1-9393-3a05de97b0cd">
    
    🟢 result in this:
    <img width="393" alt="Screenshot 2023-12-12 at 20 07 36"
    src="https://github.com/dnbexperience/eufemia/assets/1501870/42a2fee8-3156-4c23-999f-2024f07f2de8">
    <img width="367" alt="Screenshot 2023-12-12 at 20 36 39"
    src="https://github.com/dnbexperience/eufemia/assets/1501870/ae30966f-0b0f-4907-a8c4-eff91721f387">

--- packages/dnb-eufemia/src/extensions/forms/Field/PhoneNumber/__tests__/PhoneNumber.screenshot.test.ts
+++ packages/dnb-eufemia/src/extensions/forms/Field/PhoneNumber/__tests__/PhoneNumber.screenshot.test.ts
@@ -3,10 +3,12 @@ import {
   setupPageScreenshot,
 } from '../../../../../core/jest/jestSetupScreenshots'
 
+const url = '/uilib/extensions/forms/feature-fields/PhoneNumber/demos'
+
 describe.each(['ui'])('PhoneNumber for %s', (themeName) => {
   setupPageScreenshot({
     themeName,
-    url: '/uilib/extensions/forms/feature-fields/PhoneNumber/demos',
+    url,
   })
 
   it('have to match with a label', async () => {
@@ -20,7 +22,7 @@ describe.each(['ui'])('PhoneNumber for %s', (themeName) => {
 describe('PhoneNumber', () => {
   it('have to match medium screen', async () => {
     const screenshot = await makeScreenshot({
-      url: '/uilib/extensions/forms/feature-fields/PhoneNumber/demos',
+      url,
       pageViewport: {
         width: 600,
       },
@@ -31,11 +33,19 @@ describe('PhoneNumber', () => {
 
   it('have to match small screen', async () => {
     const screenshot = await makeScreenshot({
-      url: '/uilib/extensions/forms/feature-fields/PhoneNumber/demos',
+      url,
       pageViewport: {
         width: 300,
       },
       selector: '[data-visual-test="phone-number-label"]',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('have to match long label', async () => {
+    const screenshot = await makeScreenshot({
+      url,
+      selector: '[data-visual-test="phone-number-long-label"]',
     })
     expect(screenshot).toMatchImageSnapshot()
   })
