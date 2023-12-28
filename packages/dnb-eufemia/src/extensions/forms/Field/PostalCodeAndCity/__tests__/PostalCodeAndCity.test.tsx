f68406a72f6b13a69dcd36a00bc1b17c6e7d5e3f
Joakim Bjerknes <joakbjerk@gmail.com>
2023-12-28 10:45:08 +0100

    add numeric input mode to postal code

--- packages/dnb-eufemia/src/extensions/forms/Field/PostalCodeAndCity/__tests__/PostalCodeAndCity.test.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Field/PostalCodeAndCity/__tests__/PostalCodeAndCity.test.tsx
@@ -22,5 +22,20 @@ describe('Field.PostalCodeAndCity', () => {
     await userEvent.type(postalCodeInput, '123456')
 
     expect(postalCodeInput).toHaveValue('1234')
+  
+    it('postal could should have numeric input mode', () => {
+    render(<PostalCodeAndCity {...props} />)
+
+    const postalCodeInput = document.querySelector(
+      '.dnb-forms-field-postal-code-and-city__postal-code-input .dnb-input__input'
+    )
+    const cityInput = document.querySelector(
+      '.dnb-forms-field-postal-code-and-city__city .dnb-input__input'
+    )
+
+    expect(postalCodeInput).toHaveAttribute('inputmode')
+    expect(postalCodeInput.getAttribute('inputmode')).toBe('numeric')
+
+    expect(cityInput).not.toHaveAttribute('inputmode')
   })
 })
