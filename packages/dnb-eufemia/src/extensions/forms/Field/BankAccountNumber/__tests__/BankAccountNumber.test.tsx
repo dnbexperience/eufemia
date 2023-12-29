d587133e0c0c96f572de938f1a144c765c01073b
Joakim Bjerknes <joakbjerk@gmail.com>
2023-12-29 08:54:43 +0100

    correct component name in test label
    Co-authored-by: Tobias Høegh <tobias@tujo.no>

--- packages/dnb-eufemia/src/extensions/forms/Field/BankAccountNumber/__tests__/BankAccountNumber.test.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Field/BankAccountNumber/__tests__/BankAccountNumber.test.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import BankAccountNumber, { Props } from '..'
 
-describe('Field.ArraySelection', () => {
+describe('Field.BankAccountNumber', () => {
   it('should render with props', () => {
     const props: Props = {}
 
