a9e16970612afdf20dcc6522b3b60f6813af044a
Sundfjord <yngve.sundfjord@miles.no>
2024-01-05 12:50:32 +0100

    feat(Field.Number): add step control functionality (#3140)
    Adds a new prop `showStepControls` to the Number component. When `true`,
    this prop enables control buttons that adhere to a `step` property that
    is also added, as well as taking min/max values into account.
    
    This variant of the component is Sbanken-ready, and a number of tests
    and screenshot tests has been implemented in relation to it.

---
+++ packages/dnb-eufemia/src/extensions/forms/Field/Number/__tests__/Number.screenshot.test.ts
@@ -0,0 +1,74 @@
+/**
+ * Screenshot Test
+ * This file will not run on "test:staged" because we don't require any related files
+ */
+
+import {
+  makeScreenshot,
+  setupPageScreenshot,
+} from '../../../../../core/jest/jestSetupScreenshots'
+
+describe.each(['ui', 'sbanken'])('Number field for %s', (themeName) => {
+  setupPageScreenshot({
+    themeName,
+    url: '/uilib/extensions/forms/base-fields/Number',
+  })
+
+  describe('with step control buttons', () => {
+    it('matches the default state', async () => {
+      const screenshot = await makeScreenshot({
+        selector:
+          '[data-visual-test="number-input-step-controls"] .dnb-forms-field-number',
+      })
+      expect(screenshot).toMatchImageSnapshot()
+    })
+
+    it('matches the hover state', async () => {
+      const screenshot = await makeScreenshot({
+        selector:
+          '[data-visual-test="number-input-step-controls"] .dnb-forms-field-number',
+        simulate: 'hover',
+      })
+      expect(screenshot).toMatchImageSnapshot()
+    })
+
+    it('matches the focus state', async () => {
+      const screenshot = await makeScreenshot({
+        selector:
+          '[data-visual-test="number-input-step-controls"] .dnb-forms-field-number',
+        simulateSelector:
+          '[data-visual-test="number-input-step-controls"] .dnb-input',
+        simulate: 'focusclick',
+        waitAfterSimulate: 250,
+      })
+      expect(screenshot).toMatchImageSnapshot()
+    })
+
+    it('matches the disabled state', async () => {
+      const screenshot = await makeScreenshot({
+        selector:
+          '[data-visual-test="number-input-step-controls-disabled"] .dnb-forms-field-number',
+      })
+      expect(screenshot).toMatchImageSnapshot()
+    })
+
+    it('matches the control button hover state', async () => {
+      const screenshot = await makeScreenshot({
+        selector:
+          '[data-visual-test="number-input-step-controls"] .dnb-forms-field-number',
+        simulateSelector:
+          '[data-visual-test="number-input-step-controls"] .dnb-button--control-before',
+        simulate: 'hover',
+      })
+      expect(screenshot).toMatchImageSnapshot()
+    })
+
+    it('matches the error state', async () => {
+      const screenshot = await makeScreenshot({
+        selector:
+          '[data-visual-test="number-input-step-controls-error"] .dnb-forms-field-number',
+      })
+      expect(screenshot).toMatchImageSnapshot()
+    })
+  })
+})
