0d7ae140df5f64ab47b40b4a2dbf47c03cc6b076
Tobias Høegh <tobias@tujo.no>
2024-01-06 09:23:28 +0100

    Add more tests and currency example

--- packages/dnb-eufemia/src/extensions/forms/Field/Currency/Currency.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Field/Currency/Currency.tsx
@@ -4,13 +4,8 @@ import { Context } from '../../../../shared'
 import { getCurrencySymbol } from '../../../../components/number-format/NumberUtils'
 import { CURRENCY } from '../../../../shared/defaults'
 import NumberComponent, { Props as NumberProps } from '../Number'
-import type { FieldProps, FieldHelpProps } from '../../types'
 
-export type Props = FieldHelpProps &
-  FieldProps<number, undefined> & {
-    currency?: NumberProps['currency']
-    align?: NumberProps['align']
-  }
+export type Props = NumberProps
 
 function Currency(props: Props) {
   const context = React.useContext(Context)
