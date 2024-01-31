69939e3ea188d2cfe7c03454639a6493d7e9138b
Joakim Bjerknes <joakbjerk@gmail.com>
2024-01-31 02:24:17 +0100

    add drawer example

--- packages/dnb-design-system-portal/src/docs/uilib/extensions/forms/extended-features/StepsLayout/StepsLayout/Examples.tsx
+++ packages/dnb-design-system-portal/src/docs/uilib/extensions/forms/extended-features/StepsLayout/StepsLayout/Examples.tsx
@@ -42,3 +42,44 @@ export const Default = () => {
     </ComponentBox>
   )
 }
+
+export const Drawer = () => {
+  return (
+    <ComponentBox
+      scope={{ StepsLayout }}
+      data-visual-test="steps-layout-drawer"
+    >
+      <StepsLayout variant="drawer">
+        <StepsLayout.Step title="Step 1">
+          <Form.MainHeading>Heading</Form.MainHeading>
+          <Card>
+            <P>Contents</P>
+          </Card>
+          <Card>
+            <P>Contents</P>
+          </Card>
+          <StepsLayout.NextButton />
+        </StepsLayout.Step>
+
+        <StepsLayout.Step title="Step 2">
+          <Form.MainHeading>Heading</Form.MainHeading>
+          <Card>
+            <P>Contents</P>
+          </Card>
+          <Form.ButtonRow>
+            <StepsLayout.PreviousButton />
+            <StepsLayout.NextButton />
+          </Form.ButtonRow>
+        </StepsLayout.Step>
+
+        <StepsLayout.Step title="Summary">
+          <Form.MainHeading>Summary</Form.MainHeading>
+          <Card>
+            <P>Contents</P>
+          </Card>
+          <StepsLayout.PreviousButton />
+        </StepsLayout.Step>
+      </StepsLayout>
+    </ComponentBox>
+  )
+}
