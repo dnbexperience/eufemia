7f2dfaeeaea7c5ef292c70cce70ed60004acf49a
Joakim Bjerknes <joakbjerk@gmail.com>
2024-02-08 16:22:27 +0100

    remove visual test id
    Co-authored-by: Tobias Høegh <tobias@tujo.no>

--- packages/dnb-design-system-portal/src/docs/uilib/extensions/forms/feature-fields/Password/Examples.tsx
+++ packages/dnb-design-system-portal/src/docs/uilib/extensions/forms/feature-fields/Password/Examples.tsx
@@ -43,7 +43,7 @@ export const Label = () => {
 
 export const LabelAndValue = () => {
   return (
-    <ComponentBox data-visual-test="phone-number-label">
+    <ComponentBox>
       <Field.Password
         label="Label text"
         value="password123"
