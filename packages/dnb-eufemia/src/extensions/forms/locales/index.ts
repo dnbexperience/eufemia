69218ab245a494a1292aa41f306d6289317aabbd
Joakim Bjerknes <joakbjerk@gmail.com>
2024-02-22 10:35:48 +0100

    update translation types

--- packages/dnb-eufemia/src/extensions/forms/locales/index.ts
+++ packages/dnb-eufemia/src/extensions/forms/locales/index.ts
@@ -2,6 +2,7 @@
 import nbNO from './nb-NO'
 import enGB from './en-GB'
 
-export type FormTranslations = (typeof nbNO)['nb-NO']['Forms']
+export type FormTranslations = (typeof nbNO)['nb-NO'] &
+  (typeof enGB)['en-GB']
 
-export default { ...nbNO, ...enGB } as const
+export default { ...nbNO, ...enGB }
