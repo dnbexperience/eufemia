19de79ee8dc913f99985ce8e02a820ba76fd57bb
Joakim Bjerknes <joakbjerk@gmail.com>
2024-03-04 08:39:26 +0100

    Move form translations back to SharedContext

--- packages/dnb-eufemia/src/extensions/forms/locales/index.ts
+++
@@ -1,8 +0,0 @@
-// import default locales
-import nbNO from './nb-NO'
-import enGB from './en-GB'
-
-export type FormTranslations = (typeof nbNO)['nb-NO'] &
-  (typeof enGB)['en-GB']
-
-export default { ...nbNO, ...enGB }
