0fc68d12f5f5b6f2fa01480314947b06245acd6a
Tobias Høegh <tobias@tujo.no>
2024-03-13 19:05:33 +0100

    chore(isAsync): add support for jest.fn

--- packages/dnb-eufemia/src/shared/helpers/__tests__/isAsync.test.ts
+++ packages/dnb-eufemia/src/shared/helpers/__tests__/isAsync.test.ts
@@ -12,6 +12,17 @@ describe('isAsync', () => {
     expect(isAsync(IAmAsync)).toBeTruthy()
   })
 
+  it('should return correct result based with jest mock', () => {
+    expect(isAsync(jest.fn(() => null))).toBeFalsy()
+    expect(isAsync(jest.fn(async () => null))).toBeTruthy()
+
+    const IAmSync = jest.fn(() => null)
+    const IAmAsync = jest.fn(async () => null)
+
+    expect(isAsync(IAmSync)).toBeFalsy()
+    expect(isAsync(IAmAsync)).toBeTruthy()
+  })
+
   it('should return correct result based normal functions', () => {
     expect(
       isAsync(function () {
