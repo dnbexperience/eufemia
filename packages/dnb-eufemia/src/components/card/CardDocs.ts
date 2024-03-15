0a419cc085921c20a18efab6e153b004d5d0fa57
Tobias Høegh <tobias@tujo.no>
2024-03-15 09:03:59 +0100

    feat(Card): add `filled`, `responsive` and `title` props to support a Table and a ScrollView as a child (#3380)
    This PR adds a couple of extra properties to the Card component:
    
    - `filled`
    - `responsive`
    - `title`
    
    so its possible to get to the above look:
    
    <img width="435" alt="Screenshot 2024-03-13 at 15 50 55"
    src="https://github.com/dnbexperience/eufemia/assets/1501870/623581fc-884b-4b00-bf28-55207ee06ddd">
    
    When the ScrollView kicks in:
    
    <img width="384" alt="Screenshot 2024-03-13 at 15 51 25"
    src="https://github.com/dnbexperience/eufemia/assets/1501870/feb73276-76d6-428f-ac1d-ff6618429104">
    
    Preview:
    https://eufemia-git-feat-card-with-table-eufemia.vercel.app/uilib/components/card/demos/#with-table

---
+++ packages/dnb-eufemia/src/components/card/CardDocs.ts
@@ -0,0 +1,59 @@
+import { PropertiesTableProps } from '../../shared/types'
+
+export const CardProperties: PropertiesTableProps = {
+  stack: {
+    doc: 'True to stack the sub components with lines between. The `spacing` will default to `medium`.',
+    type: 'boolean',
+    status: 'optional',
+  },
+  direction: {
+    doc: 'Defaults to `vertical`.',
+    type: 'string',
+    status: 'optional',
+  },
+  alignSelf: {
+    doc: 'Defaults to `stretch`.',
+    type: 'string',
+    status: 'optional',
+  },
+  title: {
+    doc: 'Define a title that appears on top of the Card.',
+    type: 'React.Node',
+    status: 'optional',
+  },
+  responsive: {
+    doc: 'Define if the card should behave responsive. Defaults to `true`.',
+    type: 'boolean',
+    status: 'optional',
+  },
+  filled: {
+    doc: 'Define if the Card should get the same background color as the outline border.',
+    type: 'boolean',
+    status: 'optional',
+  },
+  element: {
+    doc: 'Define the type of element. Defaults to `section`.',
+    type: 'React.Element',
+    status: 'optional',
+  },
+  children: {
+    doc: 'Contents.',
+    type: 'React.Node',
+    status: 'required',
+  },
+  '[Flex.Container](/uilib/layout/flex/container)': {
+    doc: 'Flex.Container properties.',
+    type: 'Various',
+    status: 'optional',
+  },
+  '[Flex.Item](/uilib/layout/flex/item)': {
+    doc: 'Flex.Item properties.',
+    type: 'Various',
+    status: 'optional',
+  },
+  '[Space](/uilib/layout/space/properties)': {
+    doc: 'Spacing properties like `top` or `bottom` are supported.',
+    type: ['string', 'object'],
+    status: 'optional',
+  },
+}
