080bbee2e5cf58860b389423712544895d6da0d4
Tobias Høegh <tobias@tujo.no>
2024-04-03 21:13:27 +0200

    fix(forms): add pattern requirement for city in PostalCodeAndCity (#3431)

--- packages/dnb-eufemia/src/extensions/forms/Field/PostalCodeAndCity/__tests__/PostalCodeAndCity.test.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Field/PostalCodeAndCity/__tests__/PostalCodeAndCity.test.tsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import { axeComponent } from '../../../../../core/jest/jestSetup'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { Props } from '..'
 import { Field } from '../../..'
 
-const props: Props = { postalCode: {}, city: {} }
+import nbNO from '../../../constants/locales/nb-NO'
+const nb = nbNO['nb-NO']
 
 describe('Field.PostalCodeAndCity', () => {
   it('should render with props', () => {
-    render(<Field.PostalCodeAndCity {...props} />)
+    render(<Field.PostalCodeAndCity />)
     expect(screen.getByLabelText('Postnr.')).toBeInTheDocument()
     expect(screen.getByLabelText('Sted')).toBeInTheDocument()
   })
 
   it('postal code should only allow four numbers', async () => {
-    render(<Field.PostalCodeAndCity {...props} />)
+    render(<Field.PostalCodeAndCity />)
 
     const postalCodeInput = document.querySelector(
       '.dnb-forms-field-postal-code-and-city__postal-code .dnb-input__input'
@@ -28,8 +28,8 @@ describe('Field.PostalCodeAndCity', () => {
     expect(postalCodeInput).toHaveValue('1234')
   })
 
-  it('postal could should have numeric input mode', () => {
-    render(<Field.PostalCodeAndCity {...props} />)
+  it('postalCode should have numeric "inputmode"', () => {
+    render(<Field.PostalCodeAndCity />)
 
     const postalCodeInput = document.querySelector(
       '.dnb-forms-field-postal-code-and-city__postal-code-input .dnb-input__input'
@@ -39,7 +39,7 @@ describe('Field.PostalCodeAndCity', () => {
   })
 
   it('should show single error message and have error modifier on two inputs', () => {
-    const { rerender } = render(<Field.PostalCodeAndCity {...props} />)
+    const { rerender } = render(<Field.PostalCodeAndCity />)
 
     expect(screen.queryByRole('alert')).not.toBeInTheDocument()
 
@@ -48,10 +48,7 @@ describe('Field.PostalCodeAndCity', () => {
     ).toHaveLength(0)
 
     rerender(
-      <Field.PostalCodeAndCity
-        {...props}
-        error={new Error('Single error message')}
-      />
+      <Field.PostalCodeAndCity error={new Error('Single error message')} />
     )
 
     expect(screen.queryAllByRole('alert')).toHaveLength(1)
@@ -66,8 +63,69 @@ describe('Field.PostalCodeAndCity', () => {
     ).toHaveLength(2)
   })
 
+  it('should show error message on invalid values', async () => {
+    const { rerender } = render(<Field.PostalCodeAndCity />)
+
+    expect(screen.queryByRole('alert')).not.toBeInTheDocument()
+
+    rerender(
+      <Field.PostalCodeAndCity
+        postalCode={{
+          required: true,
+          validateInitially: true,
+        }}
+        city={{
+          required: true,
+          validateInitially: true,
+        }}
+      />
+    )
+
+    const [code, city] = Array.from(document.querySelectorAll('input'))
+
+    await userEvent.type(code, 'x{Backspace}')
+    expect(screen.queryByRole('alert')).toHaveTextContent(
+      nb.PostalCode.errorRequired
+    )
+    expect(code).toHaveValue('')
+
+    await userEvent.type(city, 'x{Backspace}')
+    expect(screen.queryByRole('alert')).toHaveTextContent(
+      nb.City.errorRequired
+    )
+    expect(city).toHaveValue('')
+
+    await userEvent.type(code, '123')
+    expect(screen.queryByRole('alert')).toHaveTextContent(
+      nb.PostalCode.errorPattern
+    )
+    expect(code).toHaveValue('123​')
+
+    await userEvent.type(city, '456')
+    expect(screen.queryByRole('alert')).toHaveTextContent(
+      nb.City.errorPattern
+    )
+    expect(city).toHaveValue('456')
+
+    await userEvent.type(city, '{Backspace>3}æ - ø - å')
+    fireEvent.blur(city)
+
+    expect(city).toHaveValue('æ - ø - å')
+  })
+
+  it('should trim the value on blur', async () => {
+    render(<Field.PostalCodeAndCity />)
+
+    const [, city] = Array.from(document.querySelectorAll('input'))
+
+    await userEvent.type(city, ' æ - ø - å ')
+    fireEvent.blur(city)
+
+    expect(city).toHaveValue('æ - ø - å')
+  })
+
   it('should have autofill attributes', () => {
-    render(<Field.PostalCodeAndCity {...props} />)
+    render(<Field.PostalCodeAndCity />)
 
     const [code, city] = Array.from(document.querySelectorAll('input'))
 
