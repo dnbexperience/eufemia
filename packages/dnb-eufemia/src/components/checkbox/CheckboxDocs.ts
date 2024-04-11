c849439b11a0b4e8ecb025a2aa8e55ec0d06ecf2
Ådne Rosenvinge <41082005+AadneRo@users.noreply.github.com>
2024-04-11 11:07:24 +0200

    docs(Checkbox): update to typed docs (#3439)
    Update to typed docs

---
+++ packages/dnb-eufemia/src/components/checkbox/CheckboxDocs.ts
@@ -0,0 +1,82 @@
+import { PropertiesTableProps } from '../../shared/types'
+
+export const CheckboxProperties: PropertiesTableProps = {
+  checked: {
+    doc: 'Determine whether the checkbox is checked or not. The default is `false`.',
+    type: 'boolean',
+    status: 'optional',
+  },
+  title: {
+    doc: 'The `title` of the input - describing it a bit further for accessibility reasons.',
+    type: 'ReactNode',
+    status: 'optional',
+  },
+  label: {
+    doc: 'Use either the `label` property or provide a custom one.',
+    type: 'ReactNode',
+    status: 'optional',
+  },
+  label_position: {
+    doc: 'Defines the position of the `label`. Use either `left` or `right`. Defaults to `right`.',
+    type: 'string',
+    status: 'optional',
+  },
+  label_sr_only: {
+    doc: 'Use `true` to make the label only readable by screen readers.',
+    type: 'string',
+    status: 'optional',
+  },
+  size: {
+    doc: 'The size of the checkbox. For now there is "medium" (default) and "large".',
+    type: ['string', 'number'],
+    status: 'optional',
+  },
+  status: {
+    doc: 'Text with a status message. The style defaults to an error message. You can use `true` to only get the status color, without a message.',
+    type: 'string',
+    status: 'optional',
+  },
+  status_state: {
+    doc: 'Defines the state of the status. Currently, there are two statuses `[error, info]`. Defaults to `error`.',
+    type: ['error', 'info'],
+    status: 'optional',
+  },
+  status_props: {
+    doc: 'Use an object to define additional FormStatus properties. See [FormStatus](/uilib/components/form-status/properties/)',
+    type: 'FormStatusProps',
+    status: 'optional',
+  },
+  globalStatus: {
+    doc: 'The [configuration](/uilib/components/global-status/properties/#configuration-object) used for the target [GlobalStatus](/uilib/components/global-status)',
+    type: 'object',
+    status: 'optional',
+  },
+  skeleton: {
+    doc: 'If set to `true`, an overlaying skeleton with animation will be shown.',
+    type: 'boolean',
+    status: 'optional',
+  },
+  suffix: {
+    doc: 'Text describing the content of the Checkbox more than the label. You can also send in a React component, so it gets wrapped inside the Checkbox component.',
+    type: 'ReactNode',
+    status: 'optional',
+  },
+  innerRef: {
+    doc: 'By providing a React.ref we can get the internally used input element (DOM). E.g. `innerRef={myRef}` by using `React.createRef()` or `React.useRef()`.',
+    type: 'string',
+    status: 'optional',
+  },
+  '[Space](/uilib/layout/space/properties)': {
+    doc: 'Spacing properties like `top` or `bottom` are supported.',
+    type: ['string', 'object'],
+    status: 'optional',
+  },
+}
+
+export const CheckboxEvents: PropertiesTableProps = {
+  on_change: {
+    doc: 'Will be called on state changes made by the user.',
+    type: '() => {checked: boolean; event: ChangeEvent}',
+    status: 'optional',
+  },
+}
