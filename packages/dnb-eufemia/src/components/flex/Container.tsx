59c3145a8ec047e4ccba8149f903d2f384082932
Joakim Bjerknes <joakbjerk@gmail.com>
2024-04-25 10:41:13 +0200

    add support for spacing prop object in nested Flex.Containers

--- packages/dnb-eufemia/src/components/flex/Container.tsx
+++ packages/dnb-eufemia/src/components/flex/Container.tsx
@@ -190,7 +190,7 @@ function FlexContainer(props: Props) {
 
     return renderWithSpacing(child, {
       key: child?.['key'] || `element-${i}`,
-      space,
+      space: child?.['props']?.space ?? space,
     })
   })
 
