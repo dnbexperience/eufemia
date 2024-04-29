52a00c9e23e493eab61e9821196b3a33e865b80e
Joakim Bjerknes <joakbjerk@gmail.com>
2024-04-29 08:51:56 +0200

    make ReactElement work as as selected_value

--- packages/dnb-eufemia/src/shared/component-helper.js
+++ packages/dnb-eufemia/src/shared/component-helper.js
@@ -644,10 +644,17 @@ export const convertJsxToString = (elements, separator = undefined) => {
 
   const process = (word) => {
     if (React.isValidElement(word)) {
-      if (typeof word.props.children === 'string') {
-        word = word.props.children.trim()
-      } else if (Array.isArray(word.props.children)) {
-        word = word.props.children.reduce((acc, word) => {
+      let children = word.props.children
+
+      // fixes issue in autocomplete where `selected_values` of `ReactElement` would not renders as string when selected
+      if (!children && word.type()?.props?.children) {
+        children = word.type().props.children
+      }
+
+      if (typeof children === 'string') {
+        word = children.trim()
+      } else if (Array.isArray(children)) {
+        word = children.reduce((acc, word) => {
           if (typeof word !== 'string') {
             word = process(word)
           }
