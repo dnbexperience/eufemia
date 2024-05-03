a47018cf1336f5b3d497caae85f53dc1a0ae080a
Joakim Bjerknes <joakbjerk@gmail.com>
2024-05-03 13:27:20 +0200

    update shortcuts type

--- packages/dnb-eufemia/src/components/date-picker/DatePickerAddon.tsx
+++ packages/dnb-eufemia/src/components/date-picker/DatePickerAddon.tsx
@@ -20,7 +20,7 @@ export type DatePickerAddonProps = React.HTMLProps<HTMLElement> & {
   /**
    * Gives you the possibility to set predefined dates and date ranges so the user can select these by one click. Define either a JSON or an object with the defined shortcuts. More info is below.
    */
-  shortcuts?: Array<DatePickerShortcut>
+  shortcuts?: Array<DatePickerShortcut> | string
   renderElement?: React.ReactNode
 }
 
