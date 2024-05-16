62f35f9ff2c4a276d6c4517e52345b65b2ab8ed7
Tobias Høegh <tobias@tujo.no>
2024-05-16 15:16:05 +0200

    fix(forms): add schema `required` support to Field.Composition

--- packages/dnb-eufemia/src/extensions/forms/Field/Composition/stories/Composition.stories.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Field/Composition/stories/Composition.stories.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Field } from '../../../Forms'
+import { Field, Form, JSONSchema } from '../../../Forms'
+import { Flex } from '../../../../../components'
 
 export default {
   title: 'Eufemia/Extensions/Forms/Composition',
@@ -11,5 +12,44 @@ export function Composition() {
       <Field.String label="A" />
       <Field.String label="B" />
     </Field.Composition>
+  )
+}
+
+export function Schema() {
+  const schema: JSONSchema = {
+    type: 'object',
+    required: ['first', 'last'],
+    properties: {
+      first: {
+        type: 'string',
+      },
+      last: {
+        type: 'string',
+      },
+    },
+  }
+
+  return (
+    <Form.Handler schema={schema}>
+      <Flex.Stack>
+        <Field.Composition width="large">
+          <Field.String
+            label="First"
+            path="/first"
+            // validateInitially
+            // error={new Error('First')}
+          />
+          <Field.String
+            label="Last"
+            path="/last"
+            // validateInitially
+            // required
+            // error={new Error('Last')}
+          />
+        </Field.Composition>
+
+        <Form.SubmitButton />
+      </Flex.Stack>
+    </Form.Handler>
   )
 }
