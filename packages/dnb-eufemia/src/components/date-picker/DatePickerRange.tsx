c8c746aeaaf72861f43226f800ee2d15bc2f5257
Joakim Bjerknes <joakbjerk@gmail.com>
2024-05-29 14:22:57 +0200

    refactor DatePickerRange

--- packages/dnb-eufemia/src/components/date-picker/DatePickerRange.tsx
+++ packages/dnb-eufemia/src/components/date-picker/DatePickerRange.tsx
@@ -3,18 +3,21 @@
  *
  */
 
-import React, { useContext } from 'react'
+import React, { useCallback, useContext } from 'react'
 
 // date-fns
 import addMonths from 'date-fns/addMonths'
 import subMonths from 'date-fns/subMonths'
 
 import DatePickerCalendar, {
+  CalendarButtonProps,
+  CalendarNavigationEvent,
   CalendarSelectEvent,
   DatePickerCalendarProps,
 } from './DatePickerCalendar'
 import DatePickerContext from './DatePickerContext'
 import { CalendarView } from './hooks/useViews'
+import { Dates } from './hooks/useDates'
 
 export type DatePickerRangeViews = number | Record<string, unknown>[]
 
@@ -30,91 +33,82 @@ export type DatePickerRangeProps = React.HTMLProps<HTMLElement> &
     onChange?: (event: CalendarSelectEvent) => void
   }
 
+const monthHandlers: {
+  [key in CalendarButtonProps['type']]: typeof subMonths
+} = {
+  prev: subMonths,
+  next: addMonths,
+}
+
 function DatePickerRange(props: DatePickerRangeProps) {
-  const context = useContext(DatePickerContext)
+  // Destructured to prevent useCallback from updating on all prop or context changes
+  const { onChange, isRange, isLink } = props
+  const {
+    views,
+    setViews,
+    updateDates,
+    callOnChangeHandler,
+    startDate,
+    endDate,
+  } = useContext(DatePickerContext)
+
+  const onNav = useCallback(
+    ({ nr, type }: CalendarNavigationEvent) => {
+      const updatedViews = views.map((view) => {
+        if (view.nr === nr) {
+          const month = monthHandlers[type](view.month, 1)
+
+          return { ...view, month }
+        }
+        if (isLink && view.nr === 1) {
+          const month = monthHandlers[type](view.month, 1)
+
+          return { ...view, month }
+        }
+        return view
+      })
+      setViews(updatedViews)
+    },
+    [views, setViews, isLink]
+  )
+
+  const onSelect = useCallback(
+    (event: CalendarSelectEvent) => {
+      callOnChangeHandler(event)
+
+      onChange?.({
+        hidePicker: !isRange,
+        startDate: startDate,
+        endDate: endDate,
+        ...event,
+      })
+    },
+    [endDate, startDate, isRange, onChange, callOnChangeHandler]
+  )
+
+  const onHover = useCallback(
+    (hoverDate: Dates['hoverDate']) => {
+      updateDates({ hoverDate })
+    },
+    [updateDates]
+  )
 
   return (
     <div className="dnb-date-picker__views">
-      {context.views.map((calendar, i) => (
+      {views.map((calendar, i) => (
         <DatePickerCalendar
           key={calendar.nr}
           {...calendar}
           {...props}
           id={`${props.id}-${i}-`}
-          onSelect={onSelectHandler}
+          onSelect={onSelect}
           onHover={onHover}
-          onPrev={onPrev}
-          onNext={onNext}
+          onPrev={onNav}
+          onNext={onNav}
         />
       ))}
     </div>
   )
-
-  function onSelectHandler(event: CalendarSelectEvent) {
-    context.callOnChangeHandler(event)
-
-    props.onChange?.({
-      hidePicker: !props.isRange,
-      startDate: context.startDate,
-      endDate: context.endDate,
-      ...event,
-    })
-  }
-
-  // TOTYPE
-  function setNavState(views: CalendarView) {
-    context.setViews([views])
-  }
-  // Combine onNext and onPrev and put in useCallback
-  function onNext({ nr }: CalendarView) {
-    const views = context.views.map((c) => {
-      if (c.nr === nr) {
-        const month = addMonths(c.month, 1)
-        setNavState({
-          nr,
-          [`${nr === 0 ? 'start' : 'end'}Month`]: month,
-        })
-        return { ...c, month }
-      }
-      if (props.isLink && c.nr === 1) {
-        const month = addMonths(c.month, 1)
-        setNavState({
-          nr,
-          [`${nr !== 0 ? 'start' : 'end'}Month`]: month,
-        })
-        return { ...c, month }
-      }
-      return c
-    })
-    context.setViews(views)
-  }
-
-  function onPrev({ nr }: CalendarView) {
-    const views = context.views.map((c) => {
-      if (c.nr === nr) {
-        const month = subMonths(c.month, 1)
-        setNavState({
-          nr,
-          [`${nr === 0 ? 'start' : 'end'}Month`]: month,
-        })
-        return { ...c, month }
-      }
-      if (props.isLink && c.nr === 1) {
-        const month = subMonths(c.month, 1)
-        setNavState({
-          nr,
-          [`${nr !== 0 ? 'start' : 'end'}Month`]: month,
-        })
-        return { ...c, month }
-      }
-      return c
-    })
-    context.setViews(views)
-  }
-  // TOTYPE
-  function onHover(hoverDate) {
-    context.updateDates({ hoverDate })
-  }
 }
 
 export default DatePickerRange
