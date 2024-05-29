c9cae372a51fc8ef387626a50560e4f778b32ef1
Tobias Høegh <tobias@tujo.no>
2024-05-29 14:51:06 +0200

    feat(forms): add `trueText` and `falseText` to Value.Boolean (#3624)

--- packages/dnb-eufemia/src/extensions/forms/Value/Boolean/Boolean.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Value/Boolean/Boolean.tsx
@@ -5,11 +5,15 @@ import { useValueProps } from '../../hooks'
 import { ValueProps } from '../../types'
 import useTranslation from '../../hooks/useTranslation'
 
-export type Props = ValueProps<boolean>
+export type Props = ValueProps<boolean> & {
+  trueText?: React.ReactNode
+  falseText?: React.ReactNode
+}
 
 function BooleanComponent(props: Props) {
   const translations = useTranslation().BooleanField
-  const { value, className, ...rest } = useValueProps(props)
+  const { value, className, trueText, falseText, ...rest } =
+    useValueProps(props)
 
   return (
     <ValueBlock
@@ -18,8 +22,8 @@ function BooleanComponent(props: Props) {
     >
       {value === true || value === false
         ? value === true
-          ? translations.yes
-          : translations.no
+          ? trueText ?? translations.yes
+          : falseText ?? translations.no
         : null}
     </ValueBlock>
   )
