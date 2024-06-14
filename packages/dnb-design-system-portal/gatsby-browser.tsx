7fd0a4eadd921c8a58701cc911d496f0a79200f4
-l <anderslangseth@gmail.com>
2024-06-14 13:00:43 +0200

    chore: replaceHydrationFunction only when testing

--- packages/dnb-design-system-portal/gatsby-browser.tsx
+++ packages/dnb-design-system-portal/gatsby-browser.tsx
@@ -22,9 +22,11 @@ function setIsTest(location) {
 export const replaceHydrateFunction = () => {
   // Added to solve the following errors, which prevented us from running screenshot tests
   // https://github.com/gatsbyjs/gatsby/discussions/36232
-  return (element: React.ReactElement, container: HTMLElement) => {
-    const root = ReactDOM.createRoot(container)
-    root.render(element)
+  if (globalThis.IS_TEST) {
+    return (element: React.ReactElement, container: HTMLElement) => {
+      const root = ReactDOM.createRoot(container)
+      root.render(element)
+    }
   }
 }
 
