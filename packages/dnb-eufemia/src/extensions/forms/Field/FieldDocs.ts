2b5f55ea50f3dd11b29264cb45ea47a291aae737
Snorre Kim <snorre.kim@dnb.no>
2024-08-13 16:01:28 +0200

    docs(Field.PhoneNumber): document onChange event args (#3807)
    * added portal documentation for second argument of `onChange(value,
    additionalArgs)` event.
    * added `onCountryCodeChange` and `onNumberChange` events to portal
    documentation
    * added helper for general field event types

--- packages/dnb-eufemia/src/extensions/forms/Field/FieldDocs.ts
+++ packages/dnb-eufemia/src/extensions/forms/Field/FieldDocs.ts
@@ -13,3 +13,34 @@ export const fieldProperties: PropertiesTableProps = {
 export const fieldEvents: PropertiesTableProps = {
   ...dataValueEvents,
 }
+
+export const getFieldEventsWithTypes = (
+  valueType: { type: string; optional?: boolean } = undefined,
+  additionalArgsType: { type: string; optional?: boolean } = undefined
+): PropertiesTableProps => {
+  const value = valueType
+    ? `value${valueType.optional ? '?' : ''}: ${valueType.type}`
+    : 'value'
+
+  const add = additionalArgsType
+    ? `, additionalArgs${additionalArgsType.optional ? '?' : ''}: ${
+        additionalArgsType.type
+      }`
+    : ''
+
+  return {
+    ...fieldEvents,
+    onChange: {
+      ...fieldEvents.onChange,
+      type: `(${value}${add}) => void`,
+    },
+    onFocus: {
+      ...fieldEvents.onFocus,
+      type: `(${value}) => void`,
+    },
+    onBlur: {
+      ...fieldEvents.onBlur,
+      type: `(${value}) => void`,
+    },
+  }
+}
