8d0cf618a9620e7de00bfac8bde834846bff3b84
Tobias Høegh <tobias@tujo.no>
2024-08-22 13:19:00 +0200

    fix(useTheme): define the return type of `null` as a possible return (#3861)

--- packages/dnb-eufemia/src/shared/useTheme.tsx
+++ packages/dnb-eufemia/src/shared/useTheme.tsx
@@ -3,16 +3,25 @@
  *
  */
 
-import React from 'react'
+import { useContext } from 'react'
 import Context from './Context'
+import type { ThemeProps } from './Theme'
 
-export default function useTheme() {
-  const context = React.useContext(Context)
+export type UseThemeReturn =
+  | (ThemeProps & {
+      isUi: boolean
+      isSbanken: boolean
+      isEiendom: boolean
+    })
+  | null
 
-  if (context?.theme) {
-    const { name } = context.theme
+export default function useTheme(): UseThemeReturn {
+  const { theme } = useContext(Context) || {}
+
+  if (theme) {
+    const { name } = theme
     return {
-      ...context.theme,
+      ...theme,
       isUi: name === 'ui',
       isSbanken: name === 'sbanken',
       isEiendom: name === 'eiendom',
