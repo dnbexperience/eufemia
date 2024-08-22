089a0a72ff2d8eccec7ea6e9bc46339f9b5959f3
Joakim Bjerknes <joakbjerk@gmail.com>
2024-08-22 14:26:33 +0200

    render title instead of value

--- packages/dnb-eufemia/src/extensions/forms/Value/Selection/Selection.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Value/Selection/Selection.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useContext, useMemo } from 'react'
 import StringValue from '../String'
 import { ValueProps } from '../../types'
+import { useValueProps } from '../../hooks'
+
+import Context from '../../DataContext/Context'
+
+import { convertJsxToString } from '../../../../shared/component-helper'
 
 export type Props = ValueProps<string>
 
 function Selection(props: Props) {
-  return <StringValue {...props} />
+  const { fieldPropsRef } = useContext(Context) || {}
+  const { path, value, ...rest } = useValueProps(props)
+
+  const valueFromField = useMemo(() => {
+    if (path && value) {
+      const selection = fieldPropsRef.current[path][
+        'children'
+      ] as Array<JSX.Element>
+
+      const option = selection.find((child) => child.props.value === value)
+
+      return option?.props
+        ? convertJsxToString(option?.props?.title)
+        : value
+    }
+  }, [fieldPropsRef, path, value])
+
+  return <StringValue value={valueFromField} {...rest} />
 }
 
 Selection._supportsSpacingProps = true
