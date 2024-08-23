a54b16da5f1f1441abeb5a9a93ee4549844267f1
Joakim Bjerknes <joakbjerk@gmail.com>
2024-08-23 09:09:11 +0200

    improve value selection

--- packages/dnb-eufemia/src/extensions/forms/Value/Selection/Selection.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Value/Selection/Selection.tsx
@@ -6,6 +6,7 @@ import { useValueProps } from '../../hooks'
 import Context from '../../DataContext/Context'
 
 import { convertJsxToString } from '../../../../shared/component-helper'
+import { Data } from '../../Field/Selection'
 
 export type Props = ValueProps<string>
 
@@ -13,21 +14,19 @@ function Selection(props: Props) {
   const { fieldPropsRef } = useContext(Context) || {}
   const { path, value, ...rest } = useValueProps(props)
 
-  const valueFromField = useMemo(() => {
-    if (path && value && fieldPropsRef.current[path]) {
-      // Children is not defined on FieldProps, hence the lack of dot notation
-      const selection = fieldPropsRef.current[path][
-        'children'
-      ] as Array<JSX.Element>
-
-      const option = selection.find((child) => child.props.value === value)
-
-      return option?.props?.title
-        ? convertJsxToString(option?.props?.title)
-        : value
+  const valueFromField = useMemo<string | undefined>(() => {
+    if (!path || (path && !fieldPropsRef.current[path])) {
+      return value
     }
+    // Children is not defined on FieldProps, hence the lack of dot notation
+    const selection = fieldPropsRef.current[path]['children'] as Array<
+      Omit<JSX.Element, 'props'> & { props: Data[number] }
+    >
 
-    return value
+    const option = selection.find((child) => child.props.value === value)
+    const title = option?.props?.title
+
+    return title ? convertJsxToString(title) : value
   }, [fieldPropsRef, path, value])
 
   return <StringValue value={valueFromField} {...rest} />
