416df35d64e37c1cbbbe400d368721041dc02374
Tobias Høegh <tobias@tujo.no>
2024-08-28 09:38:07 +0200

    feat(Forms): add `Value.SelectCountry` component (#3875)
    Quick example:
    ```tsx
    Input:
    <Field.SelectCountry path="/myCountry" />
    
    Output:
    <Value.SelectCountry path="/myCountry" />
    ```

---
+++ packages/dnb-design-system-portal/src/docs/uilib/extensions/forms/Value/SelectCountry/Examples.tsx
@@ -0,0 +1,70 @@
+import ComponentBox from '../../../../../../shared/tags/ComponentBox'
+import { Flex, P } from '@dnb/eufemia/src'
+import { Field, Form, Value } from '@dnb/eufemia/src/extensions/forms'
+
+export const Placeholder = () => {
+  return (
+    <ComponentBox>
+      <Value.SelectCountry placeholder="No values given" />
+    </ComponentBox>
+  )
+}
+
+export const WithValue = () => {
+  return (
+    <ComponentBox>
+      <Value.SelectCountry value="NO" />
+    </ComponentBox>
+  )
+}
+
+export const DifferentLocale = () => {
+  return (
+    <ComponentBox>
+      <Form.Handler locale="en-GB" data={{ myCountry: 'CH' }}>
+        <Value.SelectCountry path="/myCountry" />
+      </Form.Handler>
+    </ComponentBox>
+  )
+}
+
+export const Label = () => {
+  return (
+    <ComponentBox>
+      <Value.SelectCountry label="Label text" showEmpty />
+    </ComponentBox>
+  )
+}
+
+export const LabelAndValue = () => {
+  return (
+    <ComponentBox>
+      <Value.SelectCountry label="Label text" value="NO" />
+    </ComponentBox>
+  )
+}
+
+export const Inline = () => {
+  return (
+    <ComponentBox>
+      <P>
+        This is before the component
+        <Value.SelectCountry value="NO" inline />
+        This is after the component
+      </P>
+    </ComponentBox>
+  )
+}
+
+export const WithFieldAndValue = () => {
+  return (
+    <ComponentBox>
+      <Form.Handler data={{ myCountry: 'NO' }}>
+        <Flex.Stack>
+          <Field.SelectCountry path="/myCountry" />
+          <Value.SelectCountry path="/myCountry" />
+        </Flex.Stack>
+      </Form.Handler>
+    </ComponentBox>
+  )
+}
--- packages/dnb-eufemia/src/extensions/forms/Field/SelectCountry/SelectCountryDocs.ts
+++ packages/dnb-eufemia/src/extensions/forms/Field/SelectCountry/SelectCountryDocs.ts
@@ -1,6 +1,15 @@
+import { PropertiesTableProps } from '../../../../shared/types'
 import { getFieldEventsWithTypes } from '../FieldDocs'
 
-export const selectCountryGeneralEvents = getFieldEventsWithTypes(
+export const SelectCountryProperties: PropertiesTableProps = {
+  countries: {
+    doc: 'List only a certain set of countries: `Scandinavia`, `Nordic`, `Europe` or `Prioritized`(all countries [sorted by priority](/uilib/extensions/forms/feature-fields/SelectCountry/#filter-or-prioritize-country-listing)). Defaults to `Prioritized`.',
+    type: 'string',
+    status: 'optional',
+  },
+}
+
+export const SelectCountryGeneralEvents = getFieldEventsWithTypes(
   { type: 'string', optional: true },
   { type: 'object', optional: true }
 )
