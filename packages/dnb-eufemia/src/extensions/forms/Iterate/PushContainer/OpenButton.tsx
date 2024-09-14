7b2605659a92ebab9f7cf3767f45bb9364f590c7
Tobias Høegh <tobias@tujo.no>
2024-09-14 21:06:29 +0200

    feat(Forms): add `{nextItemNr}` support to Iterate.PushContainer open button

--- packages/dnb-eufemia/src/extensions/forms/Iterate/PushContainer/OpenButton.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Iterate/PushContainer/OpenButton.tsx
@@ -1,18 +1,34 @@
-import React, { useCallback, useContext } from 'react'
+import React, { useCallback, useContext, useMemo } from 'react'
 import classnames from 'classnames'
 import Button, { ButtonProps } from '../../../../components/Button'
 import { add } from '../../../../icons'
 import IterateItemContext from '../IterateItemContext'
+import PushContainerContext from './PushContainerContext'
+import { convertJsxToString } from '../../../../shared/component-helper'
 
 type Props = ButtonProps
 
 function OpenButton(props: Props) {
-  const { className, children, ...restProps } = props
+  const { className, text, children, ...restProps } = props
   const { switchContainerMode } = useContext(IterateItemContext) || {}
+  const { entries } = useContext(PushContainerContext) || {}
 
   const handleClick = useCallback(() => {
     switchContainerMode?.('edit')
   }, [switchContainerMode])
+
+  const content = useMemo(() => {
+    if (children || text) {
+      const str = convertJsxToString(children || text)
+
+      if (str.includes('{nextItemNr}')) {
+        const nextItemNr = (entries?.length || 0) + 1
+        return str.replace('{nextItemNr}', String(nextItemNr))
+      }
+    }
+
+    return children || text
+  }, [entries?.length, children, text])
 
   return (
     <Button
@@ -23,7 +39,7 @@ function OpenButton(props: Props) {
       on_click={handleClick}
       {...restProps}
     >
-      {children}
+      {content}
     </Button>
   )
 }
