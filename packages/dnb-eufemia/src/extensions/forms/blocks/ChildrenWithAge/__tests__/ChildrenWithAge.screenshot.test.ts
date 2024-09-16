2c8888ef436193733c7f1bccc2211c0b61cb8645
-l <anderslangseth@gmail.com>
2024-09-16 11:24:21 +0200

    chore: adds visual test for small screen

--- packages/dnb-eufemia/src/extensions/forms/blocks/ChildrenWithAge/__tests__/ChildrenWithAge.screenshot.test.ts
+++ packages/dnb-eufemia/src/extensions/forms/blocks/ChildrenWithAge/__tests__/ChildrenWithAge.screenshot.test.ts
@@ -21,3 +21,19 @@ describe.each(['ui', 'sbanken'])('ChildrenWithAge for %s', (themeName) => {
     expect(screenshot).toMatchImageSnapshot()
   })
 })
+
+describe('ChildrenWithAge', () => {
+  setupPageScreenshot({
+    url: '/uilib/extensions/forms/blocks/ChildrenWithAge',
+    pageViewport: {
+      width: 480, // 30rem
+    },
+  })
+
+  it('have to match small screens', async () => {
+    const screenshot = await makeScreenshot({
+      selector: '[data-visual-test="children-with-age-prefilled"]',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+})
