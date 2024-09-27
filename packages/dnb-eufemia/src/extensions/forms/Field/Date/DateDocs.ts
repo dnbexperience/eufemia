df5217598623a3de7252ead2a728c0647f227003
Anders <anderslangseth@gmail.com>
2024-09-27 13:16:00 +0200

    Update packages/dnb-eufemia/src/extensions/forms/Field/Date/DateDocs.ts

--- packages/dnb-eufemia/src/extensions/forms/Field/Date/DateDocs.ts
+++ packages/dnb-eufemia/src/extensions/forms/Field/Date/DateDocs.ts
@@ -9,7 +9,7 @@ export const DateProperties: PropertiesTableProps = {
   range: {
     doc:
       'Defines if the Date field should support a value of two dates (starting and ending date). ' +
-      'The `value` needs to be a stirng containing two dates, seperated by a pipe character (`|`) (`01-09-2024|30-09-2024`) when this is set to `true`. ' +
+      'The `value` needs to be a string containing two dates, separated by a pipe character (`|`) (`01-09-2024|30-09-2024`) when this is set to `true`. ' +
       'Defaults to `false`.',
     type: 'boolean',
     status: 'optional',
