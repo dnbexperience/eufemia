f1462777063b64e64a484f073551a91263ee7c2f
-l <anderslangseth@gmail.com>
2024-09-27 12:32:27 +0200

    docs(Value.Currency): improve properties listed

--- packages/dnb-eufemia/src/extensions/forms/Value/Currency/CurrencyDocs.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Value/Currency/CurrencyDocs.tsx
@@ -1,11 +1,19 @@
 import { PropertiesTableProps } from '../../../../shared/types'
 import { NumberProperties } from '../Number/NumberDocs'
 
-export const CurrencyValueProperties: PropertiesTableProps = {
-  ...NumberProperties,
+const currency: PropertiesTableProps = {
   currency: {
     doc: 'Currency code (ISO 4217) or `true` to use the default `NOK`, which use two decimals by default. Defaults to value `NOK`.',
     type: ['string', 'boolean'],
     status: 'optional',
   },
+}
+
+export const CurrencyValueProperties: PropertiesTableProps = {
+  ...NumberProperties,
+  ...currency,
+}
+
+export const CurrencyValueHighlightedProperties: PropertiesTableProps = {
+  ...currency,
 }
