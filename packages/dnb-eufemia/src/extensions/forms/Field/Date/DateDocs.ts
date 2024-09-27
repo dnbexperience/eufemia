6a21f48d9b9fd99f0920c832bb307ccc80e2424b
Joakim Bjerknes <joakbjerk@gmail.com>
2024-09-27 12:00:08 +0200

    add period
    Co-authored-by: Tobias Høegh <tobias@tujo.no>

--- packages/dnb-eufemia/src/extensions/forms/Field/Date/DateDocs.ts
+++ packages/dnb-eufemia/src/extensions/forms/Field/Date/DateDocs.ts
@@ -2,7 +2,7 @@ import { PropertiesTableProps } from '../../../../shared/types'
 
 export const DateProperties: PropertiesTableProps = {
   help: {
-    doc: 'Provide a help button. Object consisting of `title` and `content`',
+    doc: 'Provide a help button. Object consisting of `title` and `content`.',
     type: 'object',
     status: 'optional',
   },
