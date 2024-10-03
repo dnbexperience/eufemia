9ef436c3a893c6b1c80de4ce2e3aae4918e3a871
Anders <anderslangseth@gmail.com>
2024-10-03 15:07:12 +0200

    Update packages/dnb-eufemia/src/extensions/forms/Value/PhoneNumber/PhoneNumber.tsx

--- packages/dnb-eufemia/src/extensions/forms/Value/PhoneNumber/PhoneNumber.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Value/PhoneNumber/PhoneNumber.tsx
@@ -19,7 +19,7 @@ function PhoneNumber(props: Props) {
     if (splitBySpace?.length > 1) {
       // This is good enough for norwegian country code.
       // As we don't have to format the country code, only return +47.
-      // Other country codes, like Trinidad and Tobago
+      // I believe other country codes, like Trinidad and Tobago
       // Should format the country code, like so +1 868. This is not supported today.
       return `${splitBySpace[0]} ${formatPhoneNumber(splitBySpace[1])}`
     } else {
