dfebcc7f75ea497a9f63e92b7bc2841f0aa59ff7
Tobias Høegh <tobias@tujo.no>
2024-10-08 19:02:36 +0200

    chore: make visual test for tabs less flaky (#4084)
    Its not a 100% sure solution. But should improve the outcome I hope.

--- packages/dnb-eufemia/jest.config.screenshots.js
+++ packages/dnb-eufemia/jest.config.screenshots.js
@@ -12,7 +12,7 @@ module.exports = {
           // before the test suite moves on to the next one.
           // You can change this value to be whatever number you want.
           // Defaults to 30 seconds (30e3)
-          headlessTimeout: 30e3,
+          headlessTimeout: 60e3,
         },
         browsers: [
           // 'chromium',
@@ -21,6 +21,7 @@ module.exports = {
         ],
       },
     },
+    testTimeout: 60e3,
     testRegex: 'screenshot.test.(js|ts|tsx)$',
     testEnvironment: './src/core/jest/jestPuppeteerEnvironment.js',
     setupFilesAfterEnv: ['./src/core/jest/setupJestScreenshot.js'],
