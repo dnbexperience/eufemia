bf183f3c6e4b1a064af6747b3013c9caf9c50447
-l <anderslangseth@gmail.com>
2024-10-10 11:28:36 +0200

    chore: adds stories to org no

--- packages/dnb-eufemia/src/extensions/forms/Field/OrganizationNumber/stories/OrganizationNumber.stories.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Field/OrganizationNumber/stories/OrganizationNumber.stories.tsx
@@ -132,7 +132,7 @@ export function OrganizationNumberValidator() {
   )
 }
 
-export function OrganizationNumberValidatorSimple() {
+export function CustomValidator() {
   const simpleValidator = (value) => {
     return value?.length < 4 ? Error('At least 4 characters') : undefined
   }
@@ -146,7 +146,7 @@ export function OrganizationNumberValidatorSimple() {
       <Field.OrganizationNumber
         onBlurValidator={false}
         validator={simpleValidator}
-        value="123123123"
+        value="123"
       />
       <Field.OrganizationNumber
         onBlurValidator={false}
@@ -163,7 +163,7 @@ export function OrganizationNumberValidatorSimple() {
         validateInitially
         onBlurValidator={false}
         validator={simpleValidator}
-        value="123123123"
+        value="123"
       />
       <Field.OrganizationNumber
         validateInitially
@@ -175,9 +175,134 @@ export function OrganizationNumberValidatorSimple() {
   )
 }
 
-export function StringValidatorSimple() {
+export function CustomOnBlurValidator() {
   const simpleValidator = (value) => {
     return value?.length < 4 ? Error('At least 4 characters') : undefined
+  }
+
+  return (
+    <Wrapper>
+      <Field.OrganizationNumber onBlurValidator={simpleValidator} />
+      <Field.OrganizationNumber
+        onBlurValidator={simpleValidator}
+        value="123"
+      />
+      <Field.OrganizationNumber
+        onBlurValidator={simpleValidator}
+        value="321321321"
+      />
+      <h2>Validate Initially:</h2>
+      <Field.OrganizationNumber
+        validateInitially
+        onBlurValidator={simpleValidator}
+      />
+      <Field.OrganizationNumber
+        validateInitially
+        onBlurValidator={simpleValidator}
+        value="123"
+      />
+      <Field.OrganizationNumber
+        validateInitially
+        onBlurValidator={simpleValidator}
+        value="321321321"
+      />
+    </Wrapper>
+  )
+}
+
+export function CustomValidatorReturnArray() {
+  const validatorX = (value) => {
+    return value?.length < 4 ? Error('At least 4 characters') : undefined
+  }
+
+  const simpleValidator = () => {
+    return [validatorX]
+  }
+
+  return (
+    <Wrapper>
+      <Field.OrganizationNumber
+        onBlurValidator={false}
+        validator={simpleValidator}
+      />
+      <Field.OrganizationNumber
+        onBlurValidator={false}
+        validator={simpleValidator}
+        value="123"
+      />
+      <Field.OrganizationNumber
+        onBlurValidator={false}
+        validator={simpleValidator}
+        value="321321321"
+      />
+      <h2>Validate Initially:</h2>
+      <Field.OrganizationNumber
+        validateInitially
+        onBlurValidator={false}
+        validator={simpleValidator}
+      />
+      <Field.OrganizationNumber
+        validateInitially
+        onBlurValidator={false}
+        validator={simpleValidator}
+        value="123"
+      />
+      <Field.OrganizationNumber
+        validateInitially
+        onBlurValidator={false}
+        validator={simpleValidator}
+        value="321321321"
+      />
+    </Wrapper>
+  )
+}
+
+export function CustomOnBlurValidatorReturnArray() {
+  const validatorX = (value) => {
+    return value?.length < 4 ? Error('At least 4 characters') : undefined
+  }
+
+  const simpleValidator = () => {
+    return [validatorX]
+  }
+
+  return (
+    <Wrapper>
+      <Field.OrganizationNumber onBlurValidator={simpleValidator} />
+      <Field.OrganizationNumber
+        onBlurValidator={simpleValidator}
+        value="123"
+      />
+      <Field.OrganizationNumber
+        onBlurValidator={simpleValidator}
+        value="321321321"
+      />
+      <h2>Validate Initially:</h2>
+      <Field.OrganizationNumber
+        validateInitially
+        onBlurValidator={simpleValidator}
+      />
+      <Field.OrganizationNumber
+        validateInitially
+        onBlurValidator={simpleValidator}
+        value="123"
+      />
+      <Field.OrganizationNumber
+        validateInitially
+        onBlurValidator={simpleValidator}
+        value="321321321"
+      />
+    </Wrapper>
+  )
+}
+
+export function StringValidatorSimple() {
+  const validatorX = (value) => {
+    return value?.length < 4 ? Error('At least 4 characters') : undefined
+  }
+
+  const simpleValidator = () => {
+    return [validatorX]
   }
 
   return (
