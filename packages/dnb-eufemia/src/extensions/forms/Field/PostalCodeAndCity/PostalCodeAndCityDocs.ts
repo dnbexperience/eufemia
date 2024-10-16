2afaef2b740c292df30b8aafddc791df22014d69
Joakim Bjerknes <joakbjerk@gmail.com>
2024-10-16 10:57:19 +0200

    fix formatting and spacing

--- packages/dnb-eufemia/src/extensions/forms/Field/PostalCodeAndCity/PostalCodeAndCityDocs.ts
+++ packages/dnb-eufemia/src/extensions/forms/Field/PostalCodeAndCity/PostalCodeAndCityDocs.ts
@@ -3,9 +3,9 @@ import { PropertiesTableProps } from '../../../../shared/types'
 export const PostalCodeAndCityProperties: PropertiesTableProps = {
   country: {
     doc:
-      'Defines which country the postal code and city is for, based on the ISO 3166-1 alpha-2 format.' +
-      'Setting it to anything other than `no` will remove the default norwegian postal code pattern.' +
-      'You can also use the value of another field to define the country, by using a path value i.e. `/myCountryPath`.' +
+      'Defines which country the postal code and city is for, based on the ISO 3166-1 alpha-2 format i.e. `NO`, `DE` etc. ' +
+      'Setting it to anything other than `NO` will remove the default norwegian postal code pattern. ' +
+      'You can also use the value of another field to define the country, by using a path value i.e. `/myCountryPath`. ' +
       'Defaults to `NO`',
     type: ['Path', 'string'],
     status: 'optional',
