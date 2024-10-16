9ccd2406ea4c3a315902ec018fee2371d25fc932
Joakim Bjerknes <joakbjerk@gmail.com>
2024-10-16 08:48:27 +0200

    mention iso formatting in docs

--- packages/dnb-eufemia/src/extensions/forms/Field/PostalCodeAndCity/PostalCodeAndCityDocs.ts
+++ packages/dnb-eufemia/src/extensions/forms/Field/PostalCodeAndCity/PostalCodeAndCityDocs.ts
@@ -3,10 +3,10 @@ import { PropertiesTableProps } from '../../../../shared/types'
 export const PostalCodeAndCityProperties: PropertiesTableProps = {
   country: {
     doc:
-      'Defines which country the postal code and city is for.' +
+      'Defines which country the postal code and city is for, based on the ISO 3166-1 alpha-2 format.' +
       'Setting it to anything other than `no` will remove the default norwegian postal code pattern.' +
       'You can also use the value of another field to define the country, by using a path value i.e. `/myCountryPath`.' +
-      'Defaults to `no`',
+      'Defaults to `NO`',
     type: ['Path', 'string'],
     status: 'optional',
   },
