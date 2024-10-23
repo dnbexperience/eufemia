e2d57ab931249d317bc862f0dd0440d5f5485ba0
Anders <anderslangseth@gmail.com>
2024-10-23 14:48:41 +0200

    Update packages/dnb-eufemia/src/shared/locales/nb-NO.ts

--- packages/dnb-eufemia/src/shared/locales/nb-NO.ts
+++ packages/dnb-eufemia/src/shared/locales/nb-NO.ts
@@ -144,7 +144,7 @@ export default {
     },
     Upload: {
       title: 'Last opp dokumenter',
-      text: 'Dra og slipp eller velg hvilke filer du vil laste opp.',
+      text: 'Dra og slipp, eller velg hvilke filer du vil laste opp.',
       textSingular:
         'Dra og slipp eller velg hvilken fil du vil laste opp.',
       fileTypeTableCaption: 'Tillatte filformater og maks filstørrelse',
