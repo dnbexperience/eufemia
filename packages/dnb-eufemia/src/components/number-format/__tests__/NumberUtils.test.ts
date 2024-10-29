9b38904b1941f7485e7157140e0fd18e3a407fee
Anders <anderslangseth@gmail.com>
2024-10-29 15:14:13 +0100

    chore: improve test description/names (#4191)

--- packages/dnb-eufemia/src/components/number-format/__tests__/NumberUtils.test.ts
+++ packages/dnb-eufemia/src/components/number-format/__tests__/NumberUtils.test.ts
@@ -780,16 +780,16 @@ describe('getCurrencySymbol should', () => {
   it('default to space', () => {
     expect(getCurrencySymbol()).toBe('kr')
   })
-  it('return space when locale is nb-NO', () => {
+  it('return kr when locale is nb-NO', () => {
     expect(getCurrencySymbol('nb-NO')).toBe('kr')
   })
-  it('return space when locale is sv-SE', () => {
+  it('return NOK when locale is sv-SE', () => {
     expect(getCurrencySymbol('sv-SE')).toBe('NOK')
   })
-  it('return space when locale is en-GB', () => {
+  it('return NOK when locale is en-GB', () => {
     expect(getCurrencySymbol('en-GB')).toBe('NOK')
   })
-  it('return space when locale is en-US', () => {
+  it('return NOK when locale is en-US', () => {
     expect(getCurrencySymbol('en-US')).toBe('NOK')
   })
 })
