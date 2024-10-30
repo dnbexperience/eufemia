d1b0f6fa23da32e03e2715bcbeef0f2de2a2e43a
Joakim Bjerknes <joakbjerk@gmail.com>
2024-10-30 16:27:56 +0100

    fix month change flicker bug

--- packages/dnb-eufemia/src/components/date-picker/hooks/useViews.ts
+++ packages/dnb-eufemia/src/components/date-picker/hooks/useViews.ts
@@ -1,7 +1,6 @@
 import addMonths from 'date-fns/addMonths'
-import { useEffect, useState } from 'react'
+import { useMemo, useState } from 'react'
 import { DatePickerDates } from './useDates'
-import usePreviousValue from './usePreviousValue'
 
 export type CalendarView = { nr: number; month?: Date }
 
@@ -17,10 +16,28 @@ export type UseViewsParams = ViewDates & {
 }
 
 export default function useViews({ isRange, ...dates }: UseViewsParams) {
-  const prevDates = usePreviousValue(dates)
+  const [prevDates, setPrevDates] = useState(dates)
   const [views, setViews] = useState<Array<CalendarView>>(
-    getViews({ ...dates, views: undefined, isRange })
+    getViews({ views: undefined, ...dates, isRange })
   )
+
+  const hasDateChanges = useMemo(
+    () =>
+      Object.keys(dates).some(
+        (dateType) => prevDates[dateType] !== dates[dateType]
+      ),
+    [dates, prevDates]
+  )
+
+  if (hasDateChanges) {
+    const currentViews = Array.isArray(views)
+      ? views.length > 1
+        ? views
+        : views[0]
+      : views
+    setViews(getViews({ ...dates, views: currentViews, isRange }))
+    setPrevDates(dates)
+  }
 
   function updateViews(
     views: Array<CalendarView>,
@@ -29,22 +46,6 @@ export default function useViews({ isRange, ...dates }: UseViewsParams) {
     setViews(views)
     cb?.()
   }
-
-  // Update views based on date changes
-  useEffect(() => {
-    if (
-      Object.keys(dates).some(
-        (dateType) => prevDates[dateType] !== dates[dateType]
-      )
-    ) {
-      const currentViews = Array.isArray(views)
-        ? views.length > 1
-          ? views
-          : views[0]
-        : views
-      setViews(getViews({ ...dates, views: currentViews, isRange }))
-    }
-  }, [dates, isRange, prevDates, views])
 
   return [views, updateViews] as const
 }
@@ -86,6 +87,5 @@ function getMonthView(
   // Here we add that default offset to every new calendar added,
   // the first will get 0, the next one 1, and so forth
   const fallbackMonth = startMonth || startDate || new Date()
-
   return addMonths(fallbackMonth, nr)
 }
