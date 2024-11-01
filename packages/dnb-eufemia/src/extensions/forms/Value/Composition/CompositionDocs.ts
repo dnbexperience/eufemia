45120590e8f4ca37833315cf6582c476b036df55
Tobias Høegh <tobias@tujo.no>
2024-11-01 18:07:30 +0100

    feat(Forms): add `transformLabel` to Value.Composition

--- packages/dnb-eufemia/src/extensions/forms/Value/Composition/CompositionDocs.ts
+++ packages/dnb-eufemia/src/extensions/forms/Value/Composition/CompositionDocs.ts
@@ -1,10 +1,11 @@
 import { PropertiesTableProps } from '../../../../shared/types'
 import { ValueProperties } from '../ValueDocs'
 
-const { label } = ValueProperties
+const { label, transformLabel } = ValueProperties
 
 export const CompositionProperties: PropertiesTableProps = {
   label,
+  transformLabel,
   maxWidth: {
     doc: 'Use `small`, `medium` or `large` for predefined standard max widths. Defaults to `auto`.',
     type: 'string',
