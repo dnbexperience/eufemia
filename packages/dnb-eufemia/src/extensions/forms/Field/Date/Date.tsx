75a0cea864be1445b0d705fb082a6d8b189f0927
Joakim Bjerknes <joakbjerk@gmail.com>
2024-11-15 09:58:18 +0100

    remove embedded DatePickerProps

--- packages/dnb-eufemia/src/extensions/forms/Field/Date/Date.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Field/Date/Date.tsx
@@ -17,8 +17,9 @@ import {
   DatePickerEvent,
   DatePickerProps,
 } from '../../../../components/DatePicker'
-import { convertCamelCaseProps } from '../../../../shared/helpers/withCamelCaseProps'
 
+// `range`, `showInput`, `showCancelButton` and `showResetButton` are not picked from the `DatePickerProps`
+// Since they require `Field.Date` specific comments, due to them having different default values
 export type Props = FieldHelpProps &
   FieldProps<string, undefined | string> & {
     // Validation
@@ -28,151 +29,57 @@ export type Props = FieldHelpProps &
      * The value needs to be a string containing two dates, separated by a pipe character (`|`) i.e. (`01-09-2024|30-09-2024`) when this is set to `true`.
      * Defaults to `false`.
      */
-    // TODO: Replace with DatePickerProps when DatePickerProps is converted to camelCase
-    range?: boolean
-    // Adding the DatePickerProps has been done manually here, as to be able to camelCase the props, and adding the JSdoc comments
-    // We could of course have joined the DatePickerProps with the Props type, but that would lead to a breaking changes for Field.Date
-    // in the future when DatePickerProps is converted to camelCase, so solved this way as of now, to mitigate that.
-    // TODO: Remove when DatePickerProps is converted to camelCase
-    // Current solution is solved this way, as the ToCamelCase type does not does not retain the JSDOC comments,
-    // and hovering over the types themselves just shows all the DatePickerProp types instead of a type description
-    /**
-     * To display what month should be shown in the first calendar by default. Defaults to the `date` respective `start_date`.
-     */
-    month?: DatePickerProps['month']
-    /**
-     * To display what month should be shown in the first calendar by default. Defaults to the `date` respective `start_date`.
-     */
-    startMonth?: DatePickerProps['start_month']
-    /**
-     * To display what month should be shown in the second calendar by default. Defaults to the `date` respective `start_date`.
-     */
-    endMonth?: DatePickerProps['end_month']
-    /**
-     * To limit a date range to a minimum `start_date`. Defaults to `null`.
-     */
-    minDate?: DatePickerProps['min_date']
-    /**
-     * To limit a date range to a maximum `end_date`. Defaults to `null`.
-     */
-    maxDate?: DatePickerProps['max_date']
-    /**
-     * Corrects the input date value to be the same as either `min_date` or `max_date`, when the user types in a date that is either before or after one of these. Defaults to `false`.
-     */
-    correctInvalidDate?: DatePickerProps['correct_invalid_date']
-    /**
-     * To define the order of the masked placeholder input fields. Defaults to `dd/mm/yyyy`
-     */
-    maskOrder?: DatePickerProps['mask_order']
-    /**
-     * To display the placeholder on input. Defaults to `dd/mm/åååå`.
-     */
-    maskPlaceholder?: DatePickerProps['mask_placeholder']
-    /**
-     * Defines how the prop dates (`date`, `start_date` and `end_date`) should be parsed, e.g. `yyyy/MM/dd`. Defaults to `yyyy-MM-dd`.
-     */
-    dateFormat?: DatePickerProps['date_format']
-    /**
-     * Defines how the returned date, as a string, should be formatted as. Defaults to `yyyy-MM-dd`.
-     */
-    returnFormat?: DatePickerProps['return_format']
-    /**
-     * If set to `true`, the navigation will be hidden. Defaults to `false`.
-     */
-    hideNavigation?: DatePickerProps['hide_navigation']
-    /**
-     * If set to `true`, the week days will be hidden. Defaults to `false`.
-     */
-    hideDays?: DatePickerProps['hide_days']
-    /**
-     * Use `true` to only show the defined month. Disables the month navigation possibility. Defaults to `false`.
-     */
-    onlyMonth?: DatePickerProps['only_month']
-    /**
-     * Use `true` to only show the last week in the current month if it needs to be shown. The result is that there will mainly be shows five (5) weeks (rows) instead of six (6). Defaults to `false`.
-     */
-    hideLastWeek?: DatePickerProps['hide_last_week']
-    /**
-     * Once the date picker gets opened, there is a focus handling to ensure good accessibility. This can be disabled with this property. Defaults to `false`.
-     */
-    disableAutofocus?: DatePickerProps['disable_autofocus']
+    range?: DatePickerProps['range']
     /**
      * If the input fields with the mask should be visible. Defaults to `true`.
      */
-    showInput?: DatePickerProps['show_input']
-    /**
-     * If set to `true`, a submit button will be shown. You can change the default text by using `submit_button_text="Ok"`. Defaults to `false`. If the `range` prop is `true`, then the submit button is shown.
-     */
-    showSubmitButton?: DatePickerProps['show_submit_button']
+    showInput?: DatePickerProps['showInput']
+
     /**
      * If set to `true`, a cancel button will be shown. You can change the default text by using `cancel_button_text="Avbryt"` Defaults to `true`. If the `range` prop is `true`, then the cancel button is shown.
      */
-    showCancelButton?: DatePickerProps['show_cancel_button']
+    showCancelButton?: DatePickerProps['showCancelButton']
     /**
      * If set to `true`, a reset button will be shown. You can change the default text by using `reset_button_text="Tilbakestill"` Defaults to `true`.
      */
-    showResetButton?: DatePickerProps['show_reset_button']
-    submitButtonText?: DatePickerProps['submit_button_text']
-    cancelButtonText?: DatePickerProps['cancel_button_text']
-    resetButtonText?: DatePickerProps['reset_button_text']
-    /**
-     * To define the first day of the week. Defaults to `monday`.
-     */
-    firstDay?: DatePickerProps['first_day']
-    /**
-     * Link both calendars, once to the user is navigating between months. Only meant to use if the range is set to `true`. Defaults to `false`.
-     */
-    link?: DatePickerProps['link']
-    /**
-     * Sync input values with the calendars views. Once the input values get changed, the calendar changes its views in sync. Defaults to `true`.
-     */
-    sync?: DatePickerProps['sync']
-    /**
-     * Gives you the possibility to inject a React element showing up over the footer. Use it to customize `shortcuts`.
-     */
-    addonElement?: DatePickerProps['addon_element']
-    /**
-     * Gives you the possibility to set predefined dates and date ranges so the user can select these by one click. Define either a JSON or an object with the defined shortcuts. More info is below.
-     */
-    shortcuts?: DatePickerProps['shortcuts']
-    /**
-     * To open the date-picker by default. Defaults to `false`.
-     */
-    opened?: DatePickerProps['opened']
-    direction?: DatePickerProps['direction']
-    /**
-     * Use `right` to change the calendar alignment direction. Defaults to `left`.
-     */
-    alignPicker?: DatePickerProps['align_picker']
-    /**
-     * Will be called right before every new calendar view gets rendered. See the example above.
-     */
-    onDaysRender?: DatePickerProps['on_days_render']
-    /**
-     * Will be called on every input and date picker interaction. Returns an `object`. See Returned Object below.
-     */
-    onType?: DatePickerProps['on_type']
-    /**
-     * Will be called once date-picker is visible.
-     */
-    onShow?: DatePickerProps['on_show']
-    /**
-     * Will be called once date-picker is hidden.
-     */
-    onHide?: DatePickerProps['on_hide']
-    /**
-     * Will be called once a user presses the submit button.
-     */
-    onSubmit?: DatePickerProps['on_submit']
-    /**
-     * Will be called once a user presses the cancel button.
-     */
-    onCancel?: DatePickerProps['on_cancel']
-    /**
-     * Will be called once a user presses the reset button.
-     */
-    onReset?: DatePickerProps['on_reset']
-  }
+    showResetButton?: DatePickerProps['showResetButton']
+  } & Pick<
+    DatePickerProps,
+    | 'month'
+    | 'startMonth'
+    | 'endMonth'
+    | 'minDate'
+    | 'maxDate'
+    | 'correctInvalidDate'
+    | 'maskOrder'
+    | 'maskPlaceholder'
+    | 'dateFormat'
+    | 'returnFormat'
+    | 'hideNavigation'
+    | 'hideDays'
+    | 'onlyMonth'
+    | 'hideLastWeek'
+    | 'disableAutofocus'
+    | 'showSubmitButton'
+    | 'submitButtonText'
+    | 'cancelButtonText'
+    | 'resetButtonText'
+    | 'firstDay'
+    | 'link'
+    | 'sync'
+    | 'addonElement'
+    | 'shortcuts'
+    | 'opened'
+    | 'direction'
+    | 'alignPicker'
+    | 'onDaysRender'
+    | 'onType'
+    | 'onShow'
+    | 'onHide'
+    | 'onSubmit'
+    | 'onCancel'
+    | 'onReset'
+  >
 
 function DateComponent(props: Props) {
   const translations = useTranslation()
@@ -279,11 +186,11 @@ function DateComponent(props: Props) {
         id={id}
         date={value}
         disabled={disabled}
-        show_input={showInput}
-        show_cancel_button={showCancelButton}
-        show_reset_button={showResetButton}
-        start_date={startDate}
-        end_date={endDate}
+        showInput={showInput}
+        showCancelButton={showCancelButton}
+        showResetButton={showResetButton}
+        startDate={startDate}
+        endDate={endDate}
         status={hasError ? 'error' : undefined}
         suffix={
           help ? (
@@ -291,8 +198,8 @@ function DateComponent(props: Props) {
           ) : undefined
         }
         range={range}
-        on_change={handleChange}
-        on_reset={(event) => {
+        onChange={handleChange}
+        onReset={(event) => {
           handleChange(event)
           onReset?.(event)
         }}
@@ -357,8 +264,8 @@ function pickDatePickerProps(props: Props) {
     },
     {}
   )
-  // TODO: Remove convertCamelCaseProps when DatePickerProps is converted to camelCase
-  return convertCamelCaseProps(datePickerProps)
+
+  return datePickerProps
 }
 
 DateComponent._supportsSpacingProps = true
