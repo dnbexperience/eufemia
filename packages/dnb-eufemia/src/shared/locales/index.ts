0b1dfff428309b3cd0333d0c0725e0fd317709b2
-l <anderslangseth@gmail.com>
2024-11-17 23:44:19 +0100

    reverts removal of en-us locale

--- packages/dnb-eufemia/src/shared/locales/index.ts
+++ packages/dnb-eufemia/src/shared/locales/index.ts
@@ -1,10 +1,12 @@
 // import default locales
 import nbNO from './nb-NO'
 import enGB from './en-GB'
+import enUS from './en-US'
 
-const locales = { ...nbNO, ...enGB }
+const locales = { ...nbNO, ...enGB, ...enUS }
 
 export type TranslationsNbNO = (typeof locales)['nb-NO']
 export type TranslationsEnGB = (typeof locales)['en-GB']
+export type TranslationsEnUS = (typeof locales)['en-US']
 
 export default locales
