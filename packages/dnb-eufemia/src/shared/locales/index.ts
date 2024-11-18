630c5ddca1255b2fe5903704a3dcc5bfeee70a53
-l <anderslangseth@gmail.com>
2024-11-18 10:18:07 +0100

    chore: adds en-US locale

--- packages/dnb-eufemia/src/shared/locales/index.ts
+++ packages/dnb-eufemia/src/shared/locales/index.ts
@@ -1,10 +1,12 @@
 // import default locales
 import nbNO from './nb-NO'
+import enUS from './en-US'
 import enGB from './en-GB'
 
-const locales = { ...nbNO, ...enGB }
+const locales = { ...nbNO, ...enGB, ...enUS }
 
 export type TranslationsNbNO = (typeof locales)['nb-NO']
+export type TranslationsEnUS = (typeof locales)['en-US']
 export type TranslationsEnGB = (typeof locales)['en-GB']
 
 export default locales
