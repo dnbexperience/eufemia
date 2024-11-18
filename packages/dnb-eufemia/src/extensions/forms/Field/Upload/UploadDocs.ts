38290bb619e78977c78ea711d3d7d161e96794c6
Whitney Hiltz <whitney.hiltz@dnb.no>
2024-11-18 12:38:05 +0100

    Revert doc changes to master

--- packages/dnb-eufemia/src/extensions/forms/Field/Upload/UploadDocs.ts
+++ packages/dnb-eufemia/src/extensions/forms/Field/Upload/UploadDocs.ts
@@ -8,11 +8,6 @@ export const UploadFieldProperties: PropertiesTableProps = {
   ...UploadProperties,
   title: undefined,
   text: undefined,
-  asyncFileHandler: {
-    doc: 'Handler function that is triggered when new files are added to the upload field. Takes said new files as a parameter and returns a promise containing the processed files.',
-    type: 'function',
-    status: 'optional',
-  },
 }
 
 export const UploadFieldEvents: PropertiesTableProps = {
