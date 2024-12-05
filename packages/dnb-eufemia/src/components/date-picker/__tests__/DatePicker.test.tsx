cb85026543ef48cbf89640e2adda324b0d4c4155
Joakim Bjerknes <joakbjerk@gmail.com>
2024-12-05 15:46:00 +0100

    update tests

--- packages/dnb-eufemia/src/components/date-picker/__tests__/DatePicker.test.tsx
+++ packages/dnb-eufemia/src/components/date-picker/__tests__/DatePicker.test.tsx
@@ -2662,15 +2662,19 @@ describe('DatePickerPortal', () => {
 
     await userEvent.click(inputButton)
 
-    expect(
-      document.body.querySelector('.dnb-date-picker__portal')
-    ).toBeInTheDocument()
+    await waitFor(() =>
+      expect(
+        document.body.querySelector('.dnb-date-picker__portal')
+      ).toBeInTheDocument()
+    )
 
     await userEvent.click(inputButton)
 
-    expect(
-      document.body.querySelector('.dnb-date-picker__portal')
-    ).not.toBeInTheDocument()
+    await waitFor(() =>
+      expect(
+        document.body.querySelector('.dnb-date-picker__portal')
+      ).not.toBeInTheDocument()
+    )
   })
 })
 
