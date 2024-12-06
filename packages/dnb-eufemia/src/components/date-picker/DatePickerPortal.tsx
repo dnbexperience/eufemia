db28bafde9bfb70236c30c06caa34de8c4166c9f
Joakim Bjerknes <joakbjerk@gmail.com>
2024-12-06 09:23:37 +0100

    refactor portal

--- packages/dnb-eufemia/src/components/date-picker/DatePickerPortal.tsx
+++ packages/dnb-eufemia/src/components/date-picker/DatePickerPortal.tsx
@@ -3,64 +3,46 @@ import { createPortal } from 'react-dom'
 import { DatePickerProps } from './DatePicker'
 
 type DatePickerPortalProps = React.HTMLProps<HTMLDivElement> & {
-  show?: boolean
+  skipPortal?: DatePickerProps['skipPortal']
   alignment?: DatePickerProps['alignPicker']
-  portalRef?: React.RefObject<HTMLDivElement>
   targetElementRef?: React.RefObject<HTMLElement>
 }
 
 export default function DatePickerPortal({
-  show = false,
+  className,
+  skipPortal,
   alignment,
   targetElementRef,
   children,
-  className,
 }: DatePickerPortalProps) {
-  // Make sure the portal re-renders when the target element changes
-  const [targetElement, setTargetElement] = useState(
-    targetElementRef?.current
-  )
-  const [portal, setPortal] = useState<HTMLDivElement>(undefined)
+  const [position, setPosition] = useState({})
 
-  // Make sure that the portal has a bounding rect to work with, when setting the position
   useEffect(() => {
-    if (targetElementRef?.current !== targetElement) {
-      setTargetElement(targetElementRef?.current)
+    if (targetElementRef.current) {
+      setPosition(getPosition(targetElementRef.current, alignment))
     }
-  }, [targetElementRef, targetElement])
+  }, [])
 
-  if (show && !portal) {
-    const portalRoot = createPortalRoot(className)
-    document.body.appendChild(portalRoot)
-    const { top, left } = getPosition(targetElement, alignment)
-    portalRoot.style.left = left
-    portalRoot.style.top = top
-    setPortal(portalRoot)
-  }
-
-  if (!show && portal) {
-    document.body.removeChild(portal)
-    setPortal(undefined)
-  }
-
-  return portal && createPortal(children, portal)
-}
-
-function createPortalRoot(className: string) {
-  const root = document.createElement('div')
-  root.className = `${'dnb-date-picker__portal'} ${className}`
-
-  return root
+  return (
+    position &&
+    (!skipPortal
+      ? createPortal(
+          <div
+            className={`${'dnb-date-picker__portal'} ${className}`}
+            style={position}
+          >
+            {children}
+          </div>,
+          document.body
+        )
+      : children)
+  )
 }
 
 function getPosition(
   targetElement: HTMLElement,
   alignment: DatePickerPortalProps['alignment']
 ) {
-  if (!targetElement) {
-    return { top: '0', left: '0' }
-  }
-
   const rect = targetElement?.getBoundingClientRect()
 
   const scrollY = window.scrollY
