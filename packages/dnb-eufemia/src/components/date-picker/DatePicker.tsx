e3b1609d2c013ead59a2a15678baf6fe7f8b1bf8
Joakim Bjerknes <joakbjerk@gmail.com>
2024-12-06 10:37:37 +0100

    update ref name

--- packages/dnb-eufemia/src/components/date-picker/DatePicker.tsx
+++ packages/dnb-eufemia/src/components/date-picker/DatePicker.tsx
@@ -615,7 +615,7 @@ function DatePicker(externalProps: DatePickerAllProps) {
     useRef<DatePickerContextValues['getReturnObject']>()
   const hideTimeout = useRef<NodeJS.Timeout>()
   const outsideClick = useRef<DetectOutsideClickClass>()
-  const pickerContainerRef = useRef<HTMLDivElement>()
+  const calendarContainerRef = useRef<HTMLDivElement>()
 
   const translation = useTranslation().DatePicker
 
@@ -675,7 +675,7 @@ function DatePicker(externalProps: DatePickerAllProps) {
       // as that would lead to the ignoreElement being null/undefined,
       // since the portal has not been rendered yet,
       // causing the calendar to close when clicking on the calendar itself
-      [innerRef.current, pickerContainerRef],
+      [innerRef.current, calendarContainerRef],
       ({ event }: { event: MouseEvent | KeyboardEvent }) => {
         hidePicker({ ...event, focusOnHide: event?.['code'] })
       }
@@ -1005,7 +1005,7 @@ function DatePicker(externalProps: DatePickerAllProps) {
                 >
                   <span
                     className="dnb-date-picker__container"
-                    ref={pickerContainerRef}
+                    ref={calendarContainerRef}
                   >
                     <span
                       className="dnb-date-picker__triangle"
