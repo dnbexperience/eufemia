3e0a25f0c8aad13ac86ba25aedd5e87f3a4b2c78
Anders <anderslangseth@gmail.com>
2024-12-09 11:52:37 +0100

    chore(Upload): remove unused classes (#4374)

--- packages/dnb-eufemia/src/components/upload/UploadFileListCell.tsx
+++ packages/dnb-eufemia/src/components/upload/UploadFileListCell.tsx
@@ -189,11 +189,7 @@ const UploadFileListCell = ({
     ) : (
       <div className="dnb-upload__file-cell__text-container">
         {onClick ? (
-          <Button
-            variant="tertiary"
-            className={classnames('dnb-upload__file-cell__title')}
-            onClick={onClick}
-          >
+          <Button variant="tertiary" onClick={onClick}>
             {file.name}
           </Button>
         ) : (
@@ -201,10 +197,7 @@ const UploadFileListCell = ({
             target="_blank"
             href={imageUrl}
             download={download ? file.name : null}
-            className={classnames(
-              'dnb-anchor--no-launch-icon',
-              'dnb-upload__file-cell__title'
-            )}
+            className="dnb-anchor--no-launch-icon"
             rel="noopener noreferrer"
           >
             {file.name}
