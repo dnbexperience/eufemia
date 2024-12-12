6b27651f2f5b149d4c1cf6f8d8ea0eccd567038f
-l <anderslangseth@gmail.com>
2024-12-12 23:56:47 +0100

    feat(Value.Upload): add async onFileClick event

--- packages/dnb-eufemia/src/extensions/forms/Value/Upload/stories/Upload.stories.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Value/Upload/stories/Upload.stories.tsx
@@ -1,3 +1,4 @@
+import { createRequest } from '../../../Form/Handler/stories/FormHandler.stories'
 import { Form, Value } from '../../..'
 import { P } from '../../../../../elements'
 
@@ -15,13 +16,26 @@ function createMockFile(name: string, size: number, type: string) {
   return file
 }
 
+async function mockAsyncFileFetching({ fileItem }) {
+  const request = createRequest()
+  console.log(
+    'making API request to fetch the url of the file: ' +
+      fileItem.file.name
+  )
+  await request(3000) // Simulate a request
+  window.open(
+    'https://eufemia.dnb.no/images/avatars/' + fileItem.file.name,
+    '_blank'
+  )
+}
+
 export function Upload() {
   return (
-    <Form.Handler
-      data={{
-        foo: 'John',
-        bar: 'Doe',
-        baz: [
+    <>
+      <Value.Upload
+        onFileClick={mockAsyncFileFetching}
+        label="Upload"
+        value={[
           {
             file: createMockFile('fileName-1.png', 1000000, 'image/png'),
             exists: false,
@@ -37,57 +51,85 @@ export function Upload() {
             exists: false,
             id: '3',
           },
-        ],
-        what: ['Foo', 'Bar', 'Baz'],
-      }}
-    >
-      <Form.Card>
-        <P>layout="grid"</P>
-        <Value.SummaryList
-          layout="grid"
-          transformLabel={(label: string) => label.toUpperCase()}
-        >
-          <Value.String label="foo" path="/foo" />
-          <Value.String label="bar" path="/bar" />
-          <Value.Upload label="baz" path="/baz" />
-          <Value.ArraySelection label="what" path="/what" />
-        </Value.SummaryList>
-      </Form.Card>
-      <Form.Card>
-        <P>layout="horizontal"</P>
-        <Value.SummaryList
-          layout="horizontal"
-          transformLabel={(label: string) => label.toUpperCase()}
-        >
-          <Value.String label="foo" path="/foo" />
-          <Value.String label="bar" path="/bar" />
-          <Value.Upload label="baz" path="/baz" />
-          <Value.ArraySelection label="what" path="/what" />
-        </Value.SummaryList>
-      </Form.Card>
-      <Form.Card>
-        <P>layout="vertical"</P>
-        <Value.SummaryList
-          layout="vertical"
-          transformLabel={(label: string) => label.toUpperCase()}
-        >
-          <Value.String label="foo" path="/foo" />
-          <Value.String label="bar" path="/bar" />
-          <Value.Upload label="baz" path="/baz" />
-          <Value.ArraySelection label="what" path="/what" />
-        </Value.SummaryList>
-      </Form.Card>
-      <Form.Card>
-        <P>empty values</P>
-        <Value.SummaryList
-          transformLabel={(label: string) => label.toUpperCase()}
-        >
-          <Value.String label="foo" />
-          <Value.String label="bar" />
-          <Value.Upload label="baz" />
-          <Value.ArraySelection label="what" />
-        </Value.SummaryList>
-      </Form.Card>
-    </Form.Handler>
+          {
+            file: createMockFile('fileName-4.png', 4000000, 'image/png'),
+            exists: false,
+            id: '4',
+          },
+        ]}
+      />
+      <Form.Handler
+        data={{
+          foo: 'John',
+          bar: 'Doe',
+          baz: [
+            {
+              file: createMockFile('fileName-1.png', 1000000, 'image/png'),
+              exists: false,
+              id: '1',
+            },
+            {
+              file: createMockFile('fileName-2.png', 2000000, 'image/png'),
+              exists: false,
+              id: '2',
+            },
+            {
+              file: createMockFile('fileName-3.png', 3000000, 'image/png'),
+              exists: false,
+              id: '3',
+            },
+          ],
+          what: ['Foo', 'Bar', 'Baz'],
+        }}
+      >
+        <Form.Card>
+          <P>layout="grid"</P>
+          <Value.SummaryList
+            layout="grid"
+            transformLabel={(label: string) => label.toUpperCase()}
+          >
+            <Value.String label="foo" path="/foo" />
+            <Value.String label="bar" path="/bar" />
+            <Value.Upload label="baz" path="/baz" />
+            <Value.ArraySelection label="what" path="/what" />
+          </Value.SummaryList>
+        </Form.Card>
+        <Form.Card>
+          <P>layout="horizontal"</P>
+          <Value.SummaryList
+            layout="horizontal"
+            transformLabel={(label: string) => label.toUpperCase()}
+          >
+            <Value.String label="foo" path="/foo" />
+            <Value.String label="bar" path="/bar" />
+            <Value.Upload label="baz" path="/baz" />
+            <Value.ArraySelection label="what" path="/what" />
+          </Value.SummaryList>
+        </Form.Card>
+        <Form.Card>
+          <P>layout="vertical"</P>
+          <Value.SummaryList
+            layout="vertical"
+            transformLabel={(label: string) => label.toUpperCase()}
+          >
+            <Value.String label="foo" path="/foo" />
+            <Value.String label="bar" path="/bar" />
+            <Value.Upload label="baz" path="/baz" />
+            <Value.ArraySelection label="what" path="/what" />
+          </Value.SummaryList>
+        </Form.Card>
+        <Form.Card>
+          <P>empty values</P>
+          <Value.SummaryList
+            transformLabel={(label: string) => label.toUpperCase()}
+          >
+            <Value.String label="foo" />
+            <Value.String label="bar" />
+            <Value.Upload label="baz" />
+            <Value.ArraySelection label="what" />
+          </Value.SummaryList>
+        </Form.Card>
+      </Form.Handler>
+    </>
   )
 }
