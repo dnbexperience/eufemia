57c8b28dd1c52f8d23d68a6cc4991c7e10837ce3
-l <anderslangseth@gmail.com>
2025-01-14 09:14:12 +0100

    fix(Blockquote): contrast font color of code as child

--- packages/dnb-eufemia/src/elements/blockquote/stories/Blockquote.stories.tsx
+++ packages/dnb-eufemia/src/elements/blockquote/stories/Blockquote.stories.tsx
@@ -7,7 +7,7 @@ import React from 'react'
 import { Wrapper, Box } from 'storybook-utils/helpers'
 import styled from '@emotion/styled'
 import Blockquote from '../Blockquote'
-
+import { Code } from '../..'
 const CustomStyles = styled.div`
   a {
     &:has(> .dnb-icon) {
@@ -59,6 +59,12 @@ export const BlockquoteSandbox = () => (
         deterruisset per
         <cite>Cite Reference</cite>
         <figcaption>Figcaption Reference</figcaption>
+      </Blockquote>
+    </Box>
+    <Box>
+      <Blockquote data-visual-test="blockquote-with-code">
+        <Code>display</Code> and <Code>background-color</Code> are CSS
+        properties
       </Blockquote>
     </Box>
   </Wrapper>
