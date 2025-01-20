6ba0a3ac07fa6de9bc98a43fae1f829b79f5f24b
Snorre Kim <snorre.kim@dnb.no>
2025-01-20 14:41:43 +0100

    stop using wrapper class

--- packages/dnb-eufemia/src/components/step-indicator/StepIndicator.tsx
+++ packages/dnb-eufemia/src/components/step-indicator/StepIndicator.tsx
@@ -157,16 +157,18 @@ function StepIndicator({
 
   return (
     <StepIndicatorProvider {...props}>
-      <Card
-        align="stretch"
-        className={classnames(
-          'dnb-step-indicator-wrapper',
-          createSpacingClasses(restOfProps)
-        )}
-      >
-        <StepIndicatorTriggerButton />
-        <StepIndicatorList />
-      </Card>
+      <div className="dnb-step-indicator-wrapper">
+        <Card
+          align="stretch"
+          className={classnames(
+            'dnb-step-indicator',
+            createSpacingClasses(restOfProps)
+          )}
+        >
+          <StepIndicatorTriggerButton />
+          <StepIndicatorList />
+        </Card>
+      </div>
     </StepIndicatorProvider>
   )
 }
