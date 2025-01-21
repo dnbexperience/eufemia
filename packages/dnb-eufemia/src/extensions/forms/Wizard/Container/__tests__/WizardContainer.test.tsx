e678492732c7578374299f25c0f7a964d0cade9c
Snorre Kim <snorre.kim@dnb.no>
2025-01-21 17:45:39 +0100

    add status messages. fix on_render

--- packages/dnb-eufemia/src/extensions/forms/Wizard/Container/__tests__/WizardContainer.test.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Wizard/Container/__tests__/WizardContainer.test.tsx
@@ -2636,6 +2636,12 @@ describe('Wizard.Container', () => {
     )
 
     expect(
+      document.querySelector('.dnb-step-indicator__trigger__button')
+    ).toHaveTextContent('Title missing')
+
+    expandStepIndicator()
+
+    expect(
       document.querySelector('.dnb-step-indicator__item-content__text')
     ).toHaveTextContent('Title missing')
   })
