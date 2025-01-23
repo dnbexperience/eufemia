ca417413218cc1fd467a2a86a957c47ee13cfc63
Snorre Kim <snorre.kim@dnb.no>
2025-01-23 17:04:51 +0100

    clearer deprecated props

--- packages/dnb-eufemia/src/components/step-indicator/StepIndicatorSidebar.tsx
+++ packages/dnb-eufemia/src/components/step-indicator/StepIndicatorSidebar.tsx
@@ -11,18 +11,22 @@ export type StepIndicatorSidebarProps = SpacingProps &
   Omit<React.HTMLProps<HTMLAnchorElement>, 'ref' | 'data'> & {
     /**
      * Defines the data/steps showing up in a JavaScript Array or JSON format like `[{title,is_current}]`. See parameters and the example above.
+     * @deprecated StepIndicator.Sidebar variant is no longer supported
      */
     data?: StepIndicatorData
     /**
      * Defines how the StepIndicator should work. Use `static` for non-interactive steps. Use `strict` for a chronological step order, also, the user can navigate between visited steps. Use `loose` if the user should be able to navigate freely.
+     * @deprecated StepIndicator.Sidebar variant is no longer supported
      */
     mode?: StepIndicatorMode
     /**
      * Stuff
+     * @deprecated StepIndicator.Sidebar variant is no longer supported
      */
     showInitialData?: boolean
     /**
      * <em>(required)</em> a unique string-based ID in order to bind together the main component and the sidebar (`<StepIndicator.Sidebar />`). Both have to get the same ID.
+     * @deprecated StepIndicator.Sidebar variant is no longer supported
      */
     sidebar_id: string
   }
@@ -30,7 +34,7 @@ export type StepIndicatorSidebarProps = SpacingProps &
 /**
  * @deprecated StepIndicatorSidebar variant is no longer supported
  */
-function StepIndicatorSidebar(props: any) {
+function StepIndicatorSidebar(props: StepIndicatorSidebarProps) {
   return <div className="dnb-step-indicator__sidebar"></div>
 }
 
