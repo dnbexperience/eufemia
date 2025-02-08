d9e94c463ff91d621b1c89cc8706651c3db5e4f7
Joakim Bjerknes <joakbjerk@gmail.com>
2025-02-08 12:25:53 +0100

    fix failing test

--- packages/dnb-eufemia/src/extensions/forms/Field/Date/__tests__/Date.test.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Field/Date/__tests__/Date.test.tsx
@@ -1407,7 +1407,6 @@ describe('Field.Date', () => {
             value={minDate}
             minDate={minDate}
             maxDate={maxDate}
-            showInput
           />
         </Form.Handler>
       )
@@ -1563,12 +1562,16 @@ describe('Field.Date', () => {
       const maxDate = '2025-01-31'
 
       render(
-        <Field.Date
-          value="2025-01-02"
-          minDate={minDate}
-          maxDate={maxDate}
-          onBlurValidator={onBlurValidator}
-        />
+        // Setting the locale back to nb-NO, to prevent test from failing, as the above tests are in en-GB
+        // and moving this test to above tests will cause the test to fail anyway without setting the locale in this test
+        <Form.Handler locale="nb-NO">
+          <Field.Date
+            value="2025-01-02"
+            minDate={minDate}
+            maxDate={maxDate}
+            onBlurValidator={onBlurValidator}
+          />
+        </Form.Handler>
       )
 
       const [day, month]: Array<HTMLInputElement> = Array.from(
