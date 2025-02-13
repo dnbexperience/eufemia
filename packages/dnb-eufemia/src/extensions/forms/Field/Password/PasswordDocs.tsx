2904f2dbe90704d3e44f667f897c26bdf11ed089
-l <anderslangseth@gmail.com>
2025-02-13 10:43:44 +0100

    password properties table

---
+++ packages/dnb-eufemia/src/extensions/forms/Field/Password/PasswordDocs.tsx
@@ -0,0 +1,24 @@
+import { PropertiesTableProps } from '../../../../shared/types'
+
+export const PasswordProperties: PropertiesTableProps = {
+  pattern: {
+    doc: 'Validation based on regex pattern for the number field e.g. `pattern="w{8}d{4}"`.',
+    type: 'string',
+    status: 'optional',
+  },
+  size: {
+    doc: 'The sizes you can choose is small (1.5rem), default (2rem), medium (2.5rem) and large (3rem) are supported component sizes. Defaults to default / null. Also, if you define a number like size="2" then it will be forwarded as the input element attribute.',
+    type: 'object',
+    status: 'optional',
+  },
+  innerRef: {
+    doc: '`ElementRef` passed on to the password `input` element.',
+    type: 'object',
+    status: 'optional',
+  },
+  '[Space](/uilib/layout/space/properties)': {
+    doc: 'Spacing properties like `top` or `bottom` are supported.',
+    type: ['string', 'object'],
+    status: 'optional',
+  },
+}
