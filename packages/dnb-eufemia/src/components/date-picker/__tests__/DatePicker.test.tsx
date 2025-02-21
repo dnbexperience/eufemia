d37d900f8103c3a20320cd01701db9cbdb438e31
Joakim Bjerknes <joakbjerk@gmail.com>
2025-02-21 11:04:49 +0100

    remove only

--- packages/dnb-eufemia/src/components/date-picker/__tests__/DatePicker.test.tsx
+++ packages/dnb-eufemia/src/components/date-picker/__tests__/DatePicker.test.tsx
@@ -115,7 +115,7 @@ describe('DatePicker component', () => {
     ).not.toContain('dnb-date-picker--opened')
   })
 
-  it.only('will close the picker on click outside when when `onShow` callback set a state', async () => {
+  it('will close the picker on click outside when when `onShow` callback set a state', async () => {
     const onShow = jest.fn()
 
     const Component = () => {
