f4ceb7e944a8858c9ae9c28cbed6323c7f2bd0b2
Anders <anderslangseth@gmail.com>
2025-02-25 10:32:11 +0100

    chore: excepted to expected (#4615)

--- packages/dnb-eufemia/src/extensions/forms/Wizard/Step/StepDocs.ts
+++ packages/dnb-eufemia/src/extensions/forms/Wizard/Step/StepDocs.ts
@@ -22,7 +22,7 @@ export const StepProperties: PropertiesTableProps = {
     status: 'optional',
   },
   activeWhen: {
-    doc: 'Provide a `path` and a `hasValue` property with the excepted value in order to enable the step. You can alternatively provide a `hasValue` function that returns a boolean. The first parameter is the value of the path.',
+    doc: 'Provide a `path` and a `hasValue` property with the expected value in order to enable the step. You can alternatively provide a `hasValue` function that returns a boolean. The first parameter is the value of the path.',
     type: 'object',
     status: 'optional',
   },
