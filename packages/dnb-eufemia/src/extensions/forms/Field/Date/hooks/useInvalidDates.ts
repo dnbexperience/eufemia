5676739fd7038d85835cf52412d70f56d15a9ae8
Joakim Bjerknes <joakbjerk@gmail.com>
2025-03-10 14:44:21 +0100

    wrap setInvalidDates in useCallback

--- packages/dnb-eufemia/src/extensions/forms/Field/Date/hooks/useInvalidDates.ts
+++ packages/dnb-eufemia/src/extensions/forms/Field/Date/hooks/useInvalidDates.ts
@@ -1,15 +1,15 @@
-import { useRef } from 'react'
+import { useCallback, useRef } from 'react'
 import { InvalidDates } from '../../../../../components/date-picker/DatePickerInput'
 
 export default function useInvalidDates() {
   const invalidDatesRef = useRef<InvalidDates>({})
 
-  function setInvalidDates(invalidDates: InvalidDates) {
+  const setInvalidDates = useCallback((invalidDates: InvalidDates) => {
     invalidDatesRef.current = {
       ...invalidDatesRef.current,
       ...invalidDates,
     }
-  }
+  }, [])
 
   return { invalidDatesRef, setInvalidDates }
 }
