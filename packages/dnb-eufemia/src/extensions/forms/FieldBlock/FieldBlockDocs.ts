5798f7f7524dcf1badbe05ca12f2263c81553f68
Anders <anderslangseth@gmail.com>
2025-03-12 21:44:58 +0100

    Update packages/dnb-eufemia/src/extensions/forms/FieldBlock/FieldBlockDocs.ts

--- packages/dnb-eufemia/src/extensions/forms/FieldBlock/FieldBlockDocs.ts
+++ packages/dnb-eufemia/src/extensions/forms/FieldBlock/FieldBlockDocs.ts
@@ -12,7 +12,7 @@ export const FieldBlockSharedProperties: PropertiesTableProps = {
     status: 'optional',
   },
   labelDescriptionInline: {
-    doc: 'If true, the labelDescription will be displayed on the next line.',
+    doc: 'If true, the labelDescription will be displayed on the same line as the label.',
     type: 'boolean',
     status: 'optional',
   },
