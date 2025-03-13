b7435b5058d5851054e880e67bb847ed8de42fa7
Anders <anderslangseth@gmail.com>
2025-03-13 10:10:35 +0100

    Update packages/dnb-eufemia/src/extensions/forms/Field/PostalCodeAndCity/PostalCodeAndCityDocs.ts

--- packages/dnb-eufemia/src/extensions/forms/Field/PostalCodeAndCity/PostalCodeAndCityDocs.ts
+++ packages/dnb-eufemia/src/extensions/forms/Field/PostalCodeAndCity/PostalCodeAndCityDocs.ts
@@ -5,7 +5,7 @@ import { stringProperties } from '../String/StringDocs'
 export const PostalCodeAndCityProperties: PropertiesTableProps = {
   countryCode: {
     doc:
-      'Defines which country the postal code and city is for, based on the [ISO 3166-1 alpha-2 code](https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2) (country code) i.e. `NO`, `DE` etc. ' +
+      'Defines which country the postal code and city is for, based on the [ISO 3166-1 alpha-2 code](https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2) i.e. `NO`, `DE` etc. ' +
       'Setting it to anything other than `NO` will remove the default norwegian postal code pattern. ' +
       'You can also use the value of another field to define the countryCode, by using a path value i.e. `/myCountryCodePath`. ' +
       'Defaults to `NO`',
