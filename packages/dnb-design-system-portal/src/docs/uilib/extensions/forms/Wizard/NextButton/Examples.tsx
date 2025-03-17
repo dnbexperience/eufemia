baa29812381bc1da526a98bfa6b58047a5bce4fe
Tobias Høegh <tobias@tujo.no>
2025-03-17 14:33:36 +0100

    chore(Forms): refactor NextButton and PreviousButton example to not show Wizard.Provider (#4761)
    Closes #4665

--- packages/dnb-design-system-portal/src/docs/uilib/extensions/forms/Wizard/NextButton/Examples.tsx
+++ packages/dnb-design-system-portal/src/docs/uilib/extensions/forms/Wizard/NextButton/Examples.tsx
@@ -3,18 +3,18 @@ import { Wizard } from '@dnb/eufemia/src/extensions/forms'
 
 export const Default = () => {
   return (
-    <ComponentBox>
-      <Wizard.Provider
-        value={{
-          activeIndex: 0,
-          handlePrevious: () => null,
-          handleNext: () => console.log('handleNext'),
-          setActiveIndex: () => null,
-          setFormError: () => null,
-        }}
-      >
+    <Wizard.Provider
+      value={{
+        activeIndex: 0,
+        handlePrevious: () => null,
+        handleNext: () => console.log('handleNext'),
+        setActiveIndex: () => null,
+        setFormError: () => null,
+      }}
+    >
+      <ComponentBox>
         <Wizard.NextButton />
-      </Wizard.Provider>
-    </ComponentBox>
+      </ComponentBox>
+    </Wizard.Provider>
   )
 }
--- packages/dnb-design-system-portal/src/docs/uilib/extensions/forms/Wizard/PreviousButton/Examples.tsx
+++ packages/dnb-design-system-portal/src/docs/uilib/extensions/forms/Wizard/PreviousButton/Examples.tsx
@@ -3,18 +3,18 @@ import { Wizard } from '@dnb/eufemia/src/extensions/forms'
 
 export const Default = () => {
   return (
-    <ComponentBox>
-      <Wizard.Provider
-        value={{
-          activeIndex: 5,
-          handlePrevious: () => console.log('handlePrevious'),
-          handleNext: () => null,
-          setActiveIndex: () => null,
-          setFormError: () => null,
-        }}
-      >
+    <Wizard.Provider
+      value={{
+        activeIndex: 5,
+        handlePrevious: () => console.log('handlePrevious'),
+        handleNext: () => null,
+        setActiveIndex: () => null,
+        setFormError: () => null,
+      }}
+    >
+      <ComponentBox>
         <Wizard.PreviousButton />
-      </Wizard.Provider>
-    </ComponentBox>
+      </ComponentBox>
+    </Wizard.Provider>
   )
 }
