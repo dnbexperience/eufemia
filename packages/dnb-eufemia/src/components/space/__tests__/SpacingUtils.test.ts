62713143511539f0fd6aba7e2546c7f0d3a453a5
Tobias Høegh <tobias@tujo.no>
2025-03-20 14:13:01 +0100

    docs: add `xx-small` to the spacing table

--- packages/dnb-eufemia/src/components/space/__tests__/SpacingUtils.test.ts
+++ packages/dnb-eufemia/src/components/space/__tests__/SpacingUtils.test.ts
@@ -47,6 +47,7 @@ describe('translateSpace', () => {
   it('should translate and calc types', () => {
     expect(translateSpace('large')).toBe(2)
     expect(translateSpace('xx-large-x2')).toBe(7)
+    expect(translateSpace('xx-small')).toBe(0.25)
   })
 
   it('should translate and calc types -x2', () => {
@@ -93,6 +94,9 @@ describe('calc', () => {
   it('should output calc based on string types', () => {
     expect(calc('large x-small')).toEqual(
       'calc(var(--spacing-large) + var(--spacing-x-small))'
+    )
+    expect(calc('small xx-small')).toEqual(
+      'calc(var(--spacing-small) + var(--spacing-xx-small))'
     )
   })
 
@@ -186,6 +190,7 @@ describe('createTypeModifiers', () => {
       'small',
       'large',
     ])
+    expect(createTypeModifiers('0.25 1')).toEqual(['xx-small', 'small'])
     expect(createTypeModifiers('2 0.5')).toEqual(['large', 'x-small'])
     expect(createTypeModifiers(1)).toEqual(['small'])
     expect(createTypeModifiers(true)).toEqual(['small'])
@@ -195,6 +200,7 @@ describe('createTypeModifiers', () => {
 
 describe('findNearestTypes', () => {
   it('should find the nearest type in correct order', () => {
+    expect(findNearestTypes(0.25)).toEqual(['xx-small'])
     expect(findNearestTypes(2.5)).toEqual(['large', 'x-small'])
     expect(findNearestTypes(5)).toEqual(['xx-large', 'medium'])
     expect(findNearestTypes(8)).toEqual(['xx-large', 'xx-large', 'small'])
@@ -217,6 +223,10 @@ describe('createSpacing', () => {
     expect(createSpacing({ right: 'large x-small' }).className).toEqual([
       'dnb-space__right--large',
       'dnb-space__right--x-small',
+    ])
+    expect(createSpacing({ right: 'small xx-small' }).className).toEqual([
+      'dnb-space__right--small',
+      'dnb-space__right--xx-small',
     ])
   })
 
