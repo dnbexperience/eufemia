aecea5df1a32550dc5f0621bcc503d54f30d154b
-l <anderslangseth@gmail.com>
2025-03-25 13:06:19 +0100

    docs(Field.SelectCurrency): display list of available currencies

---
+++ packages/dnb-design-system-portal/src/docs/uilib/extensions/forms/feature-fields/SelectCountry/AvailableCountriesTable.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Table, Th, Tr, Td } from '@dnb/eufemia/src'
+import countries from '@dnb/eufemia/src/extensions/forms/constants/countries'
+import { FormattedCode } from '../../../../../.../../../shared/parts/PropertiesTable'
+
+export function AvailableCountriesTable() {
+  return (
+    <Table.ScrollView>
+      <Table>
+        <thead>
+          <Tr>
+            <Th>ISO 3166-1 alpha-2 code</Th>
+            {Object.entries(countries[0].i18n).map(([key]) => {
+              return <Th key={key}>{key}</Th>
+            })}
+          </Tr>
+        </thead>
+        <tbody>
+          {Object.entries(countries).map(([key, values]) => {
+            return (
+              <Tr key={key}>
+                <Td>
+                  <FormattedCode variant="prop">
+                    {values.iso}
+                  </FormattedCode>
+                </Td>
+                {Object.entries(values.i18n).map(([locale, value], i) => {
+                  return <Td key={i + locale}>{value}</Td>
+                })}
+              </Tr>
+            )
+          })}
+        </tbody>
+      </Table>
+    </Table.ScrollView>
+  )
+}
