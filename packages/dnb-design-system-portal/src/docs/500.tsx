4c2c3f5e63ed6f257ea4e02efd53a8c7ae82c336
-l <anderslangseth@gmail.com>
2025-03-26 18:35:56 +0100

    chore: localize 500 error page

--- packages/dnb-design-system-portal/src/docs/500.tsx
+++ packages/dnb-design-system-portal/src/docs/500.tsx
@@ -7,12 +7,5 @@ import React from 'react'
 import { GlobalError } from '@dnb/eufemia/src/components'
 
 export default function Error500() {
-  return (
-    <GlobalError
-      center
-      status="500"
-      title="Sorry, we got a technical issue!"
-      text="The service is not working properly at the moment. Please try again later."
-    />
-  )
+  return <GlobalError center status="500" />
 }
