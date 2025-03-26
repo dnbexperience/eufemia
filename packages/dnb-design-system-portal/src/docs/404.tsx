258a3512464fdb2b6e10b4acb2f15f04a64058e1
-l <anderslangseth@gmail.com>
2025-03-26 18:42:03 +0100

    chore: localize 404 error page

--- packages/dnb-design-system-portal/src/docs/404.tsx
+++ packages/dnb-design-system-portal/src/docs/404.tsx
@@ -5,23 +5,26 @@
 
 import React from 'react'
 import { GlobalError } from '@dnb/eufemia/src/components'
+import { Provider } from '@dnb/eufemia/src/shared'
 import Layout from '../shared/parts/Layout'
 
 export default function PageNotFound({ location }) {
   const { pathname } = location
 
   return (
-    <Layout key="layout" location={location} hideSidebar>
-      <GlobalError
-        center
-        status="404"
-        title="We couldn't find that page!"
-        text={
-          <>
-            There's not a page at <code>{pathname}</code>
-          </>
-        }
-      />
-    </Layout>
+    <Provider
+      translations={{
+        'nb-NO': {
+          'GlobalError.text': `Det er ingen side på adressen ${pathname}`,
+        },
+        'en-GB': {
+          'GlobalError.text': `There's no a page at the address ${pathname}`,
+        },
+      }}
+    >
+      <Layout key="layout" location={location} hideSidebar>
+        <GlobalError center status="404" />
+      </Layout>
+    </Provider>
   )
 }
