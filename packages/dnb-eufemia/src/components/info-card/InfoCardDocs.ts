3a17804e6013a2dec1a09341688422ba5834f460
-l <anderslangseth@gmail.com>
2025-03-27 12:27:23 +0100

    chore(InfoCard): refactor events and properties docs

---
+++ packages/dnb-eufemia/src/components/info-card/InfoCardDocs.ts
@@ -0,0 +1,97 @@
+import { PropertiesTableProps } from '../../shared/types'
+
+export const InfoCardProperties: PropertiesTableProps = {
+  text: {
+    doc: 'The content of the InfoCard can be a string or a react element.',
+    type: ['React.ReactNode'],
+    status: 'required',
+  },
+  title: {
+    doc: 'The card title is of type `string`',
+    type: ['React.ReactNode'],
+    status: 'optional',
+  },
+  centered: {
+    doc: 'Centers the content. Defaults to `false`.',
+    type: ['boolean'],
+    status: 'optional',
+  },
+  dropShadow: {
+    doc: 'Sets the drop shadow of the info card. Defaults to `true`.',
+    type: ['boolean'],
+    status: 'optional',
+  },
+  stretch: {
+    doc: 'Stretch the card to fill the container.',
+    type: ['boolean'],
+    status: 'optional',
+  },
+  className: {
+    doc: 'Custom className for the component root.',
+    type: ['x'],
+    status: 'optional',
+  },
+  icon: {
+    doc: 'Custom icon. Defaults to the `lightbulb` icon.',
+    type: 'React.ReactNode',
+    status: 'optional',
+  },
+  imgProps: {
+    doc: '[Image properties](/uilib/elements/image) applied to the `img` element if the component is used to display an image.',
+    type: ['ImgProps'],
+    status: 'optional',
+  },
+  alt: {
+    doc: 'Used in combination with `src` to provide an alt attribute for the image element.',
+    type: ['React.ReactNode'],
+    status: 'optional',
+  },
+  src: {
+    doc: 'Specifies the path to the image.',
+    type: ['string'],
+    status: 'optional',
+  },
+  skeleton: {
+    doc: 'If set to `true`, an overlaying skeleton with animation will be shown.',
+    type: 'boolean',
+    status: 'optional',
+  },
+  closeButtonText: {
+    doc: 'The close button text.',
+    type: ['React.ReactNode'],
+    status: 'optional',
+  },
+  closeButtonAttributes: {
+    doc: 'define any valid Eufemia [Button properties](/uilib/components/button/properties) or HTML attribute inside an object.',
+    type: ['ButtonProps'],
+    status: 'optional',
+  },
+  acceptButtonText: {
+    doc: 'The accept button text.',
+    type: ['React.ReactNode'],
+    status: 'optional',
+  },
+  acceptButtonAttributes: {
+    doc: 'define any valid Eufemia [Button properties](/uilib/components/button/properties) or HTML attribute inside an object.',
+    type: ['ButtonProps'],
+    status: 'optional',
+  },
+  '[Space](/uilib/layout/space/properties)': {
+    doc: 'Spacing properties like `top` or `bottom` are supported.',
+    type: ['string', 'object'],
+    status: 'optional',
+  },
+}
+
+export const InfoCardEvents: PropertiesTableProps = {
+  onAccept: {
+    doc: 'will be called when user clicks the accept button.',
+    type: 'function',
+    status: 'optional',
+  },
+  onClose: {
+    doc: 'will be called when user clicks the close button.',
+    type: 'function',
+    status: 'optional',
+  },
+}
