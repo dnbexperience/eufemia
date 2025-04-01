ac376359f7ea8d84d2baacf5b5a6ad160be17fea
Anders <anderslangseth@gmail.com>
2025-04-01 21:35:55 +0200

    chore(VisuallyHidden): refactor properties docs (#4843)

---
+++ packages/dnb-eufemia/src/components/visually-hidden/VisuallyHiddenDocs.ts
@@ -0,0 +1,14 @@
+import { PropertiesTableProps } from '../../shared/types'
+
+export const VisuallyHiddenProperties: PropertiesTableProps = {
+  focusable: {
+    doc: 'Set to `true` to hide an element by default, but to display it when it’s focused (e.g. by a keyboard-only user) root. Defaults to `false`.',
+    type: ['boolean'],
+    status: 'optional',
+  },
+  element: {
+    doc: 'Custom root HTML element for the component. Defaults to `<span>`.',
+    type: ['string', 'React.Element'],
+    status: 'optional',
+  },
+}
