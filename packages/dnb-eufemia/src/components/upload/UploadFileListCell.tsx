7286a332d749c8be16a037bda68db666b79858e8
Anders <anderslangseth@gmail.com>
2025-04-04 07:21:51 +0200

    fix(Upload): compare existing properties to determine duplicate (#4895)

--- packages/dnb-eufemia/src/components/upload/UploadFileListCell.tsx
+++ packages/dnb-eufemia/src/components/upload/UploadFileListCell.tsx
@@ -26,7 +26,7 @@ import { UploadFile, UploadFileNative } from './types'
 
 // Shared
 import { getPreviousSibling } from '../../shared/component-helper'
-import useUpload from './useUpload'
+import useUpload, { isFileEqual } from './useUpload'
 import { getFileTypeFromExtension } from './UploadVerify'
 import UploadFileLink from './UploadFileListLink'
 import { ProgressIndicatorAllProps } from '../progress-indicator/types'
@@ -192,7 +192,7 @@ function useExistsHighlight(id: string, file: File) {
 
   React.useEffect(() => {
     const exists = internalFiles.some(({ exists, file: f }) => {
-      return exists && f.name === file.name && f.size === file.size
+      return exists && isFileEqual(file, f)
     })
 
     if (exists) {
