d344d77bb378f282f83a64b2b1a5846a6847e541
Anders <anderslangseth@gmail.com>
2025-04-09 14:54:18 +0200

    chore(Wizard.Buttons): refactor properties docs (#4923)

---
+++ packages/dnb-eufemia/src/extensions/forms/Wizard/Buttons/ButtonsDocs.ts
@@ -0,0 +1,9 @@
+import { PropertiesTableProps } from '../../../../shared/types'
+
+export const WizardButtonsProperties: PropertiesTableProps = {
+  '[ButtonRow](/uilib/extensions/forms/Form/ButtonRow/properties/)': {
+    doc: 'ButtonRow properties.',
+    type: 'Various',
+    status: 'optional',
+  },
+}
