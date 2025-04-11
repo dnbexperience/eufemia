9e02114eb94d70aa4c5453c740cc29b2e454d4de
kspiteri <contact@keithspiteri.com>
2025-04-11 12:14:38 +0200

    feat: add 'compact' mode
    Introduced a compact variant for the PaymentCard component with updated SCSS styles and adjustments to its layout.
    Added logic in `CardFigure` to handle compact-specific designs, including simplified content and optimized provider icons rendering.

--- packages/dnb-eufemia/src/extensions/payment-card/components/CardFigure.js
+++ packages/dnb-eufemia/src/extensions/payment-card/components/CardFigure.js
@@ -18,14 +18,10 @@ export const cardDataPropTypes = PropTypes.shape({
   bankAxept: PropTypes.object.isRequired,
 })
 
-
-const CardProvidersSeparator = ({ hasBankAxept, hasCardType }) => {
-  return (hasBankAxept && hasCardType) && (<div className="separator" />)
-}
-
 CardFigure.propTypes = {
   id: PropTypes.string,
   skeleton: PropTypes.bool,
+  compact: PropTypes.bool,
   data: cardDataPropTypes.isRequired,
   cardStatus: PropTypes.string.isRequired,
   cardNumber: PropTypes.string.isRequired,
@@ -38,74 +34,86 @@ function CardFigure({
   cardNumber,
   id = null,
   skeleton = false,
+  compact = false,
   translations,
 }) {
   const cardClasses = classnames(
-      'dnb-payment-card__card',
-      `dnb-payment-card__${data.cardDesign.cardStyle}`,
-      `${isCardBlocked(cardStatus) ? 'dnb-payment-card__card--blocked' : ''}`,
+    'dnb-payment-card__card',
+    `dnb-payment-card__${data.cardDesign.cardStyle}`,
+    `${isCardBlocked(cardStatus) ? 'dnb-payment-card__card--blocked' : ''}`,
   );
 
+  const ProviderIcons = () => {
+      const multipleProviders = (data.bankAxept === BankAxeptType.BankAxept && data.cardType !== CardType.None);
+      return (
+          <div className={classnames(
+              'dnb-payment-card__card__providers',
+              `${multipleProviders ? 'dnb-payment-card__card__providers--multiple' : ''}`,
+          )}>
+              <BankAxeptLogo
+                  bankAxept={data.bankAxept}
+                  cardDesign={data.cardDesign}
+              />
+              { multipleProviders && <div className="provider--separator"/>}
+              <TypeLogo
+                  cardType={data.cardType}
+                  cardDesign={data.cardDesign}
+              />
+          </div>
+      )
+  }
+
   return (
-      <div className="dnb-payment-card__card__wrapper">
-        <div
-            id={id}
-            className={cardClasses}
-            {...(data.cardDesign?.backgroundImage
-                ? {
-                  style: {
-                    backgroundImage: `url(${data.cardDesign.backgroundImage})`,
-                  },
-                }
-                : {})}
-        >
-          <div className="dnb-payment-card__card__content">
-            <div className="dnb-payment-card__card__top">
-              <div className="dnb-payment-card__card__top__left">
-                <BankLogo logoType={data.cardDesign.bankLogo} />
-                <ProductLogo
-                    productType={data.productType}
-                    cardDesign={data.cardDesign}
-                />
-              </div>
-              <div className="dnb-payment-card__card__top__right">
-                <CardTypeText
-                    isCredit={data.bankAxept === BankAxeptType.Credit}
-                    translations={translations}
-                    skeleton={skeleton}
-                />
-              </div>
+    <div className="dnb-payment-card__card__wrapper">
+      <div
+        id={id}
+        className={cardClasses}
+        {...(data.cardDesign?.backgroundImage
+          ? {
+            style: {backgroundImage: `url(${data.cardDesign.backgroundImage})`},
+          } : {})}
+      >
+        <div className="dnb-payment-card__card__content">
+          <div className="dnb-payment-card__card__top">
+            <div className="dnb-payment-card__card__top__left">
+              <BankLogo logoType={data.cardDesign.bankLogo} />
+              <ProductLogo
+                productType={data.productType}
+                cardDesign={data.cardDesign}
+              />
             </div>
-            <div className="dnb-payment-card__card__bottom">
-              <div className="dnb-payment-card__card__bottom__left">
-                <CardNumberText
-                    cardNumber={cardNumber}
-                    skeleton={skeleton}
-                />
-              </div>
-              <div className="dnb-payment-card__card__bottom__right">
-                <BankAxeptLogo
-                    bankAxept={data.bankAxept}
-                    cardDesign={data.cardDesign}
-                />
-                <CardProvidersSeparator
-                    hasBankAxept={data.bankAxept === BankAxeptType.BankAxept}
-                    hasCardType={data.cardType !== CardType.None}
-                />
-                <TypeLogo
-                    cardType={data.cardType}
-                    cardDesign={data.cardDesign}
-                />
-              </div>
+            <div className="dnb-payment-card__card__top__right">
+              {compact ?
+                  <ProviderIcons />:
+                  <CardTypeText
+                      isCredit={data.bankAxept === BankAxeptType.Credit}
+                      translations={translations}
+                      skeleton={skeleton}
+                  />
+              }
             </div>
           </div>
+          {!compact &&
+              <div className="dnb-payment-card__card__bottom">
+                <div className="dnb-payment-card__card__bottom__left">
+                  <CardNumberText
+                      cardNumber={cardNumber}
+                      skeleton={skeleton}
+                  />
+                </div>
+                <div className="dnb-payment-card__card__bottom__right">
+                  <ProviderIcons />
+                </div>
+              </div>
+          }
         </div>
-        <StatusOverlay
-            cardStatus={cardStatus}
-            cardDesign={data.cardDesign.cardStyle}
-            translations={translations}
-        />
       </div>
+      <StatusOverlay
+        cardStatus={cardStatus}
+        cardDesign={data.cardDesign.cardStyle}
+        translations={translations}
+      />
+    </div>
   )
 }
 
