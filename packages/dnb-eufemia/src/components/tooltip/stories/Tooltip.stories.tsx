66f3c5278b409fac32558b0a0c53708b0b0c8f12
Nathan Brodin <nathan.victor.jean-claude.brodin@dnb.no>
2025-04-11 15:15:19 +0200

    feat(Tooltip): improve tooltip stories to cover all use cases

--- packages/dnb-eufemia/src/components/tooltip/stories/Tooltip.stories.tsx
+++ packages/dnb-eufemia/src/components/tooltip/stories/Tooltip.stories.tsx
@@ -1,17 +1,32 @@
 /* eslint-disable jsx-a11y/aria-role */
 /**
  * @dnb/eufemia Component Story
- *
+ * Comprehensive Tooltip Placement Demo
  */
-
 import React from 'react'
 import { Wrapper, Box } from 'storybook-utils/helpers'
 import styled from '@emotion/styled'
+import { Button, NumberFormat, Tooltip } from '../../../'
 
-import { NumberFormat, Button, Tooltip, P } from '../../../'
+const BoxWithPadding = styled(Box)`
+  padding: 3rem 0 3rem 7rem !important;
+`
 
-const StyledTooltip = styled(Tooltip)`
-  margin-bottom: 4rem;
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2rem;
+  margin-bottom: 3rem;
+
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+  }
+`
+
+const SectionTitle = styled.h3`
+  margin-top: 2rem;
+  margin-bottom: 1rem;
+  grid-column: 1 / -1;
 `
 
 export default {
@@ -21,96 +36,195 @@ export default {
 export const TooltipSandbox = () => {
   return (
     <Wrapper>
-      <Box>
-        <Tooltip
-          skipPortal
-          hideDelay={1e3}
-          targetElement={<Button right>Skipped Portal</Button>}
-          position="bottom"
-        >
-          Tooltip
-        </Tooltip>
-      </Box>
-      <Box>
-        <button className="target-1">Show the Tooltip</button>
-        <button className="target-2">Tooltip</button>
+      <SectionTitle>Basic Position Examples</SectionTitle>
+      <GridContainer>
+        <BoxWithPadding>
+          <Tooltip targetElement={<Button>Default Tooltip</Button>} active>
+            Default position (top center)
+          </Tooltip>
+        </BoxWithPadding>
 
-        <hr />
+        <BoxWithPadding>
+          <Tooltip
+            targetElement={<Button>Position Top</Button>}
+            position="top"
+            active
+          >
+            Top position
+          </Tooltip>
+        </BoxWithPadding>
 
-        <Tooltip id="unique-1" active targetSelector=".target-1">
-          Tooltip
-        </Tooltip>
+        <BoxWithPadding>
+          <Tooltip
+            targetElement={<Button>Position Right</Button>}
+            position="right"
+            active
+          >
+            Right position
+          </Tooltip>
+        </BoxWithPadding>
 
-        <Tooltip id="unique-2" targetSelector=".target-2">
-          Tooltip
-        </Tooltip>
-      </Box>
-      <Box>
-        <Tooltip
-          // hideDelay={1e3}
-          targetElement={<Button right>Top</Button>}
-        >
-          Tooltip 1
-        </Tooltip>
-        <Tooltip position="bottom" targetElement={<Button>Bottom</Button>}>
-          Tooltip 2
-        </Tooltip>
-      </Box>
-      <Box>
-        <Button>Clickable</Button>
-        <br />
-        <StyledTooltip
-          // id="button1"
-          position="top"
-          // target="#button1"
-          // active
-          targetElement={<Button right>Bottom 1</Button>}
-        >
-          Tooltip 1
-        </StyledTooltip>
-        <StyledTooltip
-          // id="button2"
-          position="bottom"
-          // target="#button2"
-          // active
-          targetElement={<Button>Bottom 2</Button>}
-        >
-          Tooltip 2
-        </StyledTooltip>
-      </Box>
-      <Box>
-        <Button
-          on_click={(e) => {
-            console.log(e)
-          }}
-          // aria-describedby="customId"
-          tooltip={
-            // 'Tooltip for this Button 1a'
-            <Tooltip>Tooltip for this Button 1b</Tooltip>
-          }
-        >
-          Button with Tooltip 1
-        </Button>
-        <Button
-          on_click={(e) => {
-            console.log(e)
-          }}
-          tooltip={
-            // 'Tooltip for this Button 2a'
-            <Tooltip position="bottom">Tooltip for this Button 2b</Tooltip>
-          }
-        >
-          Button with Tooltip 2
-        </Button>
-      </Box>
-      <Box>
-        <P>
-          <Tooltip targetElement={<NumberFormat>1234</NumberFormat>}>
+        <BoxWithPadding>
+          <Tooltip
+            targetElement={<Button>Position Bottom</Button>}
+            position="bottom"
+            active
+          >
+            Bottom position
+          </Tooltip>
+        </BoxWithPadding>
+
+        <BoxWithPadding>
+          <Tooltip
+            targetElement={<Button>Position Left</Button>}
+            position="left"
+            active
+          >
+            Left position
+          </Tooltip>
+        </BoxWithPadding>
+      </GridContainer>
+
+      <SectionTitle>Position with Alignment Examples</SectionTitle>
+      <GridContainer>
+        <BoxWithPadding>
+          <Tooltip
+            targetElement={<Button>Top Center</Button>}
+            position="top"
+            align="center"
+            active
+          >
+            Top with center alignment
+          </Tooltip>
+        </BoxWithPadding>
+
+        <BoxWithPadding>
+          <Tooltip
+            targetElement={<Button>Top Left</Button>}
+            position="top"
+            align="left"
+            arrow="left"
+            active
+          >
+            Top with left alignment
+          </Tooltip>
+        </BoxWithPadding>
+
+        <BoxWithPadding>
+          <Tooltip
+            targetElement={<Button>Top Right</Button>}
+            position="top"
+            align="right"
+            arrow="right"
+            active
+          >
+            Top with right alignment
+          </Tooltip>
+        </BoxWithPadding>
+
+        <BoxWithPadding>
+          <Tooltip
+            targetElement={<Button>Bottom Center</Button>}
+            position="bottom"
+            align="center"
+            active
+          >
+            Bottom with center alignment
+          </Tooltip>
+        </BoxWithPadding>
+
+        <BoxWithPadding>
+          <Tooltip
+            targetElement={<Button>Bottom Left</Button>}
+            position="bottom"
+            align="left"
+            arrow="left"
+            active
+          >
+            Bottom with left alignment
+          </Tooltip>
+        </BoxWithPadding>
+
+        <BoxWithPadding>
+          <Tooltip
+            targetElement={<Button>Bottom Right</Button>}
+            position="bottom"
+            align="right"
+            arrow="right"
+            active
+          >
+            Bottom with right alignment
+          </Tooltip>
+        </BoxWithPadding>
+      </GridContainer>
+
+      <SectionTitle>Size Variations</SectionTitle>
+      <GridContainer>
+        <BoxWithPadding>
+          <Tooltip targetElement={<Button>Default Size</Button>} active>
+            Default size (basis)
+          </Tooltip>
+        </BoxWithPadding>
+
+        <BoxWithPadding>
+          <Tooltip
+            targetElement={<Button>Large Size</Button>}
+            size="large"
+            active
+          >
+            Large size with more padding
+          </Tooltip>
+        </BoxWithPadding>
+      </GridContainer>
+
+      <SectionTitle>Additional Features</SectionTitle>
+      <GridContainer>
+        <BoxWithPadding>
+          <Tooltip
+            targetElement={<Button>No Animation</Button>}
+            noAnimation
+          >
+            Tooltip without fade-in animation
+          </Tooltip>
+        </BoxWithPadding>
+
+        <BoxWithPadding>
+          <Tooltip
+            targetElement={<Button>Delayed Show</Button>}
+            showDelay={800}
+          >
+            Tooltip with show delay (800ms)
+          </Tooltip>
+        </BoxWithPadding>
+
+        <BoxWithPadding>
+          <Tooltip
+            targetElement={<Button>Delayed Hide</Button>}
+            hideDelay={800}
+          >
+            Tooltip with hide delay (800ms)
+          </Tooltip>
+        </BoxWithPadding>
+
+        <BoxWithPadding>
+          <button className="target-1">Vanilla button</button>
+          <Tooltip id="unique" active targetSelector=".target-1">
             Tooltip
           </Tooltip>
-        </P>
+        </BoxWithPadding>
 
-        <P>
+        <BoxWithPadding>
+          <Tooltip
+            skipPortal
+            hideDelay={1e3}
+            targetElement={<Button right>Skipped Portal</Button>}
+            position="bottom"
+          >
+            Tooltip
+          </Tooltip>
+        </BoxWithPadding>
+
+        <BoxWithPadding>
           <NumberFormat
             tooltip={
               <Tooltip position="bottom">
@@ -120,13 +234,8 @@ export const TooltipSandbox = () => {
           >
             5678
           </NumberFormat>
-        </P>
-      </Box>
-      <Box>
-        <Tooltip skipPortal active>
-          Tooltip
-        </Tooltip>
-      </Box>
+        </BoxWithPadding>
+      </GridContainer>
     </Wrapper>
   )
 }
