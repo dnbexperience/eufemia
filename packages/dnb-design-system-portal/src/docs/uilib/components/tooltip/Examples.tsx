602ff65ac92d3ebc3e06454a7177ef4ec0aa40f5
-l <anderslangseth@gmail.com>
2025-04-11 14:05:49 +0200

    chore: add screenshot tests for existing tooltip functionality

--- packages/dnb-design-system-portal/src/docs/uilib/components/tooltip/Examples.tsx
+++ packages/dnb-design-system-portal/src/docs/uilib/components/tooltip/Examples.tsx
@@ -7,6 +7,9 @@ import React from 'react'
 import ComponentBox from '../../../../shared/tags/ComponentBox'
 import { Button, Tooltip, Span, NumberFormat } from '@dnb/eufemia/src'
 
+const swissJoke =
+  'What’s the best thing about Switzerland? I’m not sure, but the flag is a big plus. 🇨🇭'
+
 export const TooltipExampleDefault = () => (
   <ComponentBox data-visual-test="tooltip-hover">
     <Button tooltip="Tooltip" text="Hover" />
@@ -53,6 +56,115 @@ export const TooltipExampleNumberFormatWrapped = () => (
   <ComponentBox>
     <Tooltip targetElement={<NumberFormat>1234</NumberFormat>}>
       Tooltip
+    </Tooltip>
+  </ComponentBox>
+)
+
+export const TooltipExampleButtonLongText = () => (
+  <ComponentBox
+    data-visual-test="tooltip-hover-button-long-text"
+    scope={{ swissJoke }}
+  >
+    <Button tooltip={swissJoke + ' ' + swissJoke} text="Long Tooltip" />
+  </ComponentBox>
+)
+
+export const TooltipExampleAlignLeft = () => (
+  <ComponentBox
+    data-visual-test="tooltip-hover-align-left"
+    scope={{ swissJoke }}
+  >
+    <Button className="target-align-left">Align tooltip left</Button>
+    <Tooltip
+      id="unique-align-left"
+      targetSelector=".target-align-left"
+      align="left"
+    >
+      {swissJoke}
+    </Tooltip>
+  </ComponentBox>
+)
+
+export const TooltipExampleAlignRight = () => (
+  <ComponentBox
+    data-visual-test="tooltip-hover-align-right"
+    scope={{ swissJoke }}
+  >
+    <Button className="target-align-right">Align tooltip right</Button>
+    <Tooltip
+      id="unique-align-right"
+      targetSelector=".target-align-right"
+      align="right"
+    >
+      {swissJoke}
+    </Tooltip>
+  </ComponentBox>
+)
+
+export const TooltipExampleArrowLeft = () => (
+  <ComponentBox
+    data-visual-test="tooltip-hover-arrow-left"
+    scope={{ swissJoke }}
+  >
+    <Button className="target-arrow-left">Arrow tooltip left</Button>
+    <Tooltip
+      id="unique-arrow-left"
+      targetSelector=".target-arrow-left"
+      arrow="left"
+    >
+      {swissJoke}
+    </Tooltip>
+  </ComponentBox>
+)
+
+export const TooltipExampleArrowRight = () => (
+  <ComponentBox
+    data-visual-test="tooltip-hover-arrow-right"
+    scope={{ swissJoke }}
+  >
+    <Button className="target-arrow-right">Arrow tooltip right</Button>
+    <Tooltip
+      id="unique-arrow-right"
+      targetSelector=".target-arrow-right"
+      arrow="right"
+    >
+      {swissJoke}
+    </Tooltip>
+  </ComponentBox>
+)
+
+export const TooltipExamplePositionRight = () => (
+  <ComponentBox
+    data-visual-test="tooltip-hover-position-right"
+    scope={{ swissJoke }}
+  >
+    <Button className="target-position-right">
+      Position tooltip right
+    </Button>
+    <Tooltip
+      id="unique-position-right"
+      targetSelector=".target-position-right"
+      position="right"
+    >
+      {swissJoke}
+    </Tooltip>
+  </ComponentBox>
+)
+
+export const TooltipExamplePositionBottom = () => (
+  <ComponentBox
+    data-visual-test="tooltip-hover-position-bottom"
+    scope={{ swissJoke }}
+  >
+    <Button className="target-position-bottom">
+      Position tooltip bottom
+    </Button>
+    <Tooltip
+      id="unique-position-bottom"
+      targetSelector=".target-position-bottom"
+      position="bottom"
+    >
+      {swissJoke}
     </Tooltip>
   </ComponentBox>
 )
