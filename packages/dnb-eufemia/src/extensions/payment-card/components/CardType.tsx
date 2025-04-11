1d27b7e73cf27d8a0d45767bef8940b7e98e09a5
kspiteri <contact@keithspiteri.com>
2025-04-11 10:22:21 +0200

    style: update class names for payment card components

--- packages/dnb-eufemia/src/extensions/payment-card/components/CardType.tsx
+++ packages/dnb-eufemia/src/extensions/payment-card/components/CardType.tsx
@@ -15,7 +15,7 @@ interface CardTypeTextProps {
 const CardTypeText: React.FC<CardTypeTextProps> = ({ isCredit, translations, skeleton }) => {
   return (
       <span className={classnames(
-          'dnb-payment-card__card__wrapper', createSkeletonClass('font', skeleton))}>
+          'dnb-payment-card__card__element--wrapper', createSkeletonClass('font', skeleton))}>
     <P className={'dnb-payment-card__card__type-text'}>
       {isCredit ? translations.text_type_credit : translations.text_type_debit}
     </P>
