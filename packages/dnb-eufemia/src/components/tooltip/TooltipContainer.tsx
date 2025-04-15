dnbexperience/eufemia

commit 0d32910fe64a541b6b6b99bc04f01bacdf558b0f

fix(Tooltip): make skipPortal properly inline and accessible to screen readers (#1613)

packages/dnb-eufemia/src/components/tooltip/TooltipContainer.tsx
@@ -5,7 +5,7 @@
 
 import React from 'react'
 import { isTrue } from '../../shared/component-helper'
-import { getOffsetLeft } from '../../shared/helpers'
+import { getOffsetLeft, getOffsetTop } from '../../shared/helpers'
 import classnames from 'classnames'
 import { TooltipProps } from './types'
 
@@ -26,11 +26,16 @@ export default function TooltipContainer(
     attributes,
     arrow,
     position,
+    align,
+    group,
+    hideDelay,
     animatePosition,
     fixedPosition,
     noAnimation,
+    skipPortal,
     useHover,
     children,
+    targetElement: target,
   } = props
 
   const [style, setStyle] = React.useState(null)
@@ -91,9 +96,26 @@ export default function TooltipContainer(
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isActive])
 
+  const offsetLeft = React.useRef(0)
+  const offsetTop = React.useRef(0)
+
   React.useLayoutEffect(() => {
+    if (!isActive) {
+      /**
+       * This "resets" the position between elements,
+       * when not active. Else it will always first show on the older position.
+       */
+      if (group && wasActive) {
+        clearTimeout(debounceTimeout.current)
+        debounceTimeout.current = setTimeout(
+          () => setStyle(null),
+          hideDelay
+        )
+      }
+      return // stop here
+    }
+
     const element = elementRef?.current
-    const { targetElement: target, align, fixedPosition } = props
 
     if (
       typeof window === 'undefined' ||
@@ -103,13 +125,11 @@ export default function TooltipContainer(
       return // stop here
     }
 
-    const elementWidth = element.offsetWidth
-    const elementHeight = element.offsetHeight
-
     let alignOffset = 0
 
+    const elementWidth = element.offsetWidth
+    const elementHeight = element.offsetHeight
     const rect = target.getBoundingClientRect()
-
     const targetBodySize = {
       width: target.offsetWidth,
       height: target.offsetHeight,
@@ -121,11 +141,17 @@ export default function TooltipContainer(
       targetBodySize.height = rect.height
     }
 
+    if (skipPortal && (!offsetLeft.current || !offsetTop.current)) {
+      offsetLeft.current = getOffsetLeft(element) - offset.current
+      offsetTop.current = getOffsetTop(element) - offset.current
+    }
+
     const scrollY =
       window.scrollY !== undefined ? window.scrollY : window.pageYOffset
     const scrollX =
       window.scrollX !== undefined ? window.scrollX : window.pageXOffset
-    const top = (isTrue(fixedPosition) ? 0 : scrollY) + rect.top
+    const top =
+      (isTrue(fixedPosition) ? 0 : scrollY) + rect.top - offsetTop.current
 
     // Use Mouse position when target is too wide
     const useMouseWhen = targetBodySize.width > 400
@@ -135,9 +161,9 @@ export default function TooltipContainer(
       (element ? element.offsetWidth : 0)
     const widthBased = scrollX + rect.left
     const left =
-      useMouseWhen && mousePos < targetBodySize.width
+      (useMouseWhen && mousePos < targetBodySize.width
         ? mousePos
-        : widthBased
+        : widthBased) - offsetLeft.current
 
     const style = { ...props.style }
 
@@ -195,8 +221,7 @@ export default function TooltipContainer(
 
     if (stylesFromPosition[position]) {
       stylesFromPosition[position]()
-    }
-    if (stylesFromArrow[arrow]) {
+    } else if (stylesFromArrow[arrow]) {
       stylesFromArrow[arrow]()
     }
 
@@ -213,12 +238,10 @@ export default function TooltipContainer(
       style.top = 0
     }
 
-    if (isActive) {
-      setStyle(style)
-    }
+    setStyle(style)
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isActive, arrow, position, children, renewStyles])
+  }, [active, arrow, position, children, renewStyles])
 
   const handleMouseEnter = () => {
     if (isTrue(active) && useHover !== false) {
@@ -232,13 +255,22 @@ export default function TooltipContainer(
     }
   }
 
+  /**
+   * By stopping propagation, we allow the user to select text when Toolip is used in the Slider component
+   */
+  const handlePropagation = (e: React.SyntheticEvent) =>
+    e.stopPropagation()
+
   return (
     <span
       role="tooltip"
-      aria-hidden // make sure SR does not find it in the DOM, because we use "aria-describedby" for that
+      aria-hidden={target ? true : undefined} // make sure SR does not find it in the DOM, because we use "aria-describedby" for that
       ref={elementRef}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
+      onMouseMove={handlePropagation}
+      onMouseDown={handlePropagation}
+      onTouchStart={handlePropagation}
       {...attributes}
       className={classnames(
         attributes?.className,

packages/dnb-eufemia/src/components/dialog/__tests__/__snapshots__/Dialog.test.tsx.snap
@@ -470,6 +470,7 @@ exports[`Dialog component snapshot should match component snapshot 1`] = `
             tooltip="dialog_title"
           >
             <TooltipWithEvents
+              active={null}
               align={null}
               animatePosition={false}
               arrow="center"
@@ -491,45 +492,43 @@ exports[`Dialog component snapshot should match component snapshot 1`] = `
               size="basis"
               skipPortal={null}
               target={
-                Object {
-                  "current": <button
-                    aria-describedby="dialog_id-tooltip"
-                    aria-label="dialog_title"
-                    aria-roledescription="Hjelp-knapp"
-                    class="dnb-button dnb-button--secondary dnb-help-button dnb-modal__trigger dnb-button--icon-position-left dnb-button--has-icon dnb-button--size-medium"
-                    id="dialog_id"
-                    type="button"
+                <button
+                  aria-describedby="dialog_id-tooltip"
+                  aria-label="dialog_title"
+                  aria-roledescription="Hjelp-knapp"
+                  class="dnb-button dnb-button--secondary dnb-help-button dnb-modal__trigger dnb-button--icon-position-left dnb-button--has-icon dnb-button--size-medium"
+                  id="dialog_id"
+                  type="button"
+                >
+                  <span
+                    aria-hidden="true"
+                    class="dnb-button__alignment"
                   >
-                    <span
-                      aria-hidden="true"
-                      class="dnb-button__alignment"
+                    ‌
+                  </span>
+                  <span
+                    aria-hidden="true"
+                    class="dnb-icon dnb-icon--default dnb-button__icon dnb-icon--inherit-color"
+                    data-testid="question icon"
+                    role="presentation"
+                  >
+                    <svg
+                      fill="none"
+                      height="16"
+                      viewBox="0 0 16 16"
+                      width="16"
+                      xmlns="http://www.w3.org/2000/svg"
                     >
-                      ‌
-                    </span>
-                    <span
-                      aria-hidden="true"
-                      class="dnb-icon dnb-icon--default dnb-button__icon dnb-icon--inherit-color"
-                      data-testid="question icon"
-                      role="presentation"
-                    >
-                      <svg
-                        fill="none"
-                        height="16"
-                        viewBox="0 0 16 16"
-                        width="16"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M5.392 4.875c0-1.642 1.452-2.98 3.074-2.868 1.638.113 2.874 1.654 2.65 3.264a2.923 2.923 0 0 1-1.89 2.316c-1.216.43-.958 1.9-.958 2.913m-.177 3.004h.346m.079 0c0 .184-.16.244-.248.244-.092 0-.252-.06-.252-.244 0-.197.16-.256.252-.256.088 0 .248.06.248.256Z"
-                          stroke="#000"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="1.5"
-                        />
-                      </svg>
-                    </span>
-                  </button>,
-                }
+                      <path
+                        d="M5.392 4.875c0-1.642 1.452-2.98 3.074-2.868 1.638.113 2.874 1.654 2.65 3.264a2.923 2.923 0 0 1-1.89 2.316c-1.216.43-.958 1.9-.958 2.913m-.177 3.004h.346m.079 0c0 .184-.16.244-.248.244-.092 0-.252-.06-.252-.244 0-.197.16-.256.252-.256.088 0 .248.06.248.256Z"
+                        stroke="#000"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="1.5"
+                      />
+                    </svg>
+                  </span>
+                </button>
               }
               targetElement={
                 Object {
@@ -576,7 +575,7 @@ exports[`Dialog component snapshot should match component snapshot 1`] = `
               tooltip="dialog_title"
             >
               <TooltipPortal
-                active={false}
+                active={null}
                 align={null}
                 animatePosition={false}
                 arrow="center"
@@ -593,7 +592,6 @@ exports[`Dialog component snapshot should match component snapshot 1`] = `
                 id="dialog_id-tooltip"
                 internalId="dialog_id-tooltip"
                 keepInDOM={true}
-                key="tooltip"
                 noAnimation={false}
                 position="top"
                 showDelay={300}
@@ -639,45 +637,43 @@ exports[`Dialog component snapshot should match component snapshot 1`] = `
                   </button>
                 }
                 targetElement={
-                  Object {
-                    "current": <button
-                      aria-describedby="dialog_id-tooltip"
-                      aria-label="dialog_title"
-                      aria-roledescription="Hjelp-knapp"
-                      class="dnb-button dnb-button--secondary dnb-help-button dnb-modal__trigger dnb-button--icon-position-left dnb-button--has-icon dnb-button--size-medium"
-                      id="dialog_id"
-                      type="button"
+                  <button
+                    aria-describedby="dialog_id-tooltip"
+                    aria-label="dialog_title"
+                    aria-roledescription="Hjelp-knapp"
+                    class="dnb-button dnb-button--secondary dnb-help-button dnb-modal__trigger dnb-button--icon-position-left dnb-button--has-icon dnb-button--size-medium"
+                    id="dialog_id"
+                    type="button"
+                  >
+                    <span
+                      aria-hidden="true"
+                      class="dnb-button__alignment"
                     >
-                      <span
-                        aria-hidden="true"
-                        class="dnb-button__alignment"
+                      ‌
+                    </span>
+                    <span
+                      aria-hidden="true"
+                      class="dnb-icon dnb-icon--default dnb-button__icon dnb-icon--inherit-color"
+                      data-testid="question icon"
+                      role="presentation"
+                    >
+                      <svg
+                        fill="none"
+                        height="16"
+                        viewBox="0 0 16 16"
+                        width="16"
+                        xmlns="http://www.w3.org/2000/svg"
                       >
-                        ‌
-                      </span>
-                      <span
-                        aria-hidden="true"
-                        class="dnb-icon dnb-icon--default dnb-button__icon dnb-icon--inherit-color"
-                        data-testid="question icon"
-                        role="presentation"
-                      >
-                        <svg
-                          fill="none"
-                          height="16"
-                          viewBox="0 0 16 16"
-                          width="16"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            d="M5.392 4.875c0-1.642 1.452-2.98 3.074-2.868 1.638.113 2.874 1.654 2.65 3.264a2.923 2.923 0 0 1-1.89 2.316c-1.216.43-.958 1.9-.958 2.913m-.177 3.004h.346m.079 0c0 .184-.16.244-.248.244-.092 0-.252-.06-.252-.244 0-.197.16-.256.252-.256.088 0 .248.06.248.256Z"
-                            stroke="#000"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="1.5"
-                          />
-                        </svg>
-                      </span>
-                    </button>,
-                  }
+                        <path
+                          d="M5.392 4.875c0-1.642 1.452-2.98 3.074-2.868 1.638.113 2.874 1.654 2.65 3.264a2.923 2.923 0 0 1-1.89 2.316c-1.216.43-.958 1.9-.958 2.913m-.177 3.004h.346m.079 0c0 .184-.16.244-.248.244-.092 0-.252-.06-.252-.244 0-.197.16-.256.252-.256.088 0 .248.06.248.256Z"
+                          stroke="#000"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="1.5"
+                        />
+                      </svg>
+                    </span>
+                  </button>
                 }
                 targetSelector={null}
                 tooltip="dialog_title"
@@ -706,7 +702,7 @@ exports[`Dialog component snapshot should match component snapshot 1`] = `
                   }
                 >
                   <TooltipContainer
-                    active={false}
+                    active={null}
                     align={null}
                     animatePosition={false}
                     arrow="center"
@@ -812,8 +808,11 @@ exports[`Dialog component snapshot should match component snapshot 1`] = `
                     <span
                       aria-hidden={true}
                       className="dnb-tooltip"
+                      onMouseDown={[Function]}
                       onMouseEnter={[Function]}
                       onMouseLeave={[Function]}
+                      onMouseMove={[Function]}
+                      onTouchStart={[Function]}
                       role="tooltip"
                       style={Object {}}
                     >

packages/dnb-eufemia/src/components/drawer/__tests__/__snapshots__/Drawer.test.tsx.snap
@@ -468,6 +468,7 @@ exports[`Drawer component snapshot should match component snapshot 1`] = `
             tooltip="drawer_title"
           >
             <TooltipWithEvents
+              active={null}
               align={null}
               animatePosition={false}
               arrow="center"
@@ -489,45 +490,43 @@ exports[`Drawer component snapshot should match component snapshot 1`] = `
               size="basis"
               skipPortal={null}
               target={
-                Object {
-                  "current": <button
-                    aria-describedby="drawer_id-tooltip"
-                    aria-label="drawer_title"
-                    aria-roledescription="Hjelp-knapp"
-                    class="dnb-button dnb-button--secondary dnb-help-button dnb-modal__trigger dnb-button--icon-position-left dnb-button--has-icon dnb-button--size-medium"
-                    id="drawer_id"
-                    type="button"
+                <button
+                  aria-describedby="drawer_id-tooltip"
+                  aria-label="drawer_title"
+                  aria-roledescription="Hjelp-knapp"
+                  class="dnb-button dnb-button--secondary dnb-help-button dnb-modal__trigger dnb-button--icon-position-left dnb-button--has-icon dnb-button--size-medium"
+                  id="drawer_id"
+                  type="button"
+                >
+                  <span
+                    aria-hidden="true"
+                    class="dnb-button__alignment"
                   >
-                    <span
-                      aria-hidden="true"
-                      class="dnb-button__alignment"
+                    ‌
+                  </span>
+                  <span
+                    aria-hidden="true"
+                    class="dnb-icon dnb-icon--default dnb-button__icon dnb-icon--inherit-color"
+                    data-testid="question icon"
+                    role="presentation"
+                  >
+                    <svg
+                      fill="none"
+                      height="16"
+                      viewBox="0 0 16 16"
+                      width="16"
+                      xmlns="http://www.w3.org/2000/svg"
                     >
-                      ‌
-                    </span>
-                    <span
-                      aria-hidden="true"
-                      class="dnb-icon dnb-icon--default dnb-button__icon dnb-icon--inherit-color"
-                      data-testid="question icon"
-                      role="presentation"
-                    >
-                      <svg
-                        fill="none"
-                        height="16"
-                        viewBox="0 0 16 16"
-                        width="16"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M5.392 4.875c0-1.642 1.452-2.98 3.074-2.868 1.638.113 2.874 1.654 2.65 3.264a2.923 2.923 0 0 1-1.89 2.316c-1.216.43-.958 1.9-.958 2.913m-.177 3.004h.346m.079 0c0 .184-.16.244-.248.244-.092 0-.252-.06-.252-.244 0-.197.16-.256.252-.256.088 0 .248.06.248.256Z"
-                          stroke="#000"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="1.5"
-                        />
-                      </svg>
-                    </span>
-                  </button>,
-                }
+                      <path
+                        d="M5.392 4.875c0-1.642 1.452-2.98 3.074-2.868 1.638.113 2.874 1.654 2.65 3.264a2.923 2.923 0 0 1-1.89 2.316c-1.216.43-.958 1.9-.958 2.913m-.177 3.004h.346m.079 0c0 .184-.16.244-.248.244-.092 0-.252-.06-.252-.244 0-.197.16-.256.252-.256.088 0 .248.06.248.256Z"
+                        stroke="#000"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="1.5"
+                      />
+                    </svg>
+                  </span>
+                </button>
               }
               targetElement={
                 Object {
@@ -574,7 +573,7 @@ exports[`Drawer component snapshot should match component snapshot 1`] = `
               tooltip="drawer_title"
             >
               <TooltipPortal
-                active={false}
+                active={null}
                 align={null}
                 animatePosition={false}
                 arrow="center"
@@ -591,7 +590,6 @@ exports[`Drawer component snapshot should match component snapshot 1`] = `
                 id="drawer_id-tooltip"
                 internalId="drawer_id-tooltip"
                 keepInDOM={true}
-                key="tooltip"
                 noAnimation={false}
                 position="top"
                 showDelay={300}
@@ -637,45 +635,43 @@ exports[`Drawer component snapshot should match component snapshot 1`] = `
                   </button>
                 }
                 targetElement={
-                  Object {
-                    "current": <button
-                      aria-describedby="drawer_id-tooltip"
-                      aria-label="drawer_title"
-                      aria-roledescription="Hjelp-knapp"
-                      class="dnb-button dnb-button--secondary dnb-help-button dnb-modal__trigger dnb-button--icon-position-left dnb-button--has-icon dnb-button--size-medium"
-                      id="drawer_id"
-                      type="button"
+                  <button
+                    aria-describedby="drawer_id-tooltip"
+                    aria-label="drawer_title"
+                    aria-roledescription="Hjelp-knapp"
+                    class="dnb-button dnb-button--secondary dnb-help-button dnb-modal__trigger dnb-button--icon-position-left dnb-button--has-icon dnb-button--size-medium"
+                    id="drawer_id"
+                    type="button"
+                  >
+                    <span
+                      aria-hidden="true"
+                      class="dnb-button__alignment"
                     >
-                      <span
-                        aria-hidden="true"
-                        class="dnb-button__alignment"
+                      ‌
+                    </span>
+                    <span
+                      aria-hidden="true"
+                      class="dnb-icon dnb-icon--default dnb-button__icon dnb-icon--inherit-color"
+                      data-testid="question icon"
+                      role="presentation"
+                    >
+                      <svg
+                        fill="none"
+                        height="16"
+                        viewBox="0 0 16 16"
+                        width="16"
+                        xmlns="http://www.w3.org/2000/svg"
                       >
-                        ‌
-                      </span>
-                      <span
-                        aria-hidden="true"
-                        class="dnb-icon dnb-icon--default dnb-button__icon dnb-icon--inherit-color"
-                        data-testid="question icon"
-                        role="presentation"
-                      >
-                        <svg
-                          fill="none"
-                          height="16"
-                          viewBox="0 0 16 16"
-                          width="16"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            d="M5.392 4.875c0-1.642 1.452-2.98 3.074-2.868 1.638.113 2.874 1.654 2.65 3.264a2.923 2.923 0 0 1-1.89 2.316c-1.216.43-.958 1.9-.958 2.913m-.177 3.004h.346m.079 0c0 .184-.16.244-.248.244-.092 0-.252-.06-.252-.244 0-.197.16-.256.252-.256.088 0 .248.06.248.256Z"
-                            stroke="#000"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="1.5"
-                          />
-                        </svg>
-                      </span>
-                    </button>,
-                  }
+                        <path
+                          d="M5.392 4.875c0-1.642 1.452-2.98 3.074-2.868 1.638.113 2.874 1.654 2.65 3.264a2.923 2.923 0 0 1-1.89 2.316c-1.216.43-.958 1.9-.958 2.913m-.177 3.004h.346m.079 0c0 .184-.16.244-.248.244-.092 0-.252-.06-.252-.244 0-.197.16-.256.252-.256.088 0 .248.06.248.256Z"
+                          stroke="#000"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="1.5"
+                        />
+                      </svg>
+                    </span>
+                  </button>
                 }
                 targetSelector={null}
                 tooltip="drawer_title"
@@ -704,7 +700,7 @@ exports[`Drawer component snapshot should match component snapshot 1`] = `
                   }
                 >
                   <TooltipContainer
-                    active={false}
+                    active={null}
                     align={null}
                     animatePosition={false}
                     arrow="center"
@@ -810,8 +806,11 @@ exports[`Drawer component snapshot should match component snapshot 1`] = `
                     <span
                       aria-hidden={true}
                       className="dnb-tooltip"
+                      onMouseDown={[Function]}
                       onMouseEnter={[Function]}
                       onMouseLeave={[Function]}
+                      onMouseMove={[Function]}
+                      onTouchStart={[Function]}
                       role="tooltip"
                       style={Object {}}
                     >

commit 1e528e333ead9e14b9e7d4209147ce60544a1299

fix(Tooltip): alignment issues (#4968)
Fixes https://github.com/dnbexperience/eufemia/issues/4931

The error/bug was introduced in
https://github.com/dnbexperience/eufemia/pull/1613/files#diff-b2805e65ae4b7b277abc5b023aae4c0962edf5c86c8fd5dcdcbdf7350f1ed536R224

packages/dnb-eufemia/src/components/tooltip/TooltipContainer.tsx
@@ -223,7 +223,8 @@ export default function TooltipContainer(
 
     if (stylesFromPosition[position]) {
       stylesFromPosition[position]()
-    } else if (stylesFromArrow[arrow]) {
+    }
+    if (stylesFromArrow[arrow]) {
       stylesFromArrow[arrow]()
     }
 