4f7e6fcfd77f2b42c1d127e379784c1ae1c0bda5
Joakim Bjerknes <joakbjerk@gmail.com>
2025-04-22 10:38:37 +0200

    fix test

--- packages/dnb-eufemia/src/components/date-picker/__tests__/DatePicker.test.tsx
+++ packages/dnb-eufemia/src/components/date-picker/__tests__/DatePicker.test.tsx
@@ -3226,7 +3226,7 @@ describe('DatePicker component', () => {
 
     render(<Component />)
 
-    await userEvent.click(screen.getByLabelText('åpne datovelger'))
+    await userEvent.click(screen.getByLabelText('Åpne datovelger'))
 
     // Check date
     expect(
@@ -3270,7 +3270,7 @@ describe('DatePicker component', () => {
       </StrictMode>
     )
 
-    await userEvent.click(screen.getByLabelText('åpne datovelger'))
+    await userEvent.click(screen.getByLabelText('Åpne datovelger'))
 
     // Check date
     expect(
@@ -3305,7 +3305,7 @@ describe('DatePicker component', () => {
       />
     )
 
-    await userEvent.click(screen.getByLabelText('åpne datovelger'))
+    await userEvent.click(screen.getByLabelText('Åpne datovelger'))
 
     const [monthTitle, yearTitle] = Array.from(
       document.querySelectorAll('.dnb-date-picker__header__title')
@@ -3400,7 +3400,7 @@ describe('DatePicker component', () => {
       />
     )
 
-    await userEvent.click(screen.getByLabelText('åpne datovelger'))
+    await userEvent.click(screen.getByLabelText('Åpne datovelger'))
 
     const [, yearTitle] = Array.from(
       document.querySelectorAll('.dnb-date-picker__header__title')
