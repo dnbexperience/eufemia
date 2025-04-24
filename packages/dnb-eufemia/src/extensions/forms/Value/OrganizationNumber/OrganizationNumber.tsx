fb8987c41ffa7634fd3e511237dfc0b62bd07e60
Anders <anderslangseth@gmail.com>
2025-04-24 21:33:58 +0200

    fix(Value.OrganizationNumber): should not render when value is empty (#4997)

--- packages/dnb-eufemia/src/extensions/forms/Value/OrganizationNumber/OrganizationNumber.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Value/OrganizationNumber/OrganizationNumber.tsx
@@ -5,6 +5,7 @@ import {
   cleanNumber,
 } from '../../../../components/number-format/NumberUtils'
 import useTranslation from '../../hooks/useTranslation'
+import { isValueEmpty } from '../../ValueBlock'
 
 export type Props = StringValueProps
 
@@ -12,6 +13,9 @@ function OrganizationNumber(props: Props) {
   const translations = useTranslation().OrganizationNumber
 
   const toInput = useCallback((value) => {
+    if (isValueEmpty(value)) {
+      return undefined
+    }
     return format(cleanNumber(value), {
       org: true,
     }).toString()
