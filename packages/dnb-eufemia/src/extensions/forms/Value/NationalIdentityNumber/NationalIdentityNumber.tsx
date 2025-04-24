7982070b26253f33866b977e18a6c9aaa28a720a
Anders <anderslangseth@gmail.com>
2025-04-24 21:33:44 +0200

    fix(Value.NationalIdentityNumber): should not render when value is empty (#4995)

--- packages/dnb-eufemia/src/extensions/forms/Value/NationalIdentityNumber/NationalIdentityNumber.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Value/NationalIdentityNumber/NationalIdentityNumber.tsx
@@ -5,6 +5,7 @@ import {
   cleanNumber,
 } from '../../../../components/number-format/NumberUtils'
 import useTranslation from '../../hooks/useTranslation'
+import { isValueEmpty } from '../../ValueBlock'
 
 export type Props = StringValueProps
 
@@ -12,6 +13,9 @@ function NationalIdentityNumber(props: Props) {
   const translations = useTranslation().NationalIdentityNumber
 
   const toInput = useCallback((value) => {
+    if (isValueEmpty(value)) {
+      return undefined
+    }
     return format(cleanNumber(value), {
       nin: true,
     }).toString()
