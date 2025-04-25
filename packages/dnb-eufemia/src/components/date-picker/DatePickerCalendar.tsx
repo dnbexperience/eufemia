549a1b51ab9c99a21f650531052c32aa025196b8
Joakim Bjerknes <joakbjerk@gmail.com>
2025-04-25 12:24:26 +0200

    fix(DatePicker): prevent selection of invalid dates when navigating the calendar with arrow keys (#5006)

--- packages/dnb-eufemia/src/components/date-picker/DatePickerCalendar.tsx
+++ packages/dnb-eufemia/src/components/date-picker/DatePickerCalendar.tsx
@@ -278,18 +278,14 @@ function DatePickerCalendar(restOfProps: DatePickerCalendarProps) {
 
   const findValid = useCallback(
     (date: Date, keyCode: string) => {
-      if (!onDaysRender) {
-        return date
-      }
-
-      if (!days.current) {
+      if (!onDaysRender || !days.current) {
         return date
       }
 
       const month = format(date, 'yyyy-MM')
 
+      // re-render with new month
       if (!days.current[month]) {
-        // re-render with new month
         getDays(date)
       }
 
@@ -305,7 +301,7 @@ function DatePickerCalendar(restOfProps: DatePickerCalendarProps) {
             foundDate.isInactive)
         ) {
           const nextDate = keyNavCalc(foundDate.date, keyCode)
-          foundDate.date = findValid(nextDate, keyCode)
+          return findValid(nextDate, keyCode)
         }
 
         if (foundDate?.date) {
@@ -485,17 +481,20 @@ function DatePickerCalendar(restOfProps: DatePickerCalendarProps) {
 
     let count = 0
 
-    return (cache.current[cacheKey] = Object.values(
-      getDays(month).reduce((acc, cur, i) => {
-        // Normalize the data for table consumption
-        acc[count] = acc[count] || []
-        acc[count].push(cur)
-        if (i % 7 === 6) {
-          count++
-        }
-        return acc
-      }, {})
-    ))
+    const days = getDays(month).reduce((acc, cur, i) => {
+      // Normalize the data for table consumption
+      acc[count] = acc[count] || []
+      acc[count].push(cur)
+      if (i % 7 === 6) {
+        count++
+      }
+
+      return acc
+    }, {})
+
+    cache.current[cacheKey] = Object.values(days)
+
+    return cache.current[cacheKey]
   }, [cacheKey, getDays, month])
 
   return (
