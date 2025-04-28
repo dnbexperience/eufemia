3035a11513fbab9545a4ab8e4f11ea5e7d1a3fe2
Joakim Bjerknes <joakbjerk@gmail.com>
2025-04-28 12:56:24 +0200

    update label text

--- packages/dnb-eufemia/src/components/date-picker/DatePicker.tsx
+++ packages/dnb-eufemia/src/components/date-picker/DatePicker.tsx
@@ -577,7 +577,7 @@ const defaultProps: DatePickerProps = {
   showInput: false,
   submitButtonText: 'Ok',
   cancelButtonText: 'Avbryt',
-  resetButtonText: 'Tilbakestill',
+  resetButtonText: 'Nullstill',
   resetDate: true,
   firstDay: 'monday',
   range: false,
