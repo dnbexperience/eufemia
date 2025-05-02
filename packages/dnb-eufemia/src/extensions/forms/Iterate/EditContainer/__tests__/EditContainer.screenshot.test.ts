827d1e8c36fbf41e356912bd1c3050b9bf39ffa6
Tobias Høegh <tobias@tujo.no>
2025-05-02 13:27:05 +0200

    Add visual test

---
+++ packages/dnb-eufemia/src/extensions/forms/Iterate/EditContainer/__tests__/EditContainer.screenshot.test.ts
@@ -0,0 +1,21 @@
+/**
+ * Screenshot Test
+ * This file will not run on "test:staged" because we don't require any related files
+ */
+
+import { makeScreenshot } from '../../../../../core/jest/jestSetupScreenshots'
+
+describe('Iterate.EditContainer', () => {
+  const url = '/uilib/extensions/forms/Iterate/EditContainer/demos'
+
+  it('have to match edit container with error', async () => {
+    const screenshot = await makeScreenshot({
+      url,
+      selector: '[data-visual-test="edit-container-error"]',
+      simulate: 'click',
+      simulateSelector:
+        '[data-visual-test="edit-container-error"] .dnb-forms-submit-button',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+})
