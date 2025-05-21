dc273ab450c1ba32911b31fd6630511369641e88
Tobias Høegh <tobias@tujo.no>
2025-05-21 08:12:47 +0200

    Add entry for v11 about the deprecation

--- packages/dnb-eufemia/src/components/date-picker/DatePicker.tsx
+++ packages/dnb-eufemia/src/components/date-picker/DatePicker.tsx
@@ -368,14 +368,16 @@ type DatePickerDeprecatedProps = {
   /**
    * @deprecated use `Field.Date` instead, for {@link https://eufemia.dnb.no/uilib/extensions/forms/feature-fields/Date/#date-limit-validation | built in validation}.
    * It's not good UX, or best practice to automatically change the user input. This often leads to confusion, as what they typed in, magically changes for seemingly no reason. It's better to inform them about the error and let them correct it themselves.
+   *
+   * Deprecated – can be removed in v11
    */
-  // Deprecated – can be removed in v11
   correctInvalidDate?: boolean
   /**
    * @deprecated use `Field.Date` instead, for {@link https://eufemia.dnb.no/uilib/extensions/forms/feature-fields/Date/#date-limit-validation | built in validation}.
    * It's not good UX, or best practice to automatically change the user input. This often leads to confusion, as what they typed in, magically changes for seemingly no reason. It's better to inform them about the error and let them correct it themselves.
+   *
+   * Deprecated – can be removed in v11
    */
-  // Deprecated – can be removed in v11
   correct_invalid_date?: boolean
   /**
    * @deprecated use `maskOrder` instead.
