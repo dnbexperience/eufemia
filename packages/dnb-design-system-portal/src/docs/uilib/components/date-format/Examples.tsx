b9bc83dc5b9fb735d4643213c38895088b85853d
Joakim Bjerknes <joakbjerk@gmail.com>
2025-05-22 16:24:04 +0200

    add some demo examples

--- packages/dnb-design-system-portal/src/docs/uilib/components/date-format/Examples.tsx
+++ packages/dnb-design-system-portal/src/docs/uilib/components/date-format/Examples.tsx
@@ -5,10 +5,39 @@
 
 import React from 'react'
 import ComponentBox from '../../../../shared/tags/ComponentBox'
-import { DateFormat } from '@dnb/eufemia/src'
+import styled from '@emotion/styled'
+import { DateFormat, P } from '@dnb/eufemia/src'
 
-export const DateFormatExample = () => (
-  <ComponentBox>
-    <DateFormat date={new Date()} />
-  </ComponentBox>
+const Style = styled.div`
+  p > .dnb-date-format {
+    display: block;
+  }
+`
+
+export const SupportedFormats = () => {
+  return (
+    <Style>
+      <ComponentBox>
+        <P>
+          <DateFormat>2025-08-01</DateFormat>
+          <DateFormat>01.08.2025</DateFormat>
+          <DateFormat>01/08/2025</DateFormat>
+          <DateFormat date={new Date('2025-08-01')} />
+        </P>
+      </ComponentBox>
+    </Style>
+  )
+}
+
+export const DateStyles = () => (
+  <Style>
+    <ComponentBox>
+      <P>
+        <DateFormat dateStyle="full">2025-08-01</DateFormat>
+        <DateFormat dateStyle="long">2025-08-01</DateFormat>
+        <DateFormat dateStyle="medium">2025-08-01</DateFormat>
+        <DateFormat dateStyle="short">2025-08-01</DateFormat>
+      </P>
+    </ComponentBox>
+  </Style>
 )
