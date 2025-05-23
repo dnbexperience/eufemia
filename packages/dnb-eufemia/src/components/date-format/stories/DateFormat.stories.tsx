b25d018d9edef11d87d525343a9823d56b587cc7
Joakim Bjerknes <joakbjerk@gmail.com>
2025-05-23 17:14:39 +0200

    add examples to story

--- packages/dnb-eufemia/src/components/date-format/stories/DateFormat.stories.tsx
+++ packages/dnb-eufemia/src/components/date-format/stories/DateFormat.stories.tsx
@@ -6,116 +6,56 @@
 import React from 'react'
 import DateFormat from '../'
 import Card from '../../Card'
+import { H2 } from '../../../elements'
 
 export default {
   title: 'Eufemia/Components/DateFormat',
 }
 
 export function DateFormatExample() {
-  const date = new Date('2025-11-02')
-
   return (
     <Card stack>
-      {/* <DateFormat date="2025-11-01" variant="long" />
-      <DateFormat date={new Date('2027-05-03')} variant="long" />
-      <DateFormat variant="long">2026-12-02|2027-05-03</DateFormat>
+      <Card stack bottom="large">
+        <H2>Date styles</H2>
+        <DateFormat date="2025-08-01" dateStyle="full" />
+        <DateFormat date="2025-08-01" dateStyle="long" />
+        <DateFormat date="2025-08-01" dateStyle="medium" />
+        <DateFormat date="2025-08-01" dateStyle="short" />
+      </Card>
 
-      <DateFormat date="2025-11-01" variant="numeric" />
-      <DateFormat date={new Date('2027-05-03')} variant="numeric" />
-      <DateFormat variant="numeric">2026-12-02</DateFormat>
+      <Card stack>
+        <H2>Weekday styles</H2>
+        <DateFormat date="2025-08-01" weekday="long" />
+        <DateFormat date="2025-08-01" weekday="short" />
+        <DateFormat date="2025-08-01" weekday="narrow" />
+      </Card>
 
-      <DateFormat date="2025-11-01" variant="short" />
-      <DateFormat date={new Date('2027-05-03')} variant="short" />
-      <DateFormat variant="short">2026-12-02</DateFormat> */}
+      <Card stack>
+        <H2>Day styles</H2>
+        <DateFormat date="2025-08-01" day="2-digit" />
+        <DateFormat date="2025-08-01" day="numeric" />
+      </Card>
 
-      {/* <DateTimeFormatTest
-        date={new Date()}
-        options={{ weekday: 'long', day: '2-digit' }}
-      />
-      <DateTimeFormatTest
-        date={new Date()}
-        options={{ weekday: 'short', day: '2-digit' }}
-      />
-      <DateTimeFormatTest
-        date={new Date()}
-        options={{ weekday: 'narrow', day: 'numeric' }}
-      /> */}
+      <Card stack>
+        <H2>Day styles</H2>
+        <DateFormat date="2025-08-01" day="2-digit" />
+        <DateFormat date="2025-08-01" day="numeric" />
+      </Card>
 
-      <DateFormat date={date} dateStyle="full" weekday="long" />
-      <DateFormat date={date} dateStyle="long" />
-      <DateFormat date={date} dateStyle="medium" />
-      <DateFormat date={date} dateStyle="short" />
+      <Card stack>
+        <H2>Month styles</H2>
+        <DateFormat date="2025-08-01" month="long" />
+        <DateFormat date="2025-08-01" month="short" />
+        <DateFormat date="2025-08-01" month="narrow" />
+        <DateFormat date="2025-08-01" month="2-digit" />
+        <DateFormat date="2025-08-01" month="numeric" />
+      </Card>
 
-      <DateTimeFormatTest
-        date={date}
-        options={{
-          weekday: 'long',
-          month: 'long',
-          day: 'numeric',
-          year: 'numeric',
-        }}
-      />
-      <DateTimeFormatTest
-        date={date}
-        options={{
-          // weekday: 'long',
-          // month: 'long',
-          // day: 'numeric',
-          // year: 'numeric',
-          dateStyle: 'full',
-        }}
-      />
-      <DateTimeFormatTest
-        date={date}
-        options={{
-          // weekday: 'long',
-          // month: 'long',
-          // day: 'numeric',
-          // year: 'numeric',
-          dateStyle: 'long',
-        }}
-      />
-      <DateTimeFormatTest
-        date={date}
-        options={{
-          // weekday: 'long',
-          // month: 'long',
-          // day: 'numeric',
-          // year: 'numeric',
-          dateStyle: 'medium',
-        }}
-      />
-      <DateTimeFormatTest
-        date={date}
-        options={{
-          // weekday: 'long',
-          // month: 'long',
-          // day: 'numeric',
-          // year: 'numeric',
-
-          timeStyle: 'short',
-        }}
-      />
+      <Card stack>
+        <H2>Year styles</H2>
+        <DateFormat date="2025-08-01" year="2-digit" />
+        <DateFormat date="2025-08-01" year="numeric" />
+      </Card>
     </Card>
-  )
-}
-
-type DateTimeFormatTestOptions = {
-  date: Date
-  options: Intl.DateTimeFormatOptions
-}
-
-function DateTimeFormatTest({ date, options }: DateTimeFormatTestOptions) {
-  const formattedDate = new Intl.DateTimeFormat('nb-NO', options).format(
-    date
-  )
-  console.log('Object.entries(options)', Object.entries(options))
-  return (
-    <p>
-      {Object.keys(options)
-        .map((o) => `${o}-${options[o]}`)
-        .join('-')}
-      : {formattedDate}
-    </p>
   )
 }
