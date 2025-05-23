f911666e16ba0aee8f5cf9f19e89cb30d85f3461
Joakim Bjerknes <joakbjerk@gmail.com>
2025-05-23 15:25:30 +0200

    add tests for date formatting

--- packages/dnb-eufemia/src/components/date-format/stories/__tests__/DateFormat.test.tsx
+++ packages/dnb-eufemia/src/components/date-format/stories/__tests__/DateFormat.test.tsx
@@ -3,44 +3,145 @@ import { render } from '@testing-library/react'
 import DateFormat from '../../DateFormat'
 
 describe('DateFormat', () => {
-  it('should support dates in `yyyy-MM-dd` format', () => {
-    const { rerender } = render(<DateFormat>2025-05-23</DateFormat>)
+  describe('formats', () => {
+    it('should support dates in `yyyy-MM-dd` format', () => {
+      const { rerender } = render(<DateFormat>2025-05-23</DateFormat>)
 
-    const dateFormat = document.querySelector('.dnb-date-format')
+      const dateFormat = document.querySelector('.dnb-date-format')
 
-    expect(dateFormat).toHaveTextContent('23. mai 2025')
+      expect(dateFormat).toHaveTextContent('23. mai 2025')
 
-    rerender(<DateFormat date="2026-06-08" />)
-    expect(dateFormat).toHaveTextContent('8. juni 2026')
+      rerender(<DateFormat date="2026-06-08" />)
+      expect(dateFormat).toHaveTextContent('8. juni 2026')
+    })
+
+    it('should support dates in `dd.MM.yyyy` format', () => {
+      const { rerender } = render(<DateFormat>23.05.2025</DateFormat>)
+
+      const dateFormat = document.querySelector('.dnb-date-format')
+
+      expect(dateFormat).toHaveTextContent('23. mai 2025')
+
+      rerender(<DateFormat date="08.06.2026" />)
+      expect(dateFormat).toHaveTextContent('8. juni 2026')
+    })
+
+    it('should support dates in `dd/MM/yyyy` format', () => {
+      const { rerender } = render(<DateFormat>23/05/2025</DateFormat>)
+
+      const dateFormat = document.querySelector('.dnb-date-format')
+
+      expect(dateFormat).toHaveTextContent('23. mai 2025')
+
+      rerender(<DateFormat date="08/06/2026" />)
+      expect(dateFormat).toHaveTextContent('8. juni 2026')
+    })
+
+    it('should support dates in `Date object` format', () => {
+      render(<DateFormat date={new Date('2026-06-08')} />)
+
+      const dateFormat = document.querySelector('.dnb-date-format')
+
+      expect(dateFormat).toHaveTextContent('8. juni 2026')
+    })
   })
 
-  it('should support dates in `dd.MM.yyyy` format', () => {
-    const { rerender } = render(<DateFormat>23.05.2025</DateFormat>)
+  describe('formatting date', () => {
+    it('should support formatting the whole date with `dateStyle`', () => {
+      const { rerender } = render(
+        <DateFormat date="2025-08-01" dateStyle="full" />
+      )
 
-    const dateFormat = document.querySelector('.dnb-date-format')
+      const dateFormat = document.querySelector('.dnb-date-format')
 
-    expect(dateFormat).toHaveTextContent('23. mai 2025')
+      expect(dateFormat).toHaveTextContent('fredag 1. august 2025')
 
-    rerender(<DateFormat date="08.06.2026" />)
-    expect(dateFormat).toHaveTextContent('8. juni 2026')
-  })
+      rerender(<DateFormat date="2025-08-01" dateStyle="long" />)
+      expect(dateFormat).toHaveTextContent('1. august 2025')
 
-  it('should support dates in `dd/MM/yyyy` format', () => {
-    const { rerender } = render(<DateFormat>23/05/2025</DateFormat>)
+      rerender(<DateFormat date="2025-08-01" dateStyle="medium" />)
+      expect(dateFormat).toHaveTextContent('1. aug. 2025')
 
-    const dateFormat = document.querySelector('.dnb-date-format')
+      rerender(<DateFormat date="2025-08-01" dateStyle="short" />)
+      expect(dateFormat).toHaveTextContent('01.08.2025')
+    })
 
-    expect(dateFormat).toHaveTextContent('23. mai 2025')
+    it('should support formatting `weekday`', () => {
+      const { rerender } = render(
+        <DateFormat date="2025-05-23" weekday="long" />
+      )
+      const dateFormat = document.querySelector('.dnb-date-format')
 
-    rerender(<DateFormat date="08/06/2026" />)
-    expect(dateFormat).toHaveTextContent('8. juni 2026')
-  })
+      expect(dateFormat).toHaveTextContent('fredag')
 
-  it('should support dates in `Date object` format', () => {
-    render(<DateFormat date={new Date('2026-06-08')} />)
+      rerender(<DateFormat date="2025-05-23" weekday="narrow" />)
+      expect(dateFormat).toHaveTextContent('F')
 
-    const dateFormat = document.querySelector('.dnb-date-format')
+      rerender(<DateFormat date="2025-05-23" weekday="short" />)
+      expect(dateFormat).toHaveTextContent('fre.')
+    })
 
-    expect(dateFormat).toHaveTextContent('8. juni 2026')
+    it('should support formatting `day`', () => {
+      const { rerender } = render(
+        <DateFormat date="2025-05-08" day="2-digit" />
+      )
+      const dateFormat = document.querySelector('.dnb-date-format')
+
+      expect(dateFormat).toHaveTextContent('08.')
+
+      rerender(<DateFormat date="2025-05-08" day="numeric" />)
+      expect(dateFormat).toHaveTextContent('8.')
+    })
+
+    it('should support formatting `month`', () => {
+      const { rerender } = render(
+        <DateFormat date="2025-08-01" month="2-digit" />
+      )
+      const dateFormat = document.querySelector('.dnb-date-format')
+
+      expect(dateFormat).toHaveTextContent('08.')
+
+      rerender(<DateFormat date="2025-08-01" month="long" />)
+      expect(dateFormat).toHaveTextContent('august')
+
+      rerender(<DateFormat date="2025-08-01" month="narrow" />)
+      expect(dateFormat).toHaveTextContent('A')
+
+      rerender(<DateFormat date="2025-08-01" month="numeric" />)
+      expect(dateFormat).toHaveTextContent('8.')
+
+      rerender(<DateFormat date="2025-08-01" month="short" />)
+      expect(dateFormat).toHaveTextContent('aug')
+    })
+
+    it('should support formatting `year`', () => {
+      const { rerender } = render(
+        <DateFormat date="2025-05-08" year="2-digit" />
+      )
+      const dateFormat = document.querySelector('.dnb-date-format')
+
+      expect(dateFormat).toHaveTextContent('25')
+
+      rerender(<DateFormat date="2025-05-08" year="numeric" />)
+      expect(dateFormat).toHaveTextContent('2025')
+    })
+
+    it('should prioritize `weekday`, `day`, `month`, and `year` over `dateStyle`', () => {
+      render(
+        <DateFormat
+          date="2025-08-01"
+          dateStyle="full"
+          weekday="short"
+          day="2-digit"
+          month="short"
+          year="2-digit"
+        />
+      )
+
+      const dateFormat = document.querySelector('.dnb-date-format')
+
+      expect(dateFormat).not.toHaveTextContent('fredag 1. august 2025')
+      expect(dateFormat).toHaveTextContent('fre. 01. aug. 25')
+    })
   })
 })
