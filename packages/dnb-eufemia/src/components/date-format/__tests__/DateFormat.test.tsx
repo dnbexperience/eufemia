f1d89a83926e0b2c404f4c8c1751d5c26ff0bdac
-l <anderslangseth@gmail.com>
2025-06-24 10:01:28 +0200

    chore(DateFormat): move tests out of stories dir

--- packages/dnb-eufemia/src/components/date-format/__tests__/DateFormat.test.tsx
+++ packages/dnb-eufemia/src/components/date-format/__tests__/DateFormat.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import DateFormat from '../../DateFormat'
-import { axeComponent } from '../../../../core/jest/jestSetup'
+import { axeComponent } from '../../../core/jest/jestSetup'
 import { Provider } from '../../../../shared'
 
 describe('DateFormat', () => {
