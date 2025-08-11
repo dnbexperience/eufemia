63b5944c04b883bd7c660417bb747c80511785a7
Anders <anderslangseth@gmail.com>
2025-08-11 14:25:04 +0200

    chore(PushContainer): add visual tests of variants (#5372)

---
+++ packages/dnb-eufemia/src/extensions/forms/Iterate/PushContainer/__tests__/PushContainer.screenshot.test.ts
@@ -0,0 +1,60 @@
+/**
+ * Screenshot Test
+ * This file will not run on "test:staged" because we don't require any related files
+ */
+
+import {
+  makeScreenshot,
+  setupPageScreenshot,
+} from '../../../../../core/jest/jestSetupScreenshots'
+
+const url = '/uilib/extensions/forms/Iterate/PushContainer/demos'
+
+describe('PushContainer', () => {
+  setupPageScreenshot({
+    url,
+  })
+
+  it('have to match variants', async () => {
+    const screenshot = await makeScreenshot({
+      selector: '[data-visual-test="push-container-variants"]',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('have to match variants in error state', async () => {
+    const screenshot = await makeScreenshot({
+      selector: '[data-visual-test="push-container-variants"]',
+      simulateSelector:
+        '[data-visual-test="push-container-variants"] .dnb-forms-next-button',
+      simulate: 'click',
+      recalculateHeightAfterSimulate: true,
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+})
+
+describe('PushContainer on small screen', () => {
+  setupPageScreenshot({
+    url,
+    pageViewport: { width: 640 },
+  })
+
+  it('have to match variants', async () => {
+    const screenshot = await makeScreenshot({
+      selector: '[data-visual-test="push-container-variants"]',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('have to match variants in error state', async () => {
+    const screenshot = await makeScreenshot({
+      selector: '[data-visual-test="push-container-variants"]',
+      simulateSelector:
+        '[data-visual-test="push-container-variants"] .dnb-forms-next-button',
+      simulate: 'click',
+      recalculateHeightAfterSimulate: true,
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+})
