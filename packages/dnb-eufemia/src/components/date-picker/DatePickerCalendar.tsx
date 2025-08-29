f609a1c3cf48b0f0e2a509c6ef42a7ac38772a48
Anders <anderslangseth@gmail.com>
2025-08-29 13:52:05 +0200

    chore: minor text improvements (#5422)

--- packages/dnb-eufemia/src/components/date-picker/DatePickerCalendar.tsx
+++ packages/dnb-eufemia/src/components/date-picker/DatePickerCalendar.tsx
@@ -97,7 +97,7 @@ export type DatePickerCalendarProps = Omit<
     nr: number
   ) => void
   /**
-   * To define the locale used in the calendar. Needs to be an `date-fns` "v2" locale object, like `import enLocale from &#39;date-fns/locale/en-GB&#39;`. Defaults to `nb-NO`.
+   * To define the locale used in the calendar. Needs to be an `date-fns` locale object, like `import enLocale from &#39;date-fns/locale/en-GB&#39;`. Defaults to `nb-NO`.
    */
   locale?: InternalLocale
   rtl?: boolean
