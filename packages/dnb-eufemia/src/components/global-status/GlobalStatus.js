c8235a778d2c5d86c958e138f1cbcf87261f153f
Anders <anderslangseth@gmail.com>
2025-10-01 19:00:52 +0200

    fix(GlobalStatus): scroll into view on Firefox (#5550)

--- packages/dnb-eufemia/src/components/global-status/GlobalStatus.js
+++ packages/dnb-eufemia/src/components/global-status/GlobalStatus.js
@@ -342,7 +342,7 @@ export default class GlobalStatus extends React.PureComponent {
     )
   }
 
-  scrollToStatus(isDone = null) {
+  async scrollToStatus(isDone = null) {
     if (
       typeof window === 'undefined' ||
       isTrue(this.state.globalStatus.autoscroll) === false
@@ -353,6 +353,10 @@ export default class GlobalStatus extends React.PureComponent {
       const element = this._wrapperRef.current
       this._scrollToStatusTimeout = isElementVisible(element, isDone)
       if (element && typeof element.scrollIntoView === 'function') {
+        // wait a tick, to make sure that the element is visible, as firefox needs that
+        // or else it reports:
+        // scroll anchoring was disabled in a scroll container because of too many consecutive adjustments
+        await wait(1)
         element.scrollIntoView({
           block: 'center',
           behavior: 'smooth',
@@ -738,5 +742,7 @@ const isElementVisible = (elem, callback, delayFallback = 1e3) => {
   }
   return null
 }
+
+const wait = (duration) => new Promise((r) => setTimeout(r, duration))
 
 GlobalStatus._supportsSpacingProps = true
