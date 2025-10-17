b42077287d0fc7c81e61109b5cb40111c7e26a0e
Anders <anderslangseth@gmail.com>
2025-10-17 09:51:12 +0200

    break(Table)!: remove `accordion` property (#5661)

--- packages/dnb-eufemia/src/components/table/TableTr.tsx
+++ packages/dnb-eufemia/src/components/table/TableTr.tsx
@@ -87,13 +87,7 @@ export default function Tr(
 
   const tableContext = React.useContext(TableContext)
 
-  // Deprecated – can be removed in v11
-  const deprecatedAccordionProp = tableContext?.allProps?.accordion
-
-  if (
-    deprecatedAccordionProp ||
-    tableContext?.allProps?.mode === 'accordion'
-  ) {
+  if (tableContext?.allProps?.mode == 'accordion') {
     return (
       <TableAccordionHead
         count={count}
