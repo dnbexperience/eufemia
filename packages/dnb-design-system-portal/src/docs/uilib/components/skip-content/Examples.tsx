18a4a42c72d5a162492d746d64eaf79df5b14a0a
Anders <anderslangseth@gmail.com>
2025-10-17 12:38:28 +0200

    break(Checkbox)!: remove `label_sr_only` property (#5647)

--- packages/dnb-design-system-portal/src/docs/uilib/components/skip-content/Examples.tsx
+++ packages/dnb-design-system-portal/src/docs/uilib/components/skip-content/Examples.tsx
@@ -45,7 +45,7 @@ export const SkipContentTable = () => (
 
 const LargeTableWithInteractiveElements = (props) => {
   const TdCheckbox = () => {
-    return <Checkbox label="Select row" label_sr_only />
+    return <Checkbox label="Select row" labelSrOnly />
   }
   const TdInput = () => {
     return <Input label="Label" label_sr_only size={4} />
