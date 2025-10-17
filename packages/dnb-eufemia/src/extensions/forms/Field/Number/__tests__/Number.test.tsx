1e66aee5c32ce58c2b0aea5dccaa042c99c59de7
Anders <anderslangseth@gmail.com>
2025-10-17 09:35:12 +0200

    break(Field.*)!: remove `validator` property (#5686)

--- packages/dnb-eufemia/src/extensions/forms/Field/Number/__tests__/Number.test.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Field/Number/__tests__/Number.test.tsx
@@ -2292,7 +2292,9 @@ describe('Field.Number', () => {
         }
       }
 
-      render(<Field.Number validateContinuously validator={validator} />)
+      render(
+        <Field.Number validateContinuously onChangeValidator={validator} />
+      )
       const input = document.querySelector('input')
 
       // Type a value that exceeds the validator limit
