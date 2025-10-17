95ddb470aff2ce38a3f8dbff479bc2f73a3ebdd1
Anders <anderslangseth@gmail.com>
2025-10-17 09:32:06 +0200

    break(Form.Visibility)!: remove `continuousValidation` property (#5677)

--- packages/dnb-eufemia/src/extensions/forms/Form/Visibility/__tests__/useVisibility.test.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Form/Visibility/__tests__/useVisibility.test.tsx
@@ -428,51 +428,6 @@ describe('useVisibility', () => {
         ).toBe(true)
       })
 
-      it('should return true immediately when "continuousValidation" is true', () => {
-        const { result } = renderHook(useVisibility, {
-          wrapper: ({ children }) => (
-            <Provider>
-              <Field.Number path="/myPath" required minimum={2} />
-              {children}
-            </Provider>
-          ),
-        })
-
-        expect(
-          result.current.check({
-            visibleWhen: {
-              path: '/myPath',
-              isValid: true,
-              continuousValidation: true,
-            },
-          })
-        ).toBe(false)
-
-        fireEvent.focus(document.querySelector('input'))
-        fireEvent.change(document.querySelector('input'), {
-          target: { value: '2' },
-        })
-        expect(
-          result.current.check({
-            visibleWhen: {
-              path: '/myPath',
-              isValid: true,
-              continuousValidation: true,
-            },
-          })
-        ).toBe(true)
-
-        fireEvent.blur(document.querySelector('input'))
-        expect(
-          result.current.check({
-            visibleWhen: {
-              path: '/myPath',
-              isValid: true,
-            },
-          })
-        ).toBe(true)
-      })
-
       it('should return true immediately when "validateContinuously" is true', () => {
         const { result } = renderHook(useVisibility, {
           wrapper: ({ children }) => (
