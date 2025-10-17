60ed4a6f184816994c4643328e77120c429254ae
Anders <anderslangseth@gmail.com>
2025-10-17 09:40:15 +0200

    break(Forms)!: remove `Form.useErrorMessage` hook (#5691)

--- packages/dnb-eufemia/src/extensions/forms/hooks/index.ts
+++ packages/dnb-eufemia/src/extensions/forms/hooks/index.ts
@@ -4,6 +4,3 @@ export { default as useValueProps } from './useValueProps'
 export { default as useExternalValue } from './useExternalValue'
 export { default as useProcessManager } from './useProcessManager'
 export { default as useTranslation } from './useTranslation'
-
-/** @deprecated – can be removed in v11 */
-export { default as useErrorMessage } from './useErrorMessage'
