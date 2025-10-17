9dd684c06700fbb1bd6d9f6c0ee53ddf1e166fea
Anders <anderslangseth@gmail.com>
2025-10-17 12:57:35 +0200

    break(NumberFormat)!: remove `omit_rounding` property (#5634)

--- packages/dnb-eufemia/src/components/number-format/NumberFormat.tsx
+++ packages/dnb-eufemia/src/components/number-format/NumberFormat.tsx
@@ -94,7 +94,6 @@ export type NumberFormatProps = {
   always_selectall?: boolean
   copy_selection?: boolean
   clean_copy_value?: boolean
-  omit_rounding?: boolean
   rounding?: 'omit' | 'half-even' | 'half-up'
   signDisplay?: NumberFormatSignDisplay
   clean?: boolean
@@ -142,7 +141,6 @@ export default class NumberFormat extends React.PureComponent<NumberFormatAllPro
     always_selectall: false,
     copy_selection: true,
     clean_copy_value: false,
-    omit_rounding: null,
     rounding: null,
     clean: null,
     srLabel: null,
@@ -326,7 +324,6 @@ export default class NumberFormat extends React.PureComponent<NumberFormatAllPro
       options,
       locale,
       decimals,
-      omit_rounding,
       rounding,
       signDisplay,
       clean,
@@ -364,7 +361,6 @@ export default class NumberFormat extends React.PureComponent<NumberFormatAllPro
       org,
       percent,
       decimals,
-      omit_rounding: isTrue(omit_rounding),
       rounding,
       signDisplay,
       options,
