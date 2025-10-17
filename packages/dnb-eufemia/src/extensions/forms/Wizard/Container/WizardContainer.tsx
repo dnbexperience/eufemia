572514610385252d464ed2f23698ff9db89639c7
Anders <anderslangseth@gmail.com>
2025-10-17 10:26:14 +0200

    break(StepIndicator)!: remove `sidebar_id` property (#5699)

--- packages/dnb-eufemia/src/extensions/forms/Wizard/Container/WizardContainer.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Wizard/Container/WizardContainer.tsx
@@ -73,11 +73,6 @@ export type Props = ComponentProps & {
   onStepChange?: OnStepChange
 
   /**
-   * @deprecated there is no longer a sidebar. This prop does nothing.
-   */
-  sidebarId?: string
-
-  /**
    * If set to `true`, the wizard will not animate the steps.
    */
   noAnimation?: boolean
@@ -110,11 +105,6 @@ export type Props = ComponentProps & {
   children: React.ReactNode
 }
 
-function handleDeprecatedProps(props: Props): Omit<Props, 'sidebarId'> {
-  const { sidebarId, ...rest } = props
-  return rest
-}
-
 function WizardContainer(props: Props) {
   const {
     className,
@@ -132,7 +122,7 @@ function WizardContainer(props: Props) {
     validationMode,
     outset = true,
     ...rest
-  } = handleDeprecatedProps(props)
+  } = props
 
   const dataContext = useContext(DataContext)
   const {
