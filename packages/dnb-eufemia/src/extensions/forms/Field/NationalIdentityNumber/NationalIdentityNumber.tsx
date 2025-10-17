3df307f175d9740ee8528125cdf5a845f3a3dea0
Anders <anderslangseth@gmail.com>
2025-10-17 09:34:58 +0200

    break(FormError)!: remove `validationRule` (#5685)

--- packages/dnb-eufemia/src/extensions/forms/Field/NationalIdentityNumber/NationalIdentityNumber.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Field/NationalIdentityNumber/NationalIdentityNumber.tsx
@@ -199,10 +199,7 @@ export function createMinimumAgeValidator(age: number) {
 
     if (!identificationNumberIs7DigitsOrMore) {
       return new FormError(
-        'NationalIdentityNumber.errorMinimumAgeValidatorLength',
-        {
-          validationRule: 'errorMinimumAgeValidatorLength', // "validationRule" Will be removed in future PR
-        }
+        'NationalIdentityNumber.errorMinimumAgeValidatorLength'
       )
     }
 
@@ -215,10 +212,7 @@ export function createMinimumAgeValidator(age: number) {
 
     return new FormError(
       'NationalIdentityNumber.errorMinimumAgeValidator',
-      {
-        validationRule: 'errorMinimumAgeValidator', // "validationRule" Will be removed in future PR
-        messageValues: { age: String(age) },
-      }
+      { messageValues: { age: String(age) } }
     )
   }
 }
