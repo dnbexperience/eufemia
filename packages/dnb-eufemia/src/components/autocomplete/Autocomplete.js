9a01eac699137e4067749e68b0778d588339b3ef
Anders <anderslangseth@gmail.com>
2025-10-20 13:36:52 +0200

    break(Dropdown, Autocomplete, DrawerList)!: remove `fixed_postion` property (#5759)

--- packages/dnb-eufemia/src/components/autocomplete/Autocomplete.js
+++ packages/dnb-eufemia/src/components/autocomplete/Autocomplete.js
@@ -1780,7 +1780,7 @@ class AutocompleteInstance extends React.PureComponent {
       input_icon,
       size,
       align_autocomplete,
-      fixed_position,
+      fixedPosition,
       status,
       status_state,
       status_props,
@@ -2096,7 +2096,7 @@ class AutocompleteInstance extends React.PureComponent {
                 keepOpen={keepOpen}
                 preventClose={preventClose}
                 align_drawer={align_autocomplete}
-                fixed_position={fixed_position}
+                fixedPosition={fixedPosition}
                 disabled={disabled}
                 max_height={max_height}
                 direction={direction}
