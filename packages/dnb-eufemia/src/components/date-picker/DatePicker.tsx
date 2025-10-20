d2e33cecd37c4a184269c73bd4b4994328f83463
Anders <anderslangseth@gmail.com>
2025-10-20 15:11:42 +0200

    chore(DatePicker): revert prevent_close -> preventClose (#5779)

--- packages/dnb-eufemia/src/components/date-picker/DatePicker.tsx
+++ packages/dnb-eufemia/src/components/date-picker/DatePicker.tsx
@@ -499,7 +499,7 @@ type DatePickerDeprecatedProps = {
   /**
    * @deprecated use `preventClose` instead.
    */
-  preventClose?: boolean
+  prevent_close?: boolean
   /**
    * @deprecated use `no_animation` instead.
    */
