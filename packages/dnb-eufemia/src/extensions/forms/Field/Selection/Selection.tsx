46adcd412e3956e1b57c734e7a29da8ad0a0f08e
Anders <anderslangseth@gmail.com>
2025-10-20 14:18:44 +0200

    break(Dropdown, Autocomplete, DrawerList)!: remove `ignore_events` property (#5775)

--- packages/dnb-eufemia/src/extensions/forms/Field/Selection/Selection.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Field/Selection/Selection.tsx
@@ -136,7 +136,6 @@ export type Props = FieldProps<IOption['value']> & {
 const validDrawerListProps = [
   // DrawerList Properties
   'label_direction',
-  'ignore_events',
   'no_animation',
 
   // DrawerList Events
