53131dd6c316a610f5be7ccfd71d8cfdd42d1df6
-l <anderslangseth@gmail.com>
2025-10-20 13:24:04 +0200

    break(Dropdown, Autocomplete, DrawerList): remove prevent_selection property

--- packages/dnb-eufemia/src/components/autocomplete/Autocomplete.js
+++ packages/dnb-eufemia/src/components/autocomplete/Autocomplete.js
@@ -161,7 +161,7 @@ export default class Autocomplete extends React.PureComponent {
       PropTypes.bool,
     ]),
     submit_element: PropTypes.node,
-    prevent_selection: PropTypes.oneOfType([
+    preventSelection: PropTypes.oneOfType([
       PropTypes.string,
       PropTypes.bool,
     ]),
@@ -299,7 +299,7 @@ export default class Autocomplete extends React.PureComponent {
     no_scroll_animation: false,
     show_submit_button: false,
     submit_element: null,
-    prevent_selection: false,
+    preventSelection: false,
     size: 'default',
     align_autocomplete: null,
     options_render: null,
@@ -1035,7 +1035,7 @@ class AutocompleteInstance extends React.PureComponent {
       open_on_focus,
       keep_value,
       keep_value_and_selection,
-      prevent_selection,
+      preventSelection,
       no_animation,
     } = this.props
 
@@ -1051,7 +1051,7 @@ class AutocompleteInstance extends React.PureComponent {
       })
     }
 
-    if (!isTrue(prevent_selection)) {
+    if (!isTrue(preventSelection)) {
       const existingValue = this.state.inputValue
 
       this.resetInputValue()
@@ -1645,9 +1645,9 @@ class AutocompleteInstance extends React.PureComponent {
   onChangeHandler = (args) => {
     const selected_item = args.selected_item
 
-    const { prevent_selection, keep_open } = this.props
+    const { preventSelection, keep_open } = this.props
 
-    if (!isTrue(prevent_selection)) {
+    if (!isTrue(preventSelection)) {
       if (!isTrue(keep_open)) {
         this.setState({
           skipFocusDuringChange: true,
@@ -1772,7 +1772,7 @@ class AutocompleteInstance extends React.PureComponent {
       submit_element,
       input_element: CustomInput,
       options_render,
-      prevent_selection,
+      preventSelection,
       max_height,
       defaultValue,
       search_numbers, // eslint-disable-line
@@ -1900,7 +1900,7 @@ class AutocompleteInstance extends React.PureComponent {
           'aria-activedescendant'
         ] = `option-${id}-${active_item}`
       } else if (
-        !isTrue(prevent_selection) &&
+        !isTrue(preventSelection) &&
         parseFloat(selected_item) > -1
       ) {
         inputParams[
@@ -2072,7 +2072,7 @@ class AutocompleteInstance extends React.PureComponent {
                 no_animation={no_animation}
                 no_scroll_animation={no_scroll_animation}
                 skip_portal={skip_portal}
-                prevent_selection={prevent_selection}
+                preventSelection={preventSelection}
                 triangle_position={triangle_position || icon_position}
                 keep_open={keep_open}
                 prevent_close={prevent_close}
--- packages/dnb-eufemia/src/components/autocomplete/__tests__/Autocomplete.test.tsx
+++ packages/dnb-eufemia/src/components/autocomplete/__tests__/Autocomplete.test.tsx
@@ -3033,7 +3033,7 @@ describe('Autocomplete component', () => {
   })
 
   it('should support "preventSelection"', async () => {
-    render(<Autocomplete data={mockData} prevent_selection />)
+    render(<Autocomplete data={mockData} preventSelection />)
 
     const input = document.querySelector('input')
     await userEvent.type(input, 'aa')
