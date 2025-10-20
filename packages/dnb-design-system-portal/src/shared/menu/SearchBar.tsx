46f42fc981bedbd8545a7c4dc1ba4ee346a840b6
Anders <anderslangseth@gmail.com>
2025-10-20 13:36:52 +0200

    break(Dropdown, Autocomplete, DrawerList)!: remove `fixed_postion` property (#5759)

--- packages/dnb-design-system-portal/src/shared/menu/SearchBar.tsx
+++ packages/dnb-design-system-portal/src/shared/menu/SearchBar.tsx
@@ -76,7 +76,7 @@ export const SearchBarInput = () => {
       no_scroll_animation
       preventSelection
       disable_filter
-      fixed_position
+      fixedPosition
       size="medium"
       align_autocomplete="right"
       placeholder="Search ..."
