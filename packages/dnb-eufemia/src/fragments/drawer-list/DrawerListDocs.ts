34a598bc1e1d073dced9cc5e3228453e8ce1add9
Anders <anderslangseth@gmail.com>
2025-10-20 14:18:44 +0200

    break(Dropdown, Autocomplete, DrawerList)!: remove `ignore_events` property (#5775)

--- packages/dnb-eufemia/src/fragments/drawer-list/DrawerListDocs.ts
+++ packages/dnb-eufemia/src/fragments/drawer-list/DrawerListDocs.ts
@@ -76,7 +76,7 @@ export const DrawerListProperties = {
     type: 'boolean',
     status: 'optional',
   },
-  ignore_events: {
+  ignoreEvents: {
     doc: 'If set to `true`, all keyboard and mouse events will be ignored.',
     type: 'boolean',
     status: 'optional',
