2a9214a0edd869d994350fca5a33632435178144
Anders <anderslangseth@gmail.com>
2025-10-20 10:19:13 +0200

    break(Autocomplete)!: remove `selected_key` property (#5706)

--- packages/dnb-design-system-portal/src/docs/uilib/components/dropdown/Examples.tsx
+++ packages/dnb-design-system-portal/src/docs/uilib/components/dropdown/Examples.tsx
@@ -67,7 +67,7 @@ const data = [
   // Every data item can, beside "content" - contain what ever
   {
     // (optional) can be what ever
-    selected_key: 'key_0',
+    selectedKey: 'key_0',
 
     // (optional) is show instead of "content", once selected
     selected_value: 'Item 1 Value',
@@ -76,16 +76,16 @@ const data = [
     content: 'Item 1 Content',
   },
   {
-    selected_key: 'key_1',
+    selectedKey: 'key_1',
     content: ['Item 2 Value', 'Item 2 Content'],
   },
   {
-    selected_key: 'key_2',
+    selectedKey: 'key_2',
     selected_value: 'Item 3 Value',
     content: ['Item 3 Content A', 'Item 3 Content B'],
   },
   {
-    selected_key: 'key_3',
+    selectedKey: 'key_3',
     selected_value: 'Item 4 Value',
     content: ['Item 4 Content A', <>Custom Component</>],
   },
