47fd3b9bb80c4ed931a3d37fff54d0b7c9691458
Anders <anderslangseth@gmail.com>
2025-10-20 10:30:44 +0200

    break(DrawerList)!: remove `selected_key` property (#5708)

--- packages/dnb-eufemia/src/fragments/drawer-list/DrawerList.tsx
+++ packages/dnb-eufemia/src/fragments/drawer-list/DrawerList.tsx
@@ -54,7 +54,6 @@ export type DrawerListDataArrayObjectStrict = {
   groupIndex?: number
   selected_value?: string | React.ReactNode
   selectedKey?: string | number
-  selected_key?: string | number
   suffix_value?: string | React.ReactNode
   content: DrawerListContent
   disabled?: boolean
