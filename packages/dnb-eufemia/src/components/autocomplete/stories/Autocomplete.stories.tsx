80b3eb2c6f836322bae354ef792b55c7bcf5e6e5
Anders <anderslangseth@gmail.com>
2025-10-20 14:07:49 +0200

    break(Dropdown, Autocomplete, DrawerList)!: remove `options_render` property (#5771)

--- packages/dnb-eufemia/src/components/autocomplete/stories/Autocomplete.stories.tsx
+++ packages/dnb-eufemia/src/components/autocomplete/stories/Autocomplete.stories.tsx
@@ -341,7 +341,7 @@ export const AutocompleteSandbox = () => {
           on_change={(e) => {
             console.log('on_change', e)
           }}
-          options_render={({
+          optionsRender={({
             Items,
             // , Item
           }) => (
