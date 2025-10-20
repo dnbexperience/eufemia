51a31642bb786f45c729146041a1b95f3d963a39
Anders <anderslangseth@gmail.com>
2025-10-20 13:28:42 +0200

    break(Dropdown, Autocomplete, DrawerList)!: remove `prevent_close` property (#5757)

--- packages/dnb-eufemia/src/fragments/drawer-list/DrawerListHelpers.ts
+++ packages/dnb-eufemia/src/fragments/drawer-list/DrawerListHelpers.ts
@@ -66,7 +66,7 @@ export const drawerListPropTypes = {
   skip_portal: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
   portal_class: PropTypes.string,
   list_class: PropTypes.string,
-  prevent_close: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
+  preventClose: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
   independent_width: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.bool,
@@ -159,7 +159,7 @@ export const drawerListDefaultProps = {
   portal_class: null,
   list_class: null,
   skip_portal: null,
-  prevent_close: false,
+  preventClose: false,
   keep_open: false,
   prevent_focus: false,
   fixed_position: false,
