254a615fe6265046d39f1aacc17e1b368a341282
Anders <anderslangseth@gmail.com>
2025-10-21 09:54:57 +0200

    break(Breadcrumb)!: remove passing down props to span (#5747)

--- packages/dnb-eufemia/src/components/breadcrumb/BreadcrumbItem.tsx
+++ packages/dnb-eufemia/src/components/breadcrumb/BreadcrumbItem.tsx
@@ -17,10 +17,7 @@ import homeIcon from '../../icons/home'
 import { useMediaQuery } from '../../shared'
 import Context from '../../shared/Context'
 import type { SkeletonShow } from '../skeleton/Skeleton'
-import {
-  extendPropsWithContext,
-  filterProps,
-} from '../../shared/component-helper'
+import { extendPropsWithContext } from '../../shared/component-helper'
 
 // SSR warning fix: https://gist.github.com/gaearon/e7d97cdf38a2907924ea12e4ebdf3c85
 const useLayoutEffect =
@@ -181,11 +178,7 @@ const BreadcrumbItem = (localProps: BreadcrumbItemProps) => {
           </>
         )
       ) : (
-        <span
-          className="dnb-breadcrumb__item__span"
-          // TODO: Consider deprecating passing down props to span in v11
-          {...filterProps(props, (key) => !key.includes('-'))}
-        >
+        <span className="dnb-breadcrumb__item__span">
           <IconPrimary
             icon={iconToUse}
             className="dnb-breadcrumb__item__span__icon"
