0c7959ac749f459ab86a8a4ec4b2a6b99354abc3
-l <anderslangseth@gmail.com>
2025-10-21 19:40:51 +0200

    props

--- packages/dnb-design-system-portal/src/docs/uilib/components/textarea/Examples.tsx
+++ packages/dnb-design-system-portal/src/docs/uilib/components/textarea/Examples.tsx
@@ -89,7 +89,7 @@ export const Autoresize = () => (
         label="Autogrow"
         rows={1}
         autoresize={true}
-        autoresize_max_rows={4}
+        autoresizeMaxRows={4}
         placeholder="Placeholder"
         on_key_down={({ rows, event }) => {
           if (rows >= 4 && event.key === 'Enter') {
