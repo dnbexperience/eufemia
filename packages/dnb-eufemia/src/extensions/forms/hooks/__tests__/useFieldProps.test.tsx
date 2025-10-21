b2d695e73d12a04ebbabba6486e764859358987a
Anders <anderslangseth@gmail.com>
2025-10-21 10:45:22 +0200

    chore: remove `overwriteErrorMessagesWithGivenAjvKeys` (#5711)

--- packages/dnb-eufemia/src/extensions/forms/hooks/__tests__/useFieldProps.test.tsx
+++ packages/dnb-eufemia/src/extensions/forms/hooks/__tests__/useFieldProps.test.tsx
@@ -1386,26 +1386,6 @@ describe('useFieldProps', () => {
           document.querySelector('.dnb-form-status')
         ).not.toBeInTheDocument()
       })
-
-      /**
-       * @deprecated – can be removed in v11
-       */
-      it('with backwards compatibility', () => {
-        const { result } = renderHook(() =>
-          useFieldProps({
-            value: undefined,
-            required: true,
-            validateInitially: true,
-            errorMessages: {
-              required: 'Show this message',
-            },
-          })
-        )
-        expect(result.current.error).toBeInstanceOf(Error)
-        expect(getError(result.current.error).message).toBe(
-          'Show this message'
-        )
-      })
     })
 
     it('should validate required when value is empty string', () => {
