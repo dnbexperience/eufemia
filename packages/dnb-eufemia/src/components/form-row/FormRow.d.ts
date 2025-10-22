104d4b9fea078b6bfc09fd3c2ec6a434ba7ccbba
Anders <anderslangseth@gmail.com>
2025-10-22 16:43:02 +0200

    break(Dropdown, Autocomplete, Input, InputMasked, MultiInputMasked)!: remove `label_direction` property (#5776)

--- packages/dnb-eufemia/src/components/form-row/FormRow.d.ts
+++ packages/dnb-eufemia/src/components/form-row/FormRow.d.ts
@@ -19,9 +19,9 @@ export interface FormRowProps
    */
   label?: React.ReactNode;
   /**
-   * Use `label_direction="vertical"` to change the label/legend layout direction. Defaults to `horizontal`.
+   * Use `labelDirection="vertical"` to change the label/legend layout direction. Defaults to `horizontal`.
    */
-  label_direction?: FormLabelLabelDirection;
+  labelDirection?: FormLabelLabelDirection;
   /**
    * Use `true` to make the label only readable by screen readers.
    */
@@ -49,7 +49,7 @@ export interface FormRowProps
    */
   direction?: FormRowDirection;
   /**
-   * Will force both `direction` and `label_direction` to be **vertical** if set to `true`.
+   * Will force both `direction` and `labelDirection` to be **vertical** if set to `true`.
    */
   vertical?: boolean;
   /**
