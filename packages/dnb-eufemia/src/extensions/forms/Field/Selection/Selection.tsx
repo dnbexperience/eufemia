6ea6dde440d0ecf247aafbe030b989135521d00d
Anders <anderslangseth@gmail.com>
2025-10-22 12:37:05 +0200

    chore(Field.Selection): use layoutDirection (#5824)

--- packages/dnb-eufemia/src/extensions/forms/Field/Selection/Selection.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Field/Selection/Selection.tsx
@@ -410,9 +410,6 @@ function Selection(props: Props) {
           <Component.Group
             size={size}
             className={cn}
-            layout_direction={
-              optionsLayout === 'horizontal' ? 'row' : 'column'
-            }
             layoutDirection={
               optionsLayout === 'horizontal' ? 'row' : 'column'
             }
