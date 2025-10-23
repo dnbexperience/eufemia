0e551ce45420bff17a09b6e6b24cd6e62fb40d88
Anders <anderslangseth@gmail.com>
2025-10-23 13:23:46 +0200

    break(Modal)!: replace `snake_case` properties with `camelCase` (#5839)

--- packages/dnb-eufemia/src/components/dialog/DialogContent.tsx
+++ packages/dnb-eufemia/src/components/dialog/DialogContent.tsx
@@ -33,8 +33,8 @@ export default function DialogContent({
   fullscreen,
   noAnimation = false,
   noAnimationOnMobile = false,
-  minWidth: min_width = null,
-  maxWidth: max_width = null,
+  minWidth: minWidthProp = null,
+  maxWidth: maxWidthProp = null,
   variant = 'information',
   confirmType = 'info',
   icon = null,
@@ -48,7 +48,10 @@ export default function DialogContent({
   ...rest
 }: DialogContentProps): JSX.Element {
   const context = useContext(ModalContext)
-  const { minWidth, maxWidth } = checkMinMaxWidth(min_width, max_width)
+  const { minWidth, maxWidth } = checkMinMaxWidth(
+    minWidthProp,
+    maxWidthProp
+  )
   const content =
     modalContent ||
     getContent(
