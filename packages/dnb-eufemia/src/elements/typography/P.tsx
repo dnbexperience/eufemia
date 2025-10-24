22d23a43f1e53ed3a1b410f533bd9482f1c24cbf
Anders <anderslangseth@gmail.com>
2025-10-24 14:11:15 +0200

    break(Paragraph)!: remove `medium`, `bold`, `modifier` (#5715)
    Would be great if @snorrekim could assist looking into this, as I
    believe he's done some work here previously 🙏
    
    ---------
    
    Co-authored-by: Snorre Kim <snorre.kim@dnb.no>

--- packages/dnb-eufemia/src/elements/typography/P.tsx
+++ packages/dnb-eufemia/src/elements/typography/P.tsx
@@ -11,37 +11,17 @@ import Typography, {
   TypographyContext,
 } from './Typography'
 
-/** @deprecated use TypographySize instead */
 export type PSize = TypographySize
 
-export type PProps = TypographyProps<HTMLParagraphElement> & {
-  /**
-   * Tells the component to use the medium font-weight styling dnb-t__weight--medium defined in paragraphStyle - typography-mixins.scss. Find more details here https://eufemia.dnb.no/uilib/typography/font-weight/
-   * @deprecated use the `weight` prop instead
-   */
-  medium?: boolean
-  /**
-   * Tells the component to use the bold font-weight styling dnb-t__weight--bold defined in paragraphStyle - typography-mixins.scss. Find more details here https://eufemia.dnb.no/uilib/typography/font-weight/
-   * @deprecated use the `weight` prop instead
-   */
-  bold?: boolean
-  /**
-   * A string containing a combination of modifiers, used to set both font-size and weight in one property. e.g. "x-small bold" would make the paragraph extra small and bold.
-   * Works as a flexible alternative to setting the medium, bold and size props.
-   * List of modifiers can be found at https://eufemia.dnb.no/uilib/typography/font-size/ and https://eufemia.dnb.no/uilib/typography/font-weight/
-   * @deprecated only font weights "bold" and "medium" and sizes "x-small" and "small" are supported. Use the `size` and `weight` props instead.
-   */
-  modifier?: string
-}
+export type PProps = TypographyProps<HTMLParagraphElement>
 
 function P(props: PProps) {
   const {
-    remainingModifiers,
     element = 'p',
     className,
     proseMaxWidth: proseMaxWidthProp,
     ...rest
-  } = handleDeprecatedProps(props)
+  } = props
 
   const paragraphContext = useContext(ParagraphContext)
   const { proseMaxWidth: proseMaxWidthContext } =
@@ -50,76 +30,18 @@ function P(props: PProps) {
   // Use prop value if provided, otherwise fall back to context
   const proseMaxWidth = proseMaxWidthProp ?? proseMaxWidthContext
 
-  const deprecatedModifierString = remainingModifiers.reduce(
-    (acc, cur) => {
-      // This entire string could possibly be deprecated. There are no remaining modifiers
-      // that should be supported, but technically this allows for any class "dnb-p--[modifier]".
-      // But "dnb-p--lead" is the only class that we have, and it's not supposed to be added here.
-      return `${acc} dnb-p--${cur}`
-    },
-    ''
-  )
-
   return (
     <ParagraphContext.Provider value={{ isNested: true }}>
       <Typography
         element={
           element === 'p' && paragraphContext?.isNested ? 'span' : element
         }
-        className={classnames(
-          'dnb-p',
-          deprecatedModifierString,
-          className
-        )}
+        className={classnames('dnb-p', className)}
         proseMaxWidth={proseMaxWidth}
         {...rest}
       />
     </ParagraphContext.Provider>
   )
-}
-
-const handleDeprecatedProps = ({
-  weight,
-  size,
-  modifier,
-  bold,
-  medium,
-  ...rest
-}: PProps): TypographyProps<HTMLParagraphElement> & {
-  remainingModifiers?: string[]
-} => {
-  let oldWeight
-  let oldSize
-
-  const allModifiers = [bold && 'bold', medium && 'medium']
-  if (modifier) {
-    modifier
-      .split(/\s/g)
-      .forEach((modifier) => allModifiers.push(modifier))
-  }
-
-  const remainingModifiers = allModifiers.filter(Boolean).filter((cur) => {
-    if (['x-small'].includes(cur)) {
-      oldSize = 'x-small'
-    } else if (['small'].includes(cur)) {
-      oldSize = oldSize || 'small'
-    } else if (['medium'].includes(cur)) {
-      oldWeight = oldWeight || 'medium'
-    } else if (['bold'].includes(cur)) {
-      oldWeight = 'bold'
-    } else {
-      // There should never be anything here unless it's a custom modifier.
-      return true
-    }
-    return false
-  }, [])
-
-  return {
-    weight: weight || oldWeight,
-    size: oldSize && size !== 'x-small' ? oldSize : size,
-    remainingModifiers,
-    ...rest,
-  }
 }
 
 P._supportsSpacingProps = true
