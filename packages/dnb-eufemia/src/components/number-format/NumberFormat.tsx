e7b8dacac2f5af3ad28d451841b2fe48ee6cece8
Anders <anderslangseth@gmail.com>
2025-10-29 08:24:45 +0100

    break(NumberFormat)!: replace `snake_case` translations with `camelCase` (#5868)

--- packages/dnb-eufemia/src/components/number-format/NumberFormat.tsx
+++ packages/dnb-eufemia/src/components/number-format/NumberFormat.tsx
@@ -175,7 +175,7 @@ export default class NumberFormat extends React.PureComponent<NumberFormatAllPro
   showCopyTooltip = (message) => {
     const translations = this.context.getTranslation?.(this.props)
       ?.NumberFormat
-    const label = message || translations?.clipboard_copy
+    const label = message || translations?.clipboardCopy
 
     if (!label) {
       return
@@ -196,7 +196,7 @@ export default class NumberFormat extends React.PureComponent<NumberFormatAllPro
 
   shortcutHandler = () => {
     const label = this.context.getTranslation(this.props)?.NumberFormat
-      .clipboard_copy
+      .clipboardCopy
     this.showCopyTooltip(label)
   }
 
@@ -358,7 +358,7 @@ export default class NumberFormat extends React.PureComponent<NumberFormatAllPro
       invalidAriaText:
         locale && locale !== this.context.locale
           ? null
-          : translations?.not_available,
+          : translations?.notAvailable,
     }
 
     // use only the props from context, who are available here anyway
