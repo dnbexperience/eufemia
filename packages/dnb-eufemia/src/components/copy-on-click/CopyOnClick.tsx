12c90434cbc818ee4ecaffa44457cbbb2b4062d4
Anders <anderslangseth@gmail.com>
2025-10-29 08:25:25 +0100

    break(CopyOnClick)!: replace `snake_case` translations with `camelCase` (#5867)

--- packages/dnb-eufemia/src/components/copy-on-click/CopyOnClick.tsx
+++ packages/dnb-eufemia/src/components/copy-on-click/CopyOnClick.tsx
@@ -37,7 +37,7 @@ const CopyOnClick = ({
   }, [])
 
   const {
-    CopyOnClick: { clipboard_copy },
+    CopyOnClick: { clipboardCopy },
   } = useTranslation()
 
   const copy = useCallback(async (str: string) => {
@@ -83,7 +83,7 @@ const CopyOnClick = ({
   const params = {
     onClick: disabled ? undefined : onClickHandler,
   }
-  const message = tooltipContent ?? clipboard_copy
+  const message = tooltipContent ?? clipboardCopy
 
   return (
     <Span
