ec91366f438a50e431c881654b9b7d4d65a8e730
Anders <anderslangseth@gmail.com>
2025-11-03 13:18:14 +0100

    chore: rename Carnegie to DNB Carnegie (#5894)

--- packages/dnb-design-system-portal/src/shared/menu/SidebarMenu.tsx
+++ packages/dnb-design-system-portal/src/shared/menu/SidebarMenu.tsx
@@ -222,7 +222,7 @@ const ThemeBadge = ({ theme, ...props }: { theme: ThemeNames }) => {
       ui: 'DNB',
       sbanken: 'Sbanken',
       eiendom: 'Eiendom',
-      carnegie: 'Carnegie',
+      carnegie: 'DNB Carnegie',
     }[theme]
   const themeTitleTitle =
     theme && `This component is ready for use with the ${themeTitle} theme`
