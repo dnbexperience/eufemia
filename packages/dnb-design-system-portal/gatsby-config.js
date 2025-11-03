8c424d6cc8e92220481651727fb7e85b47ce3a23
Anders <anderslangseth@gmail.com>
2025-11-03 13:18:14 +0100

    chore: rename Carnegie to DNB Carnegie (#5894)

--- packages/dnb-design-system-portal/gatsby-config.js
+++ packages/dnb-design-system-portal/gatsby-config.js
@@ -132,7 +132,7 @@ const plugins = [
         ui: { name: 'DNB' }, // universal identity
         eiendom: { name: 'DNB Eiendom' },
         sbanken: { name: 'Sbanken (WIP)' },
-        carnegie: { name: 'Carnegie (WIP)' },
+        carnegie: { name: 'DNB Carnegie (WIP)' },
       },
       filesGlobs:
         enableBuildStyleScope() || enablePortalStyleScope()
