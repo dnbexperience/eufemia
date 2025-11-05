749da193bc666ec4778c0e99c515f22f5f636b2f
Anders <anderslangseth@gmail.com>
2025-11-05 12:34:39 +0100

    break(Field.PhoneNumber)!: replace `label` translation with `numberLabel` (#5910)
    Continuation of https://github.com/dnbexperience/eufemia/pull/5888
    
    ---------
    
    Co-authored-by: Tobias Høegh <tobias@tujo.no>

--- packages/dnb-eufemia/src/extensions/forms/Field/PhoneNumber/PhoneNumber.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Field/PhoneNumber/PhoneNumber.tsx
@@ -97,7 +97,7 @@ type EventValues = {
 function PhoneNumber(props: Props = {}) {
   const sharedContext = useContext(SharedContext)
   const {
-    label: defaultLabel,
+    numberLabel: defaultLabel,
     countryCodeLabel: defaultCountryCodeLabel,
     errorRequired,
   } = useTranslation().PhoneNumber
