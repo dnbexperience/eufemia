2f8987e8a837d7dad293dc35998364e7266f9fac
Tobias Høegh <tobias@tujo.no>
2025-11-07 14:17:43 +0100

    feat(PortalRoot): allow custom portal root placement and custom wrapper `id`

--- packages/dnb-eufemia/src/components/portal-root/PortalRootDocs.ts
+++ packages/dnb-eufemia/src/components/portal-root/PortalRootDocs.ts
@@ -9,6 +9,21 @@ export const PortalRootProperties: PropertiesTableProps = {
     ],
     status: 'optional',
   },
+  id: {
+    doc: 'The id attribute for the portal root element.',
+    type: 'string',
+    status: 'optional',
+  },
+  insideSelector: {
+    doc: 'CSS selector for a container to place the portal root inside. The portal element is inserted as the first child of the matched element.',
+    type: 'string',
+    status: 'optional',
+  },
+  beforeSelector: {
+    doc: 'CSS selector for a target element; the portal root will be inserted directly before the first matched element.',
+    type: 'string',
+    status: 'optional',
+  },
   children: {
     doc: 'The content that will be placed in a React Portal.',
     type: 'ReactNode',
