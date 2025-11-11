03f644704fabba9af5ce235e7774e9b8618a99e2
Anders <anderslangseth@gmail.com>
2025-11-11 13:30:50 +0100

    chore(Field.Selection): replace usage of `on_change` with `onChange` (#5934)

--- packages/dnb-eufemia/src/extensions/forms/Field/Selection/Selection.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Field/Selection/Selection.tsx
@@ -364,7 +364,7 @@ function Selection(props: Props) {
               optionsLayout === 'horizontal' ? 'row' : 'column'
             }
             disabled={disabled}
-            on_change={onChangeHandler}
+            onChange={onChangeHandler}
             value={String(value ?? '')}
           >
             {items}
