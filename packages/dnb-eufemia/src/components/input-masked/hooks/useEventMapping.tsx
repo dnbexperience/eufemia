5c56f6ff0731b5c64d7df69afbcb1bba026bd20d
Anders <anderslangseth@gmail.com>
2025-11-12 15:04:23 +0100

    break(Input, InputMasked)!: remove snake_case events (#5931)

--- packages/dnb-eufemia/src/components/input-masked/hooks/useEventMapping.tsx
+++ packages/dnb-eufemia/src/components/input-masked/hooks/useEventMapping.tsx
@@ -14,18 +14,12 @@ export const useEventMapping = ({ setLocalValue }) => {
   return {
     onBeforeInput: (event) => callEvent({ event }, 'onBeforeInput'),
     onInput: (event) => callEvent({ event }, 'onInput'),
-    onFocus: (params) => callEvent(params, 'on_focus'),
-    onBlur: (params) => callEvent(params, 'on_blur'),
+    onFocus: (params) => callEvent(params, 'onFocus'),
+    onBlur: (params) => callEvent(params, 'onBlur'),
     onMouseUp: (event) => callEvent({ event }, 'on_mouse_up'),
     onMouseDown: (event) => callEvent({ event }, 'on_mouse_down'),
     onKeyDown: (params) => callEvent(params, 'on_key_down'),
-    onSubmit: (params) => callEvent(params, 'on_submit'),
-    onChange: (params) => callEvent(params, 'on_change'),
-
-    on_focus: undefined,
-    on_blur: undefined,
-    on_key_down: undefined,
-    on_submit: undefined,
-    on_change: undefined,
+    onSubmit: (params) => callEvent(params, 'onSubmit'),
+    onChange: (params) => callEvent(params, 'onChange'),
   }
 }
