34dd9e2e8a6b00e44e166c6e183accfbb2e98fc6
Anders <anderslangseth@gmail.com>
2025-11-13 15:37:45 +0100

    chore: remove leftover `on_` events (#5944)

--- packages/dnb-eufemia/src/components/date-picker/DatePickerProvider.tsx
+++ packages/dnb-eufemia/src/components/date-picker/DatePickerProvider.tsx
@@ -213,7 +213,7 @@ function DatePickerProvider(props: DatePickerProviderProps) {
   const callOnChangeHandler = useCallback(
     <E,>(event: E & DatePickerDates) => {
       /**
-       * Prevent on_change to be fired twice if date not has actually changed
+       * Prevent onChange to be fired twice if date not has actually changed
        */
       if (
         lastEventCallCache &&
