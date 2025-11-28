b617ad4ad703670fc66c09b1fee60effa11fbbf4
Tobias Høegh <tobias@tujo.no>
2025-11-28 09:12:13 +0100

    Add additional visual test

--- packages/dnb-eufemia/src/components/word-definition/__tests__/WordDefinition.screenshot.test.ts
+++ packages/dnb-eufemia/src/components/word-definition/__tests__/WordDefinition.screenshot.test.ts
@@ -8,26 +8,36 @@ import {
   setupPageScreenshot,
 } from '../../../core/jest/jestSetupScreenshots'
 
-describe.each(['ui', 'sbanken'])(
-  'WordDefinition (%s theme)',
-  (themeName) => {
-    setupPageScreenshot({
-      url: '/uilib/components/word-definition/demos',
-      themeName,
-    })
+describe.each(['ui', 'sbanken'])('WordDefinition for %s', (themeName) => {
+  setupPageScreenshot({
+    url: '/uilib/components/word-definition/demos',
+    themeName,
+  })
 
-    it('matches the basic style', async () => {
-      const screenshot = await makeScreenshot({
-        selector: '[data-visual-test="word-definition-basic"]',
-      })
-      expect(screenshot).toMatchImageSnapshot()
+  it('matches the basic style', async () => {
+    const screenshot = await makeScreenshot({
+      selector: '[data-visual-test="word-definition-basic"]',
     })
+    expect(screenshot).toMatchImageSnapshot()
+  })
 
-    it('matches various typography styles', async () => {
-      const screenshot = await makeScreenshot({
-        selector: '[data-visual-test="word-definition-typography"]',
-      })
-      expect(screenshot).toMatchImageSnapshot()
+  it('matches the popover', async () => {
+    const screenshot = await makeScreenshot({
+      style: {
+        'padding-bottom': '8rem',
+      },
+      simulate: 'click',
+      simulateSelector:
+        '[data-visual-test="word-definition-basic"] .dnb-word-definition__trigger',
+      selector: '[data-visual-test="word-definition-basic"]',
     })
-  }
-)
+    expect(screenshot).toMatchImageSnapshot()
+  })
+
+  it('matches various typography styles', async () => {
+    const screenshot = await makeScreenshot({
+      selector: '[data-visual-test="word-definition-typography"]',
+    })
+    expect(screenshot).toMatchImageSnapshot()
+  })
+})
