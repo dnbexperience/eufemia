e9d4e8e86f32b5b1bd32b6711ce84b3e5d24f4fa
Tobias Høegh <tobias@tujo.no>
2025-12-02 08:09:54 +0100

    feat(Field.OrganizationNumber): allow input to overflow the given mask

--- packages/dnb-eufemia/src/extensions/forms/Field/OrganizationNumber/OrganizationNumber.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Field/OrganizationNumber/OrganizationNumber.tsx
@@ -71,6 +71,7 @@ function OrganizationNumber(props: Props) {
     label: labelProp ?? label,
     errorMessages,
     mask,
+    allowOverflow: true,
     width: width ?? 'medium',
     inputMode: 'numeric',
     onChangeValidator: validate ? onChangeValidator : undefined,
