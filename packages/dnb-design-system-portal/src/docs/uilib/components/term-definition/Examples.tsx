b908569146f59d728c7c92cdab56cbb7ad9829c4
Tobias Høegh <tobias@tujo.no>
2025-12-03 14:41:34 +0100

    docs(TermDefinition): add examples of how to use inside label and help text (#6028)

--- packages/dnb-design-system-portal/src/docs/uilib/components/term-definition/Examples.tsx
+++ packages/dnb-design-system-portal/src/docs/uilib/components/term-definition/Examples.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { H2, Lead, P, TermDefinition } from '@dnb/eufemia/src'
+import { Field, Form } from '@dnb/eufemia/src/extensions/forms'
 import ComponentBox from '../../../../shared/tags/ComponentBox'
 
-export function TermDefinitionExampleBasic() {
+export function TermDefinitionBasic() {
   return (
     <ComponentBox
       data-visual-test="term-definition-basic"
@@ -20,7 +21,7 @@ export function TermDefinitionExampleBasic() {
   )
 }
 
-export function TermDefinitionExampleLead() {
+export function TermDefinitionLead() {
   return (
     <ComponentBox
       data-visual-test="term-definition-typography"
@@ -40,6 +41,81 @@ export function TermDefinitionExampleLead() {
         </TermDefinition>
         .
       </H2>
+    </ComponentBox>
+  )
+}
+
+export function TermDefinitionHelp() {
+  return (
+    <ComponentBox>
+      <Form.Handler>
+        <Form.Card>
+          <Form.SubHeading>My form</Form.SubHeading>
+          <Field.Email
+            help={{
+              title: 'Email help',
+              content: (
+                <>
+                  Enter your{' '}
+                  <TermDefinition content="Email is a method of exchanging messages between people using electronic devices.">
+                    email
+                  </TermDefinition>{' '}
+                  address
+                </>
+              ),
+            }}
+          />
+          <Field.Boolean
+            variant="radio"
+            label="Consent"
+            help={{
+              title: 'Data processing help',
+              content: (
+                <>
+                  I consent to{' '}
+                  <TermDefinition content="Data processing refers to any operation performed on personal data, such as collection, storage, use, or disclosure.">
+                    data processing
+                  </TermDefinition>
+                </>
+              ),
+            }}
+          />
+        </Form.Card>
+      </Form.Handler>
+    </ComponentBox>
+  )
+}
+
+export function TermDefinitionLabel() {
+  return (
+    <ComponentBox>
+      <Form.Handler>
+        <Form.Card>
+          <Form.SubHeading>My form</Form.SubHeading>
+          <Field.Email
+            label={
+              <>
+                Enter your{' '}
+                <TermDefinition content="Email is a method of exchanging messages between people using electronic devices.">
+                  email
+                </TermDefinition>{' '}
+                address
+              </>
+            }
+          />
+          <Field.Boolean
+            variant="checkbox"
+            label={
+              <>
+                I consent to{' '}
+                <TermDefinition content="Data processing refers to any operation performed on personal data, such as collection, storage, use, or disclosure.">
+                  data processing
+                </TermDefinition>
+              </>
+            }
+          />
+        </Form.Card>
+      </Form.Handler>
     </ComponentBox>
   )
 }
