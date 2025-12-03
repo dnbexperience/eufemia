be520f74e15777fa7eb719ca03383522525401bb
-l <anderslangseth@gmail.com>
2025-12-03 11:35:17 +0100

    temp

--- packages/dnb-eufemia/src/extensions/forms/Field/Upload/stories/Upload.stories.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Field/Upload/stories/Upload.stories.tsx
@@ -1,6 +1,9 @@
-import { Field, Form, Tools, Value } from '../../..'
+import { useCallback } from 'react'
+import { Field, Form, Tools, Value, Wizard } from '../../..'
 import { Flex } from '../../../../../components'
 import { UploadFileNative } from '../../../../../components/Upload'
+import { P } from '../../../../../elements'
+import { debounceAsync } from '../../../../../shared/helpers'
 import { createRequest } from '../../../Form/Handler/stories/FormHandler.stories'
 import { UploadValue } from '../Upload'
 
@@ -444,6 +447,58 @@ export const AcceptedFilesTypesProperty = () => {
         />
         <Tools.Log />
       </Flex.Stack>
+    </Form.Handler>
+  )
+}
+
+export const WizardWithAsyncFileHandler = () => {
+  return (
+    <Form.Handler onSubmit={async (form) => console.log(form)}>
+      <Wizard.Container>
+        <Wizard.Step title="Step 1">
+          <Form.Card>
+            <Field.Upload
+              id="async_upload_context_id"
+              path="/attachments"
+              labelDescription="Upload multiple files at once to see the upload error message. This demo has been set up so that every other file in a batch will fail."
+              fileHandler={mockAsyncFileUpload__withoutPromises}
+              required
+            />
+            <Field.String
+              label="async validator works with Field.String"
+              path="/field2"
+              required
+              onChangeValidator={debounceAsync(
+                useCallback(async (value) => {
+                  try {
+                    const request = createRequest()
+                    await request(12000) // Simulate a request
+                  } catch (error) {
+                    return error
+                  }
+
+                  return Error('Error message')
+                }, [])
+              )}
+            />
+          </Form.Card>
+
+          <Tools.Log />
+          <Wizard.Buttons />
+        </Wizard.Step>
+        <Wizard.Step title="Step 2">
+          <Form.MainHeading>Heading</Form.MainHeading>
+
+          <Form.Card>
+            <P>Contents of step 2</P>
+          </Form.Card>
+
+          <Form.ButtonRow>
+            <Wizard.Buttons />
+            <Form.SubmitButton variant="send" />
+          </Form.ButtonRow>
+        </Wizard.Step>
+      </Wizard.Container>
     </Form.Handler>
   )
 }
