dnbexperience/eufemia

commit 79ee68f86835630292b1304ec690825c7cdede68

fix(Field.Upload): `onChange` should return `undefined` after removing input (#5998)
Motivation:
https://dnb-it.slack.com/archives/CMXABCHEY/p1764074316592609

packages/dnb-eufemia/src/extensions/forms/Field/Upload/Upload.tsx
@@ -92,7 +92,7 @@ function UploadComponent(props: Props) {
   )
 
   const fromInput = useCallback((value: UploadValue) => {
-    value.forEach((item, index) => {
+    value?.forEach((item, index) => {
       if (!item) {
         return
       }
@@ -229,14 +229,15 @@ function UploadComponent(props: Props) {
 
   const changeHandler = useCallback(
     ({ files }: { files: UploadValue }) => {
+      const changeValue = files?.length === 0 ? undefined : files
       // Prevents the form-status from showing up
       handleBlur()
       handleFocus()
 
       if (fileHandler) {
-        handleChangeAsync(files)
+        handleChangeAsync(changeValue)
       } else {
-        handleChange(files)
+        handleChange(changeValue)
       }
     },
     [handleBlur, handleFocus, fileHandler, handleChangeAsync, handleChange]

packages/dnb-eufemia/src/extensions/forms/Field/Upload/__tests__/Upload.test.tsx
@@ -232,6 +232,80 @@ describe('Field.Upload', () => {
       )
     })
 
+    it('should return undefined value when no files are present', async () => {
+      const onChangeContext = jest.fn()
+      const onChangeField = jest.fn()
+
+      render(
+        <Form.Handler onChange={onChangeContext}>
+          <Field.Upload path="/myFiles" onChange={onChangeField} />
+        </Form.Handler>
+      )
+
+      const getRootElement = () => document.querySelector('.dnb-upload')
+
+      const element = getRootElement()
+
+      const file1 = createMockFile('fileName-1.png', 100, 'image/png')
+
+      fireEvent.drop(element, {
+        dataTransfer: { files: [file1] },
+      })
+
+      expect(onChangeContext).toHaveBeenCalledTimes(1)
+      expect(onChangeContext).toHaveBeenLastCalledWith(
+        {
+          myFiles: [
+            {
+              file: file1,
+              exists: false,
+              id: expect.anything(),
+              name: 'fileName-1.png',
+            },
+          ],
+        },
+        expect.anything()
+      )
+      expect(onChangeField).toHaveBeenCalledTimes(1)
+      expect(onChangeField).toHaveBeenLastCalledWith(
+        [
+          {
+            file: file1,
+            exists: false,
+            id: expect.anything(),
+            name: 'fileName-1.png',
+          },
+        ],
+        expect.anything()
+      )
+
+      // delete the file
+      fireEvent.click(
+        document
+          .querySelectorAll('.dnb-upload__file-cell')[0]
+          .querySelector('button')
+      )
+
+      await waitFor(() => {
+        expect(
+          document.querySelectorAll('.dnb-upload__file-cell').length
+        ).toBe(0)
+      })
+
+      expect(onChangeContext).toHaveBeenCalledTimes(2)
+      expect(onChangeContext).toHaveBeenLastCalledWith(
+        {
+          myFiles: undefined,
+        },
+        expect.anything()
+      )
+      expect(onChangeField).toHaveBeenCalledTimes(2)
+      expect(onChangeField).toHaveBeenLastCalledWith(
+        undefined,
+        expect.anything()
+      )
+    })
+
     it('should prevent submit when error in one file is present', async () => {
       const onChangeContext = jest.fn()
       const onChangeField = jest.fn()
@@ -520,7 +594,7 @@ describe('Field.Upload', () => {
 
       expect(onChange).toHaveBeenCalledTimes(2)
       expect(onChange).toHaveBeenLastCalledWith(
-        { myFiles: [] },
+        { myFiles: undefined },
         expect.anything()
       )
 
@@ -780,7 +854,7 @@ describe('Field.Upload', () => {
       expect(onChange).toHaveBeenCalledTimes(2)
       expect(onChange).toHaveBeenLastCalledWith(
         {
-          myFiles: [],
+          myFiles: undefined,
         },
         expect.anything()
       )

commit 57418f166ec6893475728ed38cbc47f16049b42d

fix(Field.Upload): should not display error removing file using async `fileHandler` (#6025)
Fixes an issue introduced in
https://github.com/dnbexperience/eufemia/pull/5998, as we now have to
handle `undefined` value as well. Previously it was only an empty list
`[]`.


[Can be demoed here, by uploading a file, then pressing
remove](https://eufemia.dnb.no/uilib/extensions/forms/feature-fields/more-fields/Upload/demos/#with-asynchronous-file-handler)

<img width="866" height="425" alt="image"
src="https://github.com/user-attachments/assets/899e4488-f0a5-4b35-b97f-8a6d8be6d3d3"
/>

packages/dnb-eufemia/src/extensions/forms/Field/Upload/Upload.tsx
@@ -164,13 +164,14 @@ function UploadComponent(props: Props) {
 
   const handleChangeAsync = useCallback(
     async (files: UploadValue) => {
+      const filesArray = files || []
       // Filter out existing files
       const existingFileIds =
         filesRef.current?.map((file) => file.id) || []
-      const existingFiles = files.filter((file) =>
+      const existingFiles = filesArray.filter((file) =>
         existingFileIds.includes(file.id)
       )
-      const newFiles = files.filter(
+      const newFiles = filesArray.filter(
         (file) => !existingFileIds.includes(file.id)
       )
       const newValidFiles = newFiles.filter((file) => !file.errorMessage)

packages/dnb-eufemia/src/extensions/forms/Field/Upload/__tests__/Upload.test.tsx
@@ -1251,6 +1251,19 @@ describe('Field.Upload', () => {
           document.querySelector('.dnb-form-status')
         ).toHaveTextContent('customError')
       })
+
+      // delete the file
+      fireEvent.click(
+        document
+          .querySelectorAll('.dnb-upload__file-cell')[0]
+          .querySelector('button')
+      )
+
+      await waitFor(() => {
+        expect(
+          document.querySelector('.dnb-form-status')
+        ).not.toBeInTheDocument()
+      })
     })
 
     it('should handle displaying success from fileHandler with async function', async () => {