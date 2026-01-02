cdea9369db9a4f97de99050b12eddeb54c18f5d2
-l <anderslangseth@gmail.com>
2026-01-02 13:40:03 +0100

    chore: iOS 13

--- packages/dnb-eufemia/src/components/number-format/NumberUtils.ts
+++ packages/dnb-eufemia/src/components/number-format/NumberUtils.ts
@@ -978,21 +978,6 @@ export function cleanNumber(
 }
 
 /**
- * So iOS v13 can select something on the first try, we run this add range trick.
- * NB: This hack may be removed in future iOS versions.
- */
-export function runIOSSelectionFix() {
-  try {
-    const selection = window.getSelection()
-    const range = document.createRange()
-    selection.removeAllRanges()
-    selection.addRange(range)
-  } catch (e) {
-    //
-  }
-}
-
-/**
  * Will return currency display value based on navigator/browser and locale
  *
  * @property {string} currencyDisplay (optional) code, name, symbol or narrowSymbol
