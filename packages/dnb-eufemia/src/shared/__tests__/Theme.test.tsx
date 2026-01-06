2ce80915b0f4e33c45bd5e323f0292201bc360ef
-l <anderslangseth@gmail.com>
2026-01-06 14:45:15 +0100

    fix

--- packages/dnb-eufemia/src/shared/__tests__/Theme.test.tsx
+++ packages/dnb-eufemia/src/shared/__tests__/Theme.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, waitFor } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import Theme, { ThemeAllProps } from '../Theme'
 import {
   Autocomplete,
