d6664c1281ad8146573778ea676c26b56c330fd7
-l <anderslangseth@gmail.com>
2026-01-16 22:39:56 +0100

    wip

--- packages/dnb-eufemia/scripts/postbuild/__tests__/postbuild.test.ts
+++ packages/dnb-eufemia/scripts/postbuild/__tests__/postbuild.test.ts
@@ -415,13 +415,13 @@ describe('style build', () => {
         ),
         'utf-8'
       )
-      expect(content).toContain(`@import './core/scopes.scss';`)
+      expect(content).toContain(`@use './core/scopes.scss';`)
       expect(content).toContain(`
 .dnb-core-style {
   @include bodyDefault();
 }`)
       expect(content).toContain(
-        `@import './core/helper-classes/helper-classes.scss';`
+        `@use './core/helper-classes/helper-classes.scss';`
       )
     }
 
@@ -433,12 +433,10 @@ describe('style build', () => {
         ),
         'utf-8'
       )
-      expect(content).toContain(`@import './properties.scss';`)
-      expect(content).toContain(`@import './fonts.scss';`)
-      expect(content).toContain(`@import './ui-theme-elements.scss';`)
-      expect(content).not.toContain(
-        `@import '../../dnb-ui-elements.scss';`
-      )
+      expect(content).toContain(`@use './properties.scss';`)
+      expect(content).toContain(`@use './fonts.scss';`)
+      expect(content).toContain(`@use './ui-theme-elements.scss';`)
+      expect(content).not.toContain(`@use '../../dnb-ui-elements.scss';`)
     }
 
     {
@@ -449,12 +447,10 @@ describe('style build', () => {
         ),
         'utf-8'
       )
-      expect(content).toContain(`@import './properties.scss';`)
-      expect(content).toContain(`@import './fonts.scss';`)
-      expect(content).toContain(`@import './sbanken-theme-elements.scss';`)
-      expect(content).not.toContain(
-        `@import '../../dnb-ui-elements.scss';`
-      )
+      expect(content).toContain(`@use './properties.scss';`)
+      expect(content).toContain(`@use './fonts.scss';`)
+      expect(content).toContain(`@use './sbanken-theme-elements.scss';`)
+      expect(content).not.toContain(`@use '../../dnb-ui-elements.scss';`)
     }
 
     {
