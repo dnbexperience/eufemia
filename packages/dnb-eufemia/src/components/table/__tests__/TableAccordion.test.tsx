3d62a55df3ef274d886956547af54b320d65ad67
Anders <anderslangseth@gmail.com>
2026-01-20 16:10:54 +0100

    break(Table)!: rename event `onOpened` -> `onOpen` (#6368)

--- packages/dnb-eufemia/src/components/table/__tests__/TableAccordion.test.tsx
+++ packages/dnb-eufemia/src/components/table/__tests__/TableAccordion.test.tsx
@@ -973,13 +973,13 @@ describe('TableAccordion', () => {
       expect(target.dataset.trid).toBe(trid)
     })
 
-    it('should emit onOpened event', () => {
-      const onOpened = jest.fn()
+    it('should emit onOpen event', () => {
+      const onOpen = jest.fn()
 
       render(
         <Table mode="accordion">
           <tbody>
-            <Tr onOpened={onOpened}>
+            <Tr onOpen={onOpen}>
               <Td>content</Td>
               <Td.AccordionContent>accordion content</Td.AccordionContent>
             </Tr>
@@ -991,20 +991,20 @@ describe('TableAccordion', () => {
 
       fireEvent.click(trElement)
 
-      expect(onOpened).toHaveBeenCalledTimes(1)
-      expect(onOpened).toHaveBeenCalledWith({
+      expect(onOpen).toHaveBeenCalledTimes(1)
+      expect(onOpen).toHaveBeenCalledWith({
         target: expect.any(Element),
       })
     })
 
     it('should emit onClose event', () => {
       const onClose = jest.fn()
-      const onOpened = jest.fn()
+      const onOpen = jest.fn()
 
       render(
         <Table mode="accordion">
           <tbody>
-            <Tr onOpened={onOpened} onClose={onClose}>
+            <Tr onOpen={onOpen} onClose={onClose}>
               <Td>content</Td>
               <Td.AccordionContent>accordion content</Td.AccordionContent>
             </Tr>
@@ -1017,8 +1017,8 @@ describe('TableAccordion', () => {
       fireEvent.click(trElement)
       fireEvent.click(trElement)
 
-      expect(onOpened).toHaveBeenCalledTimes(1)
-      expect(onOpened).toHaveBeenCalledWith({
+      expect(onOpen).toHaveBeenCalledTimes(1)
+      expect(onOpen).toHaveBeenCalledWith({
         target: expect.any(Element),
       })
 
@@ -1027,7 +1027,7 @@ describe('TableAccordion', () => {
       fireEvent.click(trElement)
       fireEvent.click(trElement)
 
-      expect(onOpened).toHaveBeenCalledTimes(2)
+      expect(onOpen).toHaveBeenCalledTimes(2)
       expect(onClose).toHaveBeenCalledTimes(2)
     })
   })
