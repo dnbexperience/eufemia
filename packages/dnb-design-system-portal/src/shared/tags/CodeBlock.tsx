2826df40c26b5fbc9d0b15bafa207a9193662fb2
-l <anderslangseth@gmail.com>
2026-01-22 21:26:23 +0100

    chore: migrate to fc

--- packages/dnb-design-system-portal/src/shared/tags/CodeBlock.tsx
+++ packages/dnb-design-system-portal/src/shared/tags/CodeBlock.tsx
@@ -111,46 +111,12 @@ type LiveCodeProps = {
   noFragments?: boolean
 } & Omit<CodeSectionProps, 'children'>
 
-class LiveCode extends React.PureComponent<LiveCodeProps, LiveCodeProps> {
-  static contextType = Context
+function LiveCode(props: LiveCodeProps) {
+  const context = React.useContext(Context)
+  const editorElementRef = React.useRef<HTMLDivElement>(null)
+  const idRef = React.useRef(makeUniqueId())
 
-  context!: ContextProps
-
-  _editorElementRef: React.RefObject<HTMLDivElement> = null
-  _id: string = null
-
-  constructor(props: LiveCodeProps) {
-    super(props)
-    const { code, hideToolbar, hideCode, hidePreview } = props
-
-    this.state = {
-      code,
-      hideToolbar,
-      hideCode,
-      hidePreview,
-      tabMode: 'focus',
-    }
-
-    this._editorElementRef = React.createRef()
-    this._id = makeUniqueId()
-  }
-
-  toggleCode = () => {
-    this.setState(() => ({ hideCode: !this.state.hideCode }))
-  }
-  togglePreview = () => {
-    this.setState(() => ({ hidePreview: !this.state.hidePreview }))
-  }
-
-  componentDidUpdate(prevProps) {
-    if (this.props.code !== prevProps.code) {
-      this.setState({
-        code: this.prepareCode(this.props.code),
-      })
-    }
-  }
-
-  prepareCode(code: string) {
+  const prepareCode = React.useCallback((code: string) => {
     code = String(code).trim()
     if (
       /data-visual-test|visualTestProp/.test(code) &&
@@ -161,127 +127,150 @@ class LiveCode extends React.PureComponent<LiveCodeProps, LiveCodeProps> {
       code = code.replace(/ +{\.+visualTestProp.*}\n/g, '') // remove test data
     }
     return code
-  }
+  }, [])
 
-  render() {
-    const {
-      scope = {},
-      noInline,
-      noFragments = true,
-      language = 'jsx',
-      background,
-
-      code: _code, // eslint-disable-line
-      hideToolbar: _hideToolbar, // eslint-disable-line
-      hideCode: _hideCode, // eslint-disable-line
-      hidePreview: _hidePreview, // eslint-disable-line
-      'data-visual-test': visualTest, // eslint-disable-line
-
-      ...props
-    } = this.props
-
-    const { code, hideToolbar, hideCode, hidePreview } = this.state
-
-    const codeToUse =
-      typeof code === 'string' ? this.prepareCode(code) : ''
-
-    if (codeToUse.trim().length === 0) {
-      return <span>No Code provided</span>
+  const [state, setState] = React.useState(() => {
+    const { code, hideToolbar, hideCode, hidePreview } = props
+    return {
+      code,
+      hideToolbar,
+      hideCode,
+      hidePreview,
+      tabMode: 'focus' as const,
     }
+  })
 
-    return (
-      <div
-        className={classnames(
-          liveCodeEditorStyle,
-          background && whiteBackgroundStyle,
-        )}
-      >
-        <LiveProvider
-          theme={prismTheme}
-          code={codeToUse}
-          scope={scope}
-          language={language}
-          transformCode={(code: string) =>
-            !noInline && noFragments ? `<>${code}</>` : code
-          }
-          noInline={noInline}
-          {...props}
-        >
-          {!hidePreview && (
-            <div className="example-box">
-              <LivePreview
-                className="dnb-live-preview"
-                data-visual-test={visualTest}
-              />
-            </div>
-          )}
-          {!global.IS_TEST && !hideCode && (
-            <div
-              className={classnames(
-                'dnb-live-editor',
-                createSkeletonClass('code', this.context.skeleton),
-              )}
-              ref={this._editorElementRef}
-            >
-              <span className="dnb-sr-only">Code Editor</span>
-              <LiveEditor
-                prism={Prism}
-                id={this._id}
-                tabMode={this.state.tabMode}
-                className="dnb-live-editor__editable dnb-pre"
-                onChange={(code) => {
-                  this.setState({ code })
-                }}
-                onFocus={() => {
-                  if (this._editorElementRef.current) {
-                    this._editorElementRef.current.classList.add(
-                      'dnb-pre--focus',
-                    )
-                  }
-                }}
-                onBlur={() => {
-                  if (this._editorElementRef.current) {
-                    this._editorElementRef.current.classList.remove(
-                      'dnb-pre--focus',
-                    )
-                  }
-                }}
-              />
-            </div>
-          )}
+  React.useEffect(() => {
+    if (props.code !== state.code) {
+      setState((prev) => ({
+        ...prev,
+        code: prepareCode(props.code),
+      }))
+    }
+  }, [props.code, state.code, prepareCode])
 
-          <LiveError className="dnb-form-status dnb-form-status__text dnb-form-status--error" />
-
-          {!global.IS_TEST && !hideToolbar && (
-            <div className={classnames(toolbarStyle, 'dnb-live-toolbar')}>
-              {this.props.hideCode && (
-                <Button
-                  className="toggle-button"
-                  onClick={this.toggleCode}
-                  variant="secondary"
-                  text="Code"
-                  title="Toggle Code Snippet"
-                  icon={`arrow-${hideCode ? 'down' : 'up'}`}
-                  size="medium"
-                />
-              )}
-              {this.props.hidePreview && (
-                <Button
-                  className="toggle-button"
-                  onClick={this.togglePreview}
-                  variant="secondary"
-                  text="Preview"
-                  title="Toggle Preview"
-                  icon={`arrow-${!hidePreview ? 'down' : 'up'}`}
-                  size="medium"
-                />
-              )}
-            </div>
-          )}
-        </LiveProvider>
-      </div>
-    )
+  const toggleCode = () => {
+    setState((prev) => ({ ...prev, hideCode: !prev.hideCode }))
   }
+
+  const togglePreview = () => {
+    setState((prev) => ({ ...prev, hidePreview: !prev.hidePreview }))
+  }
+
+  const {
+    scope = {},
+    noInline,
+    noFragments = true,
+    language = 'jsx',
+    background,
+
+    code: _code, // eslint-disable-line
+    hideToolbar: _hideToolbar, // eslint-disable-line
+    hideCode: _hideCode, // eslint-disable-line
+    hidePreview: _hidePreview, // eslint-disable-line
+    'data-visual-test': visualTest, // eslint-disable-line
+
+    ...restProps
+  } = props
+
+  const { code, hideToolbar, hideCode, hidePreview } = state
+
+  const codeToUse = typeof code === 'string' ? prepareCode(code) : ''
+
+  if (codeToUse.trim().length === 0) {
+    return <span>No Code provided</span>
+  }
+
+  return (
+    <div
+      className={classnames(
+        liveCodeEditorStyle,
+        background && whiteBackgroundStyle,
+      )}
+    >
+      <LiveProvider
+        theme={prismTheme}
+        code={codeToUse}
+        scope={scope}
+        language={language}
+        transformCode={(code: string) =>
+          !noInline && noFragments ? `<>${code}</>` : code
+        }
+        noInline={noInline}
+        {...restProps}
+      >
+        {!hidePreview && (
+          <div className="example-box">
+            <LivePreview
+              className="dnb-live-preview"
+              data-visual-test={visualTest}
+            />
+          </div>
+        )}
+        {!global.IS_TEST && !hideCode && (
+          <div
+            className={classnames(
+              'dnb-live-editor',
+              createSkeletonClass('code', context.skeleton),
+            )}
+            ref={editorElementRef}
+          >
+            <span className="dnb-sr-only">Code Editor</span>
+            <LiveEditor
+              prism={Prism}
+              id={idRef.current}
+              tabMode={state.tabMode}
+              className="dnb-live-editor__editable dnb-pre"
+              onChange={(code) => {
+                setState((prev) => ({ ...prev, code }))
+              }}
+              onFocus={() => {
+                if (editorElementRef.current) {
+                  editorElementRef.current.classList.add('dnb-pre--focus')
+                }
+              }}
+              onBlur={() => {
+                if (editorElementRef.current) {
+                  editorElementRef.current.classList.remove(
+                    'dnb-pre--focus',
+                  )
+                }
+              }}
+            />
+          </div>
+        )}
+
+        <LiveError className="dnb-form-status dnb-form-status__text dnb-form-status--error" />
+
+        {!global.IS_TEST && !hideToolbar && (
+          <div className={classnames(toolbarStyle, 'dnb-live-toolbar')}>
+            {props.hideCode && (
+              <Button
+                className="toggle-button"
+                onClick={toggleCode}
+                variant="secondary"
+                text="Code"
+                title="Toggle Code Snippet"
+                icon={`arrow-${hideCode ? 'down' : 'up'}`}
+                size="medium"
+              />
+            )}
+            {props.hidePreview && (
+              <Button
+                className="toggle-button"
+                onClick={togglePreview}
+                variant="secondary"
+                text="Preview"
+                title="Toggle Preview"
+                icon={`arrow-${!hidePreview ? 'down' : 'up'}`}
+                size="medium"
+              />
+            )}
+          </div>
+        )}
+      </LiveProvider>
+    </div>
+  )
 }
 
 /** Removes the last token from a code example if it's empty. */
