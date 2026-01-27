62180b6daf5651d5201c54fa7c5a5362aa08fc18
Anders <anderslangseth@gmail.com>
2026-01-27 13:10:18 +0100

    chore(html): migrate to FC (#6428)

--- packages/dnb-design-system-portal/src/html.tsx
+++ packages/dnb-design-system-portal/src/html.tsx
@@ -23,69 +23,67 @@ type HTMLProps = {
   postBodyComponents: ReactNode[]
 }
 
-export default class HTML extends React.PureComponent<HTMLProps> {
-  render() {
-    const {
-      htmlAttributes = null,
-      bodyAttributes = null,
-      headComponents,
-      preBodyComponents,
-      postBodyComponents,
-      body,
-    } = this.props
+export default function HTML(props: HTMLProps) {
+  const {
+    htmlAttributes = null,
+    bodyAttributes = null,
+    headComponents,
+    preBodyComponents,
+    postBodyComponents,
+    body,
+  } = props
 
-    return (
-      <html lang="en" {...htmlAttributes}>
-        <head>
-          <meta charSet="utf-8" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=yes"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href={withPrefix('/dnb/apple-touch-icon.png')}
-          />
-          <link
-            rel="shortcut icon"
-            type="image/x-icon"
-            href={withPrefix('/dnb/favicon.ico')}
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href={withPrefix('/dnb/favicon-32x32.png')}
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href={withPrefix('/dnb/favicon-16x16.png')}
-          />
-          <link
-            rel="mask-icon"
-            href={withPrefix('/dnb/safari-pinned-tab.svg')}
-            color={mainColor}
-          />
-          <meta name="msapplication-TileColor" content={mainColor} />
-          <meta name="theme-color" content={mainColor} />
-          {headComponents}
-        </head>
-        <body {...bodyAttributes}>
-          <noscript key="noscript" id="gatsby-noscript">
-            Eufemia works best with JavaScript enabled.
-          </noscript>
-          {preBodyComponents}
-          <div
-            key="body"
-            id="___gatsby"
-            dangerouslySetInnerHTML={{ __html: body }}
-          />
-          {postBodyComponents}
-        </body>
-      </html>
-    )
-  }
+  return (
+    <html lang="en" {...htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=yes"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href={withPrefix('/dnb/apple-touch-icon.png')}
+        />
+        <link
+          rel="shortcut icon"
+          type="image/x-icon"
+          href={withPrefix('/dnb/favicon.ico')}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href={withPrefix('/dnb/favicon-32x32.png')}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href={withPrefix('/dnb/favicon-16x16.png')}
+        />
+        <link
+          rel="mask-icon"
+          href={withPrefix('/dnb/safari-pinned-tab.svg')}
+          color={mainColor}
+        />
+        <meta name="msapplication-TileColor" content={mainColor} />
+        <meta name="theme-color" content={mainColor} />
+        {headComponents}
+      </head>
+      <body {...bodyAttributes}>
+        <noscript key="noscript" id="gatsby-noscript">
+          Eufemia works best with JavaScript enabled.
+        </noscript>
+        {preBodyComponents}
+        <div
+          key="body"
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: body }}
+        />
+        {postBodyComponents}
+      </body>
+    </html>
+  )
 }
