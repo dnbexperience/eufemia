c4c9e5f2fd151df7906ba828976d434c803a63a6
Anders <anderslangseth@gmail.com>
2026-01-27 09:52:26 +0100

    chore(Skeleton): migrate SkeletonHelper and figures to FC (#6403)

--- packages/dnb-eufemia/src/components/skeleton/figures/Circle.tsx
+++ packages/dnb-eufemia/src/components/skeleton/figures/Circle.tsx
@@ -3,8 +3,7 @@
  *
  */
 
-import React from 'react'
-import Context from '../../../shared/Context'
+import React, { useMemo } from 'react'
 import clsx from 'clsx'
 
 export type SkeletonCircleRows = string | number
@@ -18,85 +17,68 @@ export interface SkeletonCircleProps
   children?: SkeletonCircleChildren
 }
 
-export default class SkeletonCircle extends React.Component<
-  SkeletonCircleProps,
-  any
-> {
-  static contextType = Context
-  static defaultProps = {
-    rows: 3,
-    children: null,
-  }
-
-  rowsLength: number[]
-
-  constructor(props: SkeletonCircleProps) {
-    super(props)
-
-    const { rows } = props
-
+const SkeletonCircle: React.FC<SkeletonCircleProps> = ({
+  rows = 3,
+  children = null,
+  ...rest
+}) => {
+  const rowsLength = useMemo(() => {
     // Do this so we get the same result each time
     // because of static generated markup
     const fill = [70, 80, 60, 40, 50, 20, 0]
-    this.rowsLength = new Array(Number(rows)).fill(true).map((_, i) => {
+    return new Array(Number(rows)).fill(true).map((_, i) => {
       const c = i % fill.length
       if (c === fill.length - 1) {
         fill.concat(fill.reverse())
       }
       return fill[c]
     })
-  }
+  }, [rows])
 
-  render() {
-    const {
-      rows, // eslint-disable-line
-      children,
-      ...rest
-    } = this.props
-
-    return (
+  return (
+    <div
+      className={clsx(
+        'dnb-skeleton__figure',
+        'dnb-skeleton__figure--show'
+      )}
+      aria-busy
+      {...rest}
+    >
       <div
         className={clsx(
-          'dnb-skeleton__figure',
-          'dnb-skeleton__figure--show'
+          'dnb-h--xx-large',
+          'dnb-skeleton',
+          'dnb-skeleton--shape',
+          'dnb-space__bottom--large'
         )}
-        aria-busy
-        {...rest}
+        aria-hidden
+        style={{
+          width: '50%',
+        }}
       >
+        &zwnj;
+      </div>
+
+      {rowsLength.map((p, i) => (
         <div
+          key={i}
           className={clsx(
-            'dnb-h--xx-large',
+            'dnb-p',
             'dnb-skeleton',
             'dnb-skeleton--shape',
-            'dnb-space__bottom--large'
+            'dnb-space__top--x-small'
           )}
-          aria-hidden
           style={{
-            width: '50%',
+            width: `${p}%`,
           }}
         >
           &zwnj;
         </div>
+      ))}
 
-        {this.rowsLength.map((p, i) => (
-          <div
-            key={i}
-            className={clsx(
-              'dnb-p',
-              'dnb-skeleton',
-              'dnb-skeleton--shape',
-              'dnb-space__top--x-small'
-            )}
-            style={{
-              width: `${p}%`,
-            }}
-          >
-            &zwnj;
-          </div>
-        ))}
-
-        {typeof children === 'function' ? children() : children}
-      </div>
-    )
-  }
+      {typeof children === 'function' ? children() : children}
+    </div>
+  )
 }
+
+export default SkeletonCircle
