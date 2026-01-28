a1034d3307af569ec164066dd977364b8ba140db
Anders <anderslangseth@gmail.com>
2026-01-28 23:23:18 +0100

    break(Autocomplete, Dropdown)!: replace `alignAutocomplete` and `alignDropdown` with `align` (#6502)

--- packages/dnb-design-system-portal/src/shared/menu/SearchBar.tsx
+++ packages/dnb-design-system-portal/src/shared/menu/SearchBar.tsx
@@ -89,7 +89,7 @@ export const SearchBarInput = () => {
       disableFilter
       fixedPosition
       size="medium"
-      alignAutocomplete="right"
+      align="right"
       placeholder="Search ..."
       label="Search the Eufemia documentation"
       labelSrOnly
