2dcf4b6b6bae3b03dae9bba4a42eda8c9d58ab86
Anders <anderslangseth@gmail.com>
2026-01-30 11:30:16 +0100

    chore: minor typing improvements (#6518)

--- packages/dnb-eufemia/src/extensions/forms/hooks/useFieldProps.ts
+++ packages/dnb-eufemia/src/extensions/forms/hooks/useFieldProps.ts
@@ -3009,7 +3009,9 @@ export interface ReturnAdditional<Value> {
   additionalArgs: ReceiveAdditionalEventArgs<Value>
 }
 
-function resolveValidatingState(state: SubmitStateWithValidating) {
+function resolveValidatingState(
+  state: SubmitStateWithValidating
+): SubmitState {
   return state === 'validating' ? 'pending' : state
 }
 
@@ -3019,7 +3021,7 @@ export function checkForError(
     | FieldPropsGeneric['warning']
     | FieldPropsGeneric['info']
   >
-) {
+): boolean {
   return potentialErrors.some((error) => {
     return error instanceof Error || error instanceof FormError
   })
