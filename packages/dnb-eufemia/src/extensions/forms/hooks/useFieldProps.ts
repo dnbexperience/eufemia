4350fbc135c92cbb550eb6ec1a89bc80c7fe8d8e
Anders <anderslangseth@gmail.com>
2026-02-10 20:13:44 +0100

    chore(useFieldProps): remove deprecated additionalArgs (#6593)

--- packages/dnb-eufemia/src/extensions/forms/hooks/useFieldProps.ts
+++ packages/dnb-eufemia/src/extensions/forms/hooks/useFieldProps.ts
@@ -944,10 +944,7 @@ export default function useFieldProps<Value, EmptyValue, Props>(
   additionalArgsRef.current.validators = exportValidators
   additionalArgsRef.current.props = props
   const additionalArgs = useMemo(() => {
-    const args: ReceiveAdditionalEventArgs<Value> = {
-      /** Deprecated – can be removed in v11 */
-      ...combinedErrorMessages,
-
+    const args = {
       errorMessages: combinedErrorMessages,
       ...additionalArgsRef.current,
       connectWithPath: (path) => {
@@ -956,7 +953,7 @@ export default function useFieldProps<Value, EmptyValue, Props>(
       connectWithItemPath: (itemPath) => {
         return handleConnectWithPath(makeIteratePath(itemPath))
       },
-    }
+    } as ReceiveAdditionalEventArgs<Value>
 
     return args
   }, [combinedErrorMessages, handleConnectWithPath, makeIteratePath])
