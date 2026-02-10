a867c9817da00d62a886ecb11c67b689d8d3f73e
Anders <anderslangseth@gmail.com>
2026-02-10 20:53:22 +0100

    chore: replace snake_case variables with camelCase (#6596)

--- packages/dnb-design-system-portal/src/docs/uilib/components/upload/Examples.tsx
+++ packages/dnb-design-system-portal/src/docs/uilib/components/upload/Examples.tsx
@@ -454,9 +454,9 @@ export const UploadOnFileDeleteAsync = () => (
         console.log('making API request to remove:', fileItem.file.name)
         await request(3000) // Simulate a request
         const mockResponse = {
-          successful_removal: Math.random() < 0.5, // Randomly fails to remove the file
+          successfulRemoval: Math.random() < 0.5, // Randomly fails to remove the file
         }
-        if (!mockResponse.successful_removal) {
+        if (!mockResponse.successfulRemoval) {
           throw new Error('Unable to remove this file')
         }
       }
