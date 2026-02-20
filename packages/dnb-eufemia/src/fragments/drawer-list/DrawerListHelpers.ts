aacb1982c87dcb47dc27023102f804bb5355aaf2
Anders Langseth <anderslangseth@gmail.com>
2026-02-20 12:43:02 +0100

    chore(DrawerList): remove deprecated onStateUpdate prop and dispatch

--- packages/dnb-eufemia/src/fragments/drawer-list/DrawerListHelpers.ts
+++ packages/dnb-eufemia/src/fragments/drawer-list/DrawerListHelpers.ts
@@ -10,7 +10,6 @@ import PropTypes from 'prop-types'
 import {
   isTrue,
   makeUniqueId,
-  dispatchCustomElementEvent,
   convertJsxToString,
 } from '../../shared/component-helper'
 import { spacingPropTypes } from '../../components/space/SpacingHelper'
@@ -135,7 +134,6 @@ export const drawerListPropTypes = {
   onPreChange: PropTypes.func,
   onResize: PropTypes.func,
   onSelect: PropTypes.func,
-  onStateUpdate: PropTypes.func,
 }
 
 export const drawerListDefaultProps = {
@@ -181,7 +179,6 @@ export const drawerListDefaultProps = {
   onPreChange: null,
   onResize: null,
   onSelect: null,
-  onStateUpdate: null,
   optionsRender: null,
 }
 
@@ -525,16 +522,7 @@ export const prepareDerivedState = (
       state.selectedItem = getCurrentIndex(props.value, state.originalData)
     }
 
-    if (typeof props.onStateUpdate === 'function') {
-      dispatchCustomElementEvent({ props }, 'onStateUpdate', {
-        selectedItem: state.selectedItem,
-        value: getSelectedItemValue(state.selectedItem, state),
-        data: getEventData(state.selectedItem, state.data),
-      })
-    }
-  }
-
-  // activeItem can be -1, so we check for -2
+    // activeItem can be -1, so we check for -2
   if (
     !(
       state.activeItem !== null &&
