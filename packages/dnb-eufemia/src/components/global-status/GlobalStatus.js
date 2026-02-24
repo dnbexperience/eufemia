a6ddbbbbc60e2eed8be18d38e67b01cc1775053d
Tobias Høegh <tobias@tujo.no>
2026-02-24 12:38:30 +0100

    Apply suggestion from @tujoworker

--- packages/dnb-eufemia/src/components/global-status/GlobalStatus.js
+++ packages/dnb-eufemia/src/components/global-status/GlobalStatus.js
@@ -246,7 +246,6 @@ export default class GlobalStatus extends React.PureComponent {
   }
 
   componentDidUpdate(prevProps, newProps) {
-    console.log('prevProps ', prevProps)
     console.log('newProps', newProps)
     if (prevProps.show !== this.props.show) {
       if (isTrue(this.props.show)) {
