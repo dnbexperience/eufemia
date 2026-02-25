b4c951ffcb0ac8d9e8fbbca282d2738ff0599e5d
Anders <anderslangseth@gmail.com>
2026-02-25 21:09:06 +0100

    chore: remove sass legacy-js-api usage (#6787)
    Fixes: DEPRECATION WARNING [legacy-js-api]: The legacy JS API is
    deprecated and will be removed in Dart Sass 2.0.0.

--- packages/dnb-eufemia/src/core/jest/jestSetup.js
+++ packages/dnb-eufemia/src/core/jest/jestSetup.js
@@ -19,16 +19,15 @@ export const loadScss = (file, options = {}) => {
     const importPath1 = path.dirname(file)
     const importPath2 = path.resolve(__dirname, '../../style/core/')
 
-    const sassResult = sass.renderSync({
-      file,
-      includePaths: [importPath1, importPath2], // use loadPaths for new API
+    const sassResult = sass.compile(file, {
+      loadPaths: [importPath1, importPath2],
       sourceMap: false,
       // TODO: Remove silenceDeprecations before v11 release and migrate to Sass module system (@use/@forward)
       silenceDeprecations: ['import', 'global-builtin'],
       ...options,
     })
 
-    return String(sassResult.css)
+    return sassResult.css
   } catch (e) {
     console.error('loadScss error:', e)
     return e
