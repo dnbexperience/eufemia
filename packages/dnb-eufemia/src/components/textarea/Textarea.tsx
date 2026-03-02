770fe28bfa48b9b677ad49e3759ef40b1b207594
Anders <anderslangseth@gmail.com>
2026-03-02 21:28:26 +0100

    Apply suggestion from @langz

--- packages/dnb-eufemia/src/components/textarea/Textarea.tsx
+++ packages/dnb-eufemia/src/components/textarea/Textarea.tsx
@@ -339,7 +339,7 @@ class TextareaClass extends React.PureComponent<
       this.prepareAutosize()
     }
 
-    const rows = this.getRows(value)
+    const rows = this.getRows()
 
     const ret = dispatchCustomElementEvent(this, 'onChange', {
       value,
