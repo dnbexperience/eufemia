f534616320c04592a085218700211f72657e47aa
-l <anderslangseth@gmail.com>
2026-03-02 19:50:57 +0100

    jsdoc

--- packages/dnb-eufemia/src/components/global-status/GlobalStatus.tsx
+++ packages/dnb-eufemia/src/components/global-status/GlobalStatus.tsx
@@ -62,27 +62,84 @@ export interface GlobalStatusProps
       'ref' | 'children' | 'onClose' | 'onAdjust' | 'onShow' | 'title'
     >,
     SpacingProps {
+  /**
+   * The main ID. Defaults to `main`.
+   */
   id?: string
   statusId?: string
+  /**
+   * The title appears as a part of the status content. Defaults to `En feil har skjedd`.
+   */
   title?: GlobalStatusTitle
   defaultTitle?: string
+  /**
+   * The text appears as the status content. Besides plain text, you can send in a React component as well. Defaults to `null`.
+   */
   text?: GlobalStatusText
+  /**
+   * The items (list items) appear as a part of the status content. you can both use an JSON array, or a vanilla array with a string or an object content. See **Item Object** example below.
+   */
   items?: GlobalStatusItem[]
+  /**
+   * The icon shown before the status title. Defaults to `exclamation`.
+   */
   icon?: IconIcon
+  /**
+   * The icon size of the title icon shows. Defaults to `medium`.
+   */
   iconSize?: IconSize
+  /**
+   * Defines the visual appearance of the status. There are four main statuses `error`, `warning`, `info` and `success`. The default status is `error`.
+   */
   state?: GlobalStatusState
+  /**
+   * Set to `true` or `false` to manually make the global status visible. Defaults to `true`.
+   */
   show?: GlobalStatusShow
+  /**
+   * Set to `true` to automatically scroll the page to the appeared global status. Defaults to `true`.
+   */
   autoScroll?: boolean
+  /**
+   * Set to `true` to automatically close the global status if there are no more left items in the provider stack. Defaults to `true`.
+   */
   autoClose?: boolean
+  /**
+   * Set to `true` to disable the show/hide/slide/fade/grow/shrink animation. Defaults to `false`.
+   */
   noAnimation?: boolean
+  /**
+   * Defines the delay on how long the automated visibility should wait before it appears to the user. Defaults to `200ms`.
+   */
   delay?: GlobalStatusDelay
+  /**
+   * Text of the close button. Defaults to `Lukk`.
+   */
   closeText?: React.ReactNode
+  /**
+   * Set to `true` if the close button should be hidden for the user. Defaults to `false`.
+   */
   hideCloseButton?: boolean
+  /**
+   * Set to `true` to omit setting the focus during visibility. Defaults to `false`. Additionally, there is `omitSetFocusOnUpdate` which is set to `true` by default.
+   */
   omitSetFocus?: boolean
+  /**
+   * Set to `true` to omit setting the focus during update. Defaults to `true`.
+   */
   omitSetFocusOnUpdate?: boolean
+  /**
+   * Defines the anchor text showing up after every item, in case there is a `statusId` defined. Defaults to `Gå til %s`. The `%s` represents the optional and internal handled label addition.
+   */
   statusAnchorText?: React.ReactNode
+  /**
+   * If set to `true`, an overlaying skeleton with animation will be shown.
+   */
   skeleton?: SkeletonShow
   className?: string
+  /**
+   * The text appears as the status content. Besides plain text, you can send in a React component as well. Defaults to `null`.
+   */
   children?: GlobalStatusChildren
   onAdjust?: (...args: any[]) => any
   onOpen?: (...args: any[]) => any
@@ -93,28 +150,61 @@ export interface GlobalStatusProps
 
 export type GlobalStatusStatusId = string
 export type GlobalStatusAddProps = {
+  /**
+   * The main ID. Defaults to `main`.
+   */
   id: string
   statusId: GlobalStatusStatusId
+  /**
+   * The title appears as a part of the status content. Defaults to `En feil har skjedd`.
+   */
   title?: string
+  /**
+   * The text appears as the status content. Besides plain text, you can send in a React component as well. Defaults to `null`.
+   */
   text: string
   item?: GlobalStatusItem
+  /**
+   * The items (list items) appear as a part of the status content. you can both use an JSON array, or a vanilla array with a string or an object content. See **Item Object** example below.
+   */
   items?: GlobalStatusItem[]
   onClose?: ({ statusId }: { statusId: GlobalStatusStatusId }) => void
 }
 export type GlobalStatusUpdateProps = {
+  /**
+   * The main ID. Defaults to `main`.
+   */
   id: string
+  /**
+   * The text appears as the status content. Besides plain text, you can send in a React component as well. Defaults to `null`.
+   */
   text: string
 }
 export type GlobalStatusRemoveProps = {
+  /**
+   * The main ID. Defaults to `main`.
+   */
   id: string
   statusId: GlobalStatusStatusId
   bufferDelay?: number
 }
 export type GlobalStatusInterceptorProps = {
+  /**
+   * The main ID. Defaults to `main`.
+   */
   id: string
+  /**
+   * The title appears as a part of the status content. Defaults to `En feil har skjedd`.
+   */
   title: string
+  /**
+   * The text appears as the status content. Besides plain text, you can send in a React component as well. Defaults to `null`.
+   */
   text: string
   statusId: GlobalStatusStatusId
+  /**
+   * Set to `true` or `false` to manually make the global status visible. Defaults to `true`.
+   */
   show: boolean
   item?: GlobalStatusItem
 }
@@ -122,7 +212,13 @@ export type GlobalStatusInterceptorUpdateEvents = {
   onShow?: (...args: any[]) => any
   onHide?: (...args: any[]) => any
   onClose?: (...args: any[]) => any
+  /**
+   * Set to `true` or `false` to manually make the global status visible. Defaults to `true`.
+   */
   show?: boolean
+  /**
+   * The text appears as the status content. Besides plain text, you can send in a React component as well. Defaults to `null`.
+   */
   text?: string
 }
 
