4583a22909990ddbfc9e1c9187e94ff4f3b1b698
Anders <anderslangseth@gmail.com>
2026-03-03 23:35:07 +0100

    refactor(Radio): improve TypeScript types (#6884)

---
+++ packages/dnb-eufemia/src/components/radio/RadioGroupContext.ts
@@ -0,0 +1,21 @@
+/**
+ * Web RadioGroup Context
+ *
+ */
+
+import React from 'react'
+
+import type { RadioGroupLabelPosition, RadioGroupSize } from './RadioGroup'
+
+export interface RadioGroupContextValue {
+  name?: string
+  value?: string
+  size?: RadioGroupSize
+  disabled?: boolean
+  labelPosition?: RadioGroupLabelPosition
+  onChange?: (args: { value: string; event: React.SyntheticEvent }) => void
+}
+
+const RadioGroupContext = React.createContext<RadioGroupContextValue>({})
+
+export default RadioGroupContext
