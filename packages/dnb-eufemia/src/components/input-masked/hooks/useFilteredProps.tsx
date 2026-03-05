5b02876d60cfc8544314ad0d688589570a31652f
Joakim Bjerknes <joakbjerk@gmail.com>
2026-03-05 10:06:17 +0100

    filter out innerRef from html attributes

--- packages/dnb-eufemia/src/components/input-masked/hooks/useFilteredProps.tsx
+++ packages/dnb-eufemia/src/components/input-masked/hooks/useFilteredProps.tsx
@@ -22,6 +22,7 @@ export const useFilteredProps = () => {
     locale, // eslint-disable-line
     showMask, // eslint-disable-line
     allowOverflow, // eslint-disable-line
+    innerRef, // eslint-disable-line
 
     // Get rest of possible attributes
     ...attributes
