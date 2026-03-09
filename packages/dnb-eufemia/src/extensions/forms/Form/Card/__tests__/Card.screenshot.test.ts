45aca24849e67e2736a19933525eec1520141dc2
Tobias Høegh <tobias@tujo.no>
2026-03-09 19:36:54 +0100

    break(Card)!: remove `Card.Provider` and `Form.Card.Provider` (#6940)
    Following up on #6929

--- packages/dnb-eufemia/src/extensions/forms/Form/Card/__tests__/Card.screenshot.test.ts
+++ packages/dnb-eufemia/src/extensions/forms/Form/Card/__tests__/Card.screenshot.test.ts
@@ -8,17 +8,6 @@ import { makeScreenshot } from '../../../../../core/jest/jestSetupScreenshots'
 const url = '/uilib/extensions/forms/Form/Card/demos/'
 
 describe('Form.Card', () => {
-  it('have to match outset', async () => {
-    const screenshot = await makeScreenshot({
-      url,
-      selector: '[data-visual-test="forms-card"]',
-      wrapperStyle: {
-        padding: '2rem',
-      },
-    })
-    expect(screenshot).toMatchImageSnapshot()
-  })
-
   it('have to match when used in wizard', async () => {
     const screenshot = await makeScreenshot({
       url,
@@ -27,24 +16,3 @@ describe('Form.Card', () => {
     expect(screenshot).toMatchImageSnapshot()
   })
 })
-
-describe.each(['ui', 'sbanken'])(
-  'Card small screen for %s',
-  (themeName) => {
-    const params = {
-      themeName,
-      pageViewport: {
-        width: 400,
-      },
-      url,
-    }
-
-    it('have to match outset', async () => {
-      const screenshot = await makeScreenshot({
-        ...params,
-        selector: '[data-visual-test="forms-card"]',
-      })
-      expect(screenshot).toMatchImageSnapshot()
-    })
-  }
-)
