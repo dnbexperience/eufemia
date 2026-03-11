867ed1121b4dc6ef186da648f8c4692225ecbc64
Anders Langseth <anderslangseth@gmail.com>
2026-03-11 12:27:39 +0100

    fix

--- packages/dnb-eufemia/src/components/autocomplete/Autocomplete.tsx
+++ packages/dnb-eufemia/src/components/autocomplete/Autocomplete.tsx
@@ -56,7 +56,7 @@ import ProgressIndicator from '../progress-indicator/ProgressIndicator'
 import DrawerList from '../../fragments/drawer-list/DrawerList'
 import { ItemContent } from '../../fragments/drawer-list/DrawerListItem'
 import DrawerListContext, {
-  DrawerListContextProps,
+  type DrawerListContextProps,
 } from '../../fragments/drawer-list/DrawerListContext'
 import DrawerListProvider from '../../fragments/drawer-list/DrawerListProvider'
 import {
