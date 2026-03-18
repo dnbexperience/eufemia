29d154a546e3c1f8efbfeb4ea6a82d364396880d
-l <anderslangseth@gmail.com>
2026-03-18 21:04:43 +0100

    add rule

--- packages/dnb-eufemia/eslint.config.mjs
+++ packages/dnb-eufemia/eslint.config.mjs
@@ -281,6 +281,14 @@ export default [
     rules: {
       'import/named': 'off',
       'no-unused-vars': 'off',
+      '@typescript-eslint/ban-ts-comment': [
+        'error',
+        {
+          'ts-expect-error': 'allow-with-description',
+          'ts-ignore': 'allow-with-description',
+          minimumDescriptionLength: 3,
+        },
+      ],
       '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/no-inferrable-types': 'error',
       '@typescript-eslint/no-non-null-assertion': 'warn',
