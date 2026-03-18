62245b369c85f73128c6677ccd73937191ccd83f
Anders Langseth <anderslangseth@gmail.com>
2026-03-18 10:15:43 +0100

    docs: document keepInDOM requirement for visibleOnly option
    Explain that visibleOnly requires keepInDOM on Form.Visibility to
    track visibility state. Without keepInDOM, hidden fields unmount
    and are already excluded from hasErrors() by default.
    
    Also add keepInDOM to the docs example.

--- packages/dnb-design-system-portal/src/docs/uilib/extensions/forms/Form/useValidation/Examples.tsx
+++ packages/dnb-design-system-portal/src/docs/uilib/extensions/forms/Form/useValidation/Examples.tsx
@@ -103,7 +103,7 @@ export function VisibleOnly() {
                   path="/showField"
                 />
 
-                <Form.Visibility pathTrue="/showField" animate>
+                <Form.Visibility pathTrue="/showField" keepInDOM animate>
                   <Field.String
                     path="/conditionalField"
                     label="Conditional field"
