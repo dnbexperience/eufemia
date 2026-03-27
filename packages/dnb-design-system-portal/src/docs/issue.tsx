74e765a30843ea41f0a645f1b4f8b82e9c35f839
Anders <anderslangseth@gmail.com>
2026-03-27 20:23:10 +0100

    docs: add StackBlitz starter to /issue page (#7250)
    Deploy preview:
    https://chore-add-stackblitz-starter.eufemia-e25.pages.dev/issue/
    
    - Replace issue.tsx redirect with issue.mdx page that lets users choose
    between StackBlitz and CodeSandbox starter templates
    - Update references in issues.mdx and contribute.mdx
    
    TODO: Improve the visuals in
    https://chore-add-stackblitz-starter.eufemia-e25.pages.dev/issue/

--- packages/dnb-design-system-portal/src/docs/issue.tsx
+++
@@ -1,9 +0,0 @@
-/**
- * Redirect to Codesandbox starter
- */
-export default function Page() {
-  if (typeof location !== 'undefined') {
-    location.href = 'https://codesandbox.io/s/eufemia-starter-0gcwo'
-  }
-  return 'Redirecting ...'
-}
