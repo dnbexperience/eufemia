fd016fb20ed2ffb98713053379fc1ccbddca2d1f
Joakim Bjerknes <joakbjerk@gmail.com>
2026-04-10 21:43:54 +0200

    add `carnegie` screenshots and tests

--- packages/dnb-eufemia/src/elements/typography/__tests__/Paragraph.screenshot.test.ts
+++ packages/dnb-eufemia/src/elements/typography/__tests__/Paragraph.screenshot.test.ts
@@ -21,90 +21,96 @@ describe('Paragraph', () => {
   })
 })
 
-describe.each(['ui', 'sbanken'])('Paragraph for %s', (themeName) => {
-  setupPageScreenshot({
-    themeName,
-    url: '/uilib/elements/paragraph/demos/',
-  })
-
-  it('have to match the paragraph with weight modifiers', async () => {
-    const screenshot = await makeScreenshot({
-      selector: '[data-visual-test="paragraph-modifiers-weight"]',
+describe.each(['ui', 'sbanken', 'carnegie'])(
+  'Paragraph for %s',
+  (themeName) => {
+    setupPageScreenshot({
+      themeName,
+      url: '/uilib/elements/paragraph/demos/',
     })
-    expect(screenshot).toMatchImageSnapshot()
-  })
 
-  it('have to match the paragraph with size modifiers', async () => {
-    const screenshot = await makeScreenshot({
-      selector: '[data-visual-test="paragraph-modifiers-size"]',
+    it('have to match the paragraph with weight modifiers', async () => {
+      const screenshot = await makeScreenshot({
+        selector: '[data-visual-test="paragraph-modifiers-weight"]',
+      })
+      expect(screenshot).toMatchImageSnapshot()
     })
-    expect(screenshot).toMatchImageSnapshot()
-  })
 
-  it('have to match the paragraph with align modifiers', async () => {
-    const screenshot = await makeScreenshot({
-      style: { width: '30rem' },
-      selector: '[data-visual-test="paragraph-modifiers-align"]',
+    it('have to match the paragraph with size modifiers', async () => {
+      const screenshot = await makeScreenshot({
+        selector: '[data-visual-test="paragraph-modifiers-size"]',
+      })
+      expect(screenshot).toMatchImageSnapshot()
     })
-    expect(screenshot).toMatchImageSnapshot()
-  })
 
-  it('have to match the paragraph with family modifiers', async () => {
-    const screenshot = await makeScreenshot({
-      selector: '[data-visual-test="paragraph-modifiers-family"]',
+    it('have to match the paragraph with align modifiers', async () => {
+      const screenshot = await makeScreenshot({
+        style: { width: '30rem' },
+        selector: '[data-visual-test="paragraph-modifiers-align"]',
+      })
+      expect(screenshot).toMatchImageSnapshot()
     })
-    expect(screenshot).toMatchImageSnapshot()
-  })
 
-  it('have to match the paragraph with line modifiers', async () => {
-    const screenshot = await makeScreenshot({
-      selector: '[data-visual-test="paragraph-modifiers-line"]',
+    it('have to match the paragraph with family modifiers', async () => {
+      const screenshot = await makeScreenshot({
+        selector: '[data-visual-test="paragraph-modifiers-family"]',
+      })
+      expect(screenshot).toMatchImageSnapshot()
     })
-    expect(screenshot).toMatchImageSnapshot()
-  })
 
-  it('have to match the paragraph with other modifiers', async () => {
-    const screenshot = await makeScreenshot({
-      selector: '[data-visual-test="paragraph-modifiers-other"]',
+    it('have to match the paragraph with line modifiers', async () => {
+      const screenshot = await makeScreenshot({
+        selector: '[data-visual-test="paragraph-modifiers-line"]',
+      })
+      expect(screenshot).toMatchImageSnapshot()
     })
-    expect(screenshot).toMatchImageSnapshot()
-  })
 
-  it('have to match the paragraph example', async () => {
-    const screenshot = await makeScreenshot({
-      selector: '[data-visual-test="paragraph-default"]',
+    it('have to match the paragraph with other modifiers', async () => {
+      const screenshot = await makeScreenshot({
+        selector: '[data-visual-test="paragraph-modifiers-other"]',
+      })
+      expect(screenshot).toMatchImageSnapshot()
     })
-    expect(screenshot).toMatchImageSnapshot()
-  })
 
-  it('have to match the paragraph with small text', async () => {
-    const screenshot = await makeScreenshot({
-      selector: '[data-visual-test="paragraph-small"]',
+    it('have to match the paragraph example', async () => {
+      const screenshot = await makeScreenshot({
+        selector: '[data-visual-test="paragraph-default"]',
+      })
+      expect(screenshot).toMatchImageSnapshot()
     })
-    expect(screenshot).toMatchImageSnapshot()
-  })
 
-  it('have to match the paragraph with additional elements', async () => {
-    const screenshot = await makeScreenshot({
-      selector: '[data-visual-test="paragraph-additional"]',
-      matchConfig: {
-        failureThreshold: 0.0099, // the underlines gets blurry in the build version
-      },
+    it('have to match the paragraph with small text', async () => {
+      const screenshot = await makeScreenshot({
+        selector: '[data-visual-test="paragraph-small"]',
+      })
+      expect(screenshot).toMatchImageSnapshot()
     })
-    expect(screenshot).toMatchImageSnapshot()
-  })
-})
 
-describe.each(['ui', 'sbanken'])('Paragraph for %s', (themeName) => {
-  setupPageScreenshot({
-    themeName,
-    url: '/uilib/elements/paragraph/demos/',
-  })
-
-  it('matches all sizes and weights', async () => {
-    const screenshot = await makeScreenshot({
-      selector: '[data-visual-test="paragraph-sizes"]',
+    it('have to match the paragraph with additional elements', async () => {
+      const screenshot = await makeScreenshot({
+        selector: '[data-visual-test="paragraph-additional"]',
+        matchConfig: {
+          failureThreshold: 0.0099, // the underlines gets blurry in the build version
+        },
+      })
+      expect(screenshot).toMatchImageSnapshot()
     })
-    expect(screenshot).toMatchImageSnapshot()
-  })
-})
+  }
+)
+
+describe.each(['ui', 'sbanken', 'carnegie'])(
+  'Paragraph for %s',
+  (themeName) => {
+    setupPageScreenshot({
+      themeName,
+      url: '/uilib/elements/paragraph/demos/',
+    })
+
+    it('matches all sizes and weights', async () => {
+      const screenshot = await makeScreenshot({
+        selector: '[data-visual-test="paragraph-sizes"]',
+      })
+      expect(screenshot).toMatchImageSnapshot()
+    })
+  }
+)
