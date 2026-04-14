c739346313d0c708e87dc37c5634f846037fa29f
Anders <anderslangseth@gmail.com>
2026-04-14 22:12:26 +0200

    chore(Form.Appearance): use specific union type for size (#7464)

--- packages/dnb-eufemia/src/extensions/forms/Form/Appearance/AppearanceDocs.ts
+++ packages/dnb-eufemia/src/extensions/forms/Form/Appearance/AppearanceDocs.ts
@@ -3,7 +3,7 @@ import type { PropertiesTableProps } from '../../../../shared/types'
 export const FormAppearanceProperties: PropertiesTableProps = {
   size: {
     doc: 'The sizes you can choose is `default`, `medium` and `large`.',
-    type: 'string',
+    type: ['"medium"', '"large"'],
     status: 'optional',
   },
 }
