f0dcef5ad5907837dacb653aceaadf985b59558e
Anders <anderslangseth@gmail.com>
2026-04-14 22:12:20 +0200

    chore(CloseButton): replace HTML em tags with backticks (#7465)

--- packages/dnb-eufemia/src/components/modal/parts/CloseButton.tsx
+++ packages/dnb-eufemia/src/components/modal/parts/CloseButton.tsx
@@ -12,7 +12,7 @@ import type { ButtonProps } from '../../button/Button'
 
 export type CloseButtonProps = {
   /**
-   * The title of the close button. Defaults to <em>Close</em> or <em>Lukk</em>.
+   * The title of the close button. Defaults to `Close` or `Lukk`.
    */
   closeTitle?: string
 } & Partial<ButtonProps>
