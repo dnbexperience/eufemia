28c8500bf7ec6e5da40e4c07f8cbf594694ba26d
Anders Langseth <anderslangseth@gmail.com>
2026-04-14 21:54:55 +0200

    fix: use specific union type for layout in DlDocs.ts

--- packages/dnb-eufemia/src/elements/lists/DlDocs.ts
+++ packages/dnb-eufemia/src/elements/lists/DlDocs.ts
@@ -4,7 +4,7 @@ import { ElementPropertiesWithoutSkeleton } from './../ElementDocs'
 export const DlProperties: PropertiesTableProps = {
   layout: {
     doc: 'Sets the layout of the list. Can be `vertical`, `horizontal` or `grid`. Defaults to `vertical`.',
-    type: 'string',
+    type: ['"vertical"', '"horizontal"', '"grid"'],
     status: 'optional',
   },
   ...ElementPropertiesWithoutSkeleton,
