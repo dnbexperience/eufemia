fe6ff06b10dfc32404ab1cbb9104341319703fe5
Anders <anderslangseth@gmail.com>
2026-04-15 10:13:34 +0200

    docs(Value.Composition): fix maxWidth and gap types to use union types (#7490)

--- packages/dnb-eufemia/src/extensions/forms/Value/Composition/CompositionDocs.ts
+++ packages/dnb-eufemia/src/extensions/forms/Value/Composition/CompositionDocs.ts
@@ -9,12 +9,19 @@ export const CompositionProperties: PropertiesTableProps = {
   transformLabel,
   maxWidth: {
     doc: 'Use `small`, `medium` or `large` for predefined standard max widths. Defaults to `auto`.',
-    type: 'string',
+    type: ['"auto"', '"small"', '"medium"', '"large"'],
     status: 'optional',
   },
   gap: {
     doc: 'The gap between the different value blocks. Can be `xx-small`, `x-small`, `small`, `medium`, `large` or `false`. Defaults to `xx-small`.',
-    type: 'string',
+    type: [
+      '"xx-small"',
+      '"x-small"',
+      '"small"',
+      '"medium"',
+      '"large"',
+      'false',
+    ],
     status: 'optional',
   },
 }
