8e813113576d33ef34b8bf5f49a3dcd1e3cc31a7
Tobias Høegh <tobias@tujo.no>
2026-04-19 15:29:12 +0200

    chore(Space): remove unused theme file (#7555)

--- packages/dnb-eufemia/src/components/space/__tests__/Space.test.tsx
+++ packages/dnb-eufemia/src/components/space/__tests__/Space.test.tsx
@@ -147,11 +147,4 @@ describe('Space scss', () => {
     const css = loadScss(require.resolve('../style/deps.scss'))
     expect(css).toMatchSnapshot()
   })
-
-  it('should match default theme snapshot', () => {
-    const css = loadScss(
-      require.resolve('../style/themes/dnb-space-theme-ui.scss')
-    )
-    expect(css).toMatchSnapshot()
-  })
 })
