f11e483f2ce45e9faa7f4b336d1bbf5e8ecf38c4
Anders <anderslangseth@gmail.com>
2026-04-20 15:14:31 +0200

    refactor(Blockquote): use design tokens (#7581)
    Co-authored-by: Tobias Høegh <tobias@tujo.no>

---
+++ packages/dnb-eufemia/src/elements/code/stories/Code.stories.tsx
@@ -0,0 +1,31 @@
+/**
+ * @dnb/eufemia Element Story
+ *
+ */
+
+import React from 'react'
+import { Wrapper, Box } from 'storybook-utils/helpers'
+import Code from '../Code'
+import Section from '../../../components/section/Section'
+
+export default {
+  title: 'Eufemia/Elements/Code',
+}
+
+export const CodeSandbox = () => (
+  <Wrapper className="dnb-spacing">
+    <Box>
+      <p className="dnb-p">
+        Inline code: <Code>const x = 1</Code>
+      </p>
+    </Box>
+
+    <Box>
+      <Section variant="surface-dark">
+        <p className="dnb-p">
+          On dark surface: <Code>const x = 1</Code>
+        </p>
+      </Section>
+    </Box>
+  </Wrapper>
+)
