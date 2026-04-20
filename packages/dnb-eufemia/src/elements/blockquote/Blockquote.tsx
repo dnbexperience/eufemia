3737f0dc38e9047eaa8e97f5caa83438fbbcde87
Anders <anderslangseth@gmail.com>
2026-04-20 15:14:31 +0200

    refactor(Blockquote): use design tokens (#7581)
    Co-authored-by: Tobias Høegh <tobias@tujo.no>

--- packages/dnb-eufemia/src/elements/blockquote/Blockquote.tsx
+++ packages/dnb-eufemia/src/elements/blockquote/Blockquote.tsx
@@ -3,10 +3,11 @@
  *
  */
 
-import React from 'react'
+import React, { useContext } from 'react'
 import clsx from 'clsx'
 import type { SpacingProps } from '../../shared/types'
 import E from '../Element'
+import { Context, Theme } from '../../shared'
 import withComponentMarkers from '../../shared/helpers/withComponentMarkers'
 
 type BlockquoteProps = SpacingProps &
@@ -26,9 +27,12 @@ function Blockquote({
   noBackground,
   direction = 'horizontal',
   className,
+  children,
   ref,
   ...props
 }: BlockquoteProps & { ref?: React.Ref<HTMLQuoteElement> }) {
+  const context = useContext(Context)
+
   return (
     <E
       as="blockquote"
@@ -40,7 +44,32 @@ function Blockquote({
         direction === 'vertical' && 'dnb-blockquote--top'
       )}
       {...props}
-    />
+    >
+      <svg
+        className="dnb-blockquote__quote-icon"
+        width="48"
+        height="48"
+        viewBox="0 0 48 48"
+        fill="none"
+        aria-hidden
+      >
+        <path d="M37.5 27.496a9 9 0 1 0 0-18 9 9 0 0 0 0 18M13.5 27.496a9 9 0 1 0 0-18 9 9 0 0 0 0 18" />
+        <path
+          d="M46.5 18.496a21 21 0 0 1-21 21m-3-21a21 21 0 0 1-21 21"
+          fill="none"
+        />
+      </svg>
+
+      <Theme.Context
+        surface={
+          noBackground || context?.theme?.name === 'sbanken'
+            ? 'initial'
+            : 'dark'
+        }
+      >
+        {children}
+      </Theme.Context>
+    </E>
   )
 }
 
---
+++ packages/dnb-eufemia/src/elements/blockquote/__tests__/Blockquote.test.tsx
@@ -0,0 +1,49 @@
+/**
+ * Blockquote Test
+ *
+ */
+
+import React from 'react'
+import { render } from '@testing-library/react'
+import Blockquote from '../Blockquote'
+import Theme from '../../../shared/Theme'
+import Code from '../../code/Code'
+
+describe('Blockquote', () => {
+  describe('surface context', () => {
+    it('should set surface to dark for children by default', () => {
+      render(
+        <Blockquote>
+          <Code>code</Code>
+        </Blockquote>
+      )
+
+      const code = document.querySelector('.dnb-code')
+      expect(code).toHaveClass('dnb-code--surface-dark')
+    })
+
+    it('should set surface to initial when noBackground is true', () => {
+      render(
+        <Blockquote noBackground>
+          <Code>code</Code>
+        </Blockquote>
+      )
+
+      const code = document.querySelector('.dnb-code')
+      expect(code).not.toHaveClass('dnb-code--surface-dark')
+    })
+
+    it('should set surface to initial for sbanken theme', () => {
+      render(
+        <Theme name="sbanken">
+          <Blockquote>
+            <Code>code</Code>
+          </Blockquote>
+        </Theme>
+      )
+
+      const code = document.querySelector('.dnb-code')
+      expect(code).not.toHaveClass('dnb-code--surface-dark')
+    })
+  })
+})
