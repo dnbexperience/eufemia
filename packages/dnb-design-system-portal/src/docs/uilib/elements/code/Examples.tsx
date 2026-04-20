c6d20dd8de8b2766f6bda732ee6767a177bd9f9b
Tobias Høegh <tobias@tujo.no>
2026-04-20 14:11:34 +0200

    Implement svg as shared inline code without css filter

---
+++ packages/dnb-design-system-portal/src/docs/uilib/elements/code/Examples.tsx
@@ -0,0 +1,29 @@
+/**
+ * UI lib Component Example
+ *
+ */
+
+import React from 'react'
+import ComponentBox from '../../../../shared/tags/ComponentBox'
+import { Code, Flex, P, Section } from '@dnb/eufemia/src'
+
+export function CodeExample() {
+  return (
+    <ComponentBox>
+      <Flex.Stack>
+        <P>
+          My <Code>formatted text</Code> inside a paragraph
+        </P>
+
+        <Section surface="dark" innerSpace={{ block: 'small' }}>
+          <P>
+            My <Code>formatted text</Code> inside a paragraph on dark
+            surface
+          </P>
+        </Section>
+
+        <pre className="dnb-pre">Code Syntax</pre>
+      </Flex.Stack>
+    </ComponentBox>
+  )
+}
--- packages/dnb-eufemia/src/elements/code/Code.tsx
+++ packages/dnb-eufemia/src/elements/code/Code.tsx
@@ -3,18 +3,33 @@
  *
  */
 
-import React from 'react'
+import React, { useContext } from 'react'
+import clsx from 'clsx'
 import type { SpacingProps } from '../../shared/types'
+import Context from '../../shared/Context'
 import E from '../Element'
 import withComponentMarkers from '../../shared/helpers/withComponentMarkers'
 
 type CodeProps = SpacingProps & React.HTMLAttributes<HTMLElement>
 
 function Code({
+  className,
   ref,
   ...props
 }: CodeProps & { ref?: React.Ref<HTMLElement> }) {
-  return <E as="code" ref={ref} {...props} />
+  const context = useContext(Context)
+
+  return (
+    <E
+      as="code"
+      ref={ref}
+      className={clsx(
+        context?.theme?.surface === 'dark' && 'dnb-code--surface-dark',
+        className
+      )}
+      {...props}
+    />
+  )
 }
 
 withComponentMarkers(Code, { _supportsSpacingProps: true })
