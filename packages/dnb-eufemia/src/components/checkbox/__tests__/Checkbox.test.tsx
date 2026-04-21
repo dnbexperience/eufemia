c2a5b7f2a0f3a253921b2c517e2a7768cd37eb34
Joakim Bjerknes <joakbjerk@gmail.com>
2026-04-21 11:35:33 +0200

    refactor(Checkbox): use design tokens (#7515)
    Co-authored-by: Tobias Høegh <tobias@tujo.no>

--- packages/dnb-eufemia/src/components/checkbox/__tests__/Checkbox.test.tsx
+++ packages/dnb-eufemia/src/components/checkbox/__tests__/Checkbox.test.tsx
@@ -449,11 +449,4 @@ describe('Checkbox scss', () => {
     const css = loadScss(require.resolve('../style/deps.scss'))
     expect(css).toMatchSnapshot()
   })
-
-  it('should match default theme snapshot', () => {
-    const css = loadScss(
-      require.resolve('../style/themes/dnb-checkbox-theme-ui.scss')
-    )
-    expect(css).toMatchSnapshot()
-  })
 })
