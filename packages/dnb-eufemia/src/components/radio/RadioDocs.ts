a24ce3f0112e391a39fe3bf0b6dc2bc47ff5d5f6
Anders <anderslangseth@gmail.com>
2026-04-22 12:56:44 +0200

    chore: recommend useRef over createRef in component docs (#7672)
    React 19 makes ref a regular prop. Update docs for Radio, Section,
    Checkbox, Input, Dropdown, Textarea, and Switch to recommend
    React.useRef() instead of React.createRef().

--- packages/dnb-eufemia/src/components/radio/RadioDocs.ts
+++ packages/dnb-eufemia/src/components/radio/RadioDocs.ts
@@ -57,7 +57,7 @@ export const RadioProperties: PropertiesTableProps = {
     status: 'optional',
   },
   ref: {
-    doc: 'By providing a `React.Ref` we can get the internally used input element (DOM), e.g. `ref={myRef}` by using `React.createRef()` or `React.useRef()`.',
+    doc: 'By providing a `React.Ref` we can get the internally used input element (DOM), e.g. `ref={myRef}` by using `React.useRef()`.',
     type: 'React.RefObject',
     status: 'optional',
   },
