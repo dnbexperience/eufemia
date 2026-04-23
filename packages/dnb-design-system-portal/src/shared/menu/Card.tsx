9e18a2363126ca45eea2d85f7e2844c913ab2581
Tobias Høegh <tobias@tujo.no>
2026-04-23 22:21:47 +0200

    Refactor home

--- packages/dnb-design-system-portal/src/shared/menu/Card.tsx
+++ packages/dnb-design-system-portal/src/shared/menu/Card.tsx
@@ -5,14 +5,8 @@
 
 import React from 'react'
 import { Link } from '../tags/Anchor'
-import clsx from 'clsx'
-import { Button, P, Span } from '@dnb/eufemia/src'
-import {
-  liItemStyle,
-  linkStyle,
-  linkInnerStyle,
-  boxStyle,
-} from './Card.module.scss'
+import styled from '@emotion/styled'
+import { Button, Card, P } from '@dnb/eufemia/src'
 
 type MenuCardProps = {
   url: string
@@ -26,44 +20,60 @@ type MenuCardProps = {
 export default function MenuCard(props: MenuCardProps) {
   const { url, title, about, icon: Svg } = props
 
-  const isSelected = () => {
-    if (typeof window !== 'undefined') {
-      const { url } = props
-      const { pathname } = window.location
-      return pathname.length > 1 && pathname.includes(url)
-    }
-    return false
-  }
-
   return (
-    <li className={clsx(liItemStyle, isSelected() && 'is-selected')}>
-      <Link
-        className={clsx(
-          linkStyle,
-          'dnb-anchor--no-style',
-          isSelected() && 'current-card'
-        )}
-        to={url}
-        aria-current={isSelected()}
-      >
-        <Span className={linkInnerStyle}>
-          <Span className={boxStyle}>
-            {Svg && <Svg />}
-            <P className="dnb-p--lead">{title}</P>
-            <P top="x-small">{about}</P>
-          </Span>
+    <StyledLi>
+      <StyledLink to={url}>
+        <StyledCard stack dropShadow>
+          {Svg && <Svg />}
+          <P className="dnb-p--lead">{title}</P>
+          <P top="x-small">{about}</P>
 
-          <Span aria-hidden top bottom>
-            <Button
-              variant="tertiary"
-              icon="chevron_right"
-              text="Read more"
-              tabIndex={-1}
-              element="span"
-            />
-          </Span>
-        </Span>
-      </Link>
-    </li>
+          <Button
+            variant="tertiary"
+            icon="chevron_right"
+            text="Read more"
+            tabIndex={-1}
+            element="span"
+          />
+        </StyledCard>
+      </StyledLink>
+    </StyledLi>
   )
 }
+
+const StyledLi = styled.li`
+  list-style-type: none;
+  width: calc(33.333333% - 1rem);
+  margin: 0.5rem;
+`
+
+const StyledLink = styled(Link)`
+  outline: none;
+  text-decoration: none;
+  color: inherit;
+
+  &:hover,
+  &:active,
+  &:focus {
+    text-decoration: none;
+  }
+
+  &:hover .dnb-card {
+    --card-outline-color: var(--token-color-stroke-action-hover);
+  }
+  &:focus-visible .dnb-card {
+    --card-outline-color: var(--token-color-stroke-action-focus);
+    --card-outline-width: 0.125rem;
+    --card-background-color: var(
+      --token-color-background-action-focus-subtle
+    );
+    /* color: var(--token-color-text-action-focus); */
+  }
+`
+
+const StyledCard = styled(Card)`
+  svg {
+    align-self: center;
+    stroke: var(--token-color-icon-neutral);
+  }
+`
