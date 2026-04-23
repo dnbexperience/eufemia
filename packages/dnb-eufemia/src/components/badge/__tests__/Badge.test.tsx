495e8af6837cbfe7a3e4061f917ec8ec0aa3774f
Anders <anderslangseth@gmail.com>
2026-04-23 22:06:59 +0200

    refactor(Badge): use design tokens for notification (#7590)
    Co-authored-by: Joakim Bjerknes <joakbjerk@gmail.com>

--- packages/dnb-eufemia/src/components/badge/__tests__/Badge.test.tsx
+++ packages/dnb-eufemia/src/components/badge/__tests__/Badge.test.tsx
@@ -348,11 +348,4 @@ describe('Badge scss', () => {
     const css = loadScss(require.resolve('../style/deps.scss'))
     expect(css).toMatchSnapshot()
   })
-
-  it('should match default theme snapshot', () => {
-    const css = loadScss(
-      require.resolve('../style/themes/dnb-badge-theme-ui.scss')
-    )
-    expect(css).toMatchSnapshot()
-  })
 })
