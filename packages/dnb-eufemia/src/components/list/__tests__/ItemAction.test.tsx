61df5808f981d9a97c883ac139c97dfe636202f0
Anders <anderslangseth@gmail.com>
2026-04-23 12:09:24 +0200

    chore: remove forwardRef from test (#7719)

--- packages/dnb-eufemia/src/components/list/__tests__/ItemAction.test.tsx
+++ packages/dnb-eufemia/src/components/list/__tests__/ItemAction.test.tsx
@@ -584,19 +584,23 @@ describe('ItemAction', () => {
   })
 
   describe('element and to', () => {
-    const MockLink = React.forwardRef<
-      HTMLAnchorElement,
-      {
-        to: string
-        children: React.ReactNode
-        preventScrollReset?: boolean
-      }
-    >(({ to, children, ...rest }, ref) => (
-      <a href={to} ref={ref} {...rest}>
-        {children}
-      </a>
-    ))
-    MockLink.displayName = 'MockLink'
+    function MockLink({
+      to,
+      children,
+      ref,
+      ...rest
+    }: {
+      to: string
+      children: React.ReactNode
+      ref?: React.Ref<HTMLAnchorElement>
+      preventScrollReset?: boolean
+    }) {
+      return (
+        <a href={to} ref={ref} {...rest}>
+          {children}
+        </a>
+      )
+    }
 
     it('renders with a custom router Link component via element and to', () => {
       render(
