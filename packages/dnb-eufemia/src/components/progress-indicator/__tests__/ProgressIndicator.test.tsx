0fb42b7e1e0ce516fed91e66c476ac04cbfdeb63
Joakim Bjerknes <joakbjerk@gmail.com>
2026-04-27 19:08:02 +0200

    refactor(ProgressIndicator): use design tokens (#7797)

--- packages/dnb-eufemia/src/components/progress-indicator/__tests__/ProgressIndicator.test.tsx
+++ packages/dnb-eufemia/src/components/progress-indicator/__tests__/ProgressIndicator.test.tsx
@@ -536,11 +536,4 @@ describe('ProgressIndicator scss', () => {
     const css = loadScss(require.resolve('../style/deps.scss'))
     expect(css).toMatchSnapshot()
   })
-
-  it('should match default theme snapshot', () => {
-    const css = loadScss(
-      require.resolve('../style/themes/dnb-progress-indicator-theme-ui.scss')
-    )
-    expect(css).toMatchSnapshot()
-  })
 })
--- packages/dnb-eufemia/src/components/progress-indicator/style/themes/ui.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Imports the default theme
- *
- */
-
-import './dnb-progress-indicator-theme-ui.scss'
