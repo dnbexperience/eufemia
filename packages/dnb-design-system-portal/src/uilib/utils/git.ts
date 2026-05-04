6f13dd2fcc874164a02a3cb2439afd339906356a
Anders Langseth <anderslangseth@gmail.com>
2026-05-04 13:48:21 +0200

    fix(portal): remove unused code
    - Delete unused src/uilib/utils/git.ts (never imported, duplicate of direct require in gatsby-config.js)
    - Remove unused ToggleMenu SVG export from MainMenuGraphics.tsx
    - Remove unused .center modifier on .exampleBoxStyle in CodeBlock.module.scss

--- packages/dnb-design-system-portal/src/uilib/utils/git.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Git utils
- *
- */
-
-import branchName from 'current-git-branch' // More info: https://github.com/steveukx/git-js#readme
-
-// because we want the name in sync, we do not use "simple-git"
-export const getCurrentBranchName = (): string => branchName()
