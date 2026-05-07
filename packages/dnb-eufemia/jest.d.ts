d902dbdcd923bdf47d66219ea9976852363e8437
Anders Langseth <anderslangseth@gmail.com>
2026-05-07 12:04:56 +0200

    chore: clean up useId by removing unnecessary makeUniqueId fallback

---
+++ packages/dnb-eufemia/jest.d.ts
@@ -0,0 +1,10 @@
+import '@testing-library/jest-dom';
+
+declare global {
+  namespace jest {
+    interface Matchers<R, T = {}> {
+      toBeType(received: string, expected?: string): R;
+      toNeverResolve(): Promise<R>;
+    }
+  }
+}
