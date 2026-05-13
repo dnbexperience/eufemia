396e10020790b92598f756c634a365aa8263cb5f
Joakim Bjerknes <joakbjerk@gmail.com>
2026-05-13 14:23:51 +0200

    add docs

---
+++ packages/dnb-eufemia/src/extensions/forms/Field/Time/TimeDocs.ts
@@ -0,0 +1,19 @@
+import type { PropertiesTableProps } from '../../../../shared/types'
+import { InputProperties } from '../../../../components/input/InputDocs'
+
+export const TimeProperties: PropertiesTableProps = {
+  size: {
+    ...InputProperties.size,
+    doc: `${InputProperties.size.doc} Consider rather setting field sizes with [Form.Appearance](/uilib/extensions/forms/Form/Appearance/).`,
+  },
+  withSeconds: {
+    doc: 'If set to `true`, a seconds input is shown in addition to hours and minutes.',
+    type: 'boolean',
+    status: 'optional',
+  },
+  onBlurValidator: {
+    doc: 'Custom validator function that is triggered when the user leaves a field (e.g., blurring a text input or closing a dropdown). The function can be either asynchronous or synchronous. The first parameter is the value, and the second parameter returns an object containing { errorMessages, connectWithPath, validators }. Defaults to validating invalid hours and minutes, using `timeValidator`.',
+    type: 'function',
+    status: 'optional',
+  },
+}
