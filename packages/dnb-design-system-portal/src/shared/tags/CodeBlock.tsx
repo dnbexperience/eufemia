786020f4d640e2be4e7c2bf944a16f244e98dbfc
Anders Langseth <anderslangseth@gmail.com>
2026-05-20 13:15:43 +0200

    fix(portal): remove size=small from Menu.Button for consistent spacing
    Match the default size of other tertiary buttons in toolbar

--- packages/dnb-design-system-portal/src/shared/tags/CodeBlock.tsx
+++ packages/dnb-design-system-portal/src/shared/tags/CodeBlock.tsx
@@ -434,7 +434,6 @@ function LiveCode(props: LiveCodeProps) {
         title="Change locale"
         aria-label={`Change locale (current: ${currentLocaleOption?.label ?? context.locale})`}
         variant="tertiary"
-        size="small"
       />
       <Menu.List>
         {localeOptions.map(({ value, flag, label }) => (
