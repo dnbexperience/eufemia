82deffde005893ef12088b673acfef0f3daf4d61
Anders <anderslangseth@gmail.com>
2026-05-22 14:02:56 +0200

    chore: update broken internal links in documentation (#8213)

--- packages/dnb-eufemia/src/components/grid/ItemDocs.ts
+++ packages/dnb-eufemia/src/components/grid/ItemDocs.ts
@@ -2,7 +2,7 @@ import type { PropertiesTableProps } from '../../shared/types'
 
 export const GridItemProperties: PropertiesTableProps = {
   span: {
-    doc: 'To place the item on the grid with numbers from 1 to 12 (`columns`). Needs always two values, a so called span (from - to) `span={[1, 6]}`. Accepts also `end` like so `span={[1, "end"]}` or `full` like so `span={"full"}`. You can also provide [Media Query](/uilib/usage/layout/media-queries/) types in an object like so like so `span={{ small: [1, 4], medium: [1, 6], large: [1, 12]}}`.',
+    doc: 'To place the item on the grid with numbers from 1 to 12 (`columns`). Needs always two values, a so called span (from - to) `span={[1, 6]}`. Accepts also `end` like so `span={[1, "end"]}` or `full` like so `span={"full"}`. You can also provide [Media Query](/uilib/layout/media-queries/) types in an object like so like so `span={{ small: [1, 4], medium: [1, 6], large: [1, 12]}}`.',
     type: ['object', 'array'],
     status: 'optional',
   },
