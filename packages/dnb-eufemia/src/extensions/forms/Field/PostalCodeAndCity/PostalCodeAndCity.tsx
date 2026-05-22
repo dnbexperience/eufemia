47b7365a68d0eee663dde752c134417b1bd6261e
Joakim Bjerknes <joakbjerk@gmail.com>
2026-05-22 16:15:51 +0200

    add `allowOverflow`

--- packages/dnb-eufemia/src/extensions/forms/Field/PostalCodeAndCity/PostalCodeAndCity.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Field/PostalCodeAndCity/PostalCodeAndCity.tsx
@@ -122,6 +122,7 @@ function PostalCodeAndCity(props: FieldPostalCodeAndCityProps) {
         )}
         label={postalCodeLabel ?? translations.PostalCode.label}
         mask={postalCodeMask}
+        allowOverflow
         pattern={postalCodePattern}
         placeholder={postalCodePlaceHolder}
         errorMessages={useMemo(
