765d9c30fd6ffdd2c4625bb026f4c67653b6f011
Tobias Høegh <tobias@tujo.no>
2026-05-22 13:12:33 +0200

    design(Table): use new row color and outline xl corner radius (#8208)
    Based on #8206

--- packages/dnb-design-system-portal/src/docs/uilib/components/table/Examples.tsx
+++ packages/dnb-design-system-portal/src/docs/uilib/components/table/Examples.tsx
@@ -2200,7 +2200,7 @@ export const ColumnHighlight = () => (
 export const MultipleTbody = () => (
   <ComponentBox data-visual-test="table-multiple-tbody">
     <Table.ScrollView>
-      <Table>
+      <Table outline>
         <caption className="dnb-sr-only">
           Market data with delay and opening hours
         </caption>
