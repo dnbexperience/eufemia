83cff8426dfb9fca5f12b00d654fb84887507212
Joakim Bjerknes <joakbjerk@gmail.com>
2026-06-09 13:42:21 +0200

    chore(DatePicker): remove unnecessary export from `getViews` (#8511)

--- packages/dnb-eufemia/src/components/date-picker/hooks/useViews.ts
+++ packages/dnb-eufemia/src/components/date-picker/hooks/useViews.ts
@@ -66,7 +66,7 @@ export default function useViews({ isRange, ...dates }: UseViewsParams) {
   } as const
 }
 
-export function getViews({
+function getViews({
   isRange,
   rangeSingleCalendar = false,
   ...dates
