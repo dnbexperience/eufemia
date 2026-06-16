6f90bb1284be4ab320be2b4b617e25be3acfc4aa
Anders Langseth <anderslangseth@gmail.com>
2026-06-16 09:13:55 +0200

    chore(Forms): add generic constraint to FieldProviderContext.extend
    Change `T = UseFieldProps` to `T extends UseFieldProps = UseFieldProps`
    to ensure only compatible prop objects can be passed through extend.

--- packages/dnb-eufemia/src/extensions/forms/Field/Provider/FieldProviderContext.ts
+++ packages/dnb-eufemia/src/extensions/forms/Field/Provider/FieldProviderContext.ts
@@ -2,7 +2,7 @@ import { createContext } from 'react'
 import type { UseFieldProps } from '../../types'
 
 export type FieldProviderContextProps = {
-  extend: <T = UseFieldProps>(props: T) => T
+  extend: <T extends UseFieldProps = UseFieldProps>(props: T) => T
   inheritedProps?: UseFieldProps
   inheritedContext?: UseFieldProps
 }
