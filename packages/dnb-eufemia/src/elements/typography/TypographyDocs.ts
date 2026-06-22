cc9e7981aebb0854a5ac2267cfbe3ec25fee5299
-l <anderslangseth@gmail.com>
2026-06-22 11:09:04 +0200

    docs(elements): sync JSDoc comments with Docs definitions

--- packages/dnb-eufemia/src/elements/typography/TypographyDocs.ts
+++ packages/dnb-eufemia/src/elements/typography/TypographyDocs.ts
@@ -58,7 +58,7 @@ export const TypographyProperties: PropertiesTableProps = {
     status: 'optional',
   },
   proseMaxWidth: {
-    doc: 'Sets the maximum width based on character count. This will limit the text width to approximately the specified number of characters. Use `true` for a default value of 60ch.',
+    doc: 'Sets the maximum width based on character count for all Typography children. This will limit the text width to approximately the specified number of characters. Use `true` for a default value of 60ch.',
     type: ['number', 'boolean'],
     status: 'optional',
   },
