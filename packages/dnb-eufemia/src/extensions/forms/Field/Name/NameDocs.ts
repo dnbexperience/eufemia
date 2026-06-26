61ee808ab4de5284bfb2498a1627cd0838342a90
Anders Langseth <anderslangseth@gmail.com>
2026-06-26 10:39:38 +0200

    docs(Field.Name): document field-specific onBlurValidator event and minLength default

--- packages/dnb-eufemia/src/extensions/forms/Field/Name/NameDocs.ts
+++ packages/dnb-eufemia/src/extensions/forms/Field/Name/NameDocs.ts
@@ -3,4 +3,16 @@ import { StringProperties } from '../String/StringDocs'
 
 export const NameProperties: PropertiesTableProps = {
   ...StringProperties,
+  minLength: {
+    ...StringProperties.minLength,
+    doc: 'Validation for minimum length of the text (number of characters). Defaults to `1`.',
+  },
+}
+
+export const NameEvents: PropertiesTableProps = {
+  onBlurValidator: {
+    doc: 'Custom validator function that is triggered when the user leaves a field (e.g., blurring a text input). The function can be either asynchronous or synchronous. The first parameter is the value, and the second parameter returns an object containing { errorMessages, connectWithPath, validators }. Defaults to name validation, using `nameValidator`. For `Field.Name.Company` it uses `companyValidator`. Can be disabled using `false`.',
+    type: 'function',
+    status: 'optional',
+  },
 }
