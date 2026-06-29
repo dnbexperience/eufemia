2387e96ff1ba5427d041e04e9bae0e861c1461c3
Anders <anderslangseth@gmail.com>
2026-06-29 12:53:30 +0200

    chore(Code): use tabbed sub-pages for info, demos and properties (#8742)
    Aligns the Code element docs with all other element docs (blockquote,
    span, etc.) by splitting the single page into Info, Demos and Properties
    tabs. Adds CodeProperties (Space) and hides the Events tab.

---
+++ packages/dnb-eufemia/src/elements/code/CodeDocs.ts
@@ -0,0 +1,9 @@
+import type { PropertiesTableProps } from '../../shared/types'
+
+export const CodeProperties: PropertiesTableProps = {
+  '[Space](/uilib/layout/space/properties)': {
+    doc: 'Spacing properties like `top` or `bottom` are supported.',
+    type: ['string', 'object'],
+    status: 'optional',
+  },
+}
