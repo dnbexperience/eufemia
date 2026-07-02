7d2dac399f5ca21b9e2ea4c0e1edab1ca0c8daa2
Anders Langseth <anderslangseth@gmail.com>
2026-07-02 13:54:25 +0200

    docs(Form.Element): explain non-string id guard

--- packages/dnb-eufemia/src/extensions/forms/Form/Element/Element.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Form/Element/Element.tsx
@@ -90,6 +90,8 @@ function FormElementComponent(props: FormElementProps) {
       element="form"
       className={clsx('dnb-forms-form', className)}
       onSubmit={onSubmitHandler}
+      // The id can be a non-string value (e.g. a function reference used as a shared-state id).
+      // Only forward a string to the DOM, to avoid React's "Invalid value for prop `id`" warning.
       id={typeof idProp === 'string' ? idProp : undefined}
       aria-labelledby={
         combineLabelledBy(
