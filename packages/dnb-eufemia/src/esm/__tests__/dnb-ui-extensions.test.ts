8a96bc73cb58117707e32f24f07a5b76e2c4f717
Tobias Høegh <tobias@tujo.no>
2026-08-07 17:23:05 +0200

    feat(SidebarMenu): add nested sidebar menu

--- packages/dnb-eufemia/src/esm/__tests__/dnb-ui-extensions.test.ts
+++ packages/dnb-eufemia/src/esm/__tests__/dnb-ui-extensions.test.ts
@@ -6,10 +6,14 @@
  */
 
 import '../../core/test-utils/testSetup'
-import { PaymentCard } from '../dnb-ui-extensions'
+import { PaymentCard, SidebarMenu } from '../dnb-ui-extensions'
 
 describe('ESM extensions library package', () => {
   it('has to have a PaymentCard Component', () => {
     expect(typeof PaymentCard).toBe('function')
+  })
+
+  it('has to have a SidebarMenu extension', () => {
+    expect(typeof SidebarMenu.Container).toBe('function')
   })
 })
