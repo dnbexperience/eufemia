9df6b84007c2e301ae68e9ae1d0f4aa28b73118e
Anders <anderslangseth@gmail.com>
2026-09-02 20:33:58 +0200

    Apply suggestion from @langz

--- packages/dnb-eufemia/src/extensions/forms/Form/Section/Toolbar/Toolbar.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Form/Section/Toolbar/Toolbar.tsx
@@ -11,13 +11,6 @@ import Space from '../../../../../components/Space'
 
 export type FormSectionToolbarProps = SpaceAllProps & {
   onEdit?: () => void
-  /**
-   * Callback for the done button.
-   * Return a Promise to keep the section in edit mode until it settles:
-   * it switches to view mode when the Promise resolves,
-   * and stays in edit mode when it rejects.
-   * Any other return value is ignored.
-   */
   onDone?: () => void
   onCancel?: () => void
 }
