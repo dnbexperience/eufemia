875c1b72f3e098b74e0b6cfd75b60de0ccdeb641
Tobias Høegh <tobias@tujo.no>
2026-09-02 14:05:41 +0200

    Keep Carnegie radius token unchanged

--- packages/dnb-eufemia/scripts/prebuild/tasks/__tests__/makePropertiesFile.test.ts
+++ packages/dnb-eufemia/scripts/prebuild/tasks/__tests__/makePropertiesFile.test.ts
@@ -119,13 +119,6 @@ describe('makePropertiesFile', () => {
       expect(global.carnegieTokens).toMatchSnapshot()
       expect(global.carnegieFoundation).toMatchSnapshot()
     })
-
-    it('uses the Carnegie radius tokens', () => {
-      expect(global.carnegieTokens).toContain('--token-radius-xl: 0.5rem;')
-      expect(global.carnegieTokensTailwind).toContain(
-        '--radius-xl: 0.5rem;'
-      )
-    })
   })
 
   describe('Native color scheme', () => {
