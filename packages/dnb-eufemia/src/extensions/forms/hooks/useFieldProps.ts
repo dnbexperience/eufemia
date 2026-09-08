891a74a46b06497d5cc582e50b4e785d48110f50
Tobias Høegh <tobias@tujo.no>
2026-09-08 15:22:14 +0200

    Clean up submit validation listeners

--- packages/dnb-eufemia/src/extensions/forms/hooks/useFieldProps.ts
+++ packages/dnb-eufemia/src/extensions/forms/hooks/useFieldProps.ts
@@ -1776,6 +1776,17 @@ export default function useFieldProps<Value, EmptyValue, Props>(
   // Validate/call validator functions during submit of the form
   useEffect(() => {
     setFieldEventListener?.(identifier, 'onSubmitCall', onSubmitHandler)
+
+    return () => {
+      setFieldEventListener?.(
+        identifier,
+        'onSubmitCall',
+        onSubmitHandler,
+        {
+          remove: true,
+        }
+      )
+    }
   }, [identifier, onSubmitHandler, setFieldEventListener])
 
   // Set the error in the field block context if this field is inside a field block
