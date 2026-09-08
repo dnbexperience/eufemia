c945897052beafb24b8c20955ca0658cc474f792
Tobias Høegh <tobias@tujo.no>
2026-09-08 18:22:07 +0200

    Move the Portal home to MDX

--- packages/dnb-design-system-portal/src/shared/menu/SidebarMenu.tsx
+++ packages/dnb-design-system-portal/src/shared/menu/SidebarMenu.tsx
@@ -141,7 +141,7 @@ export default function SidebarLayout({
           location
         ).filter(({ title, menuTitle }) => title || menuTitle)
       ),
-      true
+      location.pathname !== '/'
     )
   )
   const navItems = toSidebarMenuData(groupedNavItems, closeMenu)
