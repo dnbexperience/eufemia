4cf1b220a1078f0a3fad01b42ab1fb39ff39a3cb
Tobias Høegh <tobias@tujo.no>
2026-09-18 15:39:59 +0200

    Use responsive SidebarMenu integration

--- packages/dnb-eufemia/src/icons/dnb/hamburger.tsx
+++ packages/dnb-eufemia/src/icons/dnb/hamburger.tsx
@@ -14,19 +14,7 @@ const hamburger = (props?: IconSVGProps) => (
       stroke="#000"
       strokeLinecap="round"
       strokeWidth={1.5}
-      d="M1 2h14"
-    />
-    <path
-      stroke="#000"
-      strokeLinecap="round"
-      strokeWidth={1.5}
-      d="M1 8h14"
-    />
-    <path
-      stroke="#000"
-      strokeLinecap="round"
-      strokeWidth={1.5}
-      d="M1 14h14"
+      d="M1 14h14M1 8h14M1 2h14"
     />
   </svg>
 )
