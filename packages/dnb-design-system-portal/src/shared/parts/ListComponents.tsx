42a69f1dd407ab24f068b74739fd59abc34dd9d4
Tobias Høegh <tobias@tujo.no>
2026-09-22 09:08:25 +0200

    Align component list filtering

--- packages/dnb-design-system-portal/src/shared/parts/ListComponents.tsx
+++ packages/dnb-design-system-portal/src/shared/parts/ListComponents.tsx
@@ -1,14 +1,12 @@
 import ListSummaryFromEdges from './ListSummaryFromEdges'
 import { regularMdxNodes } from 'virtual:portal-pages'
 import { getComponents } from './listEdges'
-import { excludedSlugs } from './componentCategories'
 
 export default function ListComponents(props) {
-  const visibleComponents = getComponents(regularMdxNodes).filter(
-    (node) => !excludedSlugs.has(node.fields.slug)
-  )
-
   return (
-    <ListSummaryFromEdges edges={visibleComponents} {...props} />
+    <ListSummaryFromEdges
+      edges={getComponents(regularMdxNodes)}
+      {...props}
+    />
   )
 }
