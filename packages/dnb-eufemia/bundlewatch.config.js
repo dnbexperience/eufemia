c863f65f5249c1cfee1aaf8ef4f809c5fd16b171
Thayanan Tharmapalan <thayanan@gmail.com>
2026-09-25 13:24:44 +0200

    chore: increase UMD lib bundle budget
    The Tabs tooltip support puts dnb-ui-lib.min.js at 250.04KB gzip, just over the 250KB budget.

--- packages/dnb-eufemia/bundlewatch.config.js
+++ packages/dnb-eufemia/bundlewatch.config.js
@@ -5,13 +5,13 @@ const jsBundles = [
   { path: 'build/umd/dnb-ui-components.min.js', maxSize: '250kB' },
   { path: 'build/umd/dnb-ui-elements.min.js', maxSize: '75kB' },
   { path: 'build/umd/dnb-ui-extensions.min.js', maxSize: '400kB' },
-  { path: 'build/umd/dnb-ui-lib.min.js', maxSize: '250kB' },
+  { path: 'build/umd/dnb-ui-lib.min.js', maxSize: '251kB' },
   { path: 'build/esm/dnb-ui-basis.min.mjs', maxSize: '5kB' },
   { path: 'build/esm/dnb-ui-components.min.mjs', maxSize: '250kB' },
   { path: 'build/esm/dnb-ui-elements.min.mjs', maxSize: '75kB' },
   { path: 'build/esm/dnb-ui-extensions.min.mjs', maxSize: '400kB' },
   { path: 'build/esm/dnb-ui-lib.min.mjs', maxSize: '250kB' },
-]
+];
 
 const cssBundles = [
   { path: 'build/style/dnb-ui-basis.min.css', maxSize: '5kB' },
@@ -108,9 +108,9 @@ const cssBundles = [
     path: 'build/style/themes/ui/ui-theme-properties.min.css',
     maxSize: '5kB',
   },
-]
+];
 
 module.exports = {
   defaultCompression: 'gzip',
   files: [...jsBundles, ...cssBundles],
-}
+};
